8088a76eb5b3a4ebd9b31c02554d7c4187e6ec2a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-09-12 18:38:08 +0530

    Time: 66 ms (69.39%), Space: 17.7 MB (33.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,10 @@
-class Solution 
-{
-    public int[] twoSum(int[] nums, int target) 
-    {
-        Map<Integer, Integer> numMap = new HashMap<Integer,Integer>();
-        int n = nums.length;
-        for (int i = 0; i < n; i++) 
-        {
-            int remaining = target - nums[i];
-            if (numMap.containsKey(remaining)) 
-            {
-                return new int[]{numMap.get(remaining), i};
-            }
-            numMap.put(nums[i], i);
-        }
-        return new int[]{};
-    }
-}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        map = {}
+        for i in range(n):
+            remaining = target - nums[i]
+            if remaining in map:
+                return [i,map[remaining]]
+            map[nums[i]] = i
+        return []
