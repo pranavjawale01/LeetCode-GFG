129b8400ac3e82bde270f7c8324982707faa0252
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-09-16 12:28:29 +0530

    Time: 50 ms (71.62%), Space: 16.3 MB (45.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,23 +1,11 @@
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int currentValue = map.get(s.charAt(i));
-            if (i + 1 < s.length() && map.get(s.charAt(i + 1)) > currentValue) {
-                num -= currentValue;
-            } 
-            else {
-                num += currentValue;
-            }
-        }
-        return num;
-    }
-}
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        ans = 0
+        for i in range (len(s)):
+            if i < len(s)-1 and map[s[i]] < map[s[i+1]]:
+                ans -= map[s[i]]
+            else:
+                ans += map[s[i]]
+                
+        return ans
