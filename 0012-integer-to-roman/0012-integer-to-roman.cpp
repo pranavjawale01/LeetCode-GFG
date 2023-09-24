7c33de8351bb6aa9cda649136258cb69a62aee78
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-09-24 22:44:59 +0530

    Time: 3 ms (87.44%), Space: 9.1 MB (28.58%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> value;
+        value = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        vector<string> roman;
+        roman = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        string res="";
+        for(int i=12;i>=0;i--)
+        {
+            while(num>=value[i])
+            {
+                res.append(roman[i]);
+                num-=value[i];
+            }
+        }
+        return res;
+    }
+};
