804efdf7799b93d2d1949ec2984719fa7bc49616
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-10-30 20:02:00 +0530

    Time: 100 ms (67.36%), Space: 16 MB (97.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = [[0 for _ in range(9)] for _ in range(9)]
-        col = [[0 for _ in range(9)] for _ in range(9)]
-        grid_3x3 = [[0 for _ in range(9)] for _ in range(9)]
+        row = [{} for _ in range(9)]
+        col = [{} for _ in range(9)]
+        grid_3x3 = [{} for _ in range(9)]
         
         for i in range(9):
             for j in range(9):
-                if board[i][j] == '.':
+                num = board[i][j]
+                if num == '.':
                     continue
-                num = int(board[i][j]) - 1
+                
                 k = (i // 3) * 3 + (j // 3)
-                if row[i][num] or col[j][num] or grid_3x3[k][num]:
+                if num in row[i] or num in col[j] or num in grid_3x3[k]:
                     return False
+                
                 row[i][num] = col[j][num] = grid_3x3[k][num] = 1
         
         return True
