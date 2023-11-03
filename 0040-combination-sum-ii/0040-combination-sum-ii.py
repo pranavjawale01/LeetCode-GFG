6ec439bfef58fe9fccec0ef2b90cda261ce91872
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-03 15:27:44 +0530

    Time: 59 ms (80%), Space: 16.3 MB (57.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result, current = [], []
+        candidates.sort()
+
+        def findSums(candidates: List[int], target: int, start: int, result: List[List[int]], current: List[int]):
+            if target == 0:
+                result.append(list(current)) 
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] <= target:
+                    current.append(candidates[i])
+                    findSums(candidates, target - candidates[i], i + 1, result, current)
+                    current.pop()
+
+        findSums(candidates, target, 0, result, current)
+        return result
