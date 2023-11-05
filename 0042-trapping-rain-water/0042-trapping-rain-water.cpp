da931f5e98b77c6f1a5c9d99651c247ca11107f7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-05 13:59:56 +0530

    Time: 7 ms (93.06%), Space: 20 MB (72.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,14 +5,14 @@ public:
         int maxLeft = 0, maxRight = 0;
         while (left <= right) {
             if (height[left] <= height[right]) {
-                if (height[left] >= maxLeft) {
+                if (height[left] > maxLeft) {
                     maxLeft = height[left];
                 } else {
                     res += maxLeft - height[left];
                 }
                 left++;
             } else {
-                if (height[right] >= maxRight) {
+                if (height[right] > maxRight) {
                     maxRight = height[right];
                 } else {
                     res += maxRight - height[right];
