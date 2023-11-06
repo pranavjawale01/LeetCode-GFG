be0aeb07372fc9d27b2c6e4c93427f0fa84661e8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-06 23:22:30 +0530

    Time: 4 ms (70.86%), Space: 7 MB (45.05%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        int len1 = num1.size();
-        int len2 = num2.size();
-        vector<int> result(len1 + len2, 0);
-
-        for (int i = len1 - 1; i >= 0; --i) {
-            for (int j = len2 - 1; j >= 0; --j) {
-                int mul = (num1[i] - '0') * (num2[j] - '0');
-                int sum = mul + result[i + j + 1];
+        int n = num1.size(), m = num2.size();
+        vector<int> result(n+m,0);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int product = (num1[i] - '0')*(num2[j] - '0');
+                int sum = product + result[i + j + 1];
                 result[i + j + 1] = sum % 10;
                 result[i + j] += sum / 10;
             }
         }
-        
-        string res = "";
-        for (int num : result) {
-            res.push_back(num + '0');
+        string ans;
+        for (int digit : result) {
+            if (!(ans.empty() && digit == 0)) {
+                ans += to_string(digit);
+            }
         }
-
-        auto it = res.begin();
-        while (it != res.end() && *it == '0') {
-            ++it;
-        }
-
-        return it == res.end() ? "0" : string(it, res.end());
+        return ans.empty() ? "0" : ans;
     }
 };
 
