6f81033021375b83294cda0afc250e9fb761367f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-13 12:15:56 +0530

    Time: 25 ms (84.81%), Space: 20.2 MB (72.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagramMap;
 
-        for (const string& str : strs) {
+        for (const auto& str : strs) {
             string key = str;
             sort(key.begin(), key.end());
             anagramMap[key].push_back(str);
