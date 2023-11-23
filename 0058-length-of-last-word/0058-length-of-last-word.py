504f945e3f2a75ecd70e9a161b52acd6d817909c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-23 11:47:51 +0530

    Time: 41 ms (32.96%), Space: 16.4 MB (24.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,12 +1,19 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        i , count = len(s) - 1 , 0
+        s = s.strip()
+        s = s.split(' ')
+        x = int(len(str(s[-1])))
+        return(x)
+    
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         i , count = len(s) - 1 , 0
 
-        while i >= 0 and s[i] == ' ':
-            i -= 1
+#         while i >= 0 and s[i] == ' ':
+#             i -= 1
         
-        while i >= 0 and s[i] != ' ':
-            i -= 1
-            count += 1
+#         while i >= 0 and s[i] != ' ':
+#             i -= 1
+#             count += 1
         
-        return count
+#         return count
