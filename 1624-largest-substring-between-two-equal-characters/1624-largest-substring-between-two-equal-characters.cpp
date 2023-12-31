866ce18b55cb676d9d6853ccf066f79e725f5026
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-12-31 10:58:49 +0530

    Time: 3 ms (47.38%), Space: 6.7 MB (41.15%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int n = s.length();
-        int maxLen = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n && j != i; j++) {
-                if (s[i] == s[j]) {
-                    maxLen = max(maxLen, abs(i - j - 1));
-                }
+        unordered_map<char, int> firstOccurrence;
+        int maxLength = -1;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (firstOccurrence.find(s[i]) == firstOccurrence.end()) {
+                firstOccurrence[s[i]] = i;
+            } else {
+                maxLength = max(maxLength, i - firstOccurrence[s[i]] - 1);
             }
         }
-        return maxLen;
+
+        return maxLength;
     }
 };
+
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         int n = s.length();
+//         int maxLen = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n && j != i; j++) {
+//                 if (s[i] == s[j]) {
+//                     maxLen = max(maxLen, abs(i - j));
+//                 }
+//             }
+//         }
+//         return maxLen;
+//     }
+// };
