2ddb1e10afb4d92f96a092c7d54a7248e287325e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-01 19:14:37 +0530

    Time: 8 ms (98.82%), Space: 45.1 MB (22.14%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+}
