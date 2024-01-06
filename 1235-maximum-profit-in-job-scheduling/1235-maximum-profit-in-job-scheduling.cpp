e90e821a72177edd7832d1f4780d74e5d9657621
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-06 19:15:07 +0530

    Time: 156 ms (72.88%), Space: 75.7 MB (62.38%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,28 +1,61 @@
 class Solution {
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<vector<int>> jobs(n, vector<int>(3));
-
-        for (int i = 0; i < n; i++) {
-            jobs[i] = {startTime[i], endTime[i], profit[i]};
-        }
-
-        sort(jobs.begin(), jobs.end(), [](const auto& a, const auto& b) {
-            return a[1] < b[1];
-        });
-
-        map<int, int> dp;
-        dp[0] = 0;
-
-        for (const auto& job : jobs) {
-            int val = job[2] + prev(dp.upper_bound(job[0]))->second;
-            if (val > dp.rbegin()->second) {
-                dp[job[1]] = val;
+    int memo[50001];
+    int n;
+    int getNextIndex(vector<vector<int>>& array, int l, int currentJobEnd) {
+        int r = n-1;
+        
+        int result = n+1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(array[mid][0] >= currentJobEnd) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
             }
         }
+        
+        return result;
+    }
+    
+    int solve(vector<vector<int>>& array, int i) {
+        if(i >= n)
+            return 0;
+        
+        if(memo[i] != -1)
+            return memo[i];
+        
+        int next  = getNextIndex(array, i+1, array[i][1]);
+        
+        int taken = array[i][2] + solve(array, next);
+        int notTaken = solve(array, i+1);
+        
+        return memo[i] = max(taken, notTaken);
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        n = startTime.size();
+        
+        memset(memo, -1, sizeof(memo));
+        
+        vector<vector<int>> array(n, vector<int>(3, 0)); //{s, e, p}
+        
+        for(int i = 0; i<n; i++) {
+            array[i][0] = startTime[i];
+            array[i][1] = endTime[i];
+            array[i][2] = profit[i];
+        }
+        
+        auto comp = [&](auto& vec1, auto& vec2) {
+              return vec1[0] <= vec2[0];
+        };
 
-        return dp.rbegin()->second;
+        sort(begin(array), end(array), comp);
+        
+        return solve(array, 0);
     }
 };
 
