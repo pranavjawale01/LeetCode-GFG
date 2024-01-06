f9adf7dd32ffee6a0d4be20571876ed9e8d03583
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-06 19:19:23 +0530

    Time: 32 ms (73.11%), Space: 60.1 MB (28.83%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,24 +1,81 @@
 class Solution {
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        int[][] jobs = new int[n][3];
+    private int[] memo;
+    private int n;
 
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
-        }
+    private int getNextIndex(int[][] array, int l, int currentJobEnd) {
+        int r = n - 1;
+        int result = n + 1;
 
-        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-
-        for (int[] job : jobs) {
-            int val = job[2] + dp.floorEntry(job[0]).getValue();
-            if (val > dp.lastEntry().getValue()) {
-                dp.put(job[1], val);
+            if (array[mid][0] >= currentJobEnd) {
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
 
-        return dp.lastEntry().getValue();
+        return result;
+    }
+
+    private int solve(int[][] array, int i) {
+        if (i >= n)
+            return 0;
+
+        if (memo[i] != -1)
+            return memo[i];
+
+        int next = getNextIndex(array, i + 1, array[i][1]);
+
+        int taken = array[i][2] + solve(array, next);
+        int notTaken = solve(array, i + 1);
+
+        return memo[i] = Math.max(taken, notTaken);
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        n = startTime.length;
+
+        memo = new int[n];
+        Arrays.fill(memo, -1);
+
+        int[][] array = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            array[i][0] = startTime[i];
+            array[i][1] = endTime[i];
+            array[i][2] = profit[i];
+        }
+
+        Arrays.sort(array, (vec1, vec2) -> Integer.compare(vec1[0], vec2[0]));
+
+        return solve(array, 0);
     }
 }
+
+// class Solution {
+//     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+//         int n = startTime.length;
+//         int[][] jobs = new int[n][3];
+
+//         for (int i = 0; i < n; i++) {
+//             jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+//         }
+
+//         Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+
+//         TreeMap<Integer, Integer> dp = new TreeMap<>();
+//         dp.put(0, 0);
+
+//         for (int[] job : jobs) {
+//             int val = job[2] + dp.floorEntry(job[0]).getValue();
+//             if (val > dp.lastEntry().getValue()) {
+//                 dp.put(job[1], val);
+//             }
+//         }
+
+//         return dp.lastEntry().getValue();
+//     }
+// }
