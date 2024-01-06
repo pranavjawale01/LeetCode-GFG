e4d40672c9c30d9e0bc03a10b78dd0890a143064
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-06 19:20:19 +0530

    Time: 707 ms (43.75%), Space: 51.7 MB (27.49%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,11 +1,52 @@
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        dp = [(0, 0)]
+    def jobScheduling(self, startTime, endTime, profit):
+        n = len(startTime)
+        memo = [-1] * n
 
-        for start, end, p in jobs:
-            idx = bisect.bisect(dp, (start + 1, 0)) - 1
-            max_profit = max(dp[idx][1] + p, dp[-1][1])
-            dp.append((end, max_profit))
+        def getNextIndex(l, currentJobEnd):
+            r = n - 1
+            result = n + 1
 
-        return dp[-1][1]
+            while l <= r:
+                mid = l + (r - l) // 2
+
+                if array[mid][0] >= currentJobEnd:
+                    result = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            return result
+
+        def solve(i):
+            if i >= n:
+                return 0
+
+            if memo[i] != -1:
+                return memo[i]
+
+            next_index = getNextIndex(i + 1, array[i][1])
+
+            taken = array[i][2] + solve(next_index)
+            not_taken = solve(i + 1)
+
+            memo[i] = max(taken, not_taken)
+            return memo[i]
+
+        array = list(zip(startTime, endTime, profit))
+        array.sort(key=lambda x: x[0])
+
+        return solve(0)
+
+
+# class Solution:
+#     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+#         jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
+#         dp = [(0, 0)]
+
+#         for start, end, p in jobs:
+#             idx = bisect.bisect(dp, (start + 1, 0)) - 1
+#             max_profit = max(dp[idx][1] + p, dp[-1][1])
+#             dp.append((end, max_profit))
+
+#         return dp[-1][1]
