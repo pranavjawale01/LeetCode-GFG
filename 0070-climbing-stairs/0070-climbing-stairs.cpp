63017b4f26edbfd13b4436cb719153ae53063334
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-18 08:50:56 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
+    int t[46];
+    int recursive(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (t[n] != -1) {
+            return t[n];
+        }
+        if (n == 0) {
+            return 1;
+        }
+        int oneStep = recursive(n - 1);
+        int twoStep = recursive(n - 2);
+        return t[n] = oneStep + twoStep;
+    }
     int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
-        }
-        int first = 1, second = 2;
-        for (int i = 3; i <= n; i++) {
-            int current = first + second;
-            first = second;
-            second = current;
-        }
-        return second;
+        memset(t, -1, sizeof(t));
+        return recursive(n);
     }
 };
