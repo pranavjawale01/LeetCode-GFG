e2b240b80286252d0d7c3a1f28a22c01be310d62
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-01-30 19:22:25 +0530

    Time: 6 ms (86.57%), Space: 15.6 MB (5.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,6 +3,35 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int> stk;
         int n = tokens.size();
+        unordered_map<string, function<int (int, int)>> mp= {
+            {"+", [](int a, int b) {return a + b; }},
+            {"-", [](int a, int b) {return a - b; }},
+            {"*", [](int a, int b) {return a * b; }},
+            {"/", [](int a, int b) {return a / b; }},
+        };
+    
+        for (int i = 0; i < n; i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int operator2 = stk.top();
+                stk.pop();
+                int operator1 =  stk.top();
+                stk.pop();
+                int result = mp[tokens[i]](operator1, operator2);                
+                stk.push(result);
+            } else {
+                stk.push(stoi(tokens[i]));
+            }
+        }
+        return stk.top();
+    }
+};
+
+/*
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        int n = tokens.size();
     
         for (int i = 0; i < n; i++) {
             if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
@@ -29,3 +58,4 @@ public:
         return stk.top();
     }
 };
+*/
