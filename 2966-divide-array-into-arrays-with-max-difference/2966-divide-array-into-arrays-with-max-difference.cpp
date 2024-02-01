46ae908a136fe31429cd1121dbf7d4520664eabd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-01 12:38:40 +0530

    Time: 151 ms (79.24%), Space: 127.1 MB (28.93%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -9,13 +9,10 @@ public:
             temp[0] = nums[i];
             temp[1] = nums[i+1];
             temp[2] = nums[i+2];
-            output.push_back(temp);
-        }
-        n = output.size();
-        for (int i = 0; i < n; i++) {
-            if (output[i][2] - output[i][0] > k) {
+            if (temp[2] - temp[0] > k) {
                 return {};
             }
+            output.push_back(temp);
         }
         return output;
     }
