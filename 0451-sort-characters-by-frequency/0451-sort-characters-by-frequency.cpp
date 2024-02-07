c510717745eaa51967339ee874e0f3c051d67c87
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-07 22:28:28 +0530

    Time: 3 ms (99.77%), Space: 11 MB (11.70%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
+    typedef pair<char, int> P;
+
+    struct lambda  {
+        bool operator()(P &p1, P &p2) {
+            return p1.second < p2.second;
+        }
+    };
+
     string frequencySort(string s) {
+        priority_queue<P, vector<P>, lambda> pq;
         unordered_map<char, int> mp;
         for (char c : s) {
             mp[c]++;
         }
-        auto comparator = [&] (char a, char b) {
-            if (mp[a] == mp[b]) {
-                return a < b;
-            }
-            return mp[a] > mp[b];
-        };
-        sort(s.begin(), s.end(), comparator);
-        return s;
+        for (auto &it : mp) {
+            pq.push({it.first, it.second});
+        }
+        string result = "";
+        while (!pq.empty()) {
+            P temp = pq.top();
+            pq.pop();
+            result += string(temp.second, temp.first);
+        }
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char, int> mp;
+//         for (char c : s) {
+//             mp[c]++;
+//         }
+//         auto comparator = [&] (char a, char b) {
+//             if (mp[a] == mp[b]) {
+//                 return a < b;
+//             }
+//             return mp[a] > mp[b];
+//         };
+//         sort(s.begin(), s.end(), comparator);
+//         return s;
+//     }
+// };
