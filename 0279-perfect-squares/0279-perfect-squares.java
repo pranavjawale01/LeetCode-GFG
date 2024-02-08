fb8042fa0162ed812e2c4b8000deb0b6ef99e573
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-08 12:14:14 +0530

    Time: 28 ms (82.82%), Space: 42.2 MB (91.48%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,21 +1,35 @@
 class Solution {
-    private int[] memo = new int[10001];
-    public int solve(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-        int minCount = Integer.MAX_VALUE;
-        for (int i = 1; i * i <= n; i++) {
-            int result = 1 + solve(n - i*i);
-            minCount = Math.min(minCount, result);
-        }
-        return memo[n] = minCount;
-    }
     public int numSquares(int n) {
-        Arrays.fill(memo, -1);
-        return solve(n);
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                arr[i] = Math.min(arr[i], 1 + arr[i - j * j]);
+            }
+        }
+        return arr[n];
     }
 }
+
+// class Solution {
+//     private int[] memo = new int[10001];
+//     public int solve(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (memo[n] != -1) {
+//             return memo[n];
+//         }
+//         int minCount = Integer.MAX_VALUE;
+//         for (int i = 1; i * i <= n; i++) {
+//             int result = 1 + solve(n - i*i);
+//             minCount = Math.min(minCount, result);
+//         }
+//         return memo[n] = minCount;
+//     }
+//     public int numSquares(int n) {
+//         Arrays.fill(memo, -1);
+//         return solve(n);
+//     }
+// }
