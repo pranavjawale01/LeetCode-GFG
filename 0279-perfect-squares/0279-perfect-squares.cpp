9d14019e165d76e9e7401ceb46c01d30335b8391
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-08 12:10:51 +0530

    Time: 43 ms (88.64%), Space: 10.6 MB (32.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int memo[10001];
-    int solve(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-        int minCount = INT_MAX;
-        for (int i = 1; i*i <= n; i++) {
-            int result = 1 + solve(n - i*i);
-            minCount = min(minCount, result);
-        }
-        return memo[n] = minCount;
-    }
     int numSquares(int n) {
-        memset(memo, -1, sizeof(memo));
-        return solve(n);
+        vector<int> arr(n+1, 100001);
+        arr[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j*j <= i; j++) {
+                arr[i] = min(arr[i], 1 + arr[i - j*j]);
+            }
+        }
+        return arr[n];
     }
 };
+
+// class Solution {
+// public:
+//     int memo[10001];
+//     int solve(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (memo[n] != -1) {
+//             return memo[n];
+//         }
+//         int minCount = INT_MAX;
+//         for (int i = 1; i*i <= n; i++) {
+//             int result = 1 + solve(n - i*i);
+//             minCount = min(minCount, result);
+//         }
+//         return memo[n] = minCount;
+//     }
+//     int numSquares(int n) {
+//         memset(memo, -1, sizeof(memo));
+//         return solve(n);
+//     }
+// };
