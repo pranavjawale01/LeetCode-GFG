40d16ce982ebd9f242a8c8ff006db8ffde76a587
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-09 15:21:57 +0530

    Time: 12 ms (92.77%), Space: 11.4 MB (31.82%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -10,15 +10,14 @@ public:
         int last_chosen_index = 0;
         int maxL = 1;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[i]%nums[j] == 0) {
-                    if(t[i] < t[j]+1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i]%nums[j] == 0) {
+                    if (t[i] < t[j]+1) {
                         t[i] = t[j]+1;
                         prev_idx[i] = j;
                     }
-
-                    if(t[i] > maxL) {
+                    if (t[i] > maxL) {
                         maxL = t[i];
                         last_chosen_index = i;
                     }
@@ -27,7 +26,7 @@ public:
         }
 
         vector<int> result;
-        while(last_chosen_index >= 0) {
+        while (last_chosen_index >= 0) {
             result.push_back(nums[last_chosen_index]);
             last_chosen_index = prev_idx[last_chosen_index];
         }
