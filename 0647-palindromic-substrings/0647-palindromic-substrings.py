b32123e7ac9cae4b359f53fe70c4cd5c945a7d58
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-10 19:22:57 +0530

    Time: 606 ms (6.60%), Space: 24.5 MB (19.75%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,18 +1,43 @@
 class Solution:
+    def __init__(self):
+        self.t = [[-1] * 1001 for _ in range(1001)]
+    
     def isPalindrome(self, s: str, start: int, end: int) -> bool:
-        while start < end:
-            if s[start] != s[end]:
-                return False
-            start += 1
-            end -= 1
-        return True
+        if start > end:
+            return True
+        if self.t[start][end] != -1:
+            return self.t[start][end] == 1
+        if s[start] == s[end]:
+            self.t[start][end] = self.isPalindrome(s, start + 1, end - 1)
+            return self.t[start][end] == 1
+        self.t[start][end] = 0
+        return False
     
     def countSubstrings(self, s: str) -> int:
         n = len(s)
         count = 0
         for i in range(n):
             for j in range(i, n):
-                t = s[i:j + 1]
                 if self.isPalindrome(s, i, j):
                     count += 1
         return count
+
+
+# class Solution:
+#     def isPalindrome(self, s: str, start: int, end: int) -> bool:
+#         while start < end:
+#             if s[start] != s[end]:
+#                 return False
+#             start += 1
+#             end -= 1
+#         return True
+    
+#     def countSubstrings(self, s: str) -> int:
+#         n = len(s)
+#         count = 0
+#         for i in range(n):
+#             for j in range(i, n):
+#                 t = s[i:j + 1]
+#                 if self.isPalindrome(s, i, j):
+#                     count += 1
+#         return count
