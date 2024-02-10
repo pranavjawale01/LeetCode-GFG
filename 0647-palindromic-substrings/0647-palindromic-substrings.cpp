98ed344c9b16f0f103189e33bae1d833eaa73b84
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-10 19:50:58 +0530

    Time: 30 ms (23.94%), Space: 9.5 MB (56.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,54 +1,53 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool isPalindrome(string &s, int start, int end) {
-        if (start > end) {
-            return true;
-        }
-        if (t[start][end] != -1) {
-            return t[start][end];
-        }
-        if (s[start] == s[end]) {
-            return t[start][end] = isPalindrome(s, start+1, end-1);
-        }
-        return t[start][end] = false;
-    }
-    
     int countSubstrings(string s) {
         int n = s.length();
-        memset(t, -1, sizeof(t));
+        vector<vector<bool>> t(n, vector<bool>(n, false));
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j)) {
+        
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+                if (i == j) {
+                    t[i][j] = true;
+                } else if(i+1 == j) {
+                    t[i][j] = (s[i] == s[j]);
+                } else {
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]); 
+                }
+                if (t[i][j] == true) {
                     count++;
                 }
             }
         }
+        
         return count;
     }
 };
 
 // class Solution {
 // public:
+//     int t[1001][1001];
 //     bool isPalindrome(string &s, int start, int end) {
-//         while (start < end) {
-//             if (s[start] != s[end]) {
-//                 return false;
-//             }
-//             start++;
-//             end--;
+//         if (start > end) {
+//             return true;
 //         }
-//         return true;
+//         if (t[start][end] != -1) {
+//             return t[start][end];
+//         }
+//         if (s[start] == s[end]) {
+//             return t[start][end] = isPalindrome(s, start+1, end-1);
+//         }
+//         return t[start][end] = false;
 //     }
     
 //     int countSubstrings(string s) {
 //         int n = s.length();
+//         memset(t, -1, sizeof(t));
 //         int count = 0;
 //         for (int i = 0; i < n; i++) {
 //             for (int j = i; j < n; j++) {
-//                 string t = s.substr(i, j - i + 1);
-//                 if (isPalindrome(t, 0, t.length() - 1)) {
+//                 if (isPalindrome(s, i, j)) {
 //                     count++;
 //                 }
 //             }
@@ -56,3 +55,31 @@ public:
 //         return count;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     bool isPalindrome(string &s, int start, int end) {
+// //         while (start < end) {
+// //             if (s[start] != s[end]) {
+// //                 return false;
+// //             }
+// //             start++;
+// //             end--;
+// //         }
+// //         return true;
+// //     }
+    
+// //     int countSubstrings(string s) {
+// //         int n = s.length();
+// //         int count = 0;
+// //         for (int i = 0; i < n; i++) {
+// //             for (int j = i; j < n; j++) {
+// //                 string t = s.substr(i, j - i + 1);
+// //                 if (isPalindrome(t, 0, t.length() - 1)) {
+// //                     count++;
+// //                 }
+// //             }
+// //         }
+// //         return count;
+// //     }
+// // };
