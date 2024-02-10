7eec9f723fa0b6111d6647220a6d7cd19ee940a4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-10 19:23:53 +0530

    Time: 679 ms (5.05%), Space: 56.4 MB (5.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,21 +1,27 @@
 class Solution {
-    public boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
+    int[][] t = new int[1001][1001];
+    
+    boolean isPalindrome(String s, int start, int end) {
+        if (start > end) {
+            return true;
         }
-        return true;
+        if (t[start][end] != -1) {
+            return t[start][end] == 1;
+        }
+        if (s.charAt(start) == s.charAt(end)) {
+            return isPalindrome(s, start + 1, end - 1);
+        }
+        return false;
     }
     
     public int countSubstrings(String s) {
         int n = s.length();
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                String t = s.substring(i, j + 1);
                 if (isPalindrome(s, i, j)) {
                     count++;
                 }
@@ -24,3 +30,31 @@ class Solution {
         return count;
     }
 }
+
+
+// class Solution {
+//     public boolean isPalindrome(String s, int start, int end) {
+//         while (start < end) {
+//             if (s.charAt(start) != s.charAt(end)) {
+//                 return false;
+//             }
+//             start++;
+//             end--;
+//         }
+//         return true;
+//     }
+    
+//     public int countSubstrings(String s) {
+//         int n = s.length();
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 String t = s.substring(i, j + 1);
+//                 if (isPalindrome(s, i, j)) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
