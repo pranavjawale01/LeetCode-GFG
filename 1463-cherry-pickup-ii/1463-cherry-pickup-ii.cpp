50f4549e2e0e388a7899ff4041a374142eb64827
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-11 19:47:21 +0530

    Time: 25 ms (98.67%), Space: 19.3 MB (18.05%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,32 +4,36 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        int t[71][71][71] = {0};
-        
-        t[0][0][n - 1] = (n == 1) ? grid[0][0] : grid[0][0] + grid[0][n - 1]; 
+        vector<vector<int>> prev(n, vector<int>(n));
+        prev[0][n-1] = (n == 1) ? grid[0][0] : grid[0][0] + grid[0][n-1];
         
         for (int row = 1; row < m; row++) {
-            for (int c1 = 0; c1 <= min(n-1, row); c1++) { 
-                for (int c2 = max(0, n-1-row); c2 < n; c2++) {                    
+
+            vector<vector<int>> curr(n, vector<int>(n));
+
+            for (int c1 = 0; c1 <= min(n-1, row); c1++) {
+                for (int c2 = max(0, n-1-row); c2 < n; c2++) {
+                    
                     int prev_max = 0;
                     for (int column1 = max(0, c1 - 1); column1 <= min(n - 1, c1 + 1); column1++) {
                         for (int column2 = max(0, c2 - 1); column2 <= min(n - 1, c2 + 1); column2++) {
-                                prev_max = max(prev_max, t[row - 1][column1][column2]);
+                                prev_max = max(prev_max, prev[column1][column2]);
                         }
                     }
                     
                     if (c1 == c2)
-                        t[row][c1][c2] = prev_max + grid[row][c1];
+                        curr[c1][c2] = prev_max + grid[row][c1];
                     else
-                        t[row][c1][c2] = prev_max + grid[row][c1] + grid[row][c2];
+                        curr[c1][c2] = prev_max + grid[row][c1] + grid[row][c2];
                 }
             }
+            prev = curr;
         }
         
         int res = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                res = max(res, t[m - 1][i][j]);    
+                res = max(res, prev[i][j]);    
             }
         }
         return res;
@@ -38,42 +42,80 @@ public:
 
 // class Solution {
 // public:
-//     int m, n;
-//     int t[71][71][71];
-//     vector<int> colDir{-1, 0, 1};
-//     bool isSafe(int& i) {
-//         return i >= 0 && i < n;
-//     }    
-//     int solve(vector<vector<int>>& grid, int row, int c1, int c2) {
-//         if (row >= m)
-//             return 0;       
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
         
-//         if (t[row][c1][c2] != -1) {
-//             return t[row][c1][c2];
-//         }
+//         int t[71][71][71] = {0};
         
-//         int count = grid[row][c1];
-//         if (c1 != c2) {
-//             count += grid[row][c2];
-//         }       
+//         t[0][0][n - 1] = (n == 1) ? grid[0][0] : grid[0][0] + grid[0][n - 1]; 
         
-//         int ans = 0;
-//         for(int& x : colDir) {
-//             for(int& y : colDir) {
-//                 int c1_ = x + c1;
-//                 int c2_ = y + c2;                
-//                 if(isSafe(c1_) && isSafe(c2_)) {
-//                     ans = max(ans, solve(grid, row+1, c1_, c2_));
+//         for (int row = 1; row < m; row++) {
+//             for (int c1 = 0; c1 <= min(n-1, row); c1++) { 
+//                 for (int c2 = max(0, n-1-row); c2 < n; c2++) {                    
+//                     int prev_max = 0;
+//                     for (int column1 = max(0, c1 - 1); column1 <= min(n - 1, c1 + 1); column1++) {
+//                         for (int column2 = max(0, c2 - 1); column2 <= min(n - 1, c2 + 1); column2++) {
+//                                 prev_max = max(prev_max, t[row - 1][column1][column2]);
+//                         }
+//                     }
+                    
+//                     if (c1 == c2)
+//                         t[row][c1][c2] = prev_max + grid[row][c1];
+//                     else
+//                         t[row][c1][c2] = prev_max + grid[row][c1] + grid[row][c2];
 //                 }
 //             }
-//         }        
-//         return t[row][c1][c2] = count+ans;  
-//     }
-    
-//     int cherryPickup(vector<vector<int>>& grid) {
-//         m = grid.size();
-//         n = grid[0].size();
-//         memset(t, -1, sizeof(t));
-//         return solve(grid, 0, 0, n-1);
+//         }
+        
+//         int res = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 res = max(res, t[m - 1][i][j]);    
+//             }
+//         }
+//         return res;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     int m, n;
+// //     int t[71][71][71];
+// //     vector<int> colDir{-1, 0, 1};
+// //     bool isSafe(int& i) {
+// //         return i >= 0 && i < n;
+// //     }    
+// //     int solve(vector<vector<int>>& grid, int row, int c1, int c2) {
+// //         if (row >= m)
+// //             return 0;       
+        
+// //         if (t[row][c1][c2] != -1) {
+// //             return t[row][c1][c2];
+// //         }
+        
+// //         int count = grid[row][c1];
+// //         if (c1 != c2) {
+// //             count += grid[row][c2];
+// //         }       
+        
+// //         int ans = 0;
+// //         for(int& x : colDir) {
+// //             for(int& y : colDir) {
+// //                 int c1_ = x + c1;
+// //                 int c2_ = y + c2;                
+// //                 if(isSafe(c1_) && isSafe(c2_)) {
+// //                     ans = max(ans, solve(grid, row+1, c1_, c2_));
+// //                 }
+// //             }
+// //         }        
+// //         return t[row][c1][c2] = count+ans;  
+// //     }
+    
+// //     int cherryPickup(vector<vector<int>>& grid) {
+// //         m = grid.size();
+// //         n = grid[0].size();
+// //         memset(t, -1, sizeof(t));
+// //         return solve(grid, 0, 0, n-1);
+// //     }
+// // };
