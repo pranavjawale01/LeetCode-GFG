809110b5cc582bae00dd061ea538d36230984ae0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-12 12:12:26 +0530

    Time: 142 ms (38.28%), Space: 18 MB (63.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count , n , element = 1, len(nums), nums[0]
-        for i in range(1, n):
-            if count is 0:
-                element = nums[i]
-                count += 1
-            elif nums[i] is element:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
                 count += 1
             else:
                 count -= 1
-        return element
+        
+        count = 0
+        for num in nums:
+            if num == candidate:
+                count += 1
+                
+        return candidate if count > len(nums) // 2 else -1
