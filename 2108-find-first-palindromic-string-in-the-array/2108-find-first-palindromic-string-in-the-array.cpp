72652da45cd71e72a86a932d22f88bbe929a0e99
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-13 16:23:31 +0530

    Time: 54 ms (29.73%), Space: 26.7 MB (24.97%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    bool isPalli(string word) {
-        string newWord = word;
-        reverse(newWord.begin(), newWord.end());
-        return word == newWord;
-    }
-
     string firstPalindrome(vector<string>& words) {
         for (string word : words) {
-            if (isPalli(word)) {
+            int n = word.size();
+            if (equal(word.begin(), word.begin() + n/2, word.rbegin())) {
                 return word;
             }
         }
         return "";
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalli(string word) {
+//         string newWord = word;
+//         reverse(newWord.begin(), newWord.end());
+//         return word == newWord;
+//     }
+
+//     string firstPalindrome(vector<string>& words) {
+//         for (string word : words) {
+//             if (isPalli(word)) {
+//                 return word;
+//             }
+//         }
+//         return "";
+//     }
+// };
