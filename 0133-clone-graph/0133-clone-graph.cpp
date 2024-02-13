de782b4c6c240eef2b2b9cc5f644d6978190cfaa
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-13 18:00:41 +0530

    Time: 5 ms (33.59%), Space: 11 MB (37.31%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,26 +22,65 @@ public:
 class Solution {
 public:
     unordered_map<Node*, Node*> mp;
-    void DFS(Node* node, Node* clone_node) {
-        for (Node* n : node->neighbors) {
-            if (mp.find(n) == mp.end()) {
-                Node* clone = new Node(n->val);
-                mp[n] = clone;
-                clone_node->neighbors.push_back(clone);
-                DFS(n, clone);
-            } else {
-                clone_node->neighbors.push_back(mp[n]);
-            }
-        }
-    }
+    
     Node* cloneGraph(Node* node) {
         if (!node) {
             return nullptr;
         }
+        
         mp.clear();
         Node* clone_node = new Node(node->val);
         mp[node] = clone_node;
-        DFS(node, clone_node);
+        
+        queue<Node*> q;
+        q.push(node);
+        
+        while (!q.empty()) {
+            Node* current = q.front();
+            q.pop();
+            
+            Node* clone_current = mp[current];
+            
+            for (Node* neighbor : current->neighbors) {
+                if (mp.find(neighbor) == mp.end()) {
+                    Node* clone_neighbor = new Node(neighbor->val);
+                    mp[neighbor] = clone_neighbor;
+                    clone_current->neighbors.push_back(clone_neighbor);
+                    q.push(neighbor);
+                } else {
+                    clone_current->neighbors.push_back(mp[neighbor]);
+                }
+            }
+        }
+        
         return clone_node;
     }
 };
+
+
+// class Solution {
+// public:
+//     unordered_map<Node*, Node*> mp;
+//     void DFS(Node* node, Node* clone_node) {
+//         for (Node* n : node->neighbors) {
+//             if (mp.find(n) == mp.end()) {
+//                 Node* clone = new Node(n->val);
+//                 mp[n] = clone;
+//                 clone_node->neighbors.push_back(clone);
+//                 DFS(n, clone);
+//             } else {
+//                 clone_node->neighbors.push_back(mp[n]);
+//             }
+//         }
+//     }
+//     Node* cloneGraph(Node* node) {
+//         if (!node) {
+//             return nullptr;
+//         }
+//         mp.clear();
+//         Node* clone_node = new Node(node->val);
+//         mp[node] = clone_node;
+//         DFS(node, clone_node);
+//         return clone_node;
+//     }
+// };
