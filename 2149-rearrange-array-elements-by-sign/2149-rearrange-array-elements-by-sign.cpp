6158ed03eb36a8267989a1640ad9b393466c4042
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-14 22:29:18 +0530

    Time: 155 ms (56.65%), Space: 126.3 MB (50.65%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos, neg, output;
-        int n = nums.size();
-        for (int num : nums) {
-            if (num > 0) {
-                pos.push_back(num);
+        int size = nums.size();
+        vector<int> output(size);
+        int p = 0, n = 1;
+        for (int i = 0; i < size; i++) {
+            if (nums[i] > 0) {
+                output[p] = nums[i];
+                p += 2;
             } else {
-                neg.push_back(num);
+                output[n] = nums[i];
+                n += 2;
             }
-        }
-        for (int i = 0; i < n / 2; i++) {
-            output.push_back(pos[i]);
-            output.push_back(neg[i]);
         }
         return output;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         vector<int> pos, neg, output;
+//         int n = nums.size();
+//         for (int num : nums) {
+//             if (num > 0) {
+//                 pos.push_back(num);
+//             } else {
+//                 neg.push_back(num);
+//             }
+//         }
+//         for (int i = 0; i < n / 2; i++) {
+//             output.push_back(pos[i]);
+//             output.push_back(neg[i]);
+//         }
+//         return output;
+//     }
+// };
