6fda6eb711a822c9754f96613013bd8e166309c1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-16 16:09:08 +0530

    Time: 122 ms (75.67%), Space: 66.1 MB (51.33%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -2,55 +2,81 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         unordered_map<int, int> mp;
-        for (int &ar : arr) {
-            mp[ar]++;
+        for (int &a : arr) {
+            mp[a]++;
         }
-        vector<int> freq;
-        for (auto& it : mp) {
-            freq.push_back(it.second);
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (auto &it : mp) {
+            pq.push(it.second);
         }
-        sort(freq.begin(), freq.end());
-        for (int i = 0; i < freq.size(); i++) {
-            k -= freq[i];
+
+        while (!pq.empty()) {
+            k -= pq.top();
             if (k < 0) {
-                return freq.size() - i;
+                return pq.size();
             }
+            pq.pop();
         }
+
         return 0;
     }
 };
 
 // class Solution {
 // public:
-//     int minCount = INT_MAX;
-//     void solve(vector<int>& arr, int k, int n, int start, int end) {
-//         if (end >= n) {
-//             return;
-//         }
-//         int uniqueCount = 0;
-//         unordered_map<int, int> mp;
-//         for (int i = 0; i < n; i++) {
-//             if (i < start || i > end) {
-//                 mp[arr[i]]++;
-//             }
-//         }
-//         for (auto& pair : mp) {
-//             if (pair.second > 0) {
-//                 uniqueCount++;
-//             }
-//         }
-//         minCount = min(minCount, uniqueCount);
-//         solve(arr, k, n, start+k, end+k);
-//     }
 //     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-//         int n = arr.size();
-//         if (n == 1 && k == 0) {
-//             return 1;
-//         } else if (n == k) {
-//             return 0;
+//         unordered_map<int, int> mp;
+//         for (int &ar : arr) {
+//             mp[ar]++;
 //         }
-//         sort(arr.begin(), arr.end());
-//         solve(arr, k, n, 0, k);
-//         return minCount;
+//         vector<int> freq;
+//         for (auto& it : mp) {
+//             freq.push_back(it.second);
+//         }
+//         sort(freq.begin(), freq.end());
+//         for (int i = 0; i < freq.size(); i++) {
+//             k -= freq[i];
+//             if (k < 0) {
+//                 return freq.size() - i;
+//             }
+//         }
+//         return 0;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     int minCount = INT_MAX;
+// //     void solve(vector<int>& arr, int k, int n, int start, int end) {
+// //         if (end >= n) {
+// //             return;
+// //         }
+// //         int uniqueCount = 0;
+// //         unordered_map<int, int> mp;
+// //         for (int i = 0; i < n; i++) {
+// //             if (i < start || i > end) {
+// //                 mp[arr[i]]++;
+// //             }
+// //         }
+// //         for (auto& pair : mp) {
+// //             if (pair.second > 0) {
+// //                 uniqueCount++;
+// //             }
+// //         }
+// //         minCount = min(minCount, uniqueCount);
+// //         solve(arr, k, n, start+k, end+k);
+// //     }
+// //     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+// //         int n = arr.size();
+// //         if (n == 1 && k == 0) {
+// //             return 1;
+// //         } else if (n == k) {
+// //             return 0;
+// //         }
+// //         sort(arr.begin(), arr.end());
+// //         solve(arr, k, n, 0, k);
+// //         return minCount;
+// //     }
+// // };
