48a485ac023928509e409ababb009a18d7c01b2d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-17 16:37:03 +0530

    Time: 20 ms (43.55%), Space: 59.9 MB (13.84%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,33 +1,19 @@
-import java.util.PriorityQueue;
-
-class Solution {
+public class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        int n = heights.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int i = 0;
-        for (; i < n - 1; i++) {
-            if (heights[i] >= heights[i + 1]) {
-                continue;
-            }
-            int diff = heights[i + 1] - heights[i];
-            if (diff <= bricks) {
-                bricks -= diff;
-                pq.add(diff);
-            } else if (ladders > 0) {
-                if (!pq.isEmpty()) {
-                    int maxBricksPast = pq.peek();
-                    if (maxBricksPast > diff) {
-                        bricks += maxBricksPast;
-                        pq.poll();
-                        pq.add(diff);
-                        bricks -= diff;
-                    }
-                }
-                ladders--;
-            } else {
-                break;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < heights.length - 1; i++) {
+            if (heights[i] >= heights[i + 1]) continue;
+            bricks -= heights[i + 1] - heights[i];
+            pq.add(heights[i + 1] - heights[i]);
+
+            if (bricks < 0) {
+                bricks += pq.poll();
+                if (ladders > 0) ladders--;
+                else return i;
             }
         }
-        return i;
+
+        return heights.length - 1;
     }
-};
+}
