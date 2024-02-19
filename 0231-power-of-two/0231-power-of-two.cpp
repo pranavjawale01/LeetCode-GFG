f6eb33e0d4ad4e96bd7691c757cfa4368e69460f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-19 11:24:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        return (n & (n-1)) == 0;
+        return __builtin_popcount(n) == 1;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if (n <= 0) {
+//             return false;
+//         }
+//         return (n & (n-1)) == 0;
+//     }
+// };
