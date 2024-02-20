f85cae070d92477b93c98160e8a49d026371d8e9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-20 14:42:48 +0530

    Time: 16 ms (47.45%), Space: 20.4 MB (32.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int sum = n*(n+1)/2;
+        int result = n;
         for (int i = 0; i < n; i++) {
-            sum -= nums[i];
+            result ^= i;
+            result ^= nums[i];
         }
-        return sum;
+        return result;
     }
 };
 
 // class Solution {
 // public:
 //     int missingNumber(vector<int>& nums) {
-//         sort(nums.begin(), nums.end());
-//         if (nums[0] != 0) {
-//             return 0;
-//         }
 //         int n = nums.size();
-//         for (int i = 0; i < n-1; i++) {
-//             if (nums[i] + 1 != nums[i+1]) {
-//                 return i+1;
-//             }
+//         int sum = n*(n+1)/2;
+//         for (int i = 0; i < n; i++) {
+//             sum -= nums[i];
 //         }
-//         return n;
+//         return sum;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     int missingNumber(vector<int>& nums) {
+// //         sort(nums.begin(), nums.end());
+// //         if (nums[0] != 0) {
+// //             return 0;
+// //         }
+// //         int n = nums.size();
+// //         for (int i = 0; i < n-1; i++) {
+// //             if (nums[i] + 1 != nums[i+1]) {
+// //                 return i+1;
+// //             }
+// //         }
+// //         return n;
+// //     }
+// // };
