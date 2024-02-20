2aa54b94cf5e43791fee5a6f4ae4fc33c30fbef0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-20 14:41:04 +0530

    Time: 4 ms (99.12%), Space: 20.4 MB (18.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if (nums[0] != 0) {
-            return 0;
-        }
         int n = nums.size();
-        for (int i = 0; i < n-1; i++) {
-            if (nums[i] + 1 != nums[i+1]) {
-                return i+1;
-            }
+        int sum = n*(n+1)/2;
+        for (int i = 0; i < n; i++) {
+            sum -= nums[i];
         }
-        return n;
+        return sum;
     }
 };
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         if (nums[0] != 0) {
+//             return 0;
+//         }
+//         int n = nums.size();
+//         for (int i = 0; i < n-1; i++) {
+//             if (nums[i] + 1 != nums[i+1]) {
+//                 return i+1;
+//             }
+//         }
+//         return n;
+//     }
+// };
