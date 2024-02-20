047d3af72de4307bb56963cfda22183ca77bffd7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-20 14:47:01 +0530

    Time: 13 ms (66.66%), Space: 21 MB (14.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+        sort(nums.begin(), nums.end());
         int result = n;
-        for (int i = 0; i < n; i++) {
-            result ^= i;
-            result ^= nums[i];
+        int l = 0, r = n-1;
+        while (l <= r) {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] > mid) {
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
         return result;
     }
@@ -15,27 +22,40 @@ public:
 // public:
 //     int missingNumber(vector<int>& nums) {
 //         int n = nums.size();
-//         int sum = n*(n+1)/2;
+//         int result = n;
 //         for (int i = 0; i < n; i++) {
-//             sum -= nums[i];
+//             result ^= i;
+//             result ^= nums[i];
 //         }
-//         return sum;
+//         return result;
 //     }
 // };
 
 // // class Solution {
 // // public:
 // //     int missingNumber(vector<int>& nums) {
-// //         sort(nums.begin(), nums.end());
-// //         if (nums[0] != 0) {
-// //             return 0;
-// //         }
 // //         int n = nums.size();
-// //         for (int i = 0; i < n-1; i++) {
-// //             if (nums[i] + 1 != nums[i+1]) {
-// //                 return i+1;
-// //             }
+// //         int sum = n*(n+1)/2;
+// //         for (int i = 0; i < n; i++) {
+// //             sum -= nums[i];
 // //         }
-// //         return n;
+// //         return sum;
 // //     }
 // // };
+
+// // // class Solution {
+// // // public:
+// // //     int missingNumber(vector<int>& nums) {
+// // //         sort(nums.begin(), nums.end());
+// // //         if (nums[0] != 0) {
+// // //             return 0;
+// // //         }
+// // //         int n = nums.size();
+// // //         for (int i = 0; i < n-1; i++) {
+// // //             if (nums[i] + 1 != nums[i+1]) {
+// // //                 return i+1;
+// // //             }
+// // //         }
+// // //         return n;
+// // //     }
+// // // };
