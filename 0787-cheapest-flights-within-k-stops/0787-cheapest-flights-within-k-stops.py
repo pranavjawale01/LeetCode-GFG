4f35ee0269d4952054410e2e5fc8e2fbbf340762
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-23 22:52:27 +0530

    Time: 89 ms (88.65%), Space: 17.9 MB (80.88%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,5 +1,3 @@
-from collections import defaultdict, deque
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         distance = [float('inf')] * n
