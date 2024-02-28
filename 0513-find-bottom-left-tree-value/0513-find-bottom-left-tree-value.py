41511d58ff3910b0271bdd28791e03ee85ee5a3a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-28 22:03:57 +0530

    Time: 46 ms (33.4%), Space: 18.2 MB (79.63%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -6,16 +6,35 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        maxDepth = -1
+        q = deque([root])
         value = 0
-        def solve(root: Optional[TreeNode], depth : int):
-            nonlocal maxDepth, value
-            if not root:
-                return
-            if depth > maxDepth:
-                maxDepth = depth
-                value = root.val
-            solve(root.left, depth + 1)
-            solve(root.right, depth + 1)
-        solve(root, 0)
+        while q:
+            temp = q.popleft()
+            value = temp.val
+            if temp.right:
+                q.append(temp.right)
+            if temp.left:
+                q.append(temp.left)
         return value
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+#         maxDepth = -1
+#         value = 0
+#         def solve(root: Optional[TreeNode], depth : int):
+#             nonlocal maxDepth, value
+#             if not root:
+#                 return
+#             if depth > maxDepth:
+#                 maxDepth = depth
+#                 value = root.val
+#             solve(root.left, depth + 1)
+#             solve(root.right, depth + 1)
+#         solve(root, 0)
+#         return value
