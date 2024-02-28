2c503185661a42741d6d550b0223926d2e58cb12
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-28 22:00:52 +0530

    Time: 2 ms (30.43%), Space: 44.6 MB (18.94%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,22 +14,56 @@
  * }
  */
 class Solution {
-    public int value;
-    public int maxDepth;
-    public void solve(TreeNode root, int depth) {
-        if (root == null) {
-            return;
-        }
-        if (depth > maxDepth) {
-            maxDepth = depth;
-            value = root.val;
-        }
-        solve(root.left, depth + 1);
-        solve(root.right, depth + 1);
-    }
     public int findBottomLeftValue(TreeNode root) {
-        maxDepth = -1;
-        solve(root, 0);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int value = 0;
+        while (!q.isEmpty()) {
+            TreeNode temp = q.poll();
+            value = temp.val;
+            if (temp.right != null) {
+                q.offer(temp.right);
+            }
+            if (temp.left != null) {
+                q.offer(temp.left);
+            }
+        }
         return value;
     }
 }
+
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public int value;
+//     public int maxDepth;
+//     public void solve(TreeNode root, int depth) {
+//         if (root == null) {
+//             return;
+//         }
+//         if (depth > maxDepth) {
+//             maxDepth = depth;
+//             value = root.val;
+//         }
+//         solve(root.left, depth + 1);
+//         solve(root.right, depth + 1);
+//     }
+//     public int findBottomLeftValue(TreeNode root) {
+//         maxDepth = -1;
+//         solve(root, 0);
+//         return value;
+//     }
+// }
