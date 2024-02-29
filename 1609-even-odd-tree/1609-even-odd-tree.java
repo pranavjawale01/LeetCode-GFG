3d891adaf84f954da1f0962f3307f1dc79c781b8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-02-29 23:49:46 +0530

    Time: 13 ms (50.59%), Space: 85.1 MB (6.84%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -13,32 +13,59 @@
  *     }
  * }
  */
+
 class Solution {
-    public boolean isEvenOddTree(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        boolean evenLevel = true;
-        while (!q.isEmpty()) {
-            int n = q.size();
-            int prev = evenLevel ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            while (n-- > 0) {
-                TreeNode temp = q.poll();
-                if (temp.left != null) {
-                    q.offer(temp.left);
-                }
-                if (temp.right != null) {
-                    q.offer(temp.right);
-                }
-                if (evenLevel && (temp.val % 2 == 0 || temp.val <= prev)) {
-                    return false;
-                }
-                if (!evenLevel && (temp.val % 2 != 0 || temp.val >= prev)) {
-                    return false;
-                }
-                prev = temp.val;
-            }
-            evenLevel = !evenLevel;
+    private List<Integer> levelPrev = new ArrayList<>();
+    private boolean solve(TreeNode root, int level) {
+        if (root == null) {
+            return true;
         }
-        return true;
+
+        if ((level % 2 == 0 && root.val % 2 == 0) || (level % 2 != 0 && root.val % 2 != 0)) {
+            return false;
+        }
+
+        if (level >= levelPrev.size()) {
+            levelPrev.add(root.val);
+        } else {
+            if ((level % 2 == 0 && root.val <= levelPrev.get(level)) || (level % 2 != 0 && root.val >= levelPrev.get(level))) {
+                return false;
+            }
+            levelPrev.set(level, root.val);
+        }
+        return solve(root.left, level + 1) && solve(root.right, level + 1);
+    }
+    public boolean isEvenOddTree(TreeNode root) {
+        return solve(root, 0);
     }
 }
+
+// class Solution {
+//     public boolean isEvenOddTree(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+//         boolean evenLevel = true;
+//         while (!q.isEmpty()) {
+//             int n = q.size();
+//             int prev = evenLevel ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+//             while (n-- > 0) {
+//                 TreeNode temp = q.poll();
+//                 if (temp.left != null) {
+//                     q.offer(temp.left);
+//                 }
+//                 if (temp.right != null) {
+//                     q.offer(temp.right);
+//                 }
+//                 if (evenLevel && (temp.val % 2 == 0 || temp.val <= prev)) {
+//                     return false;
+//                 }
+//                 if (!evenLevel && (temp.val % 2 != 0 || temp.val >= prev)) {
+//                     return false;
+//                 }
+//                 prev = temp.val;
+//             }
+//             evenLevel = !evenLevel;
+//         }
+//         return true;
+//     }
+// }
