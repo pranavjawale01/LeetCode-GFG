1851bffc41c00e87dd84257beb4b7dab1b253bc4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-01 11:13:18 +0530

    Time: 0 ms (100%), Space: 56.4 MB (26.34%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,9 +1,24 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i = 0; 
-        while (arr[i] < arr[i+1]) {
-            i++;
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid+1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return i;
+        return low;
     }
 }
+
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int i = 0; 
+//         while (arr[i] < arr[i+1]) {
+//             i++;
+//         }
+//         return i;
+//     }
+// }
