8cb16aee0751182f3e2b660d17bce59ec04fd74d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-01 11:10:48 +0530

    Time: 62 ms (98.44%), Space: 61.8 MB (66.26%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int i = 0;
-        while (arr[i] < arr[i+1]) {
-            i++;
+        int low = 0, high = arr.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return i;
+        return low;
     }
 };
+
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int i = 0;
+//         while (arr[i] < arr[i+1]) {
+//             i++;
+//         }
+//         return i;
+//     }
+// };
