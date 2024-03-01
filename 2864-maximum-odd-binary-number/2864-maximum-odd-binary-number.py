d04a279e6e52e7a89efd90731c01dfeede1e5620
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-01 11:52:49 +0530

    Time: 35 ms (76.4%), Space: 16.8 MB (32.65%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -2,12 +2,26 @@ class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
         n = len(s)
         result = "0" * n
-        i = 0
-        for ch in s:
-            if ch == '1':
-                if result[n - 1] == '1':
-                    result = result[:i] + '1' + result[i+1:]
-                    i += 1
-                else:
-                    result = result[:n-1] + '1'
+        count_1 = s.count('1')
+        j = 0
+        while count_1 > 1:
+            result = result[:j] + '1' + result[j+1:]
+            j += 1
+            count_1 -= 1
+        result = result[:n-1] + '1'
         return result
+
+
+# class Solution:
+#     def maximumOddBinaryNumber(self, s: str) -> str:
+#         n = len(s)
+#         result = "0" * n
+#         i = 0
+#         for ch in s:
+#             if ch == '1':
+#                 if result[n - 1] == '1':
+#                     result = result[:i] + '1' + result[i+1:]
+#                     i += 1
+#                 else:
+#                     result = result[:n-1] + '1'
+#         return result
