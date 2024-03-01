319cd3fbabe13ef5056bf32792365ae9dabc10a7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-01 11:52:24 +0530

    Time: 5 ms (22.9%), Space: 44.3 MB (27.64%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,21 +1,42 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
         int n = s.length();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            result.append('0');
-        }
-        int i = 0;
+        StringBuilder result = new StringBuilder(String.join("", Collections.nCopies(n, "0")));
+        int count_1 = 0;
         for (char ch : s.toCharArray()) {
             if (ch == '1') {
-                if (result.charAt(n-1) == '1') {
-                    result.setCharAt(i, '1');
-                    i++;
-                } else {
-                    result.setCharAt(n - 1, '1');
-                }
+                count_1++;
             }
         }
+        int j = 0;
+        while (count_1 > 1) {
+            result.setCharAt(j, '1');
+            j++;
+            count_1--;
+        }
+        result.setCharAt(n - 1, '1');
         return result.toString();
     }
 }
+
+// class Solution {
+//     public String maximumOddBinaryNumber(String s) {
+//         int n = s.length();
+//         StringBuilder result = new StringBuilder();
+//         for (int i = 0; i < n; i++) {
+//             result.append('0');
+//         }
+//         int i = 0;
+//         for (char ch : s.toCharArray()) {
+//             if (ch == '1') {
+//                 if (result.charAt(n-1) == '1') {
+//                     result.setCharAt(i, '1');
+//                     i++;
+//                 } else {
+//                     result.setCharAt(n - 1, '1');
+//                 }
+//             }
+//         }
+//         return result.toString();
+//     }
+// }
