5fa92c1ac70190530afba8d9b0dfef914911567b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-04 23:33:11 +0530

    Time: 10 ms (18.39%), Space: 15.1 MB (69.99%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    static bool myComparator(int &s1, int &s2) {
-        string a = to_string(s1);
-        string b = to_string(s2);
-        return a+b > b+a;
-    }
     string largestNumber(vector<int>& nums) {
+        auto myComparator = [](int &a, int &b) {
+            string c = to_string(a);
+            string d = to_string(b);
+            return c+d > d+c;
+        };
         sort(nums.begin(), nums.end(), myComparator);
         if (nums[0] == 0) {
             return "0";
@@ -17,3 +17,22 @@ public:
         return ans;
     }
 };
+// class Solution {
+// public:
+//     static bool myComparator(int &s1, int &s2) {
+//         string a = to_string(s1);
+//         string b = to_string(s2);
+//         return a+b > b+a;
+//     }
+//     string largestNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end(), myComparator);
+//         if (nums[0] == 0) {
+//             return "0";
+//         } 
+//         string ans = "";
+//         for (int x : nums) {
+//             ans += to_string(x);
+//         }
+//         return ans;
+//     }
+// };
