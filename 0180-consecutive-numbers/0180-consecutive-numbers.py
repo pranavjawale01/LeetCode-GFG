d046847581fbe41f7e14f959c350f14001746242
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-08 23:47:49 +0530

    Time: 608 ms (66.55%), Space: 66.3 MB (25.81%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.py
+++ 0180-consecutive-numbers/0180-consecutive-numbers.py
@@ -1,23 +1,7 @@
 import pandas as pd
 
 def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
-    prev_num = None
-    rec = []
-    count = 0
-    for index,row in logs.iterrows():
-
-        if row['num'] == prev_num and count<3:
-            count = count+1
-        elif row['num'] == prev_num and count>3:
-            continue
-        elif index==0:
-            count = count+1
-        else:
-            count = 1
-        if count==3 and row['num'] not in rec:
-            rec.append(row['num'])
-            
-        prev_num = row['num']
-    
-    newdf = pd.DataFrame({'ConsecutiveNums':rec})
-    return newdf
+    logs['lag'] = logs['num'].shift(-1)
+    logs['lead']=logs['num'].shift(1)
+    filt=(logs['num']==logs['lag']) & (logs['num']==logs['lead'])
+    return logs.loc[filt, ['num']].rename(columns={'num':'ConsecutiveNums'}).drop_duplicates()
