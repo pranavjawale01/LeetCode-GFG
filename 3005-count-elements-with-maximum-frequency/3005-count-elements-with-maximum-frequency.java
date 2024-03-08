6fc71b220c584734d1a316048d674030bf9a598f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-08 23:05:17 +0530

    Time: 1 ms (99.64%), Space: 42.3 MB (52.28%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,16 +1,34 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
+        int[] arr = new int[101];
         int maxCount = 0, ans = 0;
         for (int num : nums) {
-            mp.put(num , mp.getOrDefault(num, 0) + 1);
-            maxCount = Math.max(maxCount, mp.get(num));
-        }
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            if (entry.getValue() == maxCount) {
-                ans += entry.getValue();
+            arr[num]++;
+            if (maxCount == arr[num]) {
+                ans += arr[num];
+            }
+            if (arr[num] > maxCount) {
+                ans = arr[num];
+                maxCount = arr[num];
             }
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public int maxFrequencyElements(int[] nums) {
+//         Map<Integer, Integer> mp = new HashMap<>();
+//         int maxCount = 0, ans = 0;
+//         for (int num : nums) {
+//             mp.put(num , mp.getOrDefault(num, 0) + 1);
+//             maxCount = Math.max(maxCount, mp.get(num));
+//         }
+//         for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+//             if (entry.getValue() == maxCount) {
+//                 ans += entry.getValue();
+//             }
+//         }
+//         return ans;
+//     }
+// }
