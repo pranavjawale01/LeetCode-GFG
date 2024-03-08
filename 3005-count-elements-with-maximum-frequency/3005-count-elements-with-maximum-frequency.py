4a38093ffa366e09167901b1b073194adbee0feb
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-08 23:06:50 +0530

    Time: 41 ms (73.85%), Space: 16.5 MB (83.74%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        mp = defaultdict(int)
+        arr = [0] * 101
         ans , maxCount = 0, 0
         for num in nums:
-            mp[num] += 1
-            maxCount = max(maxCount, mp[num])
-        for num, count in mp.items():
-            if count == maxCount:
-                ans += count
+            arr[num] += 1
+            if maxCount == arr[num]:
+                ans += arr[num]
+            if arr[num] > maxCount:
+                maxCount = arr[num]
+                ans = arr[num]
         return ans
+
+# class Solution:
+#     def maxFrequencyElements(self, nums: List[int]) -> int:
+#         mp = defaultdict(int)
+#         ans , maxCount = 0, 0
+#         for num in nums:
+#             mp[num] += 1
+#             maxCount = max(maxCount, mp[num])
+#         for num, count in mp.items():
+#             if count == maxCount:
+#                 ans += count
+#         return ans
