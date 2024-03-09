db4686e63386fc212a0e3d969e81948aa22e423c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-09 16:52:13 +0530

    Time: 7 ms (34.44%), Space: 62.3 MB (88%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,17 +1,43 @@
 class Solution {
-    public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> st = new HashSet<>();
-        for (int num : nums1) {
-            st.add(num);
+    public boolean binarySearch(int[] nums, int num) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == num) {
+                return true;
+            } else if (nums[mid] > num) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        for (int num : nums2) {
-            if (st.contains(num)) {
+        return false;
+    }
+    public int getCommon(int[] nums1, int[] nums2) {
+        for (int num : nums1) {
+            if (binarySearch(nums2, num)) {
                 return num;
             }
         }
         return -1;
     }
 }
+
+// class Solution {
+//     public int getCommon(int[] nums1, int[] nums2) {
+//         Set<Integer> st = new HashSet<>();
+//         for (int num : nums1) {
+//             st.add(num);
+//         }
+//         for (int num : nums2) {
+//             if (st.contains(num)) {
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 // class Solution {
 //     public int getCommon(int[] nums1, int[] nums2) {
 //         int i = 0, j = 0;
