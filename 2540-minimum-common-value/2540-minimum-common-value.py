d981c0cac75d6e6d12f5d08b2f3d5bf1bfcb45c6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-09 16:32:26 +0530

    Time: 343 ms (78.14%), Space: 35.6 MB (73.75%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,7 +1,7 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         i , j , m , n = 0, 0, len(nums1), len(nums2)
-        while i < n and j < m:
+        while i < m and j < n:
             if nums1[i] == nums2[j]:
                 return nums1[i]
             elif nums1[i] > nums2[j]:
