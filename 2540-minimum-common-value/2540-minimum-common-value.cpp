448a7b5a5f6ad0b31c836d24145e7686cce42f77
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-09 16:48:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
+    bool binarySearch(vector<int>& nums2, int num) {
+        int low = 0, high = nums2.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums2[mid] == num) {
+                return true;
+            } else if (nums2[mid] > num) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(begin(nums1), end(nums1));
         for (int &num : nums2) {
-            if (st.find(num) != st.end()) {
+            if (binarySearch(nums2, num)) {
                 return num;
             }
         }
         return -1;
     }
 };
+// class Solution {
+// public:
+//     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_set<int> st(begin(nums1), end(nums1));
+//         for (int &num : nums2) {
+//             if (st.find(num) != st.end()) {
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 // class Solution {
 // public:
 //     int getCommon(vector<int>& nums1, vector<int>& nums2) {
