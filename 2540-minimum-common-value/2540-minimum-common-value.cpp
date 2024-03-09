e61772193dc538ff5f4e3b8d6edfeae2a33816d8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-09 16:35:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
-        int n = nums1.size(), m = nums2.size();
-        while (i < n && j < m) {
-            if (nums1[i] == nums2[j]) {
-                return nums1[i];
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
+        unordered_map<int, int> mp;
+        for (int num : nums1) {
+            mp[num]++;
+        }
+        for (int num : nums2) {
+            mp[num]++;
+            if (mp[num] > 1) {
+                return mp[num];
             }
         }
         return -1;
     }
 };
+// class Solution {
+// public:
+//     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+//         int i = 0, j = 0;
+//         int n = nums1.size(), m = nums2.size();
+//         while (i < n && j < m) {
+//             if (nums1[i] == nums2[j]) {
+//                 return nums1[i];
+//             } else if (nums1[i] > nums2[j]) {
+//                 j++;
+//             } else if (nums1[i] < nums2[j]) {
+//                 i++;
+//             }
+//         }
+//         return -1;
+//     }
+// };
