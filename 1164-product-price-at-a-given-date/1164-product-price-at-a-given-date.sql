f2cc2f0ffc30b8677d488db7248ad6138be8982c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-09 17:18:37 +0530

    Time: 1037 ms (74%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,17 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
+(
+    SELECT product_id ,(10) as price
+    FROM Products
+    GROUP BY product_id
+    HAVING MIN(change_date) > '2019-08-16'
+)
+UNION
 SELECT product_id, new_price AS price
 FROM Products
-WHERE (product_id, change_date) IN (
-    SELECT product_id , max(change_date) AS latest_date
+WHERE (product_id, change_date)
+IN (
+    SELECT product_id, MAX(change_date)
     FROM Products
-    WHERE change_date <= "2019-08-16"
+    WHERE change_date <= '2019-08-16'
     GROUP BY product_id
-) UNION
-SELECT DISTINCT product_id, 10 as price
-FROM Products
-WHERE product_id NOT IN (
-    SELECT DISTINCT product_id FROM Products
-    WHERE change_date <= "2019-08-16"
 )
