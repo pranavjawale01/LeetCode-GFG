5c5b7c487a9145bea1f2336a9c74a5eefb95a1d4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-10 12:39:49 +0530

    Time: 53 ms (33.8%), Space: 16.7 MB (53.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,23 +1,48 @@
 class Solution:
-    def binarySearch(self, nums, num):
-        low, high = 0, len(nums) - 1
-        while low <= high:
-            mid = low + (high - low) // 2
-            if nums[mid] == num:
-                return True
-            elif num > nums[mid]:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return False
-    
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1.sort()
-        ans = set()
-        for num in nums2:
-            if self.binarySearch(nums1, num):
-                ans.add(num)
-        return list(ans)
+        nums2.sort()
+        
+        ans = []
+        i, j = 0, 0
+        m, n = len(nums1), len(nums2)
+        
+        while i < m and j < n:
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                while i < m - 1 and nums1[i] == nums1[i + 1]:
+                    i += 1
+                while j < n - 1 and nums2[j] == nums2[j + 1]:
+                    j += 1
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return ans
+        
+# class Solution:
+#     def binarySearch(self, nums, num):
+#         low, high = 0, len(nums) - 1
+#         while low <= high:
+#             mid = low + (high - low) // 2
+#             if nums[mid] == num:
+#                 return True
+#             elif num > nums[mid]:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+#         return False
+    
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         nums1.sort()
+#         ans = set()
+#         for num in nums2:
+#             if self.binarySearch(nums1, num):
+#                 ans.add(num)
+#         return list(ans)
 
 # class Solution:
 #     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
