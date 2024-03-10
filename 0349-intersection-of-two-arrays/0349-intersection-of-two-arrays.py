b9dedb0f763717124e625a19eaf7864c8feb4e63
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-10 12:32:18 +0530

    Time: 45 ms (74.57%), Space: 16.7 MB (91.42%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,12 +1,33 @@
 class Solution:
+    def binarySearch(self, nums, num):
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == num:
+                return True
+            elif num > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
+    
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        s1 = set(nums1)
-        ans = []
+        nums1.sort()
+        ans = set()
         for num in nums2:
-            if num in s1:
-                ans.append(num)
-                s1.remove(num)
-        return ans
+            if self.binarySearch(nums1, num):
+                ans.add(num)
+        return list(ans)
+
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         s1 = set(nums1)
+#         ans = []
+#         for num in nums2:
+#             if num in s1:
+#                 ans.append(num)
+#                 s1.remove(num)
+#         return ans
 
 # class Solution:
 #     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
