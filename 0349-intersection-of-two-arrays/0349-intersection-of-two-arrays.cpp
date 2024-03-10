7dc1b7e70b1e0dec81aef43725ff3ec33366c97c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-10 12:30:26 +0530

    Time: 4 ms (68.61%), Space: 12.8 MB (37.74%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,14 +1,58 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> s1(begin(nums1), end(nums1));
-        unordered_set<int> s2(begin(nums2), end(nums2));
-        vector<int> ans;
-        for (int num : s2) {
-            if (s1.find(num) != s1.end()) {
-                ans.push_back(num);
+    bool binarySearch(vector<int>& nums, int num) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == num) {
+                return true;
+            } else if (num > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        return ans;
+        return false;
+    }
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(begin(nums1), end(nums1));
+        unordered_set<int> ans;
+        for (int& num : nums2) {
+            if (binarySearch(nums1, num)) {
+                ans.insert(num);
+            }
+        }
+        vector<int> output(begin(ans), end(ans));
+        return output;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_set<int> s1(begin(nums1), end(nums1));
+//         vector<int> ans;
+//         for (int num : nums2) {
+//             if (s1.find(num) != s1.end()) {
+//                 ans.push_back(num);
+//                 s1.erase(num);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_set<int> s1(begin(nums1), end(nums1));
+//         unordered_set<int> s2(begin(nums2), end(nums2));
+//         vector<int> ans;
+//         for (int num : s2) {
+//             if (s1.find(num) != s1.end()) {
+//                 ans.push_back(num);
+//             }
+//         }
+//         return ans;
+//     }
+// };
