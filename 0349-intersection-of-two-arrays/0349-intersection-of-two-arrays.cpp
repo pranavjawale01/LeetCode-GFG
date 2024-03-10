9e6df9e7e9036a348322305bff675462af1549b1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-10 12:37:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (51.34%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& nums, int num) {
-        int low = 0, high = nums.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == num) {
-                return true;
-            } else if (num > nums[mid]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return false;
-    }
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(begin(nums1), end(nums1));
-        unordered_set<int> ans;
-        for (int& num : nums2) {
-            if (binarySearch(nums1, num)) {
-                ans.insert(num);
+        sort(begin(nums2), end(nums2));
+        vector<int> ans;
+        int i = 0, j = 0;
+        int m = nums1.size(), n = nums2.size();
+        while (i < m && j < n) {
+            if (nums1[i] == nums2[j]) {
+                ans.push_back(nums1[i]);
+                while(i < m - 1 && nums1[i] == nums1[i+1]) {
+                    i++;
+                } 
+                while (j < n - 1 && nums2[j] == nums2[j+1]) {
+                    j++;
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
-        vector<int> output(begin(ans), end(ans));
-        return output;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     bool binarySearch(vector<int>& nums, int num) {
+//         int low = 0, high = nums.size() - 1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (nums[mid] == num) {
+//                 return true;
+//             } else if (num > nums[mid]) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return false;
+//     }
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         sort(begin(nums1), end(nums1));
+//         unordered_set<int> ans;
+//         for (int& num : nums2) {
+//             if (binarySearch(nums1, num)) {
+//                 ans.insert(num);
+//             }
+//         }
+//         vector<int> output(begin(ans), end(ans));
+//         return output;
+//     }
+// };
 
 // class Solution {
 // public:
