e23861ce339e6b62bd6b582804b68bd20a0ce41a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-11 21:17:55 +0530

    Time: 30 ms (87.77%), Space: 16.4 MB (91.57%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,13 +1,21 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        mp = defaultdict(int)
-        for ch in s:
-            mp[ch] += 1
-        ans = ""
-        for ch in order:
-            if mp[ch] > 0:
-                ans += ch * mp[ch]
-                del mp[ch]
-        for ch, count in mp.items():
-            ans += ch * count
-        return ans
+        arr = [-1] * 26
+        for i, ch in enumerate(order):
+            arr[ord(ch) - ord('a')] = i
+        s = sorted(s, key = lambda x : arr[ord(x) - ord('a')])
+        return ''.join(s)
+
+# class Solution:
+#     def customSortString(self, order: str, s: str) -> str:
+#         mp = defaultdict(int)
+#         for ch in s:
+#             mp[ch] += 1
+#         ans = ""
+#         for ch in order:
+#             if mp[ch] > 0:
+#                 ans += ch * mp[ch]
+#                 del mp[ch]
+#         for ch, count in mp.items():
+#             ans += ch * count
+#         return ans
