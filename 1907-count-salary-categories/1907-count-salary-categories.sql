65fb9e411690276d9f869d9565fce3fe4dd9f695
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-11 21:37:08 +0530

    Time: 1348 ms (47.67%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,33 +1,15 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
+SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
 
-SELECT 
-    categories.category,
-    COUNT(accounts.account_id) AS accounts_count
-FROM 
-    (SELECT 'Low Salary' AS category
-     UNION ALL
-     SELECT 'Average Salary' AS category
-     UNION ALL
-     SELECT 'High Salary' AS category) AS categories
-LEFT JOIN 
-    accounts ON
-    (categories.category = 
-        IF(accounts.income < 20000, 'Low Salary', 
-            IF(accounts.income BETWEEN 20000 AND 50000, 'Average Salary', 'High Salary')
-        )
-    )
-GROUP BY 
-    categories.category;
+UNION
 
--- SELECT 'Low Salary' as category, IFNULL(COUNT(*), 0) as accounts_count
--- FROM Accounts
--- WHERE income < 20000
--- UNION 
--- SELECT 'Average Salary' , IFNULL(COUNT(*), 0) as accounts_count
--- FROM Accounts
--- WHERE income BETWEEN 20000 AND 50000
--- UNION 
--- SELECT 'High Salary' , IFNULL(COUNT(*), 0) as accounts_count
--- FROM Accounts
--- WHERE income > 50000
--- ORDER BY accounts_count DESC;
+SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
+
+UNION
+SELECT 'High Salary' AS category, COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000
