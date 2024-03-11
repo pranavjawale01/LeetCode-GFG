df951758d2f687c974e2818c6729a22b1fc5ba9a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-11 21:15:25 +0530

    Time: 4 ms (24.18%), Space: 41.3 MB (85.81%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,28 +1,49 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character, Integer> mp = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        int[] arr = new int[26];
+        Arrays.fill(arr , -1);
+        for (int i = 0; i < order.length(); i++) {
+            char ch = order.charAt(i);
+            arr[ch - 'a'] = i;
         }
+        Comparator<Character> myComparator = (ch1, ch2) -> arr[ch1 - 'a'] - arr[ch2 - 'a'];
+        Character[] sChar = new Character[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            sChar[i] = s.charAt(i);
+        }
+        Arrays.sort(sChar, myComparator);
         StringBuilder ans = new StringBuilder();
-        for (char ch : order.toCharArray()) {
-            if (mp.containsKey(ch)) {
-                int count = mp.get(ch);
-                while (count > 0) {
-                    ans.append(ch);
-                    count--;
-                }
-                mp.remove(ch);
-            }
-        }
-        for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
-            char ch = entry.getKey();
-            int count = entry.getValue();
-            while (count > 0) {
-                ans.append(ch);
-                count--;
-            }
+        for (char ch : sChar) {
+            ans.append(ch);
         }
         return ans.toString();
     }
 }
+// class Solution {
+//     public String customSortString(String order, String s) {
+//         Map<Character, Integer> mp = new HashMap<>();
+//         for (char ch : s.toCharArray()) {
+//             mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+//         }
+//         StringBuilder ans = new StringBuilder();
+//         for (char ch : order.toCharArray()) {
+//             if (mp.containsKey(ch)) {
+//                 int count = mp.get(ch);
+//                 while (count > 0) {
+//                     ans.append(ch);
+//                     count--;
+//                 }
+//                 mp.remove(ch);
+//             }
+//         }
+//         for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
+//             char ch = entry.getKey();
+//             int count = entry.getValue();
+//             while (count > 0) {
+//                 ans.append(ch);
+//                 count--;
+//             }
+//         }
+//         return ans.toString();
+//     }
+// }
