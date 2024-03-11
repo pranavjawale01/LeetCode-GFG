cffcf04d3de46790cfac1cfd6ca204f04d8dd5c4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-11 21:07:17 +0530

    Time: 0 ms (100%), Space: 8.1 MB (39.54%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char, int> mp;
-        for (char ch : s) {
-            mp[ch]++;
+        vector<int> mp(26, -1);
+        for (int i = 0; i < order.length(); i++) {
+            char ch = order[i];
+            mp[ch - 'a'] = i;
         }
-        string ans = "";
-        for (char ch : order) {
-            if (mp[ch] > 0) {
-                while (mp[ch] != 0) {
-                    ans += ch;
-                    mp[ch]--;
-                }
-            }
-        }
-        for (auto x : mp) {
-            if (x.second > 0) {
-                while (x.second != 0) {
-                    ans += x.first;
-                    x.second--;
-                }
-            }
-        }
-        return ans;
+        auto myComparator = [&mp](char& ch1, char& ch2) {
+            return mp[ch1 - 'a'] < mp[ch2 - 'a'];
+        };
+        sort(begin(s), end(s), myComparator);
+        return s;
     }
 };
+// class Solution {
+// public:
+//     string customSortString(string order, string s) {
+//         unordered_map<char, int> mp;
+//         for (char ch : s) {
+//             mp[ch]++;
+//         }
+//         string ans = "";
+//         for (char ch : order) {
+//             if (mp[ch] > 0) {
+//                 while (mp[ch] != 0) {
+//                     ans += ch;
+//                     mp[ch]--;
+//                 }
+//             }
+//         }
+//         for (auto x : mp) {
+//             if (x.second > 0) {
+//                 while (x.second != 0) {
+//                     ans += x.first;
+//                     x.second--;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
