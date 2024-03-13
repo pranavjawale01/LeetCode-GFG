1b6c0be4051bed4ce0da7d0377922ffc42022426
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-13 16:40:43 +0530

    Time: 1 ms (77.54%), Space: 40.2 MB (88.87%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,16 +1,62 @@
+// 3.Two Pointer
+
 class Solution {
     public int pivotInteger(int n) {
-        int lSum = 0, rSum = 0;
-        for (int i = 1; i <= n; i++) {
-            lSum += i;
-            for (int j = i; j <= n; j++) {
-                rSum += j;
+        int lSum = 1, rSum = n;
+        int left = 1, right = n;
+        while (left < right) {
+            if (lSum == rSum) {
+                left++;
+                right--;
+                lSum += left;
+                rSum += right;
+            } else if (lSum < rSum) {
+                left++;
+                lSum += left;
+            } else {
+                right--;
+                rSum += right;
             }
-            if (rSum == lSum) {
-                return i;
-            }
-            rSum = 0;
+        }
+        if (rSum == lSum) {
+            return left;
         }
         return -1;
     }
 }
+
+// 4. Optimized Brute Force
+
+// class Solution {
+//     public int pivotInteger(int n) {
+//         int lSum = 0, rSum = 0;
+//         int totalSum = n * (n + 1) / 2;
+//         for (int i = 1; i <= n; i++) {
+//             lSum = i * (i + 1) / 2;
+//             rSum = totalSum - lSum + i;
+//             if (rSum == lSum) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// 5.Brute Force
+
+// class Solution {
+//     public int pivotInteger(int n) {
+//         int lSum = 0, rSum = 0;
+//         for (int i = 1; i <= n; i++) {
+//             lSum += i;
+//             for (int j = i; j <= n; j++) {
+//                 rSum += j;
+//             }
+//             if (rSum == lSum) {
+//                 return i;
+//             }
+//             rSum = 0;
+//         }
+//         return -1;
+//     }
+// }
