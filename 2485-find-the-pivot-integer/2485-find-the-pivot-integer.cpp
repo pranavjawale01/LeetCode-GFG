adb25405c515064a06171860ce7e59047c308411
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-13 15:11:18 +0530

    Time: 25 ms (13.5%), Space: 7.3 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int lSum = 0, rSum = 0;
+        for (int i = 1; i <= n; i++) {
+            lSum += i;
+            for (int j = i; j <= n; j++) {
+                rSum += j;
+            }
+            if (lSum == rSum) {
+                return i;
+            }
+            rSum = 0;
+        }
+        return -1;
+    }
+};
