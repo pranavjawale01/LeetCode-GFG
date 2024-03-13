76aab5c7d7913efa8c179348f8e60ed59b7fe161
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-13 15:30:01 +0530

    Time: 4 ms (35.26%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,17 +1,65 @@
+// 3. Two Pointer
+
 class Solution {
 public:
     int pivotInteger(int n) {
-        int lSum = 0, rSum = 0;
-        for (int i = 1; i <= n; i++) {
-            lSum += i;
-            for (int j = i; j <= n; j++) {
-                rSum += j;
-            }
+        int lSum = 1, rSum = n;
+        int left = 1, right = n;
+        while (left < right) {
             if (lSum == rSum) {
-                return i;
+                left++;
+                right--;
+                lSum += left;
+                rSum += right;
+            } else if (lSum > rSum) {
+                right--;
+                rSum += right;
+            } else {
+                left++;
+                lSum += left;
             }
-            rSum = 0;
+        }
+        if (lSum == rSum) {
+            return left;
         }
         return -1;
     }
 };
+
+// 4. Brute Force Optimized
+
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int lSum = 0, rSum = 0;
+//         int totalSum = n * (n + 1) / 2;
+//         for (int i = 1; i <= n; i++) {
+//             lSum = i * (i + 1) / 2;
+//             rSum = totalSum - lSum + i;
+//             if (lSum == rSum) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// 5.Brute Force
+
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int lSum = 0, rSum = 0;
+//         for (int i = 1; i <= n; i++) {
+//             lSum += i;
+//             for (int j = i; j <= n; j++) {
+//                 rSum += j;
+//             }
+//             if (lSum == rSum) {
+//                 return i;
+//             }
+//             rSum = 0;
+//         }
+//         return -1;
+//     }
+// };
