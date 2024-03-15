d0765de4125b7317d3ff0799d4e31001c6b67556
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-15 16:57:26 +0530

    Time: 12 ms (83.78%), Space: 26.3 MB (86.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,17 +3,34 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> left(n, 1);
-        vector<int> right(n, 1);
         for (int i = 1; i < n; i++) {
             left[i] = left[i-1] * nums[i-1];
-            right[n-i-1] = right[n-i] * nums[n-i];
         }
-        for (int i = 0; i < n; i++) {
-            nums[i] = left[i] * right[i];
+        int right = 1;
+        for (int i = n-1; i >= 0; i--) {
+            left[i] = left[i] * right;
+            right *= nums[i];
         }
-        return nums;
+        return left;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> left(n, 1);
+//         vector<int> right(n, 1);
+//         for (int i = 1; i < n; i++) {
+//             left[i] = left[i-1] * nums[i-1];
+//             right[n-i-1] = right[n-i] * nums[n-i];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = left[i] * right[i];
+//         }
+//         return nums;
+//     }
+// };
 
 // class Solution {
 // public:
