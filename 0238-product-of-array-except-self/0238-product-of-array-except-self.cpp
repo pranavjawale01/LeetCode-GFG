a29b386a3fb3ebb607e5347713a9d94aad6424d7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-15 16:32:56 +0530

    Time: 17 ms (47.65%), Space: 27.4 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,38 +2,55 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int zeroCount = 0;
-        int productWithoutZero = 1;
-        for (int &num : nums) {
-            if (num == 0) {
-                zeroCount++;
-            } else {
-                productWithoutZero *= num;
-            }
-        }
-        vector<int> result(n, 0);
-        if (zeroCount == 2) {
-            return result;
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i-1] * nums[i-1];
+            right[n-i-1] = right[n-i] * nums[n-i];
         }
         for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            if (num != 0) {
-                if (zeroCount > 0) {
-                    result[i] = 0;
-                } else {
-                    result[i] = productWithoutZero / nums[i];
-                }
-            } else {
-                if (zeroCount > 1) {
-                    result[i] = 0;
-                } else {
-                    result[i] = productWithoutZero;
-                }
-            }
+            nums[i] = left[i] * right[i];
         }
-        return result;
+        return nums;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         int zeroCount = 0;
+//         int productWithoutZero = 1;
+//         for (int &num : nums) {
+//             if (num == 0) {
+//                 zeroCount++;
+//             } else {
+//                 productWithoutZero *= num;
+//             }
+//         }
+//         vector<int> result(n, 0);
+//         if (zeroCount == 2) {
+//             return result;
+//         }
+//         for (int i = 0; i < n; i++) {
+//             int num = nums[i];
+//             if (num != 0) {
+//                 if (zeroCount > 0) {
+//                     result[i] = 0;
+//                 } else {
+//                     result[i] = productWithoutZero / nums[i];
+//                 }
+//             } else {
+//                 if (zeroCount > 1) {
+//                     result[i] = 0;
+//                 } else {
+//                     result[i] = productWithoutZero;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
 
 // Brute Force
 // class Solution {
