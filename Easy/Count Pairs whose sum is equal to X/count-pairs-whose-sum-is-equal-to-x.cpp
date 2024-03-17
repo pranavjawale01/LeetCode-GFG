20646fb26c789391855f9c7c79c27c83e3131125
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-17 21:59:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.cpp
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.cpp
@@ -52,23 +52,22 @@ class Solution{
     // your task is to complete this function
     int countPairs(struct Node* head1, struct Node* head2, int x) {
         // Code here
-        set<int>st;
-        Node* temp=head1;
-        while(temp){
+        std::set<int> st;
+        int count = 0;
+        Node* temp = head1;
+        while (temp) {
             st.insert(temp->data);
-            temp=temp->next;
+            temp = temp->next;
         }
-        
-        int ans=0;
-        temp=head2;
-        while(temp){
-            int val=temp->data;
-            if(st.find(x-val)!=st.end()){
-                ans++;
+        temp = head2;
+        while (temp) {
+            int val = temp->data;
+            if (st.find(x - val) != st.end()) {
+                count++;
             }
-            temp=temp->next;
+            temp = temp->next;
         }
-        return ans;
+        return count;
     }
 };
 
