bc3afdafa543b07a95608f2792e03d0303a7fd9e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-17 22:23:18 +0530

    Time: 3 ms (18.99%), Space: 44.6 MB (74.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,26 +1,39 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new ArrayList<>();
-
-        int[][] updatedIntervals = Arrays.copyOf(intervals, intervals.length + 1);
-        updatedIntervals[intervals.length] = newInterval;
-
-        if (updatedIntervals == null || updatedIntervals.length == 0) {
-            return result.toArray(new int[0][]);
+        int n = intervals.length;
+        int[][] temp = new int[n + 1][2];
+        for (int i = 0; i < n; i++) {
+            temp[i][0] = intervals[i][0];
+            temp[i][1] = intervals[i][1];
         }
+        temp[n][0] = newInterval[0];
+        temp[n][1] = newInterval[1];
 
-        Arrays.sort(updatedIntervals, Comparator.comparingInt(a -> a[0]));
+        Comparator<int[]> comparator = new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        };
 
-        result.add(updatedIntervals[0]);
+        Arrays.sort(temp, comparator);
 
-        for (int i = 1; i < updatedIntervals.length; i++) {
-            if (result.get(result.size() - 1)[1] >= updatedIntervals[i][0]) {
-                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], updatedIntervals[i][1]);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (temp[count][1] >= temp[i][0]) {
+                temp[count][1] = Math.max(temp[i][1], temp[count][1]);
             } else {
-                result.add(updatedIntervals[i]);
+                count++;
+                temp[count][0] = temp[i][0];
+                temp[count][1] = temp[i][1];
             }
         }
 
-        return result.toArray(new int[0][]);
+        int[][] result = new int[count + 1][2];
+        for (int i = 0; i <= count; i++) {
+            result[i][0] = temp[i][0];
+            result[i][1] = temp[i][1];
+        }
+
+        return result;
     }
 }
