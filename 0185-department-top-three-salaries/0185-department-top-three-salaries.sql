76898ee69c5a45de0721a3a8e0f91836307ac315
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-18 10:56:53 +0530

    Time: 1769 ms (45.17%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,11 +1,17 @@
-# Write your MySQL query statement below
-SELECT b.name AS Department, 
-       a.name AS Employee, 
-       a.salary AS Salary
-FROM Employee AS a
-INNER JOIN Department AS b ON a.departmentId = b.id
-WHERE 3 > (
-    SELECT COUNT(DISTINCT c.salary) 
-    FROM Employee AS c
-    WHERE c.salary > a.salary AND c.departmentId = a.departmentId
-);
+/* Write your PL/SQL query statement below */
+with t1 as (
+select
+    departmentId,
+    e.name as Employee,
+    salary as Salary,
+    dense_rank() over (partition by departmentId order by salary desc) as ranks
+from Employee e
+)
+select
+    d.name as Department,
+    Employee,
+    Salary
+from t1
+join Department d
+    on t1.departmentId = d.id
+where ranks <=3
