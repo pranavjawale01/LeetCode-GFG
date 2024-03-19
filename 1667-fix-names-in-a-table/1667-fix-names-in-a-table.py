0c654ce9925e4b2054222f150b9ccbdf5f8a4d71
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-19 09:34:31 +0530

    Time: 793 ms (10.54%), Space: 67.5 MB (8.22%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str[0].str.upper() + users['name'].str[1:].str.lower()
+    return users.sort_values(by='user_id')
