87fbb51ac54983a20f1d1ca794f5ca216214833a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-19 10:25:30 +0530

    Time: 46 ms (88.7%), Space: 37.9 MB (80.77%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,38 +1,58 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> mp(26, 0);
-        for (char ch : tasks) {
-            mp[ch - 'A']++;
+        int taskFreqMp[26] = {0};
+        for (char c : tasks) {
+            taskFreqMp[c - 'A']++;
         }
-        int time = 0;
-        priority_queue<int> pq;
-        for (int i = 0; i < 26; i++) {
-            if (mp[i] > 0) {
-                pq.push(mp[i]);
-            }
+        
+        sort(taskFreqMp, taskFreqMp + 26);
+        
+        int batchCnt = taskFreqMp[25];
+        int vacantSlots = (--batchCnt) * n;
+        for (int indx = 0; indx < 25; indx++) {
+            vacantSlots -= min(taskFreqMp[indx], batchCnt);
         }
-        while(!pq.empty()) {
-            vector<int> temp;
-            for (int i = 1; i <= n + 1; i++) {
-                if (!pq.empty()) {
-                    int freq = pq.top();
-                    pq.pop();
-                    freq--;
-                    temp.push_back(freq);
-                }
-            }
-            for (int &f : temp) {
-                if (f > 0) {
-                    pq.push(f);
-                }
-            }
-            if (pq.empty()) {
-                time += temp.size();
-            } else {
-                time += n + 1;
-            }
-        }
-        return time;
+        
+        return (vacantSlots > 0) ? tasks.size() + vacantSlots : tasks.size();
     }
 };
+
+// class Solution {
+// public:
+//     int leastInterval(vector<char>& tasks, int n) {
+//         vector<int> mp(26, 0);
+//         for (char ch : tasks) {
+//             mp[ch - 'A']++;
+//         }
+//         int time = 0;
+//         priority_queue<int> pq;
+//         for (int i = 0; i < 26; i++) {
+//             if (mp[i] > 0) {
+//                 pq.push(mp[i]);
+//             }
+//         }
+//         while(!pq.empty()) {
+//             vector<int> temp;
+//             for (int i = 1; i <= n + 1; i++) {
+//                 if (!pq.empty()) {
+//                     int freq = pq.top();
+//                     pq.pop();
+//                     freq--;
+//                     temp.push_back(freq);
+//                 }
+//             }
+//             for (int &f : temp) {
+//                 if (f > 0) {
+//                     pq.push(f);
+//                 }
+//             }
+//             if (pq.empty()) {
+//                 time += temp.size();
+//             } else {
+//                 time += n + 1;
+//             }
+//         }
+//         return time;
+//     }
+// };
