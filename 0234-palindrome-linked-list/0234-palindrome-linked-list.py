41cc4bab3a2b610bce93e6cd6dace84ea19cee46
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-22 17:01:39 +0530

    Time: 252 ms (99.28%), Space: 27.3 MB (98.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,38 +4,59 @@
 #         self.val = val
 #         self.next = next
 
-#two pass solution
+#one pass solution
 class Solution:
-    def reverse(head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
-        last = Solution.reverse(head.next)
-        head.next.next = head
-        head.next = None
-        return last
-
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if head is None or head.next is None:
-            return True
-        fast, slow, prev = head, head, None
-        while fast is not None and fast.next is not None:
-            prev = slow
-            slow = slow.next
+        fast = head
+        slow = head
+        prev = None
+        while fast and fast.next:
             fast = fast.next.next
-        rev = Solution.reverse(slow)
-        while rev and head:
-            if rev.val != head.val:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        if fast:
+            slow = slow.next
+        while slow and prev:
+            if prev.val != slow.val:
                 return False
-            rev = rev.next
-            head = head.next
+            prev = prev.next
+            slow = slow.next
         return True
 
-#using array
+#two pass solution
 # class Solution:
+#     def reverse(head: Optional[ListNode]) -> Optional[ListNode]:
+#         if head is None or head.next is None:
+#             return head
+#         last = Solution.reverse(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return last
+
 #     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-#         arr = []
-#         temp = head
-#         while temp:
-#             arr.append(temp.val)
-#             temp = temp.next
-#         return arr[::1] == arr[::-1]
+#         if head is None or head.next is None:
+#             return True
+#         fast, slow, prev = head, head, None
+#         while fast is not None and fast.next is not None:
+#             prev = slow
+#             slow = slow.next
+#             fast = fast.next.next
+#         rev = Solution.reverse(slow)
+#         while rev and head:
+#             if rev.val != head.val:
+#                 return False
+#             rev = rev.next
+#             head = head.next
+#         return True
+
+# #using array
+# # class Solution:
+# #     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+# #         arr = []
+# #         temp = head
+# #         while temp:
+# #             arr.append(temp.val)
+# #             temp = temp.next
+# #         return arr[::1] == arr[::-1]
