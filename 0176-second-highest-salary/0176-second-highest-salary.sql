1eeb3e05588df9d59a97158aeca49ec29cd8f665
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-22 17:16:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    IFNULL(
+        (SELECT salary AS SecondHighestSalary
+         FROM Employee
+         ORDER BY salary DESC
+         LIMIT 1 OFFSET 1),
+        NULL
+    ) AS SecondHighestSalary;
