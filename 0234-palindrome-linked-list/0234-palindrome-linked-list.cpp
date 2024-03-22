a3d5eac51c4a124f867e93fd6ce41f8faf7a59d8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-22 15:47:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,21 +9,40 @@
  * };
  */
 
-// Using Vector
+// two pass solution 
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> vec;
-        ListNode *temp = head;
-        while (temp != nullptr) {
-            vec.push_back(temp->val);
-            temp = temp->next;
+    ListNode *reverse(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        ListNode *nxt = nullptr;
+        while (curr != nullptr) {
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
         }
-        temp = head;
-        while (temp != nullptr) {
-            if (vec.back() == temp->val) {
-                temp = temp->next;
-                vec.pop_back();
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        ListNode *prev = nullptr;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
+        prev->next = nullptr;
+        if (fast) {
+            slow = slow->next;
+        }
+        slow = reverse(slow);
+        prev = head;
+        while (prev) {
+            if (prev->val == slow->val) {
+                prev = prev->next;
+                slow = slow->next;
             } else {
                 return false;
             }
@@ -32,21 +51,21 @@ public:
     }
 };
 
-// // Using Stack
+// Using Vector
 // class Solution {
 // public:
 //     bool isPalindrome(ListNode* head) {
-//         stack<int> st;
-//         ListNode* temp = head;
+//         vector<int> vec;
+//         ListNode *temp = head;
 //         while (temp != nullptr) {
-//             st.push(temp->val);
+//             vec.push_back(temp->val);
 //             temp = temp->next;
 //         }
 //         temp = head;
 //         while (temp != nullptr) {
-//             if (temp->val == st.top()) {
+//             if (vec.back() == temp->val) {
 //                 temp = temp->next;
-//                 st.pop();
+//                 vec.pop_back();
 //             } else {
 //                 return false;
 //             }
@@ -54,3 +73,26 @@ public:
 //         return true;
 //     }
 // };
+
+// // // Using Stack
+// // class Solution {
+// // public:
+// //     bool isPalindrome(ListNode* head) {
+// //         stack<int> st;
+// //         ListNode* temp = head;
+// //         while (temp != nullptr) {
+// //             st.push(temp->val);
+// //             temp = temp->next;
+// //         }
+// //         temp = head;
+// //         while (temp != nullptr) {
+// //             if (temp->val == st.top()) {
+// //                 temp = temp->next;
+// //                 st.pop();
+// //             } else {
+// //                 return false;
+// //             }
+// //         }
+// //         return true;
+// //     }
+// // };
