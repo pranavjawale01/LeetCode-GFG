da98c9aaeb8f9e074437f115bcbfe7db0d4d02c7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-22 16:58:41 +0530

    Time: 272 ms (90.16%), Space: 37.6 MB (8.68%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,11 +3,39 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+#two pass solution
 class Solution:
+    def reverse(head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        last = Solution.reverse(head.next)
+        head.next.next = head
+        head.next = None
+        return last
+
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr = []
-        temp = head
-        while temp:
-            arr.append(temp.val)
-            temp = temp.next
-        return arr[::1] == arr[::-1]
+        if head is None or head.next is None:
+            return True
+        fast, slow, prev = head, head, None
+        while fast is not None and fast.next is not None:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        rev = Solution.reverse(slow)
+        while rev and head:
+            if rev.val != head.val:
+                return False
+            rev = rev.next
+            head = head.next
+        return True
+
+#using array
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         arr = []
+#         temp = head
+#         while temp:
+#             arr.append(temp.val)
+#             temp = temp.next
+#         return arr[::1] == arr[::-1]
