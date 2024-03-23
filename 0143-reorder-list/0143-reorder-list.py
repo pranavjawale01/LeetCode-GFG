67041736f6f58f4e3a6bda8631f5883d537441ef
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-23 12:47:53 +0530

    Time: 47 ms (92.99%), Space: 24.6 MB (50.88%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,51 +5,72 @@
 #         self.next = next
 
 class Solution:
-    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:  
-        if not head or not head.next:
-            return head
-        last = self.reverse(head.next)  
-        head.next.next = head
-        head.next = None
-        return last
-
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        rev = self.reverse(slow)  
+        st = []
         curr = head
-        while rev.next:
-            tempcurr = curr.next
-            curr.next = rev
-
-            temprev = rev.next
-            rev.next = tempcurr
-
-            curr = tempcurr
-            rev = temprev
+        while curr:
+            st.append(curr)
+            curr = curr.next
+        curr = head
+        n = len(st) // 2
+        for _ in range(n):
+            tempNode = st.pop()
+            temp = curr.next
+            curr.next = tempNode
+            tempNode.next = temp
+            curr = temp
+        if curr:
+            curr.next = None
         
-
 # class Solution:
+#     def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:  
+#         if not head or not head.next:
+#             return head
+#         last = self.reverse(head.next)  
+#         head.next.next = head
+#         head.next = None
+#         return last
+
 #     def reorderList(self, head: Optional[ListNode]) -> None:
 #         """
 #         Do not return anything, modify head in-place instead.
 #         """
-#         arr = []
-#         temp = head
-#         while temp:
-#             arr.append(temp.val)
-#             temp = temp.next
-#         i , n = 0, len(arr)
-#         temp = head
-#         while temp and temp.next:
-#             temp.val = arr[i]
-#             temp.next.val = arr[n - i - 1]
-#             temp = temp.next.next
-#             i += 1
-#         if n % 2 != 0:
-#             temp.val = arr[n//2]
+#         slow, fast = head, head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         rev = self.reverse(slow)  
+#         curr = head
+#         while rev.next:
+#             tempcurr = curr.next
+#             curr.next = rev
+
+#             temprev = rev.next
+#             rev.next = tempcurr
+
+#             curr = tempcurr
+#             rev = temprev
+        
+
+# # class Solution:
+# #     def reorderList(self, head: Optional[ListNode]) -> None:
+# #         """
+# #         Do not return anything, modify head in-place instead.
+# #         """
+# #         arr = []
+# #         temp = head
+# #         while temp:
+# #             arr.append(temp.val)
+# #             temp = temp.next
+# #         i , n = 0, len(arr)
+# #         temp = head
+# #         while temp and temp.next:
+# #             temp.val = arr[i]
+# #             temp.next.val = arr[n - i - 1]
+# #             temp = temp.next.next
+# #             i += 1
+# #         if n % 2 != 0:
+# #             temp.val = arr[n//2]
