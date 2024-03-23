e347f988b08d76f3c5b4bad27baa231292957bd0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-23 14:40:31 +0530

    Time: 1 ms (99.98%), Space: 49.6 MB (6.11%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -8,81 +8,108 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public void reorderList(ListNode head) {
-        Stack<ListNode> st = new Stack<>();
-        ListNode curr = head;
-        while (curr != null) {
-            st.add(curr);
-            curr = curr.next;
-        }
-        curr = head;
-        int n = st.size() / 2;
-        while (n-- > 0) {
-            ListNode tempNode = st.pop();
 
-            ListNode temp = curr.next;
-            curr.next = tempNode;
-            tempNode.next = temp;
-            curr = temp;
+class Solution {
+    ListNode curr;
+
+    void solve(ListNode head) { 
+        if (head == null) {
+            return;
         }
-        curr.next = null;
+        solve(head.next);
+        ListNode temp = curr.next;
+        if (temp == null) {
+            return;
+        } else if (head == curr) {
+            head.next = null;
+            return;
+        }
+        curr.next = head;
+        head.next = temp == head ? null : temp;
+        curr = temp;
+    }
+
+    public void reorderList(ListNode head) {
+        curr = head;
+        solve(head);
     }
 }
 
 // class Solution {
-//     public ListNode reverse(ListNode head) {
-//         if (head == null || head.next == null) {
-//             return head;
-//         }
-//         ListNode last = reverse(head.next);
-//         head.next.next = head;
-//         head.next = null;
-//         return last;
-//     }
 //     public void reorderList(ListNode head) {
-//         if (head == null || head.next == null) {
-//             return;
-//         }
-//         ListNode slow = head, fast = head;
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         ListNode rev = reverse(slow);
+//         Stack<ListNode> st = new Stack<>();
 //         ListNode curr = head;
-//         while (rev.next != null) {
-//             ListNode tempcurr = curr.next;
-//             curr.next = rev;
+//         while (curr != null) {
+//             st.add(curr);
+//             curr = curr.next;
+//         }
+//         curr = head;
+//         int n = st.size() / 2;
+//         while (n-- > 0) {
+//             ListNode tempNode = st.pop();
 
-//             ListNode temprev = rev.next;
-//             rev.next = tempcurr;
-
-//             curr = tempcurr;
-//             rev  = temprev;  
-//         }        
+//             ListNode temp = curr.next;
+//             curr.next = tempNode;
+//             tempNode.next = temp;
+//             curr = temp;
+//         }
+//         curr.next = null;
 //     }
 // }
 
 // // class Solution {
+// //     public ListNode reverse(ListNode head) {
+// //         if (head == null || head.next == null) {
+// //             return head;
+// //         }
+// //         ListNode last = reverse(head.next);
+// //         head.next.next = head;
+// //         head.next = null;
+// //         return last;
+// //     }
 // //     public void reorderList(ListNode head) {
-// //         List<Integer> arr = new ArrayList<>();
-// //         ListNode temp = head;
-// //         while (temp != null) {
-// //             arr.add(temp.val);
-// //             temp = temp.next;
+// //         if (head == null || head.next == null) {
+// //             return;
 // //         }
-// //         int n = arr.size();
-// //         temp = head;
-// //         int i = 0;
-// //         while (temp != null && temp.next != null) {
-// //             temp.val = arr.get(i);
-// //             temp.next.val = arr.get(n - i - 1);
-// //             temp = temp.next.next;
-// //             i++;
+// //         ListNode slow = head, fast = head;
+// //         while (fast != null && fast.next != null) {
+// //             slow = slow.next;
+// //             fast = fast.next.next;
 // //         }
-// //         if (n % 2 != 0) {
-// //             temp.val = arr.get(n / 2);
-// //         }
+// //         ListNode rev = reverse(slow);
+// //         ListNode curr = head;
+// //         while (rev.next != null) {
+// //             ListNode tempcurr = curr.next;
+// //             curr.next = rev;
+
+// //             ListNode temprev = rev.next;
+// //             rev.next = tempcurr;
+
+// //             curr = tempcurr;
+// //             rev  = temprev;  
+// //         }        
 // //     }
 // // }
+
+// // // class Solution {
+// // //     public void reorderList(ListNode head) {
+// // //         List<Integer> arr = new ArrayList<>();
+// // //         ListNode temp = head;
+// // //         while (temp != null) {
+// // //             arr.add(temp.val);
+// // //             temp = temp.next;
+// // //         }
+// // //         int n = arr.size();
+// // //         temp = head;
+// // //         int i = 0;
+// // //         while (temp != null && temp.next != null) {
+// // //             temp.val = arr.get(i);
+// // //             temp.next.val = arr.get(n - i - 1);
+// // //             temp = temp.next.next;
+// // //             i++;
+// // //         }
+// // //         if (n % 2 != 0) {
+// // //             temp.val = arr.get(n / 2);
+// // //         }
+// // //     }
+// // // }
