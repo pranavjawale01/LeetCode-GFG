76c1116e6274b20bea22daf1e776659b6e211ee1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-24 12:28:06 +0530

    Time: 5 ms (68.61%), Space: 58.7 MB (14.87%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,26 +1,45 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if (nums[index] < 0) {
-                return Math.abs(nums[i]);
-            }
-            nums[index] = nums[index] * (-1); 
+        int slow = nums[0];
+        int fast = nums[0];
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        return -1;
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
     }
 }
 
 // class Solution {
 //     public int findDuplicate(int[] nums) {
-//         Map<Integer, Integer> mp = new HashMap<>();
-//         for (int num : nums) {
-//             mp.put(num, mp.getOrDefault(num, 0) + 1);
-//             if (mp.get(num) > 1) {
-//                 return num;
+//         int n = nums.length;
+//         for (int i = 0; i < n; i++) {
+//             int index = Math.abs(nums[i]) - 1;
+//             if (nums[index] < 0) {
+//                 return Math.abs(nums[i]);
 //             }
+//             nums[index] = nums[index] * (-1); 
 //         }
 //         return -1;
 //     }
 // }
+
+// // class Solution {
+// //     public int findDuplicate(int[] nums) {
+// //         Map<Integer, Integer> mp = new HashMap<>();
+// //         for (int num : nums) {
+// //             mp.put(num, mp.getOrDefault(num, 0) + 1);
+// //             if (mp.get(num) > 1) {
+// //                 return num;
+// //             }
+// //         }
+// //         return -1;
+// //     }
+// // }
