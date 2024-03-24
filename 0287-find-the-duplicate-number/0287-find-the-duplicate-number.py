ee3f94a07aa067a06e4d5d479eaed5b060f17dd5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-24 12:29:12 +0530

    Time: 498 ms (23.86%), Space: 30.5 MB (57.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,18 +1,34 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
-        for i in range(n):
-            index = abs(nums[i]) - 1
-            if nums[index] < 0:
-                return abs(nums[i])
-            nums[index] = nums[index] * (-1)
-        return -1
+        slow, fast = nums[0], nums[0]
+        slow, fast = nums[slow], nums[nums[fast]]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
 
 # class Solution:
 #     def findDuplicate(self, nums: List[int]) -> int:
-#         mp = defaultdict(int)
-#         for num in nums:
-#             mp[num] += 1
-#             if mp[num] > 1:
-#                 return num
+#         n = len(nums)
+#         for i in range(n):
+#             index = abs(nums[i]) - 1
+#             if nums[index] < 0:
+#                 return abs(nums[i])
+#             nums[index] = nums[index] * (-1)
 #         return -1
+
+# # class Solution:
+# #     def findDuplicate(self, nums: List[int]) -> int:
+# #         mp = defaultdict(int)
+# #         for num in nums:
+# #             mp[num] += 1
+# #             if mp[num] > 1:
+# #                 return num
+# #         return -1
