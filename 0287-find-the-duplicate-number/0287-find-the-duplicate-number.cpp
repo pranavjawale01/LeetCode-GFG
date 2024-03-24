b608b91397149b5499bc8366ac76292f120699da
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-24 12:10:47 +0530

    Time: 79 ms (63.15%), Space: 63.6 MB (77.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (int &num : nums) {
-            if (mp[num] == 0) {
-                mp[num]++;
-            } else {
-                return num;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                return abs(nums[i]);
             }
+            nums[index] *= -1; 
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         for (int &num : nums) {
+//             if (mp[num] == 0) {
+//                 mp[num]++;
+//             } else {
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+// };
