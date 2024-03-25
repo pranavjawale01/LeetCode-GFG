c37887acd935ad5c861d9983ef84df0d735adfdf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-25 11:03:11 +0530

    Time: 257 ms (73.25%), Space: 24.4 MB (76.89%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,18 +1,30 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        ans = []
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                ans.append(nums[i-1])
-        return ans
+        n = len(nums)
+        arr = []
+        for i in range(n):
+            index = abs(nums[i]) - 1
+            if nums[index] < 0:
+                arr.append(abs(nums[i]))
+            else:
+                nums[index] *= (-1)
+        return arr
 
 # class Solution:
 #     def findDuplicates(self, nums: List[int]) -> List[int]:
 #         ans = []
-#         mp = defaultdict(int)
-#         for num in nums:
-#             mp[num] += 1
-#             if mp[num] > 1:
-#                 ans.append(num)
+#         nums.sort()
+#         for i in range(1, len(nums)):
+#             if nums[i] == nums[i-1]:
+#                 ans.append(nums[i-1])
 #         return ans
+
+# # class Solution:
+# #     def findDuplicates(self, nums: List[int]) -> List[int]:
+# #         ans = []
+# #         mp = defaultdict(int)
+# #         for num in nums:
+# #             mp[num] += 1
+# #             if mp[num] > 1:
+# #                 ans.append(num)
+# #         return ans
