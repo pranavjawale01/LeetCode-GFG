75c906ebd155ece7a4925c29b27d13a26403faa2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-25 11:01:56 +0530

    Time: 7 ms (47.9%), Space: 53.3 MB (82.52%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
         List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
-                ans.add(nums[i-1]);
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                ans.add(Math.abs(nums[i]));
+            } else {
+                nums[index] *= (-1);
             }
         }
         return ans;
@@ -13,14 +16,27 @@ class Solution {
 
 // class Solution {
 //     public List<Integer> findDuplicates(int[] nums) {
-//         Map<Integer, Integer> mp = new HashMap<>();
+//         Arrays.sort(nums);
 //         List<Integer> ans = new ArrayList<>();
-//         for (int num : nums) {
-//             mp.put(num, mp.getOrDefault(num, 0) + 1);
-//             if (mp.get(num) > 1) {
-//                 ans.add(num);
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i-1]) {
+//                 ans.add(nums[i-1]);
 //             }
 //         }
 //         return ans;
 //     }
 // }
+
+// // class Solution {
+// //     public List<Integer> findDuplicates(int[] nums) {
+// //         Map<Integer, Integer> mp = new HashMap<>();
+// //         List<Integer> ans = new ArrayList<>();
+// //         for (int num : nums) {
+// //             mp.put(num, mp.getOrDefault(num, 0) + 1);
+// //             if (mp.get(num) > 1) {
+// //                 ans.add(num);
+// //             }
+// //         }
+// //         return ans;
+// //     }
+// // }
