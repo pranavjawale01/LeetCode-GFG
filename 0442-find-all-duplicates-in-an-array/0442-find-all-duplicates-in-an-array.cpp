f7c7d1b73fedbc5b1cb83fba02cbcd9c36e532a6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-25 10:59:57 +0530

    Time: 31 ms (94.91%), Space: 36 MB (68.64%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
         vector<int> ans;
-        sort(begin(nums), end(nums));
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i-1]) {
-                ans.push_back(nums[i-1]);
+        for (int i = 0; i < n; i++) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                ans.push_back(abs(nums[i]));
+            } else {
+                nums[index] *= (-1);
             }
         }
         return ans;
@@ -15,14 +18,28 @@ public:
 // class Solution {
 // public:
 //     vector<int> findDuplicates(vector<int>& nums) {
-//         map<int, int> mp;
 //         vector<int> ans;
-//         for (int num : nums) {
-//             mp[num]++;
-//             if (mp[num] > 1) {
-//                 ans.push_back(num);
+//         sort(begin(nums), end(nums));
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (nums[i] == nums[i-1]) {
+//                 ans.push_back(nums[i-1]);
 //             }
 //         }
 //         return ans;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     vector<int> findDuplicates(vector<int>& nums) {
+// //         map<int, int> mp;
+// //         vector<int> ans;
+// //         for (int num : nums) {
+// //             mp[num]++;
+// //             if (mp[num] > 1) {
+// //                 ans.push_back(num);
+// //             }
+// //         }
+// //         return ans;
+// //     }
+// // };
