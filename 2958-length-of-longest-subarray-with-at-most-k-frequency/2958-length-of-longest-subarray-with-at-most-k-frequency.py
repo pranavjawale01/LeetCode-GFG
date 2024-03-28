4243ab7a3cd73893a744cba86ed937b45c8e5b0a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-28 16:22:13 +0530

    Time: 1155 ms (57.49%), Space: 31.1 MB (37.58%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -2,12 +2,31 @@ class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
         mp = defaultdict(int)
         i, j , long_len , n = 0, 0, 0, len(nums)
+        culprit = 0
         while j < n:
             mp[nums[j]] += 1
-            while i < j and mp[nums[j]] > k:
-                nums[i] -= 1
+            if mp[nums[j]] == 1 + k:
+                culprit += 1
+            if culprit > 0:
+                mp[nums[i]] -= 1
+                if mp[nums[i]] == k:
+                    culprit -= 1
                 i += 1
-            long_len = max(long_len, j - i + 1)
+            if culprit == 0:
+                long_len = max(long_len, j - i + 1)
             j += 1
-
         return long_len
+
+# class Solution:
+#     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+#         mp = defaultdict(int)
+#         i, j , long_len , n = 0, 0, 0, len(nums)
+#         while j < n:
+#             mp[nums[j]] += 1
+#             while i < j and mp[nums[j]] > k:
+#                 mp[nums[i]] -= 1
+#                 i += 1
+#             long_len = max(long_len, j - i + 1)
+#             j += 1
+
+#         return long_len
