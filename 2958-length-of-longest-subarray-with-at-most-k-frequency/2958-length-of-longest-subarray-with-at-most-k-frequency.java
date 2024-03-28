2c2028e3ebf45c0c1bd4e5f5aa05c3df59c8978f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-28 15:58:45 +0530

    Time: 73 ms (35.46%), Space: 63.1 MB (8.82%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -2,15 +2,41 @@ class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
         Map<Integer, Integer> mp = new HashMap<>();
         int i = 0, j = 0, len = 0, n = nums.length;
+        int culprit = 0;
         while (j < n) {
             mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
-            while (mp.get(nums[j]) > k) {
-                mp.put(nums[i], mp.getOrDefault(nums[i], 0) - 1);
+            if (mp.get(nums[j]) == k + 1) {
+                culprit++;
+            }
+            if (culprit > 0) {
+                mp.put(nums[i], mp.getOrDefault(nums[i], 0) -1);
+                if (mp.get(nums[i]) == k) {
+                    culprit--;
+                }
                 i++;
             }
-            len = Math.max(len, j - i + 1);
+            if (culprit == 0) {
+                len = Math.max(len, j - i + 1);
+            }
             j++;
         }
         return len;
     }
 }
+
+// class Solution {
+//     public int maxSubarrayLength(int[] nums, int k) {
+//         Map<Integer, Integer> mp = new HashMap<>();
+//         int i = 0, j = 0, len = 0, n = nums.length;
+//         while (j < n) {
+//             mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+//             while (mp.get(nums[j]) > k) {
+//                 mp.put(nums[i], mp.getOrDefault(nums[i], 0) - 1);
+//                 i++;
+//             }
+//             len = Math.max(len, j - i + 1);
+//             j++;
+//         }
+//         return len;
+//     }
+// }
