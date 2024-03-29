a76844824bce1c4c9de1ba624fd695c9d6efbb80
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-29 12:28:13 +0530

    Time: 842 ms (93.13%), Space: 30.8 MB (97.33%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,14 +1,30 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        maxE = max(nums)
-        i, j, n, countMax, result = 0, 0, len(nums), 0, 0
-        while j < n:
-            if nums[j] == maxE:
-                countMax += 1
-            while countMax >= k:
-                result += n - j
-                if nums[i] == maxE:
-                    countMax -= 1
-                i += 1
-            j += 1
+        max_e = max(nums)
+        n = len(nums)
+        result = 0
+        id = []
+        
+        for i in range(n):
+            if nums[i] == max_e:
+                id.append(i)
+            size = len(id)
+            if size >= k:
+                last_i = id[size - k]
+                result += last_i + 1
         return result
+        
+# class Solution:
+#     def countSubarrays(self, nums: List[int], k: int) -> int:
+#         maxE = max(nums)
+#         i, j, n, countMax, result = 0, 0, len(nums), 0, 0
+#         while j < n:
+#             if nums[j] == maxE:
+#                 countMax += 1
+#             while countMax >= k:
+#                 result += n - j
+#                 if nums[i] == maxE:
+#                     countMax -= 1
+#                 i += 1
+#             j += 1
+#         return result
