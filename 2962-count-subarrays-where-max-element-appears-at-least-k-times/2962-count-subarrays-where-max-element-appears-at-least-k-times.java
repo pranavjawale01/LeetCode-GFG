3a6f1856a91c3465edd038a5b5a59a2af0552277
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-29 12:26:55 +0530

    Time: 12 ms (12.25%), Space: 57.6 MB (97.96%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,21 +1,42 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
         int maxE = Arrays.stream(nums).max().getAsInt();
-        int i = 0, j = 0, n = nums.length, countMax = 0;
+        int n = nums.length;
         long result = 0;
-        while (j < n) {
-            if (nums[j] == maxE) {
-                countMax++;
+        ArrayList<Integer> id = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == maxE) {
+                id.add(i);
             }
-            while (countMax >= k) {
-                result += n - j;
-                if (nums[i] == maxE) {
-                    countMax--;
-                }
-                i++;
+            int size = id.size();
+            if (size >= k) {
+                int last_i = id.get(size - k);
+                result += last_i + 1;
             }
-            j++;
         }
         return result;
     }
 }
+
+// class Solution {
+//     public long countSubarrays(int[] nums, int k) {
+//         int maxE = Arrays.stream(nums).max().getAsInt();
+//         int i = 0, j = 0, n = nums.length, countMax = 0;
+//         long result = 0;
+//         while (j < n) {
+//             if (nums[j] == maxE) {
+//                 countMax++;
+//             }
+//             while (countMax >= k) {
+//                 result += n - j;
+//                 if (nums[i] == maxE) {
+//                     countMax--;
+//                 }
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return result;
+//     }
+// }
