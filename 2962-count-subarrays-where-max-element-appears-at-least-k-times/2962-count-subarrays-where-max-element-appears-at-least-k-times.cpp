525366436c713873c3b52ca56360491021ef79ba
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-03-29 12:25:49 +0530

    Time: 136 ms (51.55%), Space: 123.8 MB (32.75%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -2,22 +2,43 @@ class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
         int maxE = *max_element(begin(nums), end(nums));
-        int n = nums.size(), i = 0, j = 0;
+        int n = nums.size();
         long long result = 0;
-        int countMax = 0;
-        while (j < n) {
-            if (nums[j] == maxE) {
-                countMax++;
+        vector<int> maxIndices;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == maxE) {
+                maxIndices.push_back(i);
             }
-            while (countMax >= k) {
-                result += n - j;
-                if (nums[i] == maxE) {
-                    countMax--;
-                }
-                i++;
+            int size = maxIndices.size();
+            if (size >= k) {
+                int last_i = maxIndices[size-k];
+                result += (last_i + 1);
             }
-            j++;
         }
         return result;
     }
 };
+
+// class Solution {
+// public:
+//     long long countSubarrays(vector<int>& nums, int k) {
+//         int maxE = *max_element(begin(nums), end(nums));
+//         int n = nums.size(), i = 0, j = 0;
+//         long long result = 0;
+//         int countMax = 0;
+//         while (j < n) {
+//             if (nums[j] == maxE) {
+//                 countMax++;
+//             }
+//             while (countMax >= k) {
+//                 result += n - j;
+//                 if (nums[i] == maxE) {
+//                     countMax--;
+//                 }
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return result;
+//     }
+// };
