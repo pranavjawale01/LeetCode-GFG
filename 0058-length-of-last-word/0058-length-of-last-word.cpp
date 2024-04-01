6ed21e1008f6700206d01c3e55a4d2fc566c160e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-01 10:52:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.length() - 1, count = 0;
-
-        while (i >= 0 && s[i] == ' ') {
-            i--;
+        int n = s.length() - 1;
+        int count = 0;
+        while (s[n] == ' ' && n >= 0) {
+            n--;
         }
-
-        while (i >= 0 && s[i] != ' ') {
+        while (s[n] != ' ' && n >= 0) {
             count++;
-            i--;
+            n--;
         }
-
         return count;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         int i = s.length() - 1, count = 0;
+
+//         while (i >= 0 && s[i] == ' ') {
+//             i--;
+//         }
+
+//         while (i >= 0 && s[i] != ' ') {
+//             count++;
+//             i--;
+//         }
+
+//         return count;
+//     }
+// };
