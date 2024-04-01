12f0d2963c8721c3b91ab65a8128e19b34417ca1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-01 11:14:48 +0530

    Time: 0 ms (100%), Space: 7.6 MB (57.5%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,33 +1,27 @@
-// class Solution {
-// public:
-//     int lengthOfLastWord(string s) {
-//         int n = s.length() - 1;
-//         int count = 0;
-//         while (s[n] == ' ' && n >= 0) {
-//             n--;
-//         }
-//         while (s[n] != ' ' && n >= 0) {
-//             count++;
-//             n--;
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.length() - 1, count = 0;
-
-        while (i >= 0 && s[i] == ' ') {
-            i--;
-        }
-
-        while (i >= 0 && s[i] != ' ') {
-            count++;
-            i--;
-        }
-
-        return count;
+        int idx = s.find_last_not_of(' ') + 1;
+        s.erase(idx);
+        int last_space_idx = s.find_last_of(' ');
+        return last_space_idx == string::npos ? s.length() : s.length() - last_space_idx - 1;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         int i = s.length() - 1, count = 0;
+
+//         while (i >= 0 && s[i] == ' ') {
+//             i--;
+//         }
+
+//         while (i >= 0 && s[i] != ' ') {
+//             count++;
+//             i--;
+//         }
+
+//         return count;
+//     }
+// };
