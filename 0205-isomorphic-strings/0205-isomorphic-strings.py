df28081a85478256867181ee3af2d5d75026d7f3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-02 08:19:27 +0530

    Time: 41 ms (67.43%), Space: 16.8 MB (64.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,27 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mp1 = [-1] * 256
-        mp2 = [-1] * 256
+        mp1 = {}
+        mp2 = {}
         n = len(s)
         for i in range(n):
             ch1 = s[i]
             ch2 = t[i]
-            if (mp1[ord(ch1)] != -1 and mp1[ord(ch1)] != ord(ch2)) or (mp2[ord(ch2)] != -1 and mp2[ord(ch2)] != ord(ch1)):
+            if (ch1 in mp1 and mp1[ch1] != ch2) or (ch2 in mp2 and mp2[ch2] != ch1):
                 return False
-            mp1[ord(ch1)] = ord(ch2)
-            mp2[ord(ch2)] = ord(ch1)
+            mp1[ch1] = ch2
+            mp2[ch2] = ch1
         return True
+
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         mp1 = [-1] * 256
+#         mp2 = [-1] * 256
+#         n = len(s)
+#         for i in range(n):
+#             ch1 = s[i]
+#             ch2 = t[i]
+#             if (mp1[ord(ch1)] != -1 and mp1[ord(ch1)] != ord(ch2)) or (mp2[ord(ch2)] != -1 and mp2[ord(ch2)] != ord(ch1)):
+#                 return False
+#             mp1[ord(ch1)] = ord(ch2)
+#             mp2[ord(ch2)] = ord(ch1)
+#         return True
