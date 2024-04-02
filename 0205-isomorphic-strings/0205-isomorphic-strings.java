c1474f9a25808c7e53d0860564c6c65a96948cf0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-02 08:18:36 +0530

    Time: 23 ms (12.63%), Space: 41.7 MB (99.34%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,19 +1,38 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] mp1 = new int[256];
-        int[] mp2 = new int[256];
-        Arrays.fill(mp1, -1);
-        Arrays.fill(mp2, -1);
+        HashMap<Character, Character> mp1 = new HashMap<>();
+        HashMap<Character, Character> mp2 = new HashMap<>();
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char ch1 = s.charAt(i);
             char ch2 = t.charAt(i);
-            if ((mp1[ch1] != -1 && mp1[ch1] != ch2) || (mp2[ch2] != -1 && mp2[ch2] != ch1)) {
+            if ((mp1.containsKey(ch1) && mp1.get(ch1) != ch2) ||
+                (mp2.containsKey(ch2) && mp2.get(ch2) != ch1)) {
                 return false;
             }
-            mp1[ch1] = ch2;
-            mp2[ch2] = ch1;
+            mp1.put(ch1, ch2);
+            mp2.put(ch2, ch1);
         }
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         int[] mp1 = new int[256];
+//         int[] mp2 = new int[256];
+//         Arrays.fill(mp1, -1);
+//         Arrays.fill(mp2, -1);
+//         int n = s.length();
+//         for (int i = 0; i < n; i++) {
+//             char ch1 = s.charAt(i);
+//             char ch2 = t.charAt(i);
+//             if ((mp1[ch1] != -1 && mp1[ch1] != ch2) || (mp2[ch2] != -1 && mp2[ch2] != ch1)) {
+//                 return false;
+//             }
+//             mp1[ch1] = ch2;
+//             mp2[ch2] = ch1;
+//         }
+//         return true;
+//     }
+// }
