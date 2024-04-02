623e7b1d4e8031af6b9076952b320c6278f1ea12
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-02 08:17:53 +0530

    Time: 7 ms (48.84%), Space: 9.4 MB (42.66%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> mp1(256, -1);
-        vector<int> mp2(256, -1);
+        unordered_map<char, char> mp1, mp2;
         int n = s.length();        
-        for(int i = 0; i<n; i++) {
+        for(int i = 0; i < n; i++) {
             char ch1 = s[i];
             char ch2 = t[i];            
-            if ((mp1[ch1] != -1 && mp1[ch1] != ch2) || (mp2[ch2] != -1 && mp2[ch2] != ch1)) {
+            if ((mp1.find(ch1) != mp1.end() && mp1[ch1] != ch2) || (mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1)) {
                 return false;
             }
             mp1[ch1] = ch2;
@@ -16,3 +15,22 @@ public:
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         vector<int> mp1(256, -1);
+//         vector<int> mp2(256, -1);
+//         int n = s.length();        
+//         for(int i = 0; i<n; i++) {
+//             char ch1 = s[i];
+//             char ch2 = t[i];            
+//             if ((mp1[ch1] != -1 && mp1[ch1] != ch2) || (mp2[ch2] != -1 && mp2[ch2] != ch1)) {
+//                 return false;
+//             }
+//             mp1[ch1] = ch2;
+//             mp2[ch2] = ch1;            
+//         }        
+//         return true;
+//     }
+// };
