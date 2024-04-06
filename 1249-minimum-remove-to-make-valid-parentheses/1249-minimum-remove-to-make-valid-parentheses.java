ed66708a76fdcb49e47eb617bb5778b08b1002ed
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-06 15:20:01 +0530

    Time: 20 ms (60.87%), Space: 45.2 MB (70.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,28 +1,57 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> idx = new HashSet<>();
-        Stack<Integer> st = new Stack<>();
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '(') {
-                st.push(i);
-            } else if (s.charAt(i) == ')') {
-                if (st.isEmpty()) {
-                    idx.add(i);
-                } else {
-                    st.pop();
-                }
+        int open = 0;
+        StringBuilder temp = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else if (c == ')') {
+                if (open == 0)
+                    continue;
+                open--;
             }
+
+            temp.append(c);
         }
-        while (!st.isEmpty()) {
-            idx.add(st.pop());
+
+        int n = temp.length();
+        StringBuilder result = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (temp.charAt(i) == '(' && open-- > 0)
+                continue;
+            result.append(temp.charAt(i));
         }
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (!idx.contains(i)) {
-                ans.append(s.charAt(i));
-            }
-        }
-        return ans.toString();
+
+        return result.reverse().toString();
     }
 }
+
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         Set<Integer> idx = new HashSet<>();
+//         Stack<Integer> st = new Stack<>();
+//         int n = s.length();
+//         for (int i = 0; i < n; i++) {
+//             if (s.charAt(i) == '(') {
+//                 st.push(i);
+//             } else if (s.charAt(i) == ')') {
+//                 if (st.isEmpty()) {
+//                     idx.add(i);
+//                 } else {
+//                     st.pop();
+//                 }
+//             }
+//         }
+//         while (!st.isEmpty()) {
+//             idx.add(st.pop());
+//         }
+//         StringBuilder ans = new StringBuilder();
+//         for (int i = 0; i < n; i++) {
+//             if (!idx.contains(i)) {
+//                 ans.append(s.charAt(i));
+//             }
+//         }
+//         return ans.toString();
+//     }
+// }
