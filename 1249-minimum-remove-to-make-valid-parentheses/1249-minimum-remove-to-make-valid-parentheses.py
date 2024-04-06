7d0bae02f09f2cc2b9ea1a9191b4f98dcb1c79cc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-06 15:21:35 +0530

    Time: 98 ms (23.09%), Space: 18.1 MB (71.52%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,19 +1,44 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        st = []
-        n = len(s)
-        idx = set()
-        for i in range(n):
-            if s[i] == '(':
-                st.append(i)
-            elif s[i] == ')':
-                if st and s[st[-1]] == '(':
-                    st.pop()
-                else:
-                    idx.add(i)
-        idx.update(st)
-        ans = ""
-        for i in range(n):
-            if i not in idx:
-                ans += s[i]
-        return ans
+        open_count = 0
+        temp = ""
+
+        for c in s:
+            if c == '(':
+                open_count += 1
+            elif c == ')':
+                if open_count == 0:
+                    continue
+                open_count -= 1
+
+            temp += c
+
+        n = len(temp)
+        result = ""
+        for i in range(n - 1, -1, -1):
+            if temp[i] == '(' and open_count > 0:
+                open_count -= 1
+                continue
+            result += temp[i]
+
+        return result[::-1]
+        
+# class Solution:
+#     def minRemoveToMakeValid(self, s: str) -> str:
+#         st = []
+#         n = len(s)
+#         idx = set()
+#         for i in range(n):
+#             if s[i] == '(':
+#                 st.append(i)
+#             elif s[i] == ')':
+#                 if st and s[st[-1]] == '(':
+#                     st.pop()
+#                 else:
+#                     idx.add(i)
+#         idx.update(st)
+#         ans = ""
+#         for i in range(n):
+#             if i not in idx:
+#                 ans += s[i]
+#         return ans
