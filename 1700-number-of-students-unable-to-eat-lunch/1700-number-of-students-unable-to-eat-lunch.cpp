c70f7e67c95e22bd7f02f96f1a450e8e2ef16acc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-08 11:22:11 +0530

    Time: 0 ms (100%), Space: 10.8 MB (79.46%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> q;
+        stack<int> st;
         int n = students.size();
-        int arr[2] = {0};
-        for (int &nums : students) {
-            if (nums == 1) {
-                arr[1]++;
-            }
-        }
-        arr[0] = n - arr[1];
         for (int i = 0; i < n; i++) {
-            int sand = sandwiches[i];
-            if (arr[sand] == 0) {
-                return n - i;
-            }
-            arr[sand]--;
+            st.push(sandwiches[n - i - 1]);
+            q.push(students[i]);
         }
-        return 0;
+        int lastServed = 0;
+        while (!q.empty() && lastServed < q.size()) {
+            if (st.top() == q.front()) {
+                st.pop();
+                q.pop();
+                lastServed = 0;
+            } else {
+                q.push(q.front());
+                q.pop();
+                lastServed++;
+            }
+        }
+        return q.size();
     }
 };
+
+// class Solution {
+// public:
+//     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+//         int n = students.size();
+//         int arr[2] = {0};
+//         for (int &nums : students) {
+//             if (nums == 1) {
+//                 arr[1]++;
+//             }
+//         }
+//         arr[0] = n - arr[1];
+//         for (int i = 0; i < n; i++) {
+//             int sand = sandwiches[i];
+//             if (arr[sand] == 0) {
+//                 return n - i;
+//             }
+//             arr[sand]--;
+//         }
+//         return 0;
+//     }
+// };
