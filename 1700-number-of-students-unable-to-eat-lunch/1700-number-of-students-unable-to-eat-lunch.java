3899ddb97281140ee566cc4fc216a246bbdbd3cc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-08 11:27:25 +0530

    Time: 2 ms (50.92%), Space: 41.2 MB (83.84%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,20 +1,45 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int n = students.length;
-        int[] arr = new int[2];
-        for (int num : students) {
-            if (num == 1) {
-                arr[1]++;
-            }
-        }
-        arr[0] = n - arr[1];
+        Queue<Integer> que = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
         for (int i = 0; i < n; i++) {
-            int sand = sandwiches[i];
-            if (arr[sand] == 0) {
-                return n - i;
-            }
-            arr[sand]--;
+            st.push(sandwiches[n - i - 1]);
+            que.add(students[i]);
         }
-        return 0;
+        int lastServed = 0;
+        while (!que.isEmpty() && lastServed < que.size()) {
+            if (st.peek() == que.peek()) {
+                st.pop();
+                que.poll();
+                lastServed = 0;
+            } else {
+                que.add(que.peek());
+                que.poll();
+                lastServed++;
+            }
+        }
+        return que.size();
     }
 }
+
+// class Solution {
+//     public int countStudents(int[] students, int[] sandwiches) {
+//         int n = students.length;
+//         int[] arr = new int[2];
+//         for (int num : students) {
+//             if (num == 1) {
+//                 arr[1]++;
+//             }
+//         }
+//         arr[0] = n - arr[1];
+//         for (int i = 0; i < n; i++) {
+//             int sand = sandwiches[i];
+//             if (arr[sand] == 0) {
+//                 return n - i;
+//             }
+//             arr[sand]--;
+//         }
+//         return 0;
+//     }
+// }
