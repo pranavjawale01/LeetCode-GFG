3bbd9bf6e8b668e6f7ef4f03b62d57c9fdd7834f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-09 12:02:21 +0530

    Time: 0 ms (100%), Space: 41.5 MB (32.97%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,22 +1,41 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int n = tickets.length;
-        Queue<Integer> q = new LinkedList<>();
+        int maxVal = tickets[k];
+        int ans = 0, n = tickets.length;
         for (int i = 0; i < n; i++) {
-            q.add(i);
-        }
-        int time = 0;
-        while (true) {
-            if (tickets[k] == 0) {
-                break;
-            }
-            int curr = q.poll();
-            tickets[curr]--;
-            time++;
-            if (tickets[curr] > 0) {
-                q.add(curr);
+            if (tickets[i] >= maxVal) {
+                if (i <= k) {
+                    ans += maxVal;
+                } else {
+                    ans += maxVal - 1;
+                }
+            } else {
+                ans += tickets[i];
             }
         }
-        return time;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+//         int n = tickets.length;
+//         Queue<Integer> q = new LinkedList<>();
+//         for (int i = 0; i < n; i++) {
+//             q.add(i);
+//         }
+//         int time = 0;
+//         while (true) {
+//             if (tickets[k] == 0) {
+//                 break;
+//             }
+//             int curr = q.poll();
+//             tickets[curr]--;
+//             time++;
+//             if (tickets[curr] > 0) {
+//                 q.add(curr);
+//             }
+//         }
+//         return time;
+//     }
+// }
