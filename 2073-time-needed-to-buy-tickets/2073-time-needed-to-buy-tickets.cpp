23b4820bfe94058d82e4c6e8ac21134ca9ca1827
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-09 12:08:46 +0530

    Time: 2 ms (57.41%), Space: 9.2 MB (99.09%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,45 +1,64 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int maxVal = tickets[k];
-        int ans = 0;
+        int i = 0, time = 0;
         int n = tickets.size();
-        for (int i = 0; i < n; i++) {
-            if (tickets[i] >= maxVal) {
-                if (i <= k) {
-                    ans += maxVal;
-                } else {
-                    ans += maxVal - 1;
-                }
-            } else {
-                ans += tickets[i];
+        while (tickets[k] > 0) {
+            if (i >= n) {
+                i = 0;
             }
+            if (tickets[i] > 0) {
+                tickets[i]--;
+                time++;
+            }
+            i++;
         }
-        return ans;
+        return time;
     }
 };
 
 // class Solution {
 // public:
 //     int timeRequiredToBuy(vector<int>& tickets, int k) {
+//         int maxVal = tickets[k];
+//         int ans = 0;
 //         int n = tickets.size();
-//         queue<int> q;
 //         for (int i = 0; i < n; i++) {
-//             q.push(i);
-//         }
-//         int time = 0;
-//         while (true) {
-//             if (tickets[k] == 0) {
-//                 break;
-//             }
-//             int curr = q.front();
-//             tickets[curr]--;
-//             time++;
-//             q.pop();
-//             if (tickets[curr] > 0) {
-//                 q.push(curr);
+//             if (tickets[i] >= maxVal) {
+//                 if (i <= k) {
+//                     ans += maxVal;
+//                 } else {
+//                     ans += maxVal - 1;
+//                 }
+//             } else {
+//                 ans += tickets[i];
 //             }
 //         }
-//         return time;
+//         return ans;
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     int timeRequiredToBuy(vector<int>& tickets, int k) {
+// //         int n = tickets.size();
+// //         queue<int> q;
+// //         for (int i = 0; i < n; i++) {
+// //             q.push(i);
+// //         }
+// //         int time = 0;
+// //         while (true) {
+// //             if (tickets[k] == 0) {
+// //                 break;
+// //             }
+// //             int curr = q.front();
+// //             tickets[curr]--;
+// //             time++;
+// //             q.pop();
+// //             if (tickets[curr] > 0) {
+// //                 q.push(curr);
+// //             }
+// //         }
+// //         return time;
+// //     }
+// // };
