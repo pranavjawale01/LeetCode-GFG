079c4f22d59dac19993aeaf0832f2d88fd044898
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-09 12:07:10 +0530

    Time: 32 ms (96.94%), Space: 16.5 MB (92.6%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,17 +1,30 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        n = len(tickets)
-        q = deque()
+        maxVal , ans, n = tickets[k], 0, len(tickets)
         for i in range(n):
-            q.append(i)
-        time = 0
-        while True:
-            if tickets[k] == 0:
-                break
-            curr = q[0]
-            tickets[curr] -= 1
-            time += 1
-            q.popleft()
-            if tickets[curr] > 0:
-                q.append(curr)
-        return time
+            if tickets[i] >= maxVal:
+                if i <= k:
+                    ans += maxVal
+                else:
+                    ans += maxVal - 1
+            else:
+                ans += tickets[i]
+        return ans
+        
+# class Solution:
+#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+#         n = len(tickets)
+#         q = deque()
+#         for i in range(n):
+#             q.append(i)
+#         time = 0
+#         while True:
+#             if tickets[k] == 0:
+#                 break
+#             curr = q[0]
+#             tickets[curr] -= 1
+#             time += 1
+#             q.popleft()
+#             if tickets[curr] > 0:
+#                 q.append(curr)
+#         return time
