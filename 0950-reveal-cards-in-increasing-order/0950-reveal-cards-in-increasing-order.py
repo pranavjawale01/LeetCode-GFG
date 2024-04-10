d0156a91c75b66fecdc9c5dec9df937d8ade27d8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-10 19:34:14 +0530

    Time: 53 ms (14.18%), Space: 16.7 MB (80.95%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,15 +1,30 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        deck.sort()
+        q = deque()
         n = len(deck)
+        deck.sort()
+        for i in range(n):
+            q.append(i)
         ans = [0] * n
-        i , j = 0, 0
-        skip = False
-        while i < n:
-            if ans[j] == 0:
-                if skip == False:
-                    ans[j] = deck[i]
-                    i += 1
-                skip = ~skip 
-            j = (j + 1) % n
+        for i in range(n):
+            idx = q.popleft()
+            ans[idx] = deck[i]
+            if q:
+                q.append(q.popleft())
         return ans
+
+# class Solution:
+#     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+#         deck.sort()
+#         n = len(deck)
+#         ans = [0] * n
+#         i , j = 0, 0
+#         skip = False
+#         while i < n:
+#             if ans[j] == 0:
+#                 if skip == False:
+#                     ans[j] = deck[i]
+#                     i += 1
+#                 skip = ~skip 
+#             j = (j + 1) % n
+#         return ans
