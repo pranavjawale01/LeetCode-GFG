a79820e15beb1c4e9e05af29b4ab59dd97978c45
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-10 19:30:20 +0530

    Time: 0 ms (100%), Space: 10.8 MB (67.85%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        queue<int> q;
         int n = deck.size();
         sort(deck.begin(), deck.end());
-        vector<int> ans(n , 0);
-        bool skip = false;
-        int i = 0, j = 0;
-        while (i < n) {
-            if (ans[j] == 0) {
-                if (skip == false) {
-                    ans[j] = deck[i];
-                    i++;
-                }
-                skip = !skip;
+        for (int i = 0; i < n; i++) {
+            q.push(i);
+        }
+        vector<int> ans(n ,0);
+        for (int i = 0; i < n; i++) {
+            int idx = q.front();
+            q.pop();
+            ans[idx] = deck[i];
+            if (!q.empty()) {
+                q.push(q.front());
+                q.pop();
             }
-            j = (j + 1) % n;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+//         int n = deck.size();
+//         sort(deck.begin(), deck.end());
+//         vector<int> ans(n , 0);
+//         bool skip = false;
+//         int i = 0, j = 0;
+//         while (i < n) {
+//             if (ans[j] == 0) {
+//                 if (skip == false) {
+//                     ans[j] = deck[i];
+//                     i++;
+//                 }
+//                 skip = !skip;
+//             }
+//             j = (j + 1) % n;
+//         }
+//         return ans;
+//     }
+// };
