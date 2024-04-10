04fa391a5e4f01109c9abaaf23b329de7370461d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-10 19:34:05 +0530

    Time: 4 ms (86.41%), Space: 43.4 MB (76.46%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,21 +1,41 @@
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
         int n = deck.length;
+        Queue<Integer> q = new LinkedList<>();
         Arrays.sort(deck);
         int[] ans = new int[n];
-        Arrays.fill(ans, 0);
-        boolean skip = false;
-        int i = 0, j = 0;
-        while (i < n) {
-            if (ans[j] == 0) {
-                if (skip == false) {
-                    ans[j] = deck[i];
-                    i++;
-                }
-                skip = !skip;
+        for (int i = 0; i < n; i++) {
+            q.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+            int idx = q.poll();
+            ans[idx] = deck[i];
+            if (!q.isEmpty()) {
+                q.add(q.poll());
             }
-            j = (j + 1) % n;
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public int[] deckRevealedIncreasing(int[] deck) {
+//         int n = deck.length;
+//         Arrays.sort(deck);
+//         int[] ans = new int[n];
+//         Arrays.fill(ans, 0);
+//         boolean skip = false;
+//         int i = 0, j = 0;
+//         while (i < n) {
+//             if (ans[j] == 0) {
+//                 if (skip == false) {
+//                     ans[j] = deck[i];
+//                     i++;
+//                 }
+//                 skip = !skip;
+//             }
+//             j = (j + 1) % n;
+//         }
+//         return ans;
+//     }
+// }
