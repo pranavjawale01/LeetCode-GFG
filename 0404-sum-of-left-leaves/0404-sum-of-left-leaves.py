12c182b1991c6348ed9b9f62c11cdbe62901c50d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-14 10:25:07 +0530

    Time: 38 ms (45.81%), Space: 16.7 MB (47.79%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -4,14 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
-    def solve(self, root: Optional[TreeNode], parent: Optional[TreeNode]) -> int:
+    def solve(self, root: Optional[TreeNode], isLeft: bool) -> int:
         if not root:
             return 0
         if not root.left and not root.right:
-            if parent and parent.left is root:
+            if isLeft:
                 return root.val
-        return self.solve(root.left, root) + self.solve(root.right, root)
+        return self.solve(root.left, True) + self.solve(root.right, False)
 
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        return self.solve(root, None)
+        return self.solve(root, False)
+
+# class Solution:
+#     def solve(self, root: Optional[TreeNode], parent: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         if not root.left and not root.right:
+#             if parent and parent.left is root:
+#                 return root.val
+#         return self.solve(root.left, root) + self.solve(root.right, root)
+
+#     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+#         return self.solve(root, None)
