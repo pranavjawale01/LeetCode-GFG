05b891de2410c01ab8739afbfe37dbf347da2558
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-14 10:22:29 +0530

    Time: 3 ms (56.94%), Space: 14.8 MB (15.62%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -11,18 +11,36 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root, TreeNode* parent) {
+    int solve(TreeNode* root, bool isLeft) {
         if (root == nullptr) {
             return 0;
         }
         if (root->left == nullptr && root->right == nullptr) {
-            if (parent != nullptr && parent->left == root) {
+            if (isLeft) {
                 return root->val;
             }
         }
-        return solve(root->left, root) + solve(root->right, root);
+        return solve(root->left, true) + solve(root->right, false);
     }
     int sumOfLeftLeaves(TreeNode* root) {
-        return solve(root, nullptr);
+        return solve(root, false);
     }
 };
+
+// class Solution {
+// public:
+//     int solve(TreeNode* root, TreeNode* parent) {
+//         if (root == nullptr) {
+//             return 0;
+//         }
+//         if (root->left == nullptr && root->right == nullptr) {
+//             if (parent != nullptr && parent->left == root) {
+//                 return root->val;
+//             }
+//         }
+//         return solve(root->left, root) + solve(root->right, root);
+//     }
+//     int sumOfLeftLeaves(TreeNode* root) {
+//         return solve(root, nullptr);
+//     }
+// };
