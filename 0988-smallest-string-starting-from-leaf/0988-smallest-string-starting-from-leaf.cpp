cd2dadcfd9184fb988f4a3a1cea7fd598dfbce9d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-17 16:21:16 +0530

    Time: 14 ms (17.17%), Space: 20.1 MB (82.55%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -9,25 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    void solve(TreeNode* root, string &ans, string str) {
-        if (root == nullptr) {
-            return;
-        }
-        str = char('a' + root->val) + str;
-        if (root->left == nullptr && root->right == nullptr) {
-            if (ans.empty() || str <= ans) {
-                ans = str;
-            }
-            return;
-        }
-        solve(root->left, ans, str);
-        solve(root->right, ans, str);
-    }
     string smallestFromLeaf(TreeNode* root) {
-        string ans = "~";
-        solve(root, ans, "");
-        return ans;
+        queue<pair<TreeNode*, string>> q;
+        q.push({root, string(1, char(root->val + 'a'))});
+        string result = "";
+        while (!q.empty()) {
+            auto[node, curr] = q.front();
+            q.pop();
+            if (!node->left && !node->right) {
+                if (result == "" || result > curr) {
+                    result = curr;
+                }
+            }
+            if (node->left) {
+                q.push({node->left, char(node->left->val + 'a') + curr});
+            }
+            if (node->right) {
+                q.push({node->right, char(node->right->val + 'a') + curr});
+            }
+        }
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     void solve(TreeNode* root, string &ans, string str) {
+//         if (root == nullptr) {
+//             return;
+//         }
+//         str = char('a' + root->val) + str;
+//         if (root->left == nullptr && root->right == nullptr) {
+//             if (ans.empty() || str <= ans) {
+//                 ans = str;
+//             }
+//             return;
+//         }
+//         solve(root->left, ans, str);
+//         solve(root->right, ans, str);
+//     }
+//     string smallestFromLeaf(TreeNode* root) {
+//         string ans = "~";
+//         solve(root, ans, "");
+//         return ans;
+//     }
+// };
