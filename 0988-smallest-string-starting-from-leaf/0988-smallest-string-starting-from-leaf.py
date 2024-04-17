5df39ed7ffacd2ef4c0fbb23c713fe4f9da4da38
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-17 16:24:28 +0530

    Time: 38 ms (85%), Space: 18.1 MB (7.08%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -5,19 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.ans = ""
-
-    def solve(self, root: Optional[TreeNode], temp : str):
-        if not root:
-            return
-        temp = chr(root.val + ord('a')) + temp
-        if not root.left and not root.right:
-            if not self.ans or self.ans > temp:
-                self.ans = temp
-        self.solve(root.left, temp)
-        self.solve(root.right, temp)
-
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        self.solve(root, "")
-        return self.ans
+        if not root:
+            return ""
+        q = deque([(root, chr(root.val + ord('a')))])
+        result = ""
+
+        while q:
+            node, curr = q.popleft()
+            if not node.left and not node.right:
+                if not result or result > curr:
+                    result = curr
+            if node.left:
+                q.append((node.left, chr(node.left.val + ord('a')) + curr))
+            if node.right:
+                q.append((node.right, chr(node.right.val + ord('a')) + curr))
+        return result
+
+# class Solution:
+#     def __init__(self):
+#         self.ans = ""
+
+#     def solve(self, root: Optional[TreeNode], temp : str):
+#         if not root:
+#             return
+#         temp = chr(root.val + ord('a')) + temp
+#         if not root.left and not root.right:
+#             if not self.ans or self.ans > temp:
+#                 self.ans = temp
+#         self.solve(root.left, temp)
+#         self.solve(root.right, temp)
+
+#     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+#         self.solve(root, "")
+#         return self.ans
