19560ff657074dba8cb76d26430c89ee5816e21e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-18 19:46:29 +0530

    Time: 352 ms (83.99%), Space: 16.9 MB (65.66%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,30 +1,48 @@
 class Solution:
-    def bfs(self, grid: List[List[int]], i: int, j: int, row: int, col: int) -> int:
-        peri = 0
-        q = deque([(i, j)])
-        grid[i][j] = -1
-        dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        while q:
-            it = q.popleft()
-            for d in dir:
-                i_ = it[0] + d[0]
-                j_ = it[1] + d[1]
-                if i_ < 0 or j_ < 0 or i_ >= row or j_ >= col or grid[i_][j_] == 0:
-                    peri += 1
-                elif grid[i_][j_] == -1:
-                    continue
-                else:
-                    q.append((i_, j_))
-                    grid[i_][j_] = -1
-        return peri
-    
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
+        row = len(grid)
+        col = len(grid[0])
+        peri = 0
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
-                    return self.bfs(grid, i, j, row, col)
-        return 0
+                    if j+1 == col or grid[i][j+1] == 0:
+                        peri += 1
+                    if j-1 < 0 or grid[i][j-1] == 0:
+                        peri += 1
+                    if i-1 < 0 or grid[i-1][j] == 0:
+                        peri += 1
+                    if i+1 == row or grid[i+1][j] == 0:
+                        peri += 1
+        return peri
+
+# class Solution:
+#     def bfs(self, grid: List[List[int]], i: int, j: int, row: int, col: int) -> int:
+#         peri = 0
+#         q = deque([(i, j)])
+#         grid[i][j] = -1
+#         dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+#         while q:
+#             it = q.popleft()
+#             for d in dir:
+#                 i_ = it[0] + d[0]
+#                 j_ = it[1] + d[1]
+#                 if i_ < 0 or j_ < 0 or i_ >= row or j_ >= col or grid[i_][j_] == 0:
+#                     peri += 1
+#                 elif grid[i_][j_] == -1:
+#                     continue
+#                 else:
+#                     q.append((i_, j_))
+#                     grid[i_][j_] = -1
+#         return peri
+    
+#     def islandPerimeter(self, grid: List[List[int]]) -> int:
+#         row, col = len(grid), len(grid[0])
+#         for i in range(row):
+#             for j in range(col):
+#                 if grid[i][j] == 1:
+#                     return self.bfs(grid, i, j, row, col)
+#         return 0
 
 # class Solution:
 #     def solve(self, grid: List[List[int]], i: int, j: int, row: int, col: int):
