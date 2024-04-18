f98d902808c25766463d21a755400f59e18b7c6b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-18 19:46:23 +0530

    Time: 5 ms (51.43%), Space: 45.8 MB (31.17%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,41 +1,68 @@
 class Solution {
-    public int bfs(int[][] grid, int i, int j, int row, int col) {
+    public int islandPerimeter(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
         int peri = 0;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{i, j});
-        grid[i][j] = -1;
-        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!q.isEmpty()) {
-            int[] it = q.poll();
-            for (int[] d : dir) {
-                int i_ = it[0] + d[0];
-                int j_ = it[1] + d[1];
-                if (i_ < 0 || j_ < 0 || i_ >= row || j_ >= col || grid[i_][j_] == 0) {
-                    peri++;
-                } else if (grid[i_][j_] == -1) {
-                    continue;
-                } else {
-                    q.offer(new int[]{i_, j_});
-                    grid[i_][j_] = -1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    if (j+1 == col || grid[i][j+1] == 0) {
+                        peri++;
+                    }
+                    if (j-1 < 0 || grid[i][j-1] == 0) {
+                        peri++;
+                    }
+                    if (i-1 < 0 || grid[i-1][j] == 0) {
+                        peri++;
+                    }
+                    if (i+1 == row || grid[i+1][j] == 0) {
+                        peri++;
+                    }
                 }
             }
         }
         return peri;
     }
-    
-    public int islandPerimeter(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) {
-                    return bfs(grid, i, j, row, col);
-                }
-            }
-        }
-        return 0;
-    }
 }
+
+// class Solution {
+//     public int bfs(int[][] grid, int i, int j, int row, int col) {
+//         int peri = 0;
+//         Queue<int[]> q = new LinkedList<>();
+//         q.offer(new int[]{i, j});
+//         grid[i][j] = -1;
+//         int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+//         while (!q.isEmpty()) {
+//             int[] it = q.poll();
+//             for (int[] d : dir) {
+//                 int i_ = it[0] + d[0];
+//                 int j_ = it[1] + d[1];
+//                 if (i_ < 0 || j_ < 0 || i_ >= row || j_ >= col || grid[i_][j_] == 0) {
+//                     peri++;
+//                 } else if (grid[i_][j_] == -1) {
+//                     continue;
+//                 } else {
+//                     q.offer(new int[]{i_, j_});
+//                     grid[i_][j_] = -1;
+//                 }
+//             }
+//         }
+//         return peri;
+//     }
+    
+//     public int islandPerimeter(int[][] grid) {
+//         int row = grid.length;
+//         int col = grid[0].length;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (grid[i][j] == 1) {
+//                     return bfs(grid, i, j, row, col);
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
+// }
 
 // class Solution {
 //     public void solve(int[][] grid, int i, int j, int row, int col, int[] peri) {
