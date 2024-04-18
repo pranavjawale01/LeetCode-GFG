f8be551f63a43ad54dbb6953df4fcd4401209fca
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-18 19:38:42 +0530

    Time: 83 ms (31.44%), Space: 105.8 MB (9.6%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,31 +1,72 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& grid, int i, int j, int &row, int &col, int &peri) {
-        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == 0) {
-            peri++;
-            return;
-        }
-        if (grid[i][j] == -1) {
-            return;
-        }
+    int bfs(vector<vector<int>>& grid, int i, int j, int row, int col) {
+        int peri = 0;
+        queue<pair<int, int>> q;
+        q.push({i, j});
         grid[i][j] = -1;
-        solve(grid, i+1, j, row, col, peri);
-        solve(grid, i-1, j, row, col, peri);
-        solve(grid, i, j+1, row, col, peri);
-        solve(grid, i, j-1, row, col, peri);
+        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            for (auto &d : dir) {
+                int i_ = it.first + d.first;
+                int j_ = it.second + d.second;
+                if (i_ < 0 || j_ < 0 || i_ >= row || j_ >= col || grid[i_][j_] == 0) {
+                    peri++;
+                } else if (grid[i_][j_] == -1) {
+                    continue;
+                } else {
+                    q.push({i_, j_});
+                    grid[i_][j_] = -1;
+                }
+            }
+        }
+        return peri;
     }
+    
     int islandPerimeter(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        int peri = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) { 
-                    solve(grid, i, j, row, col, peri);
-                    return peri;
+                if (grid[i][j] == 1) {
+                    return bfs(grid, i, j, row, col);
                 }
             }
         }
         return 0;
     }
 };
+
+// class Solution {
+// public:
+//     void solve(vector<vector<int>>& grid, int i, int j, int &row, int &col, int &peri) {
+//         if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == 0) {
+//             peri++;
+//             return;
+//         }
+//         if (grid[i][j] == -1) {
+//             return;
+//         }
+//         grid[i][j] = -1;
+//         solve(grid, i+1, j, row, col, peri);
+//         solve(grid, i-1, j, row, col, peri);
+//         solve(grid, i, j+1, row, col, peri);
+//         solve(grid, i, j-1, row, col, peri);
+//     }
+//     int islandPerimeter(vector<vector<int>>& grid) {
+//         int row = grid.size();
+//         int col = grid[0].size();
+//         int peri = 0;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (grid[i][j] == 1) { 
+//                     solve(grid, i, j, row, col, peri);
+//                     return peri;
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
+// };
