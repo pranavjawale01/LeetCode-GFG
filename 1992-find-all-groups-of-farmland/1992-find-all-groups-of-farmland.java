a70b7f5dd5f0be55fb08643e48698571aa92ce6c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-20 19:17:37 +0530

    Time: 5 ms (93.16%), Space: 60.9 MB (53.61%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,23 +1,4 @@
 class Solution {
-    public void bfs(int[][] land, int i, int j, int row, int col, int[] max) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            max[0] = Math.max(max[0], p[0]);
-            max[1] = Math.max(max[1], p[1]);
-            for (int[] dir : direction) {
-                int i_ = p[0] + dir[0];
-                int j_ = p[1] + dir[1];
-                if (i_ >= 0 && i_ < row && j_ >= 0 && j_ < col && land[i_][j_] == 1) {
-                    land[i_][j_] = 0;
-                    q.add(new int[]{i_, j_});
-                }
-            }
-        }
-    }
-
     public int[][] findFarmland(int[][] land) {
         int row = land.length;
         int col = land[0].length;
@@ -25,9 +6,22 @@ class Solution {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (land[i][j] == 1) {
-                    int[] max = new int[]{i, j};
-                    bfs(land, i, j, row, col, max);
-                    ansList.add(new int[]{i, j, max[0], max[1]});
+                    int i_max = i;
+                    while (i_max < row && land[i_max][j] == 1) {
+                        i_max++;
+                    }
+                    int j_max = j;
+                    while (j_max < col && land[i][j_max] == 1) {
+                        j_max++;
+                    }
+                    i_max = (i_max > 0) ? i_max - 1 : i_max;
+                    j_max = (j_max > 0) ? j_max - 1 : j_max;
+                    ansList.add(new int[]{i, j, i_max, j_max});
+                    for (int k = i; k <= i_max; k++) {
+                        for (int l = j; l <= j_max; l++) {
+                            land[k][l] = 0;
+                        }
+                    }
                 }
             }
         }
@@ -38,6 +32,47 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     public void bfs(int[][] land, int i, int j, int row, int col, int[] max) {
+//         Queue<int[]> q = new LinkedList<>();
+//         q.add(new int[]{i, j});
+//         int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+//         while (!q.isEmpty()) {
+//             int[] p = q.poll();
+//             max[0] = Math.max(max[0], p[0]);
+//             max[1] = Math.max(max[1], p[1]);
+//             for (int[] dir : direction) {
+//                 int i_ = p[0] + dir[0];
+//                 int j_ = p[1] + dir[1];
+//                 if (i_ >= 0 && i_ < row && j_ >= 0 && j_ < col && land[i_][j_] == 1) {
+//                     land[i_][j_] = 0;
+//                     q.add(new int[]{i_, j_});
+//                 }
+//             }
+//         }
+//     }
+
+//     public int[][] findFarmland(int[][] land) {
+//         int row = land.length;
+//         int col = land[0].length;
+//         List<int[]> ansList = new ArrayList<>();
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (land[i][j] == 1) {
+//                     int[] max = new int[]{i, j};
+//                     bfs(land, i, j, row, col, max);
+//                     ansList.add(new int[]{i, j, max[0], max[1]});
+//                 }
+//             }
+//         }
+//         int[][] ans = new int[ansList.size()][4];
+//         for (int i = 0; i < ansList.size(); i++) {
+//             ans[i] = ansList.get(i);
+//         }
+//         return ans;
+//     }
+// }
 
 
 // class Solution {
