38fa80bfba8b0792b85ae3a6cde44866c560d634
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-20 19:16:42 +0530

    Time: 99 ms (76%), Space: 59.2 MB (97.82%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& land, int i, int j, int row, int col, int& max_i, int& max_j) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        vector<vector<int>> direction{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.empty()) {
-            pair<int, int> p = q.front();
-            q.pop();
-            max_i = max(max_i, p.first);
-            max_j = max(max_j, p.second);
-            for (auto& dir : direction) {
-                int i_ = p.first + dir[0];
-                int j_ = p.second + dir[1];
-                if (i_ >= 0 && i_ < row && j_ >= 0 && j_ < col && land[i_][j_] == 1) {
-                    land[i_][j_] = 0;
-                    q.push({i_, j_});
-                }
-            }
-        }
-    }
-
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         int row = land.size();
         int col = land[0].size();
@@ -27,15 +7,67 @@ public:
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (land[i][j] == 1) {
-                    int max_i = i, max_j = j;
-                    bfs(land, i, j, row, col, max_i, max_j);
-                    ans.push_back({i, j, max_i, max_j});
+                    int i_max = i;
+                    while (i_max < row && land[i_max][j] == 1) {
+                        i_max++;
+                    }
+                    int j_max = j;
+                    while (j_max < col && land[i][j_max] == 1) {
+                        j_max++;
+                    }
+                    i_max = (i_max > 0) ? i_max - 1 : i_max;
+                    j_max = (j_max > 0) ? j_max - 1 : j_max;
+                    ans.push_back({i, j, i_max, j_max});
+                    for (int k = i; k <= i_max; k++) {
+                        for (int l = j; l <= j_max; l++) {
+                            land[k][l] = 0;
+                        }
+                    }
                 }
             }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     void bfs(vector<vector<int>>& land, int i, int j, int row, int col, int& max_i, int& max_j) {
+//         queue<pair<int, int>> q;
+//         q.push({i, j});
+//         vector<vector<int>> direction{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+//         while (!q.empty()) {
+//             pair<int, int> p = q.front();
+//             q.pop();
+//             max_i = max(max_i, p.first);
+//             max_j = max(max_j, p.second);
+//             for (auto& dir : direction) {
+//                 int i_ = p.first + dir[0];
+//                 int j_ = p.second + dir[1];
+//                 if (i_ >= 0 && i_ < row && j_ >= 0 && j_ < col && land[i_][j_] == 1) {
+//                     land[i_][j_] = 0;
+//                     q.push({i_, j_});
+//                 }
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+//         int row = land.size();
+//         int col = land[0].size();
+//         vector<vector<int>> ans;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (land[i][j] == 1) {
+//                     int max_i = i, max_j = j;
+//                     bfs(land, i, j, row, col, max_i, max_j);
+//                     ans.push_back({i, j, max_i, max_j});
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 // class Solution {
