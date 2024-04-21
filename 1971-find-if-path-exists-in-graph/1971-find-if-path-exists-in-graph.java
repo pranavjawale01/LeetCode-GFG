b88000484b520f33115c959eb10ae227190fcdbe
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-21 10:56:48 +0530

    Time: 116 ms (49.43%), Space: 154.2 MB (39.48%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,32 +1,62 @@
 class Solution {
-    public boolean check(Map<Integer, ArrayList<Integer>> mp, int s, int d, boolean[] visited) {
-        if (s == d) {
-            return true;
-        }
-        if (visited[s]) {
-            return false;
-        }
-        visited[s] = true;
-        for (int node : mp.get(s)) {
-            if (check(mp, node, d, visited)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Map<Integer, ArrayList<Integer>> mp = new HashMap<>();
         for (int i = 0; i < n; i++) {
             mp.put(i, new ArrayList<>());
         }
         for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            mp.get(u).add(v);
-            mp.get(v).add(u);
+            mp.get(edge[0]).add(edge[1]);
+            mp.get(edge[1]).add(edge[0]);
         }
         boolean[] visited = new boolean[n];
-        return check(mp, source, destination, visited);
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(source);
+        visited[source] = true;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            if (node == destination) {
+                return true;
+            }
+            for (int v : mp.get(node)) {
+                if (visited[v] == false) {
+                    q.offer(v);
+                    visited[v] = true;
+                }
+            }
+        }
+        return false;
     }
 }
+
+// class Solution {
+//     public boolean check(Map<Integer, ArrayList<Integer>> mp, int s, int d, boolean[] visited) {
+//         if (s == d) {
+//             return true;
+//         }
+//         if (visited[s]) {
+//             return false;
+//         }
+//         visited[s] = true;
+//         for (int node : mp.get(s)) {
+//             if (check(mp, node, d, visited)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+    
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         Map<Integer, ArrayList<Integer>> mp = new HashMap<>();
+//         for (int i = 0; i < n; i++) {
+//             mp.put(i, new ArrayList<>());
+//         }
+//         for (int[] edge : edges) {
+//             int u = edge[0];
+//             int v = edge[1];
+//             mp.get(u).add(v);
+//             mp.get(v).add(u);
+//         }
+//         boolean[] visited = new boolean[n];
+//         return check(mp, source, destination, visited);
+//     }
+// }
