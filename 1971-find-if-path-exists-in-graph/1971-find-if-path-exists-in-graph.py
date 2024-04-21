e85a0e0dd726de21594c5e42f031a6b79d9786bf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-21 10:57:03 +0530

    Time: 1407 ms (88.91%), Space: 108.9 MB (58.93%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,19 +1,39 @@
 class Solution:
-    def check(self, mp, s, d, visited) -> bool:
-        if s == d:
-            return True
-        if visited[s]:
-            return False
-        visited[s] = True
-        for node in mp[s]:
-            if self.check(mp, node, d, visited):
-                return True
-        return False
-
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         mp = defaultdict(list)
         for u, v in edges:
             mp[u].append(v)
             mp[v].append(u)
         visited = [False] * n
-        return self.check(mp, source, destination, visited)
+        q = deque()
+        q.append(source)
+        visited[source] = True
+        while q:
+            node = q.popleft()
+            if node == destination:
+                return True
+            for v in mp[node]:
+                if not visited[v]:
+                    q.append(v)
+                    visited[v] = True
+        return False
+
+# class Solution:
+#     def check(self, mp, s, d, visited) -> bool:
+#         if s == d:
+#             return True
+#         if visited[s]:
+#             return False
+#         visited[s] = True
+#         for node in mp[s]:
+#             if self.check(mp, node, d, visited):
+#                 return True
+#         return False
+
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#         mp = defaultdict(list)
+#         for u, v in edges:
+#             mp[u].append(v)
+#             mp[v].append(u)
+#         visited = [False] * n
+#         return self.check(mp, source, destination, visited)
