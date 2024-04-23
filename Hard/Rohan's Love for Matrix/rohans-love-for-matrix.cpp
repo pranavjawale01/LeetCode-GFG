3145710b5dc565afa3385116f9410599363b1b5c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-23 11:25:08 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int first = 1;
+        int second = 1;
+        int ans = 0;
+        int mod = 1e9 + 7;
+        for (int i = 3; i <= n; i++) {
+            ans = (first + second) % mod;
+            first = second;
+            second = ans;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
