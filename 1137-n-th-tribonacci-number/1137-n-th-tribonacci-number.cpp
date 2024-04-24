e94e61887a09c1bb0aa2ff0cdb446b4303de90a3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-24 07:41:58 +0530

    Time: 2 ms (50.95%), Space: 7.3 MB (13.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int tribonacci(int n) {
+        vector<int> memo(n + 1, -1);
+        return helper(n, memo);
+    }
+
+    int helper(int n, vector<int>& memo) {
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        if (memo[n] != -1) return memo[n];
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        return memo[n];
     }
 };
 
