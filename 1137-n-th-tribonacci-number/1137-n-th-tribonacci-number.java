73b28a81ecaa6884604cb859055f0da6c775d7a2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-24 07:42:57 +0530

    Time: 0 ms (100%), Space: 40.1 MB (59.37%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,18 +1,36 @@
 class Solution {
+    int[] memo;
+
     public int tribonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1 || n == 2) {
-            return 1;
-        }
-        int t0 = 0, t1 = 1, t2 = 1;
-        for (int i = 3; i <= n; i++) {
-            int temp = t0 + t1 + t2;
-            t0 = t1;
-            t1 = t2;
-            t2 = temp;
-        }
-        return t2;
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return helper(n);
+    }
+
+    private int helper(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        if (memo[n] != -1) return memo[n];
+        memo[n] = helper(n - 1) + helper(n - 2) + helper(n - 3);
+        return memo[n];
     }
 }
+
+// class Solution {
+//     public int tribonacci(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (n == 1 || n == 2) {
+//             return 1;
+//         }
+//         int t0 = 0, t1 = 1, t2 = 1;
+//         for (int i = 3; i <= n; i++) {
+//             int temp = t0 + t1 + t2;
+//             t0 = t1;
+//             t1 = t2;
+//             t2 = temp;
+//         }
+//         return t2;
+//     }
+// }
