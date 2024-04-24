12fc5fe96d839bb1d55ebbc2485856a74e3c00e7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-24 07:42:47 +0530

    Time: 32 ms (74.26%), Space: 16.5 MB (31.32%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,13 +1,29 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n is 0:
+        memo = [-1] * (n + 1)
+        return self.helper(n, memo)
+    
+    def helper(self, n, memo):
+        if n == 0:
             return 0
-        if n is 1 or n is 2:
+        if n == 1 or n == 2:
             return 1
-        t0, t1, t2 = 0, 1, 1
-        for i in range(3, n+1):
-            temp = t0 + t1 + t2
-            t0 = t1
-            t1 = t2
-            t2 = temp
-        return t2
+        if memo[n] != -1:
+            return memo[n]
+        memo[n] = self.helper(n - 1, memo) + self.helper(n - 2, memo) + self.helper(n - 3, memo)
+        return memo[n]
+        
+
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         if n is 0:
+#             return 0
+#         if n is 1 or n is 2:
+#             return 1
+#         t0, t1, t2 = 0, 1, 1
+#         for i in range(3, n+1):
+#             temp = t0 + t1 + t2
+#             t0 = t1
+#             t1 = t2
+#             t2 = temp
+#         return t2
