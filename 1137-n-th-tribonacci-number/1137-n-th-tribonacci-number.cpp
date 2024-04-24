a595cafb7c245076c8e149751d912bd3bf4ef1d6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-24 07:40:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1 || n == 2) {
-            return 1;
-        }
-        int t0 = 0, t1 = 1, t2 = 1;
-        for (int i = 3; i <= n; i++) {
-            int temp = t0 + t1 + t2;
-            t0 = t1;
-            t1 = t2;
-            t2 = temp;
-        }
-        return t2;
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 };
+
+// class Solution {
+// public:
+//     int tribonacci(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (n == 1 || n == 2) {
+//             return 1;
+//         }
+//         int t0 = 0, t1 = 1, t2 = 1;
+//         for (int i = 3; i <= n; i++) {
+//             int temp = t0 + t1 + t2;
+//             t0 = t1;
+//             t1 = t2;
+//             t2 = temp;
+//         }
+//         return t2;
+//     }
+// };
