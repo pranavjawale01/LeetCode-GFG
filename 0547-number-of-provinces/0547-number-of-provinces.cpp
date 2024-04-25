e62eedb21b83c4277335c950dc7472c2f6fdebf4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 00:32:34 +0530

    Time: 12 ms (97.8%), Space: 18 MB (87.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>> &adj, int i, vector<bool>& visited) {
+    void DFS(vector<vector<int>>& isConnected, int i, vector<bool>& visited) {
         visited[i] = true;
-        for (int &v : adj[i]) {
-            if (!visited[v]) {
-                DFS(adj, v, visited);
+        int n = isConnected.size();
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && isConnected[i][v]) {
+                DFS(isConnected, v, visited);
             }
         }
     }
@@ -12,21 +13,46 @@ public:
         int count = 0;
         int n = isConnected.size();
         vector<bool> visited(n, false);
-        unordered_map<int, vector<int>> adj;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                DFS(adj, i, visited);
+                DFS(isConnected, i, visited);
                 count++;
             }
         }
         return count; 
     }
 };
+
+// class Solution {
+// public:
+//     void DFS(unordered_map<int, vector<int>> &adj, int i, vector<bool>& visited) {
+//         visited[i] = true;
+//         for (int &v : adj[i]) {
+//             if (!visited[v]) {
+//                 DFS(adj, v, visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int count = 0;
+//         int n = isConnected.size();
+//         vector<bool> visited(n, false);
+//         unordered_map<int, vector<int>> adj;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (isConnected[i][j] == 1) {
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 DFS(adj, i, visited);
+//                 count++;
+//             }
+//         }
+//         return count; 
+//     }
+// }
