3f811d7691fec8ad675029ad6d203e2b0edd73ad
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-25 18:10:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        vector<int> dp(26, 0);
+        int n = s.length();
+        int ans = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            int left = max(0, idx - k);
+            int right = min(25, idx + k);
+            int longest = INT_MIN;
+            for (int j = left; j <= right; j++) {
+                longest = max(longest, dp[j]);
+            }
+            dp[i] = longest + 1;
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     int longestIdealString(string s, int k) {
+//         int ans = INT_MIN;
+//         int n = s.length();
+//         vector<int> dp(n, 1);
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i - 1; j >= 0; j--) {
+//                 if (abs(s[i] - s[j]) <= k) {
+//                     dp[i] = max(dp[i], dp[j]+1);
+//                 }
+//             }
+//             ans = max(ans, dp[i]);
+//         }
+//         return ans;
+//     }
+// };
