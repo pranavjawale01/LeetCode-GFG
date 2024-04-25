9a25a89f859b023842d4a7ae71b79d1dc19e97a4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 00:39:16 +0530

    Time: 24 ms (36.63%), Space: 21.2 MB (8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& isConnected, int i, vector<bool>& visited) {
-        visited[i] = true;
+    void BFS(vector<vector<int>>& isConnected, int i, vector<bool>& visited) {
+        queue<int> q;
+        q.push(i);
         int n = isConnected.size();
-        for (int v = 0; v < n; v++) {
-            if (!visited[v] && isConnected[i][v]) {
-                DFS(isConnected, v, visited);
+        visited[i] = true;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int v = 0; v < n; v++) {
+                if (!visited[v] && isConnected[i][v] == 1) {
+                    BFS(isConnected, v, visited);
+                }
             }
         }
     }
@@ -16,13 +22,39 @@ public:
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                DFS(isConnected, i, visited);
+                BFS(isConnected, i, visited);
                 count++;
             }
         }
         return count; 
     }
 };
+
+// class Solution {
+// public:
+//     void DFS(vector<vector<int>>& isConnected, int i, vector<bool>& visited) {
+//         visited[i] = true;
+//         int n = isConnected.size();
+//         for (int v = 0; v < n; v++) {
+//             if (!visited[v] && isConnected[i][v]) {
+//                 DFS(isConnected, v, visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int count = 0;
+//         int n = isConnected.size();
+//         vector<bool> visited(n, false);
+        
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 DFS(isConnected, i, visited);
+//                 count++;
+//             }
+//         }
+//         return count; 
+//     }
+// };
 
 // class Solution {
 // public:
