3034759204e1f62f1846382f50dab0780ec80b6e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 17:51:08 +0530

    Time: 4 ms (90.82%), Space: 51.1 MB (53.06%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -2,19 +2,35 @@ class Solution {
     public int minFallingPathSum(int[][] grid) {
         int n = grid.length;
         int[][] dp = new int[n+1][n+1];
+        int nextMinCol1 = -1, nextMinCol2 = -1;
         for (int i = 0; i < n; i++) {
             dp[n-1][i] = grid[n-1][i];
+
+            if (nextMinCol1 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol1]) {
+                nextMinCol2 = nextMinCol1;
+                nextMinCol1 = i;
+            } else if (nextMinCol2 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol2]) {
+                nextMinCol2 = i;
+            }
         }
         for (int row = n - 2; row >= 0; row--) {
+            int minCol1 = -1, minCol2 = -1;
             for (int col = 0; col < n; col++) {
-                int ans = Integer.MAX_VALUE;
-                for (int nextCol = 0; nextCol < n; nextCol++) {
-                    if (nextCol != col) {
-                        ans = Math.min(ans, dp[row+1][nextCol]);
-                    }
-                    dp[row][col] = grid[row][col] + ans;
+                if (col != nextMinCol1) {
+                    dp[row][col] = grid[row][col] + dp[row+1][nextMinCol1];
+                } else {
+                    dp[row][col] = grid[row][col] + dp[row+1][nextMinCol2];
+                }
+
+                if (minCol1 == -1 || dp[row][col] <= dp[row][minCol1]) {
+                    minCol2 = minCol1;
+                    minCol1 = col;
+                } else if (minCol2 == -1 || dp[row][col] <= dp[row][minCol2]) {
+                    minCol2 = col;
                 }
             }
+            nextMinCol1 = minCol1;
+            nextMinCol2 = minCol2;
         }
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
@@ -23,6 +39,36 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int minFallingPathSum(int[][] grid) {
+//         int n = grid.length;
+//         int[][] dp = new int[n+1][n+1];
+//         for (int i = 0; i < n; i++) {
+//             dp[n-1][i] = grid[n-1][i];
+//         }
+//         for (int row = n - 2; row >= 0; row--) {
+//             for (int col = 0; col < n; col++) {
+//                 int ans = Integer.MAX_VALUE;
+//                 for (int nextCol = 0; nextCol < n; nextCol++) {
+//                     if (nextCol != col) {
+//                         ans = Math.min(ans, dp[row+1][nextCol]);
+//                     }
+//                     dp[row][col] = grid[row][col] + ans;
+//                 }
+//             }
+//         }
+//         int ans = Integer.MAX_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             ans = Math.min(ans, dp[0][i]);
+//         }
+//         return ans;
+//     }
+// }
+
+
 
 // class Solution {
 //     public int[][] dp = new int[201][201];
