2ffb948c2894a28bb88d78f23a9d3c91615ad557
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 16:49:12 +0530

    Time: 11 ms (71.8%), Space: 13 MB (82.47%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,76 +1,56 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int row = matrix.size(), col = matrix[0].size();
-        
-        for (int i = 1; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        int n = matrix.size();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 int a = INT_MAX, b = INT_MAX;
                 if (j - 1 >= 0) {
                     a = matrix[i-1][j-1];
                 }
-                if (j + 1 < row) {
+                if (j + 1 < n) {
                     b = matrix[i-1][j+1];
                 }
-                matrix[i][j] += min({matrix[i-1][j], a, b});
+                matrix[i][j] += min(matrix[i-1][j], min({a, b}));
             }
         }
-        
-        int result = INT_MAX;
-        
-        for (int i = 0; i < col; i++) {
-            result = min(result, matrix[row - 1][i]);
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, matrix[n-1][i]);
         }
-        return result;
+        return ans;
     }
 };
 
 // class Solution {
 // public:
-//     int n;
-//     int result[101][101];
-    
-//     int minPath(vector<vector<int>>& matrix, int row, int col) {
+//     int dp[101][101];
+//     int solve (vector<vector<int>>& matrix, int n, int row, int col) {
 //         if (row == n - 1) {
 //             return matrix[row][col];
 //         }
-        
-//         if (result[row][col] != -1) {
-//             return result[row][col];
+//         if (dp[row][col] != -1) {
+//             return dp[row][col];
 //         }
-        
-//         int sum = matrix[row][col];
-//         int minSum = INT_MAX;
-
-//         for (int shift = -1; shift <= 1; shift++) {
-//              if (row + 1 < n && col + shift < n && col + shift >= 0) {
-//                  minSum = min(minSum, sum minPath(matrix, row + 1, col + shift));
-//              }
+//         int ans = INT_MAX;
+//         if (row + 1 < n && col - 1 >= 0) {
+//             ans = min(ans, matrix[row][col] + solve(matrix, n, row + 1, col - 1));
 //         }
-        
-//         // if (row + 1 < n && col - 1 >= 0)
-//         //     minSum = min(minSum, sum + minPath(matrix, row+1, col-1));
-//         // if (row + 1 < n)
-//         //     minSum = min(minSum, sum + minPath(matrix, row+1, col));
-//         // if (row + 1 < n && col + 1 < n)
-//         //     minSum = min(minSum, sum + minPath(matrix, row+1, col+1));        
-        
-//         if (row + 1 < n)
-//             return result[row][col] = minSum;
-//         else
-//             return result[row][col] = sum;
+//         if (row + 1 < n) {
+//             ans = min(ans, matrix[row][col] + solve(matrix, n, row + 1, col));
+//         }
+//         if (row + 1 < n && col + 1 < n) {
+//             ans = min(ans, matrix[row][col] + solve(matrix, n, row + 1, col + 1));
+//         }
+//         return dp[row][col] = ans;
 //     }
-    
 //     int minFallingPathSum(vector<vector<int>>& matrix) {
-//         int minCost = INT_MAX;
-//         n = matrix.size();
-//         int row = 0;
-//         memset(result, -1, sizeof(result));
-        
-//         for (int col = 0; col < n; col++) {
-//             minCost = min(minCost, minPath(matrix, row, col));
+//         int n = matrix.size();
+//         int ans = INT_MAX;
+//         memset(dp, -1, sizeof(dp));
+//         for (int i = 0; i < n; i++) {
+//             ans = min(ans, solve(matrix, n, 0, i));
 //         }
-        
-//         return minCost;
+//         return ans;
 //     }
 // };
