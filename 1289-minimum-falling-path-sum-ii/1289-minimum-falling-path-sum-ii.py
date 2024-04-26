0dab27cfd2aeb9d9b8865bec04fc95b3261df4e0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 17:41:29 +0530

    Time: 3768 ms (28.32%), Space: 20.1 MB (34.96%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,27 +1,45 @@
 class Solution:
-    def __init__(self):
-        self.dp = [[-1] * 201 for _ in range(201)]
-
-    def solve(self, grid, n, level, col):
-        if level == n - 1:
-            self.dp[level][col] = grid[level][col]
-            return self.dp[level][col]
-        
-        if self.dp[level][col] != -1:
-            return self.dp[level][col]
-        
-        ans = float('inf')
-        for j in range(n):
-            if col == j:
-                continue
-            ans = min(ans, grid[level][col] + self.solve(grid, n, level + 1, j))
-        
-        self.dp[level][col] = ans
-        return self.dp[level][col]
-
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        ans = float('inf')
-        for j in range(n):
-            ans = min(ans, self.solve(grid, n, 0, j))
+        dp = [[0] * (n+1) for _ in range (n+1)]
+        for i in range(n):
+            dp[n-1][i] = grid[n-1][i]
+        for row in range(n-2, -1, -1):
+            for col in range(0, n):
+                ans = float(inf)
+                for nextCol in range(0, n):
+                    if nextCol != col:
+                        ans = min(ans, dp[row+1][nextCol])
+                dp[row][col] = ans + grid[row][col]
+        ans = float(inf)
+        for i in range(n):
+            ans = min(ans, dp[0][i])
         return ans
+
+# class Solution:
+#     def __init__(self):
+#         self.dp = [[-1] * 201 for _ in range(201)]
+
+#     def solve(self, grid, n, level, col):
+#         if level == n - 1:
+#             self.dp[level][col] = grid[level][col]
+#             return self.dp[level][col]
+        
+#         if self.dp[level][col] != -1:
+#             return self.dp[level][col]
+        
+#         ans = float('inf')
+#         for j in range(n):
+#             if col == j:
+#                 continue
+#             ans = min(ans, grid[level][col] + self.solve(grid, n, level + 1, j))
+        
+#         self.dp[level][col] = ans
+#         return self.dp[level][col]
+
+#     def minFallingPathSum(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+#         ans = float('inf')
+#         for j in range(n):
+#             ans = min(ans, self.solve(grid, n, 0, j))
+#         return ans
