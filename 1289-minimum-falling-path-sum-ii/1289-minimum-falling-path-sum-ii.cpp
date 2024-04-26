4fbfdc7e2a5d3f04fd3e30091d4e3e473dc74a49
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 12:40:41 +0530

    Time: 292 ms (39.38%), Space: 17.1 MB (74.85%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    int dp[201][201];
+    int solve(vector<vector<int>>& grid, int n, int level, int col) {
+        if (level == n - 1) {
+            return dp[level][col] = grid[level][col];
+        }
+        if (dp[level][col] != -1) {
+            return dp[level][col];
+        }
+        int ans = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            if (col == j) {
+                continue;
+            }
+            ans = min(ans, grid[level][col] + solve(grid, n, level+1, j));
+        }
+        return dp[level][col] = ans;
+    }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        long long ans = 0;
-        int prevj = -1;
-        for (int i = 0; i < n; i++) {
-            int temp = INT_MAX;
-            for (int j = 0; j < n; j++) {
-                if (j == prevj) {
-                    continue;
-                }
-                if (temp > grid[i][j]) {
-                    temp = grid[i][j];
-                    prevj = j;
-                }
-            }
-            ans += temp;
+        int ans = INT_MAX;
+        memset(dp, -1, sizeof(dp));
+        for (int j = 0; j < n; j++) {
+            ans = min(ans, solve(grid, n, 0, j));
         }
         return ans;
     }
