49c466b46cc2947fc600d338dbe69d07a694b840
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-26 17:59:44 +0530

    Time: 21 ms (95.32%), Space: 16.8 MB (95.32%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,51 +1,121 @@
 class Solution {
-public:
+public:    
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        int dp[n+1][n+1];
 
-        int nextMinCol1 = -1, nextMinCol2 = -1;
+        int nextMin1Col = -1;
+        int nextMin2Col = -1;
         
-        for (int i = 0; i < n; i++) {
-            dp[n-1][i] = grid[n-1][i];  
+        int nextMin1Val = -1;
+        int nextMin2Val = -1;
+        
+        for (int col = 0; col < n; col++) {
 
-            if (nextMinCol1 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol1]) {
-                nextMinCol2 = nextMinCol1;
-                nextMinCol1 = i;
-            } else if (nextMinCol2 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol2]) {
-                nextMinCol2 = i;
+            if(nextMin1Col == -1 || grid[n - 1][col] <= nextMin1Val) {
+                nextMin2Col = nextMin1Col;
+                nextMin2Val = nextMin1Val;
+                
+                nextMin1Col = col;
+                nextMin1Val = grid[n-1][col];
+                
+            } else if(nextMin2Col == -1 || grid[n - 1][col] <= nextMin2Val) {
+                nextMin2Col = col;
+                nextMin2Val = grid[n-1][col];
             }
         }
 
         for (int row = n - 2; row >= 0; row--) {
-
-            int minCol1 = -1, minCol2 = -1;
+            
+            int min1Col = -1;
+            int min2Col = -1;
+            
+            int min1Val = -1;
+            int min2Val = -1;
             
             for (int col = 0; col < n; col++) {
                 
-                if (col == nextMinCol1) {
-                    dp[row][col] = grid[row][col] + dp[row+1][nextMinCol2];
+                int ans;
+                if(col != nextMin1Col) {
+                    ans = grid[row][col] + nextMin1Val;
                 } else {
-                    dp[row][col] = grid[row][col] + dp[row+1][nextMinCol1];
+                    ans = grid[row][col] + nextMin2Val;
                 }
-
-                if (minCol1 == -1 || dp[row][col] <= dp[row][minCol1]) {
-                    minCol2 = minCol1;
-                    minCol1 = col;
-                } else if (minCol2 == -1 || dp[row][col] <= dp[row][minCol2]) {
-                    minCol2 = col;
-                }                
+                
+                if(min1Col == -1 || ans <= min1Val) {
+                    min2Col = min1Col;
+                    min2Val = min1Val;
+                    
+                    min1Col = col;
+                    min1Val = ans;
+                } else if(min2Col == -1 || ans <= min2Val) {
+                    min2Col = col;
+                    min2Val = ans;
+                }
             }
-            nextMinCol1 = minCol1;
-            nextMinCol2 = minCol2;
+                            
+            nextMin1Col = min1Col;
+            nextMin1Val = min1Val;
+            
+            nextMin2Col = min2Col;
+            nextMin2Val = min2Val;
+                
         }
-        int ans = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            ans = min(ans, dp[0][i]);
-        }
-        return ans;
+
+        return nextMin1Val;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         int dp[n+1][n+1];
+
+//         int nextMinCol1 = -1, nextMinCol2 = -1;
+        
+//         for (int i = 0; i < n; i++) {
+//             dp[n-1][i] = grid[n-1][i];  
+
+//             if (nextMinCol1 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol1]) {
+//                 nextMinCol2 = nextMinCol1;
+//                 nextMinCol1 = i;
+//             } else if (nextMinCol2 == -1 || dp[n-1][i] <= dp[n-1][nextMinCol2]) {
+//                 nextMinCol2 = i;
+//             }
+//         }
+
+//         for (int row = n - 2; row >= 0; row--) {
+
+//             int minCol1 = -1, minCol2 = -1;
+            
+//             for (int col = 0; col < n; col++) {
+                
+//                 if (col == nextMinCol1) {
+//                     dp[row][col] = grid[row][col] + dp[row+1][nextMinCol2];
+//                 } else {
+//                     dp[row][col] = grid[row][col] + dp[row+1][nextMinCol1];
+//                 }
+
+//                 if (minCol1 == -1 || dp[row][col] <= dp[row][minCol1]) {
+//                     minCol2 = minCol1;
+//                     minCol1 = col;
+//                 } else if (minCol2 == -1 || dp[row][col] <= dp[row][minCol2]) {
+//                     minCol2 = col;
+//                 }                
+//             }
+//             nextMinCol1 = minCol1;
+//             nextMinCol2 = minCol2;
+//         }
+//         int ans = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             ans = min(ans, dp[0][i]);
+//         }
+//         return ans;
+//     }
+// };
 
 
 
