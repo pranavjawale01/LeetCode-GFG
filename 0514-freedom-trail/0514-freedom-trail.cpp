5b33799a8084b22b6779517e478b2248a0efc9b7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-27 22:31:43 +0530

    Time: 150 ms (21.46%), Space: 13.4 MB (72.41%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,29 +1,59 @@
 class Solution {
 public:
-    int dp[101][101];
     int countSteps(int ringIndex, int i, int n) {
-        int dist   = abs(i - ringIndex);
+        int dist = abs(i - ringIndex);
         int wrapAround = n - dist;
         return min(dist, wrapAround);
     }
-    int solve(int ringIndex, int keyIndex, string &ring, string &key) {
-        if (keyIndex == key.length()) {
-            return 0;
+    int findRotateSteps(string ring, string key) {
+        int n = ring.length(), m = key.length();
+        vector<vector<int>> dp(n, vector<int>(m+1, INT_MAX));
+        for (int i = 0; i < n; i++) {
+            dp[i][m] = 0;
         }
-        if (dp[ringIndex][keyIndex] != -1) {
-            return dp[ringIndex][keyIndex];
-        }
-        int result = INT_MAX;
-        for (int i = 0; i < ring.length(); i++) {
-            if (ring[i] == key[keyIndex]) {
-                int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 + solve(i, keyIndex+1, ring, key);
-                result = min(result, totalSteps);
+        for (int keyIndex = m - 1; keyIndex >= 0; keyIndex--) {
+            for (int ringIndex = 0; ringIndex < n; ringIndex++) {
+                int result = INT_MAX;
+                for (int i = 0; i < n; i++) {
+                    if (ring[i] == key[keyIndex]) {
+                        int totalSteps = countSteps(ringIndex, i, n) + 1 + dp[i][keyIndex+1];
+                        result = min(result, totalSteps);
+                    }
+                }
+                dp[ringIndex][keyIndex] = result;
             }
         }
-        return dp[ringIndex][keyIndex] = result;
-    }
-    int findRotateSteps(string ring, string key) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, ring, key); 
+        return dp[0][0];
     }
 };
+
+
+// class Solution {
+// public:
+//     int dp[101][101];
+//     int countSteps(int ringIndex, int i, int n) {
+//         int dist   = abs(i - ringIndex);
+//         int wrapAround = n - dist;
+//         return min(dist, wrapAround);
+//     }
+//     int solve(int ringIndex, int keyIndex, string &ring, string &key) {
+//         if (keyIndex == key.length()) {
+//             return 0;
+//         }
+//         if (dp[ringIndex][keyIndex] != -1) {
+//             return dp[ringIndex][keyIndex];
+//         }
+//         int result = INT_MAX;
+//         for (int i = 0; i < ring.length(); i++) {
+//             if (ring[i] == key[keyIndex]) {
+//                 int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 + solve(i, keyIndex+1, ring, key);
+//                 result = min(result, totalSteps);
+//             }
+//         }
+//         return dp[ringIndex][keyIndex] = result;
+//     }
+//     int findRotateSteps(string ring, string key) {
+//         memset(dp, -1, sizeof(dp));
+//         return solve(0, 0, ring, key); 
+//     }
+// };
