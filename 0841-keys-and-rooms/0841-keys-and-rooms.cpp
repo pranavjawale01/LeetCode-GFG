1a01625d989691718e0c2c738f728f4dc15e7672
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-28 15:59:57 +0530

    Time: 11 ms (25.44%), Space: 13.6 MB (74.12%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& rooms, vector<bool> &visited, int i) {
-        if (visited[i] == true) {
-            return;
-        }
-        visited[i] = true;
-        for (int room : rooms[i]) {
-            solve(rooms, visited, room);
-        }
-    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
+        queue<int> q;
         vector<bool> visited(n, false);
         visited[0] = true;
         for (int room : rooms[0]) {
-            solve(rooms, visited, room);
+            q.push(room);
         }
-
+        while (!q.empty()) {
+            int temp = q.front();
+            q.pop();
+            visited[temp] = true;
+            for (int room : rooms[temp]) {
+                if (!visited[room]) {
+                    q.push(room);
+                }
+            }
+        }
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 return false;
@@ -25,3 +26,32 @@ public:
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     void solve(vector<vector<int>>& rooms, vector<bool> &visited, int i) {
+//         if (visited[i] == true) {
+//             return;
+//         }
+//         visited[i] = true;
+//         for (int room : rooms[i]) {
+//             solve(rooms, visited, room);
+//         }
+//     }
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         int n = rooms.size();
+//         vector<bool> visited(n, false);
+//         visited[0] = true;
+//         for (int room : rooms[0]) {
+//             solve(rooms, visited, room);
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
