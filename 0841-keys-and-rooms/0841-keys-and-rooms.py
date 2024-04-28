f902bd4a070317c884f61cf91b38dcc8595d29e7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-28 16:02:57 +0530

    Time: 66 ms (53.92%), Space: 17 MB (93.5%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,18 +1,37 @@
 class Solution:
-    def solve(self, rooms: List[List[int]], visited: List[bool], i: int):
-        if visited[i]:
-            return
-        visited[i] = True
-        for room in rooms[i]:
-            self.solve(rooms, visited, room)
-
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
+        q = deque()
+        for room in rooms[0]:
+            q.append(room)
         visited = [False] * n
         visited[0] = True
-        for room in rooms[0]:
-            self.solve(rooms, visited, room)
+        while q:
+            temp = q.popleft()
+            visited[temp] = True
+            for room in rooms[temp]:
+                if not visited[room]:
+                    q.append(room)
         for i in range(n):
-            if visited[i] is False:
+            if visited[i] == False:
                 return False
         return True
+
+# class Solution:
+#     def solve(self, rooms: List[List[int]], visited: List[bool], i: int):
+#         if visited[i]:
+#             return
+#         visited[i] = True
+#         for room in rooms[i]:
+#             self.solve(rooms, visited, room)
+
+#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+#         n = len(rooms)
+#         visited = [False] * n
+#         visited[0] = True
+#         for room in rooms[0]:
+#             self.solve(rooms, visited, room)
+#         for i in range(n):
+#             if visited[i] is False:
+#                 return False
+#         return True
