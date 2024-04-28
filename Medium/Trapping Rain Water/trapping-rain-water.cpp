7c095b02afb277230c2165227f1b713a782bae9c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-28 16:38:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int height[], int n){
+        // code here
+        long long ans = 0;
+        vector<int> left(n, 0), right(n, 0);
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+        for (int i = 1; i < n; i++) {
+            left[i] = max(left[i - 1], height[i]);
+            right[n - i - 1] = max(right[n - i], height[n - i - 1]);
+        }
+        for (int i = 0; i < n; i++) {
+            ans += max(0, min(right[i], left[i]) - height[i]);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
