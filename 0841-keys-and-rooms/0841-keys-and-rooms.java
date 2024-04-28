1574a5386093d0ea1755d5b1d95afc227a79c40f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-28 16:02:48 +0530

    Time: 3 ms (28.02%), Space: 44.5 MB (12.54%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,19 +1,20 @@
 class Solution {
-    public void solve(List<List<Integer>> rooms, boolean[] visited, int i) {
-        if (visited[i] == true) {
-            return;
-        }
-        visited[i] = true;
-        for (int room : rooms.get(i)) {
-            solve(rooms, visited, room);
-        }
-    }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] visited = new boolean[n];
         visited[0] = true;
+        Queue<Integer> q = new LinkedList<>();
         for (int room : rooms.get(0)) {
-            solve(rooms, visited, room);
+            q.offer(room);
+        }
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            visited[temp] = true; 
+            for (int room : rooms.get(temp)) {
+                if (visited[room] == false) {
+                    q.offer(room);
+                }
+            }
         }
         for (int i = 0; i < n; i++) {
             if (visited[i] == false) {
@@ -23,3 +24,30 @@ class Solution {
         return true;
     }
 }
+
+
+// class Solution {
+//     public void solve(List<List<Integer>> rooms, boolean[] visited, int i) {
+//         if (visited[i] == true) {
+//             return;
+//         }
+//         visited[i] = true;
+//         for (int room : rooms.get(i)) {
+//             solve(rooms, visited, room);
+//         }
+//     }
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         int n = rooms.size();
+//         boolean[] visited = new boolean[n];
+//         visited[0] = true;
+//         for (int room : rooms.get(0)) {
+//             solve(rooms, visited, room);
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (visited[i] == false) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
