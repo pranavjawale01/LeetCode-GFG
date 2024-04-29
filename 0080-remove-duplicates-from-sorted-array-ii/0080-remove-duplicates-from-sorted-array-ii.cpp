9aa5d2e6cbd112ce62a85f07364547d0a65dded1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-29 23:02:55 +0530

    Time: 10 ms (6.94%), Space: 15.3 MB (6.01%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    void swapElements(vector<int>& nums, int start, int& n, int num) {
-        int i;
-        for (i = start; i < n - 1; i++) {
-            nums[i] = nums[i+1];
-        }
-        nums[i] = num;
-        n--;
-    }
-    
     int removeDuplicates(vector<int>& nums) {
+        int count = 0;
+        unordered_map<int, int> mp;
         int n = nums.size();
-        int count = n;
-        for (int i = 0 ; i < n - 2; i++) {
-            if (nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) {
-                swapElements(nums,i,n,nums[i]);
-                count--;
-                i--;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+            if (mp[nums[i]] <= 2) {
+                nums[count++] = nums[i];
             }
         }
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     void swapElements(vector<int>& nums, int start, int& n, int num) {
+//         int i;
+//         for (i = start; i < n - 1; i++) {
+//             nums[i] = nums[i+1];
+//         }
+//         nums[i] = num;
+//         n--;
+//     }
+    
+//     int removeDuplicates(vector<int>& nums) {
+//         int n = nums.size();
+//         int count = n;
+//         for (int i = 0 ; i < n - 2; i++) {
+//             if (nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) {
+//                 swapElements(nums,i,n,nums[i]);
+//                 count--;
+//                 i--;
+//             }
+//         }
+//         return count;
+//     }
+// };
