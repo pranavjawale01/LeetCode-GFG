e58fb807b89e494eab44d2046e84b07655c8a312
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-30 11:59:23 +0530

    Time: 349 ms (52.69%), Space: 20.6 MB (11.98%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
+    typedef long long ll;
     long long wonderfulSubstrings(string word) {
-        vector<int> prefixCount(1024, 0);
-        int prefix = 0, n = word.size();
-        long long result = 0;
-        prefixCount[0] = 1;
-        for (int i = 0; i < n; i++) {
-            int c = word[i] - 'a';
-            prefix ^= (1 << c);
-            result += prefixCount[prefix];
-            prefixCount[prefix]++;
-            for (int bit = 0; bit < 10; bit++) {
-                int diff = (1 << bit) ^ prefix;
-                result += prefixCount[diff];
+        ll ans = 0;
+        unordered_map<ll, ll> mp;
+        mp[0] = 1;
+        int cum_xor = 0;
+        for (char &ch : word) {
+            int shift = ch - 'a';
+            cum_xor ^= (1 << shift);
+            ans += mp[cum_xor];
+            for (char ch1 = 'a'; ch1 <= 'j'; ch1++) {
+                shift = ch1 - 'a';
+                ll check_xor = (cum_xor ^ (1 << shift));
+                ans += mp[check_xor];
             }
+            mp[cum_xor]++;
         }
-        return result;
+        return ans;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int wonderfulSubstrings(string word) {
-//         int n = word.size();
-//         int ans = 0;
-//         for (int i = 0; i < n; i++) {
-//             vector<int> count(10, 0);
-//             for (int j = i; j < n; j++) {
-//                 count[word[j] - 'a']++;
-//                 if (isWonderful(count)) {
-//                     ans++;
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-
-//     bool isWonderful(vector<int> &count) {
-//         int oddCount = 0;
-//         for (int i = 0; i < 10; i++) {
-//             if (count[i] % 2 != 0) {
-//                 oddCount++;
-//             }
-//         }
-//         return oddCount <= 1;
-//     }
-// };
