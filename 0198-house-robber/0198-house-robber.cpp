3eef1cc2b3bd5382002283cf33a5744c2a8cbb5d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-04-30 11:02:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    int recursive(vector<int>& nums, int amount, int house, int memory[]) {
-        if (house > nums.size() - 1) {
-            return 0;
-        }
-        if (memory[house] != -1) {
-            return memory[house];
-        }
-        
-        int selected = nums[house] + recursive(nums, amount, house + 2, memory);
-        int notSelected = recursive(nums, amount, house + 1, memory);
-        
-        return memory[house] = max(selected, notSelected);
-    }
-    
     int rob(vector<int>& nums) {
-        int memory[100];
-        memset(memory, -1, sizeof(memory));
-        int amount = 0, house = 0;
-        return recursive(nums, amount, house, memory);
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        ans[0] = nums[0];
+        ans[1] = nums[1];
+        for (int i = 2; i < n; i++) {
+            ans[i] = max(nums[i] + ans[i-2], ans[i-1]);
+        }
+        return ans[n-1];
     }
 };
+
+// class Solution {
+// public:
+//     int memo[101];
+//     int solve(vector<int> &nums, int house) {
+//         if (house >= nums.size()) {
+//             return 0;
+//         }
+//         if (memo[house] != -1) {
+//             return memo[house];
+//         }
+//         int select = nums[house] + solve(nums, house+2);
+//         int notSelect = solve(nums, house+1);
+//         return memo[house] = max(select, notSelect);
+//     }
+//     int rob(vector<int>& nums) {
+//         memset(memo, -1, sizeof(memo));
+//         return solve(nums, 0);
+//     }
+// };
