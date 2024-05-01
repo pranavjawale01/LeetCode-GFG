58df64d4a11371737fd975f69d68f34f7a2b88a9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-01 12:27:20 +0530

    Time: 0 ms (100%), Space: 57.6 MB (10.64%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,41 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-
-        List<Integer> temp = new ArrayList<>();
-        for (int i = n - k; i < n; i++) {
-            temp.add(nums[i]);
-        }
-
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i - k];
-        }
-
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp.get(i);
+        k = k % nums.length;
+        
+        reverse(nums, 0, nums.length - k);
+        reverse(nums, nums.length - k, nums.length);
+        reverse(nums, 0, nums.length);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end - 1];
+            nums[end - 1] = temp;
+            start++;
+            end--;
         }
     }
 }
+
+
+
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int n = nums.length;
+//         k = k % n;
+
+//         List<Integer> temp = new ArrayList<>();
+//         for (int i = n - k; i < n; i++) {
+//             temp.add(nums[i]);
+//         }
+
+//         for (int i = n - 1; i >= k; i--) {
+//             nums[i] = nums[i - k];
+//         }
+
+//         for (int i = 0; i < k; i++) {
+//             nums[i] = temp.get(i);
+//         }
+//     }
+// }
