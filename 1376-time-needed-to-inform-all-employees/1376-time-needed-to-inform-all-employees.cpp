2b8a809118f35087137deb022b8a5b142dd780cb
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-02 18:29:27 +0530

    Time: 320 ms (24.66%), Space: 166.2 MB (7.53%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -8,17 +8,48 @@ public:
             }
             adj[manager[i]].push_back(i);
         }
-        return dfs(headID, adj, informTime);
+        return bfs(headID, adj, informTime);
     }
     
-    int dfs(int head, unordered_map<int, vector<int>>& adj, vector<int>& informTime) {
-        if (adj[head].empty()) {
-            return 0;
-        }
+    int bfs(int head, unordered_map<int, vector<int>>& adj, vector<int>& informTime) {
+        queue<pair<int, int>> q;
+        q.push({head, 0});
         int max_time = 0;
-        for (int& sub : adj[head]) {
-            max_time = max(max_time, dfs(sub, adj, informTime));
+        while (!q.empty()) {
+            auto [employee, time] = q.front();
+            q.pop();
+            max_time = max(max_time, time);
+            for (int& sub : adj[employee]) {
+                q.push({sub, time + informTime[employee]});
+            }
         }
-        return max_time + informTime[head];
+        return max_time;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+//         unordered_map<int, vector<int>> adj;
+//         for (int i = 0; i < n; i++) {
+//             if (manager[i] == -1) {
+//                 continue;
+//             }
+//             adj[manager[i]].push_back(i);
+//         }
+//         return dfs(headID, adj, informTime);
+//     }
+    
+//     int dfs(int head, unordered_map<int, vector<int>>& adj, vector<int>& informTime) {
+//         if (adj[head].empty()) {
+//             return 0;
+//         }
+//         int max_time = 0;
+//         for (int& sub : adj[head]) {
+//             max_time = max(max_time, dfs(sub, adj, informTime));
+//         }
+//         return max_time + informTime[head];
+//     }
+// };
