7b71214a0da7005a81016b3b5d7553dcec4aa67d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-02 12:23:48 +0530

    Time: 29 ms (17.89%), Space: 26.6 MB (21.19%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int low = 0, high = nums.size() - 1;
-        while (low <= high) {
-            if ((nums[low] * (-1)) == nums[high]) {
-                return nums[high];
-            } else if ((nums[low] * (-1)) < nums[high]) {
-                high--;
-            } else if ((nums[low] * (-1)) > nums[high]) {
-                low++;
+        unordered_set<int> st(nums.begin(), nums.end());
+        int ans = -1;
+        int n = nums.size();
+        for (int num : nums) {
+            if (st.find(-num) != st.end()) {
+                ans = max(ans, abs(num));
             }
         }
-        return -1;
+        return ans != -1 ? ans : -1;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int findMaxK(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int low = 0, high = nums.size() - 1;
+//         while (low <= high) {
+//             if ((nums[low] * (-1)) == nums[high]) {
+//                 return nums[high];
+//             } else if ((nums[low] * (-1)) < nums[high]) {
+//                 high--;
+//             } else if ((nums[low] * (-1)) > nums[high]) {
+//                 low++;
+//             }
+//         }
+//         return -1;
+//     }
+// };
