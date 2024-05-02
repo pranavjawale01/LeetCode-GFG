00bdce1014367ae3d1a622506644ac61f05e9fb6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-02 18:36:41 +0530

    Time: 950 ms (63.92%), Space: 38.7 MB (93.22%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        adj = defaultdict(list)
+        for i, mgr in enumerate(manager):
+            if mgr != -1:
+                adj[mgr].append(i)
+        
+        return self.dfs(headID, adj, informTime)
+    
+    def dfs(self, head_id, adj, informTime):
+        if head_id not in adj:
+            return 0
+        max_time = 0
+        for sub in adj[head_id]:
+            max_time = max(max_time, self.dfs(sub, adj, informTime))
+        return max_time + informTime[head_id]
