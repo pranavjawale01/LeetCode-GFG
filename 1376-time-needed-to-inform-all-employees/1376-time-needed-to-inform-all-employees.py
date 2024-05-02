87cdb9ad9609facd660db25c2a3b7734a26f8978
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-02 18:36:53 +0530

    Time: 920 ms (80.49%), Space: 46.9 MB (62.84%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -5,12 +5,36 @@ class Solution:
             if mgr != -1:
                 adj[mgr].append(i)
         
-        return self.dfs(headID, adj, informTime)
+        return self.bfs(headID, adj, informTime)
     
-    def dfs(self, head_id, adj, informTime):
+    def bfs(self, head_id, adj, informTime):
         if head_id not in adj:
             return 0
         max_time = 0
-        for sub in adj[head_id]:
-            max_time = max(max_time, self.dfs(sub, adj, informTime))
-        return max_time + informTime[head_id]
+        queue = deque([(head_id, 0)])
+        while queue:
+            head, time = queue.popleft()
+            max_time = max(max_time, time)
+            for sub in adj[head]:
+                queue.append((sub, time + informTime[head]))
+        return max_time
+
+
+
+
+# class Solution:
+#     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+#         adj = defaultdict(list)
+#         for i, mgr in enumerate(manager):
+#             if mgr != -1:
+#                 adj[mgr].append(i)
+        
+#         return self.dfs(headID, adj, informTime)
+    
+#     def dfs(self, head_id, adj, informTime):
+#         if head_id not in adj:
+#             return 0
+#         max_time = 0
+#         for sub in adj[head_id]:
+#             max_time = max(max_time, self.dfs(sub, adj, informTime))
+#         return max_time + informTime[head_id]
