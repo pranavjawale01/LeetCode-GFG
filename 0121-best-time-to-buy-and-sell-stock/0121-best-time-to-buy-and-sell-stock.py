e71423f4e74c0aa19cf8bbfd35ee783e78e3d3bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-02 17:54:57 +0530

    Time: 777 ms (32.62%), Space: 27.5 MB (34.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        currMax = 0
-        finalMax = 0
-        n = len(prices)
-        for i in range(1, n):
-            currMax = max(0, currMax + prices[i] - prices[i-1])
-            finalMax = max(finalMax, currMax)
-        return finalMax
+        maxPrice, minPrice = 0, float('inf')
+        for i in range(len(prices)):
+            minPrice = min(minPrice, prices[i])
+            maxPrice = max(maxPrice, prices[i] - minPrice)
+        return maxPrice
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         currMax = 0
+#         finalMax = 0
+#         n = len(prices)
+#         for i in range(1, n):
+#             currMax = max(0, currMax + prices[i] - prices[i-1])
+#             finalMax = max(finalMax, currMax)
+#         return finalMax
