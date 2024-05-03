25d625543d4d6de4e38259b7daef8430ff0a38db
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-03 10:52:34 +0530

    Time: 1 ms (78.87%), Space: 41 MB (95.16%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -31,3 +31,37 @@ class Solution {
         return versionTokens;
     }
 }
+
+// import java.util.StringTokenizer;
+// class Solution {
+//     public int compareVersion(String version1, String version2) {
+//         List<String> v1 = getTokens(version1);
+//         List<String> v2 = getTokens(version2);
+        
+//         int m = v1.size();
+//         int n = v2.size();
+        
+//         int i = 0;
+//         while (i < m || i < n) {
+//             int a = i < m ? Integer.parseInt(v1.get(i)) : 0;
+//             int b = i < n ? Integer.parseInt(v2.get(i)) : 0;
+            
+//             if (a > b)
+//                 return 1;
+//             else if (b > a)
+//                 return -1;
+//             else
+//                 i++;
+//         }
+//         return 0;
+//     }
+    
+//     private List<String> getTokens(String version) {
+//         StringTokenizer tokenizer = new StringTokenizer(version, ".");
+//         List<String> versionTokens = new ArrayList<>();
+//         while (tokenizer.hasMoreTokens()) {
+//             versionTokens.add(tokenizer.nextToken());
+//         }
+//         return versionTokens;
+//     }
+// }
