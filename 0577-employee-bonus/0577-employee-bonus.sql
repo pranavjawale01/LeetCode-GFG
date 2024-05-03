0e460dfd7afbc2f03c338c6ea8486b11c672a157
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-03 10:59:23 +0530

    Time: 2992 ms (6.42%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,5 @@
--- Write your PostgreSQL query statement below
-SELECT e.NAME,b.BONUS FROM 
-employee e LEFT OUTER JOIN bonus b
-ON e.empId =b.empId 
-WHERE b.bonus <1000 OR bonus IS NULL
+# Write your MySQL query statement below
+SELECT name, bonus
+FROM Employee
+LEFT JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE bonus < 1000 OR bonus IS NULL;
