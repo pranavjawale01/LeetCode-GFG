c7948433b003f480dd195651846b1c92e9c3ef5a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-05 00:21:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int maxReach = nums[0];
         int n = nums.size();
-        vector<bool> dp(n, false);
-        dp[0] = true;
-        for (int i = 1; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (dp[j] == true && j + nums[j] >= i) {
-                    dp[i] = true;
-                    break;
-                }
-            }
+        if (n == 1) {
+            return true;
         }
-        return dp[n - 1];
+        for (int i = 1; i < n; i++) {
+            if (maxReach >= n - 1) {
+                return true;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+        }
+        return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<bool> dp(n, false);
+//         dp[0] = true;
+//         for (int i = 1; i < n; i++) {
+//             for (int j = i - 1; j >= 0; j--) {
+//                 if (dp[j] == true && j + nums[j] >= i) {
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[n - 1];
+//     }
+// };
 
 
 // class Solution {
