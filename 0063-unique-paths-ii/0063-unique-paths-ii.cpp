36604c4f2e34871f16bc92fcb028951299b70239
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-04 23:30:39 +0530

    Time: 0 ms (100%), Space: 9.9 MB (96.11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[101][101];
     int solve(vector<vector<int>>& arr, int i, int j) {
         if (i < 0 || j < 0 || i >= arr.size() || j >= arr[0].size() || arr[i][j]) {
             return 0;
@@ -7,9 +8,13 @@ public:
         if (i == arr.size() - 1 && j == arr[0].size() - 1) {
             return 1;
         }
-        return solve(arr, i+1, j) + solve(arr, i, j+1);
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        return dp[i][j] = solve(arr, i+1, j) + solve(arr, i, j+1);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        memset(dp, -1, sizeof(dp));
         return solve(obstacleGrid, 0, 0);
     }
 };
