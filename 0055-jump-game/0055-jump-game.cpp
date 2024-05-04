ba60fc572b046ec4a8a3dab46850b837fe29c4dc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-05 00:19:24 +0530

    Time: 216 ms (16.34%), Space: 51.2 MB (17.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int index, vector<int> &dp) {
-        if (index >= nums.size() - 1) {
-            return true;
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        int curr = nums[index];
-        while (curr > 0) {
-            if (solve(nums, index + curr, dp)) {
-                return dp[index] = true;
-            }
-            curr--;
-        }
-        return dp[index] = false;
-    }
-    
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return solve(nums, 0, dp);
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] == true && j + nums[j] >= i) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n - 1];
     }
 };
+
+
+// class Solution {
+// public:
+//     bool solve(vector<int>& nums, int index, vector<int> &dp) {
+//         if (index >= nums.size() - 1) {
+//             return true;
+//         }
+//         if (dp[index] != -1) {
+//             return dp[index];
+//         }
+//         int curr = nums[index];
+//         while (curr > 0) {
+//             if (solve(nums, index + curr, dp)) {
+//                 return dp[index] = true;
+//             }
+//             curr--;
+//         }
+//         return dp[index] = false;
+//     }
+    
+//     bool canJump(vector<int>& nums) {
+//         vector<int> dp(nums.size(), -1);
+//         return solve(nums, 0, dp);
+//     }
+// };
