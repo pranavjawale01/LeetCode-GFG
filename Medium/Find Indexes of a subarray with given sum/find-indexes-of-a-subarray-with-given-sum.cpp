9a4120208af737f5b0b94670918424217b48005c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 10:45:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Indexes of a subarray with given sum/find-indexes-of-a-subarray-with-given-sum.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> subarraySum(vector<int>& arr, int n, long long s) {
+        vector<int> ans;
+        int left = 0, right = 0;
+        long long sum = 0;
+        while (right < n) {
+            sum += arr[right];
+            while (sum > s && left < right) {
+                sum -= arr[left];
+                left++;
+            }
+            if (sum == s) {
+                return {left + 1, right + 1};
+            } else {
+                right++;
+            }
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
