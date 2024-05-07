92c4234f04933a52a3700817d16dcd67ee83fcee
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 11:59:44 +0530

    Time: 541 ms (17.64%), Space: 60.7 MB (26.84%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -7,10 +7,35 @@
 
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        nextNode = self.removeNodes(head.next)
-        if head.val < nextNode.val:
-            return nextNode
-        head.next = nextNode
-        return head
+        if not head:
+            return None
+        curr = head
+        st = []
+        while curr:
+            st.append(curr)
+            curr = curr.next
+        curr = st.pop()
+        maxNode = curr.val
+        resultHead = ListNode(curr.val)
+        while st:
+            curr = st.pop()
+            if curr.val < maxNode:
+                continue
+            else:
+                newNode = ListNode(curr.val)
+                newNode.next = resultHead
+                resultHead = newNode
+                maxNode = curr.val
+        return resultHead
+
+
+
+# class Solution:
+#     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         nextNode = self.removeNodes(head.next)
+#         if head.val < nextNode.val:
+#             return nextNode
+#         head.next = nextNode
+#         return head
