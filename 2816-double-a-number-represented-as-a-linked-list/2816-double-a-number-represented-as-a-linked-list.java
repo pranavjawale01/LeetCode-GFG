9d1e3aeafdcaf73f11ccd84f058a346795f2abc6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 12:25:06 +0530

    Time: 4 ms (77.89%), Space: 46.7 MB (41.01%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -9,37 +9,61 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public int solve(ListNode head) {
+        if (head == null) {
+            return 0;
         }
-        ListNode last = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-
-        return last;
+        int carry = solve(head.next);
+        int newVal = (head.val) * 2 + carry;
+        head.val = newVal % 10;
+        return newVal / 10;
     }
 
     public ListNode doubleIt(ListNode head) {
-        head = reverse(head);
-        ListNode curr = head;
-        ListNode prev = null;
-        int carry = 0;
-        while (curr != null) {
-            int newValue = curr.val * 2 + carry;
-            curr.val = newValue % 10;
-            if (newValue >= 10) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-            prev = curr;
-            curr = curr.next;
+        int lastCarry = solve(head);
+        if (lastCarry > 0) {
+            ListNode newHead = new ListNode(lastCarry);
+            newHead.next = head;
+            return newHead;
         }
-        if (carry != 0) {
-            ListNode newHead = new ListNode(carry);
-            prev.next = newHead;
-        }
-        return reverse(head);
+        return head;
     }
 }
+
+
+
+// class Solution {
+//     public ListNode reverse(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode last = reverse(head.next);
+//         head.next.next = head;
+//         head.next = null;
+
+//         return last;
+//     }
+
+//     public ListNode doubleIt(ListNode head) {
+//         head = reverse(head);
+//         ListNode curr = head;
+//         ListNode prev = null;
+//         int carry = 0;
+//         while (curr != null) {
+//             int newValue = curr.val * 2 + carry;
+//             curr.val = newValue % 10;
+//             if (newValue >= 10) {
+//                 carry = 1;
+//             } else {
+//                 carry = 0;
+//             }
+//             prev = curr;
+//             curr = curr.next;
+//         }
+//         if (carry != 0) {
+//             ListNode newHead = new ListNode(carry);
+//             prev.next = newHead;
+//         }
+//         return reverse(head);
+//     }
+// }
