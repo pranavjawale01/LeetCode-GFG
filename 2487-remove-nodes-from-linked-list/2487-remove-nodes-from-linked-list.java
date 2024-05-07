92911ad5ccceb73887a6afc6d44532c4d422c3bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 11:55:32 +0530

    Time: 17 ms (41.14%), Space: 75 MB (17.98%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -8,33 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
-    ListNode reverse(ListNode head) {
+    public ListNode removeNodes(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode last = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-        return last;
-    }
-    public ListNode removeNodes(ListNode head) {
-        head = reverse(head);
-        int maxNode = -1;
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            maxNode = Math.max(maxNode, curr.val);
-            if (curr.val < maxNode) {
-                prev.next = curr.next;
-                ListNode temp = curr;
-                curr = curr.next;
-                temp = null;
-            } else {
-                prev = curr;
-                curr = curr.next;
-            }
+        ListNode nextNode = removeNodes(head.next);
+        if (head.val < nextNode.val) {
+            head = null;
+            return nextNode;
         }
-        return reverse(head);
+        head.next = nextNode;
+        return head;
     }
 }
+
+
+
+// class Solution {
+//     ListNode reverse(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode last = reverse(head.next);
+//         head.next.next = head;
+//         head.next = null;
+//         return last;
+//     }
+//     public ListNode removeNodes(ListNode head) {
+//         head = reverse(head);
+//         int maxNode = -1;
+//         ListNode prev = null;
+//         ListNode curr = head;
+//         while (curr != null) {
+//             maxNode = Math.max(maxNode, curr.val);
+//             if (curr.val < maxNode) {
+//                 prev.next = curr.next;
+//                 ListNode temp = curr;
+//                 curr = curr.next;
+//                 temp = null;
+//             } else {
+//                 prev = curr;
+//                 curr = curr.next;
+//             }
+//         }
+//         return reverse(head);
+//     }
+// }
