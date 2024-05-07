754e4e4c06c4cdd908e4d2a76c796a4643970514
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 11:59:50 +0530

    Time: 537 ms (18.15%), Space: 59.7 MB (43.56%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -6,27 +6,60 @@
 
 
 class Solution:
+    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return None
-        curr = head
+        
+        head = self.reverse(head)
         st = []
-        while curr:
-            st.append(curr)
-            curr = curr.next
-        curr = st.pop()
-        maxNode = curr.val
-        resultHead = ListNode(curr.val)
+        current = head
+        while current:
+            if not st or st[-1].val <= current.val:
+                st.append(current)
+            current = current.next
+        
+        newHead = None
         while st:
-            curr = st.pop()
-            if curr.val < maxNode:
-                continue
-            else:
-                newNode = ListNode(curr.val)
-                newNode.next = resultHead
-                resultHead = newNode
-                maxNode = curr.val
-        return resultHead
+            temp = ListNode(st.pop().val)
+            temp.next = newHead
+            newHead = temp
+        
+        return self.reverse(newHead)
+
+
+
+# class Solution:
+#     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head:
+#             return None
+#         curr = head
+#         st = []
+#         while curr:
+#             st.append(curr)
+#             curr = curr.next
+#         curr = st.pop()
+#         maxNode = curr.val
+#         resultHead = ListNode(curr.val)
+#         while st:
+#             curr = st.pop()
+#             if curr.val < maxNode:
+#                 continue
+#             else:
+#                 newNode = ListNode(curr.val)
+#                 newNode.next = resultHead
+#                 resultHead = newNode
+#                 maxNode = curr.val
+#         return resultHead
 
 
 
