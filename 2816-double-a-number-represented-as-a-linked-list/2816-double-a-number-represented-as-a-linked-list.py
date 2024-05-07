9fd8fc79dc75e47391ffeb9cc2ce7d220c58c0f1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-07 12:36:55 +0530

    Time: 241 ms (60.72%), Space: 20.6 MB (17.06%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
@@ -3,29 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-        
-
 class Solution:
-    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        last = self.reverse(head.next)
-        head.next.next = head
-        head.next = None
-        return last
-
+    def solve(self, head: Optional[ListNode]) -> int:
+        if not head:
+            return 0
+        carry = self.solve(head.next)
+        newVal = (head.val) * 2 + carry
+        head.val = newVal % 10
+        return newVal // 10
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        head = self.reverse(head)
-        curr = head
-        prev = None
-        carry = 0
-        while curr:
-            newValue = curr.val * 2 + carry
-            curr.val = newValue % 10
-            carry = 1 if newValue >= 10 else 0
-            prev = curr
-            curr = curr.next
-        if carry != 0:
-            newHead = ListNode(carry)
-            prev.next = newHead
-        return self.reverse(head)
+        lastCarry = self.solve(head)
+        if lastCarry > 0:
+            newHead = ListNode(lastCarry)
+            newHead.next = head
+            return newHead
+        return head       
+
+      
+
+# class Solution:
+#     def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         last = self.reverse(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return last
+
+#     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         head = self.reverse(head)
+#         curr = head
+#         prev = None
+#         carry = 0
+#         while curr:
+#             newValue = curr.val * 2 + carry
+#             curr.val = newValue % 10
+#             carry = 1 if newValue >= 10 else 0
+#             prev = curr
+#             curr = curr.next
+#         if carry != 0:
+#             newHead = ListNode(carry)
+#             prev.next = newHead
+#         return self.reverse(head)
