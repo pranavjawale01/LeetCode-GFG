94eb3671494dd480222270d6966c7a99731fe812
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-08 22:38:37 +0530

    Time: 7 ms (93.93%), Space: 45.8 MB (17.05%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,30 +1,61 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        
         String[] result = new String[n];
-        
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         for (int i = 0; i < n; i++) {
-            mp.put(score[i], i);
+            pq.offer(new int[]{score[i], i});
         }
-        
-        Arrays.sort(score);
-        
-        for (int i = n - 1; i >= 0; i--) {
-            int ath = mp.get(score[i]);
-            if (i == n - 1) {
-                result[ath] = "Gold Medal";
-            } else if (i == n - 2) {
-                result[ath] = "Silver Medal";
-            } else if (i == n - 3) {
-                result[ath] = "Bronze Medal";
+        int rank = 1;
+        while (!pq.isEmpty()) {
+            int[] athlete = pq.poll();
+            int idx = athlete[1];
+            
+            if (rank == 1) {
+                result[idx] = "Gold Medal";
+            } else if (rank == 2) {
+                result[idx] = "Silver Medal";
+            } else if (rank == 3) {
+                result[idx] = "Bronze Medal";
             } else {
-                result[ath] = String.valueOf(n - i);
+                result[idx] = String.valueOf(rank);
             }
+            
+            rank++;
         }
         
         return result;
     }
 }
+
+
+// class Solution {
+//     public String[] findRelativeRanks(int[] score) {
+//         int n = score.length;
+        
+//         String[] result = new String[n];
+        
+//         HashMap<Integer, Integer> mp = new HashMap<>();
+        
+//         for (int i = 0; i < n; i++) {
+//             mp.put(score[i], i);
+//         }
+        
+//         Arrays.sort(score);
+        
+//         for (int i = n - 1; i >= 0; i--) {
+//             int ath = mp.get(score[i]);
+//             if (i == n - 1) {
+//                 result[ath] = "Gold Medal";
+//             } else if (i == n - 2) {
+//                 result[ath] = "Silver Medal";
+//             } else if (i == n - 3) {
+//                 result[ath] = "Bronze Medal";
+//             } else {
+//                 result[ath] = String.valueOf(n - i);
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
