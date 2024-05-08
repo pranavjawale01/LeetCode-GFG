77cfa811b35736782f30dfec8fdd1aab249c15c6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-08 22:41:30 +0530

    Time: 98 ms (28.18%), Space: 18.2 MB (5.59%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,23 +1,53 @@
+from queue import PriorityQueue
+
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         n = len(score)
         result = [None] * n
-        mp = {}
+        pq = PriorityQueue()
         
         for i in range(n):
-            mp[score[i]] = i
+            pq.put((-score[i], i))
         
-        score.sort(reverse=True)
-        
-        for i in range(n):
-            ath = mp[score[i]]
-            if i == 0:
-                result[ath] = "Gold Medal"
-            elif i == 1:
-                result[ath] = "Silver Medal"
-            elif i == 2:
-                result[ath] = "Bronze Medal"
+        rank = 1
+        while not pq.empty():
+            athlete = pq.get()
+            idx = athlete[1]
+            
+            if rank == 1:
+                result[idx] = "Gold Medal"
+            elif rank == 2:
+                result[idx] = "Silver Medal"
+            elif rank == 3:
+                result[idx] = "Bronze Medal"
             else:
-                result[ath] = str(i + 1)
+                result[idx] = str(rank)
+            
+            rank += 1
         
         return result
+
+
+# class Solution:
+#     def findRelativeRanks(self, score: List[int]) -> List[str]:
+#         n = len(score)
+#         result = [None] * n
+#         mp = {}
+        
+#         for i in range(n):
+#             mp[score[i]] = i
+        
+#         score.sort(reverse=True)
+        
+#         for i in range(n):
+#             ath = mp[score[i]]
+#             if i == 0:
+#                 result[ath] = "Gold Medal"
+#             elif i == 1:
+#                 result[ath] = "Silver Medal"
+#             elif i == 2:
+#                 result[ath] = "Bronze Medal"
+#             else:
+#                 result[ath] = str(i + 1)
+        
+#         return result
