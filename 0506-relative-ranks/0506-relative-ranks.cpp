1b3628b114b08a584764ceedfbed1c61b3755caa
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-08 22:42:03 +0530

    Time: 11 ms (46.97%), Space: 14.4 MB (55.66%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -3,28 +3,68 @@ public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
         vector<string> result(n);
-        priority_queue<pair<int, int>> pq;
-        for (int i = 0; i < n; i++) {
-            pq.push({score[i], i});
+        
+        int M = *max_element(begin(score), end(score));
+        
+        vector<int> mp(M+1, -1);
+        
+        for(int i = 0; i < n; i++) {
+            mp[score[i]] = i;
         }
         int rank = 1;
-        while (!pq.empty()) {
-            int idx = pq.top().second;
-            pq.pop();
-            if (rank == 1) {
-                result[idx] = "Gold Medal";
-            } else if(rank == 2) {
-                result[idx] = "Silver Medal";
-            } else if(rank == 3) {
-                result[idx] = "Bronze Medal";
-            } else {
-                result[idx] = to_string(rank);
-            }            
-            rank++;
-        }        
+        
+        for(int s = M; s >= 0; s--) {
+            
+            if(mp[s] != -1) {
+                int athlete = mp[s];
+                
+                if(rank == 1) {
+                    result[athlete] = "Gold Medal";
+                } else if(rank == 2) {
+                    result[athlete] = "Silver Medal";
+                } else if(rank == 3) {
+                    result[athlete] = "Bronze Medal";
+                } else {
+                    result[athlete] = to_string(rank);
+                }
+                
+                rank++;
+            }
+            
+        }
+        
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<string> findRelativeRanks(vector<int>& score) {
+//         int n = score.size();
+//         vector<string> result(n);
+//         priority_queue<pair<int, int>> pq;
+//         for (int i = 0; i < n; i++) {
+//             pq.push({score[i], i});
+//         }
+//         int rank = 1;
+//         while (!pq.empty()) {
+//             int idx = pq.top().second;
+//             pq.pop();
+//             if (rank == 1) {
+//                 result[idx] = "Gold Medal";
+//             } else if(rank == 2) {
+//                 result[idx] = "Silver Medal";
+//             } else if(rank == 3) {
+//                 result[idx] = "Bronze Medal";
+//             } else {
+//                 result[idx] = to_string(rank);
+//             }            
+//             rank++;
+//         }        
+//         return result;
+//     }
+// };
 
 
 // class Solution {
