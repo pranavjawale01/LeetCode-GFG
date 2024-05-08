e04018b245bb8468242e9d4ca8de16abd90f824f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-08 22:43:21 +0530

    Time: 5 ms (97%), Space: 45.1 MB (95.49%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -2,31 +2,68 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
         String[] result = new String[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        
+        int M = Arrays.stream(score).max().getAsInt();
+        
+        int[] mp = new int[M + 1];
+        Arrays.fill(mp, -1);
+        
         for (int i = 0; i < n; i++) {
-            pq.offer(new int[]{score[i], i});
+            mp[score[i]] = i;
         }
+        
         int rank = 1;
-        while (!pq.isEmpty()) {
-            int[] athlete = pq.poll();
-            int idx = athlete[1];
-            
-            if (rank == 1) {
-                result[idx] = "Gold Medal";
-            } else if (rank == 2) {
-                result[idx] = "Silver Medal";
-            } else if (rank == 3) {
-                result[idx] = "Bronze Medal";
-            } else {
-                result[idx] = String.valueOf(rank);
+        
+        for (int s = M; s >= 0; s--) {
+            if (mp[s] != -1) {
+                int athlete = mp[s];
+                if (rank == 1) {
+                    result[athlete] = "Gold Medal";
+                } else if (rank == 2) {
+                    result[athlete] = "Silver Medal";
+                } else if (rank == 3) {
+                    result[athlete] = "Bronze Medal";
+                } else {
+                    result[athlete] = String.valueOf(rank);
+                }
+                rank++;
             }
-            
-            rank++;
         }
         
         return result;
     }
 }
+
+
+// class Solution {
+//     public String[] findRelativeRanks(int[] score) {
+//         int n = score.length;
+//         String[] result = new String[n];
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+//         for (int i = 0; i < n; i++) {
+//             pq.offer(new int[]{score[i], i});
+//         }
+//         int rank = 1;
+//         while (!pq.isEmpty()) {
+//             int[] athlete = pq.poll();
+//             int idx = athlete[1];
+            
+//             if (rank == 1) {
+//                 result[idx] = "Gold Medal";
+//             } else if (rank == 2) {
+//                 result[idx] = "Silver Medal";
+//             } else if (rank == 3) {
+//                 result[idx] = "Bronze Medal";
+//             } else {
+//                 result[idx] = String.valueOf(rank);
+//             }
+            
+//             rank++;
+//         }
+        
+//         return result;
+//     }
+// }
 
 
 // class Solution {
