61b64661928c8e303c2fc19f13b7cd5a162dba4a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-08 22:33:23 +0530

    Time: 7 ms (78.51%), Space: 13.6 MB (89.04%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -2,27 +2,56 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
-        vector<string> ans(n);
-        unordered_map<int, int> mp;
+        vector<string> result(n);
+        priority_queue<pair<int, int>> pq;
         for (int i = 0; i < n; i++) {
-            mp[score[i]] = i;
+            pq.push({score[i], i});
         }
-        sort(score.begin(), score.end(), greater<int>());
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                int ath = mp[score[i]];
-                ans[ath] = "Gold Medal";
-            } else if (i == 1) {
-                int ath = mp[score[i]];
-                ans[ath] = "Silver Medal";
-            } else if(i == 2) {
-                int ath = mp[score[i]];
-                ans[ath] = "Bronze Medal";
+        int rank = 1;
+        while (!pq.empty()) {
+            int idx = pq.top().second;
+            pq.pop();
+            if (rank == 1) {
+                result[idx] = "Gold Medal";
+            } else if(rank == 2) {
+                result[idx] = "Silver Medal";
+            } else if(rank == 3) {
+                result[idx] = "Bronze Medal";
             } else {
-                int ath = mp[score[i]];
-                ans[ath] = to_string(i+1);
-            }
-        }
-        return ans;
+                result[idx] = to_string(rank);
+            }            
+            rank++;
+        }        
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<string> findRelativeRanks(vector<int>& score) {
+//         int n = score.size();
+//         vector<string> ans(n);
+//         unordered_map<int, int> mp;
+//         for (int i = 0; i < n; i++) {
+//             mp[score[i]] = i;
+//         }
+//         sort(score.begin(), score.end(), greater<int>());
+//         for (int i = 0; i < n; i++) {
+//             if (i == 0) {
+//                 int ath = mp[score[i]];
+//                 ans[ath] = "Gold Medal";
+//             } else if (i == 1) {
+//                 int ath = mp[score[i]];
+//                 ans[ath] = "Silver Medal";
+//             } else if(i == 2) {
+//                 int ath = mp[score[i]];
+//                 ans[ath] = "Bronze Medal";
+//             } else {
+//                 int ath = mp[score[i]];
+//                 ans[ath] = to_string(i+1);
+//             }
+//         }
+//         return ans;
+//     }
+// };
