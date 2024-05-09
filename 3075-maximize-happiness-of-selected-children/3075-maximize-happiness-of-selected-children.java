cbedd0ccc78a71cbcb8824676e9a9b8dc1ea94aa
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-09 10:38:21 +0530

    Time: 148 ms (6.39%), Space: 74.8 MB (5.49%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,15 +1,37 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        Arrays.sort(happiness);
-        long sum = 0;
-        int n = happiness.length;
-        for (int i = n - 1, j = 0; i >=  n - k; i--, j++) {
-            if (happiness[i] - j > 0) {
-                sum += happiness[i] - j;
-            } else {
-                return sum;
-            }
+        long result = 0;
+        int count = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int hap : happiness) {
+            pq.offer(hap);
         }
-        return sum;
+
+        for (int i = 0; i < k; i++) {
+            int hap = pq.poll();
+            result += Math.max(hap - count, 0);
+            count++;
+        }
+
+        return result;
     }
 }
+
+
+// class Solution {
+//     public long maximumHappinessSum(int[] happiness, int k) {
+//         Arrays.sort(happiness);
+//         long sum = 0;
+//         int n = happiness.length;
+//         for (int i = n - 1, j = 0; i >=  n - k; i--, j++) {
+//             if (happiness[i] - j > 0) {
+//                 sum += happiness[i] - j;
+//             } else {
+//                 return sum;
+//             }
+//         }
+//         return sum;
+//     }
+// }
