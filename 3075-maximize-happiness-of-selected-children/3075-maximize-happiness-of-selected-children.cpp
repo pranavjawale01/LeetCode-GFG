da7195ca31c40b36c5761fbdf61b6f275e9ed79c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-09 10:36:46 +0530

    Time: 171 ms (35.72%), Space: 116.7 MB (7.79%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        sort(happiness.begin(), happiness.end(), greater<int>());
+        priority_queue<int> pq;
+        for (int x : happiness) {
+            pq.push(x);
+        }
         long long sum = 0;
-        int n = happiness.size();
-        for (int i = 0; i < k; i++) {
-            if (happiness[i] - i > 0) {
-                sum += happiness[i] - i;
-            } else {
+        int i = 0;
+        while (k--) {
+            if (pq.top() - i <= 0) {
                 return sum;
             }
+            sum += pq.top() - i;
+            pq.pop();
+            i++;
         }
         return sum;
     }
 };
+
+
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//         sort(happiness.begin(), happiness.end(), greater<int>());
+//         long long sum = 0;
+//         for (int i = 0; i < k; i++) {
+//             if (happiness[i] - i > 0) {
+//                 sum += happiness[i] - i;
+//             } else {
+//                 return sum;
+//             }
+//         }
+//         return sum;
+//     }
+// };
