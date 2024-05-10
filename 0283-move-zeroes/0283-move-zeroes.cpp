29af990e216e8e8900e4ea84b5c17d40e6c47e72
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 11:56:17 +0530

    Time: 17 ms (45.14%), Space: 21.7 MB (37.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k = 0;
-        int count = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                count++;
-            } else {
-                nums[k++] = nums[i];
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
             }
-        }
-        for (int i = n - count; i < n; i++) {
-            nums[i] = 0;
         }
     }
 };
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int k = 0;
+//         int count = 0;
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == 0) {
+//                 count++;
+//             } else {
+//                 nums[k++] = nums[i];
+//             }
+//         }
+//         for (int i = n - count; i < n; i++) {
+//             nums[i] = 0;
+//         }
+//     }
+// };
