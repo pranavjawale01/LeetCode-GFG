804241ca138e16b2a6d0d696e37fdbfc2b1ebcae
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 12:10:40 +0530

    Time: 1072 ms (45.08%), Space: 70.1 MB (41.02%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -3,11 +3,26 @@ class Solution:
         pq = []
         n = len(arr)
         for i in range(n):
-            for j in range(n):
-                if i != j:
-                    heapq.heappush(pq, (-arr[i] / arr[j], arr[i], arr[j]))
-        while k > 1:
-            heapq.heappop(pq)
-            k -= 1
-        fraction = heapq.heappop(pq)
-        return [fraction[2], fraction[1]]
+            for j in range(i + 1, n):
+                div = arr[i] / arr[j]
+                heapq.heappush(pq, (-div, arr[i], arr[j]))
+                if len(pq) > k:
+                    heapq.heappop(pq)
+
+        vec = heapq.heappop(pq)
+        return [vec[1], vec[2]]
+
+
+# class Solution:
+#     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+#         pq = []
+#         n = len(arr)
+#         for i in range(n):
+#             for j in range(n):
+#                 if i != j:
+#                     heapq.heappush(pq, (-arr[i] / arr[j], arr[i], arr[j]))
+#         while k > 1:
+#             heapq.heappop(pq)
+#             k -= 1
+#         fraction = heapq.heappop(pq)
+#         return [fraction[2], fraction[1]]
