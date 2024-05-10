15e00c537767618fca6e5c21c9c90c7c32b25ffd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 11:58:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,31 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0;
-        int count = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                count++;
-            } else {
-                nums[k++] = nums[i];
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                nums[j] = nums[i] + nums[j];
+                nums[i] = nums[j] - nums[i];
+                nums[j] = nums[j] - nums[i];
+                i++;
             }
-        }
-        for (int i = n - count; i < n; i++) {
-            nums[i] = 0;
         }
     }
 }
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int k = 0;
+//         int count = 0;
+//         int n = nums.length;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == 0) {
+//                 count++;
+//             } else {
+//                 nums[k++] = nums[i];
+//             }
+//         }
+//         for (int i = n - count; i < n; i++) {
+//             nums[i] = 0;
+//         }
+//     }
+// }
