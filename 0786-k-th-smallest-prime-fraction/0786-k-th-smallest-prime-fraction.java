9e3eec4708b8c9a95d60ec87ef28fecb062d6399
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 12:09:05 +0530

    Time: 350 ms (58.08%), Space: 71.6 MB (39.3%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -3,16 +3,36 @@ class Solution {
         PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    pq.offer(new double[]{(double)arr[i] / arr[j], arr[i], arr[j]});
-                }
+            for (int j = i + 1; j < n; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+                if (pq.size() > k)
+                    pq.poll();
             }
         }
-        while (--k > 0) {
-            pq.poll();
-        }
-        double[] fraction = pq.poll();
-        return new int[]{(int) fraction[2], (int) fraction[1]};
+
+        double[] vec = pq.peek();
+        return new int[]{(int) vec[1], (int) vec[2]};
     }
 }
+
+
+
+// class Solution {
+//     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+//         PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i != j) {
+//                     pq.offer(new double[]{(double)arr[i] / arr[j], arr[i], arr[j]});
+//                 }
+//             }
+//         }
+//         while (--k > 0) {
+//             pq.poll();
+//         }
+//         double[] fraction = pq.poll();
+//         return new int[]{(int) fraction[2], (int) fraction[1]};
+//     }
+// }
