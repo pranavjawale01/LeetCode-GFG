63bab832bb276cd053db3ea3b6566ce3e6c1e9d2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 11:33:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        priority_queue<pair<float, pair<int, int>>> pq;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    pq.push({arr[i] / arr[j], {arr[i], arr[j]}});
+                }
+            }
+        }
+        while (k-- > 1) {
+            pq.pop();
+        }
+        return {pq.top().second.second, pq.top().second.first};
+    }
+};
