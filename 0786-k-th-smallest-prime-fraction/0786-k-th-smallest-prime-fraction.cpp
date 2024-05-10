c6efb661715e00ce3bb3c1610f44b55068f7d866
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 12:11:20 +0530

    Time: 923 ms (17.41%), Space: 178.5 MB (16.25%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,24 +1,57 @@
 class Solution {
 public:
+    typedef vector<double> V;
+    
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<vector<double>> pq;
         int n = arr.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                double div = (double) arr[i] / arr[j];
-                pq.push(vector<double>{div, (double)arr[i], (double)arr[j]});
-                if(pq.size() > k)
-                    pq.pop();
-            }
-        }
+        priority_queue<V, vector<V>, greater<V>> pq;
+        
+        for(int i = 0; i < n; i++)
+            pq.push({1.0*arr[i]/arr.back(), (double)(i), (double)(n-1)});
+        
+        int smallest = 1;
+        
+        while(smallest < k) {
+            V vec = pq.top();
+            pq.pop();
+            
+            int i = vec[1];
+            int j = vec[2]-1;
 
-        auto vec  = pq.top();
-        vector<int> result(2);
-        result[0] = vec[1];
-        result[1] = vec[2];
-        return result;
+            
+            pq.push({1.0*arr[i]/arr[j], (double)(i), (double)(j)});
+            smallest++;
+        }
+        
+        V vec = pq.top();
+        int i = vec[1];
+        int j = vec[2];
+        return {arr[i], arr[j]};
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+//         priority_queue<vector<double>> pq;
+//         int n = arr.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i+1; j < n; j++) {
+//                 double div = (double) arr[i] / arr[j];
+//                 pq.push(vector<double>{div, (double)arr[i], (double)arr[j]});
+//                 if(pq.size() > k)
+//                     pq.pop();
+//             }
+//         }
+
+//         auto vec  = pq.top();
+//         vector<int> result(2);
+//         result[0] = vec[1];
+//         result[1] = vec[2];
+//         return result;
+//     }
+// };
 
 
 // class Solution {
