788542933c5cff82ec7a0a174e245e8ac242c478
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-10 12:08:24 +0530

    Time: 1115 ms (16.25%), Space: 257.6 MB (10.83%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<pair<float, pair<int, int>>> pq;
+        priority_queue<vector<double>> pq;
         int n = arr.size();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    pq.push({(float)arr[i] / (float)arr[j], {arr[i], arr[j]}});
-                }
+            for (int j = i+1; j < n; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.push(vector<double>{div, (double)arr[i], (double)arr[j]});
+                if(pq.size() > k)
+                    pq.pop();
             }
         }
-        while (k-- > 1) {
-            pq.pop();
-        }
-        return {pq.top().second.second, pq.top().second.first};
+
+        auto vec  = pq.top();
+        vector<int> result(2);
+        result[0] = vec[1];
+        result[1] = vec[2];
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+//         priority_queue<pair<float, pair<int, int>>> pq;
+//         int n = arr.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i != j) {
+//                     pq.push({(float)arr[i] / (float)arr[j], {arr[i], arr[j]}});
+//                 }
+//             }
+//         }
+//         while (k-- > 1) {
+//             pq.pop();
+//         }
+//         return {pq.top().second.second, pq.top().second.first};
+//     }
+// };
