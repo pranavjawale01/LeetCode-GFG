e70043382ee256afc2c8ac4c52f9f47d26d459c1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-11 11:31:32 +0530

    Time: 79 ms (22.09%), Space: 22.3 MB (89.34%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
+    int row , col;
+    int dp[301][301];
+    
+    int solve(vector<vector<char>>& matrix, int i, int j) {
+        if (i < 0 || j < 0 || i >= row || j >= col || matrix[i][j] == '0') {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = 1;
+        ans += min({solve(matrix, i-1, j-1), solve(matrix, i-1, j), solve(matrix, i, j-1)});
+        return dp[i][j] = ans;
+    }
+    
     int maximalSquare(vector<vector<char>>& matrix) {
-        int row = matrix.size(), col = matrix[0].size();
-        vector<vector<int>> dp(row+1, vector<int>(col+1, 0));
+        memset(dp, -1, sizeof(dp));
+        row = matrix.size();
+        col = matrix[0].size();
         int ans = 0;
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
-                if (matrix[i-1][j-1] == '1') {
-                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
-                    ans = max(ans, dp[i][j]);
-                }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                ans = max(ans, solve(matrix, i, j));
             }
         }
-        return ans*ans;
+        return ans * ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maximalSquare(vector<vector<char>>& matrix) {
+//         int row = matrix.size(), col = matrix[0].size();
+//         vector<vector<int>> dp(row+1, vector<int>(col+1, 0));
+//         int ans = 0;
+//         for (int i = 1; i <= row; i++) {
+//             for (int j = 1; j <= col; j++) {
+//                 if (matrix[i-1][j-1] == '1') {
+//                     dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+//                     ans = max(ans, dp[i][j]);
+//                 }
+//             }
+//         }
+//         return ans*ans;
+//     }
+// };
