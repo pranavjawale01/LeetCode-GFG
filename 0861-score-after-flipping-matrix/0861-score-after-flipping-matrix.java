713187ff771172b44fb27be057ed6eb60305c544
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-13 10:02:54 +0530

    Time: 0 ms (100%), Space: 41.7 MB (16.67%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int matrixScore(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if (grid[i][0] == 0) {
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] ^= 1;
+                }
+            }
+        }
+
+        for (int j = 1; j < m; j++) {
+            int colCount = 0;
+            for (int i = 0; i < n; i++) {
+                colCount += grid[i][j];
+            }
+            if (colCount < n - colCount) {
+                for (int i = 0; i < n; i++) {
+                    grid[i][j] ^= 1;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans += grid[i][j] * (1 << (m - 1 - j));
+            }
+        }
+
+        return ans;
+    }
+}
