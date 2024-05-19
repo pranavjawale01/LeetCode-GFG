68839db19372ea90d3b4a969f9e79bb433aa6a15
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-19 12:27:36 +0530

    Time: 195 ms (54%), Space: 129.1 MB (37.56%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        long long sum = 0;
-        int count = 0;
-        int minLoss = INT_MAX;
-        for (long long num : nums) {
-            if ((num ^ k) > num) {
-                count++;
-                sum += (num ^ k);
-            } else {
-                sum += num;
+    typedef long long ll;
+    ll maximumValueSum(vector<int>& nums, int k,vector<vector<int>>& edges) {
+        vector<int> fayda;
+        ll normalSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            fayda.push_back((nums[i] ^ k) - nums[i]);
+            normalSum += 1ll * nums[i];
+        }
+        
+        sort(fayda.begin(), fayda.end(), greater<int>());
+        
+        for (int i = 0; i < fayda.size()-1; i += 2) {
+
+            ll pairSum = fayda[i] + fayda[i + 1];
+            
+            if (pairSum > 0) {
+                normalSum += pairSum;
             }
-            minLoss = min((long long)minLoss, abs(num - (num ^ k)));
         }
-        if (count % 2 == 0) {
-            return sum;
-        }
-        return sum - minLoss;
+        return normalSum;
     }
 };
+
+
+// class Solution {
+// public:
+//     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+//         long long sum = 0;
+//         int count = 0;
+//         int minLoss = INT_MAX;
+//         for (long long num : nums) {
+//             if ((num ^ k) > num) {
+//                 count++;
+//                 sum += (num ^ k);
+//             } else {
+//                 sum += num;
+//             }
+//             minLoss = min((long long)minLoss, abs(num - (num ^ k)));
+//         }
+//         if (count % 2 == 0) {
+//             return sum;
+//         }
+//         return sum - minLoss;
+//     }
+// };
