75e990ded0f7473185d46d2aa34ae05bad144d6c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-19 12:30:37 +0530

    Time: 34 ms (7.63%), Space: 55.5 MB (17.56%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -1,22 +1,46 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long sum = 0;
-        int count = 0;
-        int minLoss = Integer.MAX_VALUE;
-        
+        List<Integer> fayda = new ArrayList<>();
+        long normalSum = 0;
+
         for (int num : nums) {
-            if ((num ^ k) > num) {
-                count++;
-                sum += (num ^ k);
-            } else {
-                sum += num;
+            fayda.add((num ^ k) - num);
+            normalSum += (long) num;
+        }
+
+        Collections.sort(fayda, Collections.reverseOrder());
+
+        for (int i = 0; i < fayda.size() - 1; i += 2) {
+            long pairSum = fayda.get(i) + fayda.get(i + 1);
+
+            if (pairSum > 0) {
+                normalSum += pairSum;
             }
-            minLoss = Math.min(minLoss, Math.abs(num - (num ^ k)));
         }
-        
-        if (count % 2 == 0) {
-            return sum;
-        }
-        return sum - minLoss;
+        return normalSum;
     }
 }
+
+
+// class Solution {
+//     public long maximumValueSum(int[] nums, int k, int[][] edges) {
+//         long sum = 0;
+//         int count = 0;
+//         int minLoss = Integer.MAX_VALUE;
+        
+//         for (int num : nums) {
+//             if ((num ^ k) > num) {
+//                 count++;
+//                 sum += (num ^ k);
+//             } else {
+//                 sum += num;
+//             }
+//             minLoss = Math.min(minLoss, Math.abs(num - (num ^ k)));
+//         }
+        
+//         if (count % 2 == 0) {
+//             return sum;
+//         }
+//         return sum - minLoss;
+//     }
+// }
