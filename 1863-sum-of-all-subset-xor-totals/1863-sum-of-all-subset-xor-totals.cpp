93158099f59ff903fe448d5f5f51eba6013688e1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-20 11:59:21 +0530

    Time: 4 ms (55.14%), Space: 8.4 MB (65.34%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int i, vector<int> &currSubset, vector<vector<int>> &subsets) {
+    int solve(vector<int> &nums, int i, int xor_val) {
         if (i == nums.size()) {
-            subsets.push_back(currSubset);
-            return;
+            return xor_val;
         }
-        currSubset.push_back(nums[i]);
-        solve(nums, i+1, currSubset, subsets);
-        currSubset.pop_back();
-        solve(nums, i+1, currSubset, subsets);
+        return solve(nums, i+1, nums[i] ^ xor_val) + solve(nums, i+1, xor_val);
     }
 
     int subsetXORSum(vector<int>& nums) {
-        vector<vector<int>> subsets;
-        vector<int> currSubset;
-        solve(nums, 0, currSubset, subsets);
-        int result = 0;
-        for (vector<int> &subset : subsets) {
-            int xorSum = 0;
-            for (int &num : subset) {
-                xorSum ^= num;
-            }
-            result += xorSum;
-        }
-        return result;
+        return solve(nums, 0, 0);
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void solve(vector<int> &nums, int i, vector<int> &currSubset, vector<vector<int>> &subsets) {
+//         if (i == nums.size()) {
+//             subsets.push_back(currSubset);
+//             return;
+//         }
+//         currSubset.push_back(nums[i]);
+//         solve(nums, i+1, currSubset, subsets);
+//         currSubset.pop_back();
+//         solve(nums, i+1, currSubset, subsets);
+//     }
+
+//     int subsetXORSum(vector<int>& nums) {
+//         vector<vector<int>> subsets;
+//         vector<int> currSubset;
+//         solve(nums, 0, currSubset, subsets);
+//         int result = 0;
+//         for (vector<int> &subset : subsets) {
+//             int xorSum = 0;
+//             for (int &num : subset) {
+//                 xorSum ^= num;
+//             }
+//             result += xorSum;
+//         }
+//         return result;
+//     }
+// };
