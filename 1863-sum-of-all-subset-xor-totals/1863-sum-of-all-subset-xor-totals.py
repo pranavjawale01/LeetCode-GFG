e0d51e9cfe6af777f464857c3fe17d3f6ee99e06
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-20 12:04:54 +0530

    Time: 41 ms (78.6%), Space: 16.5 MB (84.04%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,21 +1,33 @@
 class Solution:
-    def solve(self, nums, i, curr_subset, subsets):
+    def solve(self, nums, i, xor_val) -> int:
         if i == len(nums):
-            subsets.append(curr_subset.copy())
-            return
-        curr_subset.append(nums[i])
-        self.solve(nums, i+1, curr_subset, subsets)
-        curr_subset.pop()
-        self.solve(nums, i+1, curr_subset, subsets)
-        
+            return xor_val
+        return self.solve(nums, i+1, nums[i] ^ xor_val) + self.solve(nums, i+1, xor_val)
+
     def subsetXORSum(self, nums: List[int]) -> int:
-        subsets = []
-        curr_subset = []
-        self.solve(nums, 0, curr_subset, subsets)
-        result = 0
-        for subset in subsets:
-            xor_sum = 0
-            for num in subset:
-                xor_sum ^= num
-            result += xor_sum
-        return result
+        return self.solve(nums, 0, 0)
+
+
+
+
+# class Solution:
+#     def solve(self, nums, i, curr_subset, subsets):
+#         if i == len(nums):
+#             subsets.append(curr_subset.copy())
+#             return
+#         curr_subset.append(nums[i])
+#         self.solve(nums, i+1, curr_subset, subsets)
+#         curr_subset.pop()
+#         self.solve(nums, i+1, curr_subset, subsets)
+        
+#     def subsetXORSum(self, nums: List[int]) -> int:
+#         subsets = []
+#         curr_subset = []
+#         self.solve(nums, 0, curr_subset, subsets)
+#         result = 0
+#         for subset in subsets:
+#             xor_sum = 0
+#             for num in subset:
+#                 xor_sum ^= num
+#             result += xor_sum
+#         return result
