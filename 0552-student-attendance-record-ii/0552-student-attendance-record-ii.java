330c88297ace942c2d5bbb5536549eb33878e710
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-26 15:59:36 +0530

    Time: 563 ms (15.16%), Space: 55.5 MB (67.51%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -1,24 +1,28 @@
 class Solution {
     public int checkRecord(int n) {
-        int M = 1e9+7;
-        int dp[100001][2][3];
-        for (int A = 0; A <= 1; A++) {
-            for (int L = 0; L <= 2; L++) {
-                dp[0][A][L] = 1;
+        int M = 1000000007;
+        int[][][] t = new int[100001][2][3];
+
+        for (int A = 0; A <= 1; ++A) {
+            for (int L = 0; L <= 2; ++L) {
+                t[0][A][L] = 1;
             }
         }
+
         for (int i = 1; i <= n; i++) {
             for (int A = 0; A <= 1; A++) {
                 for (int L = 0; L <= 2; L++) {
-                    long long result = dp[i-1][A][0];
-                    result += (L < 2 ? dp[i-1][A][L+1] : 0);
-                    result += (A == 0 ? dp[i-1][A+1][0] : 0);
 
-                    dp[i][A][L] = result % M;
+                    long result = t[i - 1][A][0];
+                    result += (L < 2 ? t[i - 1][A][L + 1] : 0);
+                    result += (A == 0 ? t[i - 1][A + 1][0] : 0);
+
+                    t[i][A][L] = (int) (result % M);
                 }
             }
         }
-        return dp[n][0][0];
+
+        return t[n][0][0];
     }
 }
 
