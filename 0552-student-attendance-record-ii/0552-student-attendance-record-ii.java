b673fb66f3f0bac1cd6c5c7ae0bf0ead8ef1991b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-26 15:26:05 +0530

    Time: 748 ms (5.06%), Space: 65 MB (57.4%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    private final int MOD = 1000000007;
+    private int[][][] memo = new int[100001][2][3];
+
+    private int solve(int n, int absent, int late) {
+        if (absent >= 2 || late >= 3) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (memo[n][absent][late] != -1) {
+            return memo[n][absent][late];
+        }
+        int A = solve(n - 1, absent + 1, 0) % MOD;
+        int B = solve(n - 1, absent, late + 1) % MOD;
+        int C = solve(n - 1, absent, 0) % MOD;
+        return memo[n][absent][late] = ((A + B) % MOD + C) % MOD;
+    }
+
+    public int checkRecord(int n) {
+        for (int i = 0; i < 100001; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+        return solve(n, 0, 0);
+    }
+}
