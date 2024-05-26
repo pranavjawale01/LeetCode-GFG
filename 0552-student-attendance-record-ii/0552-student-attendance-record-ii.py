b88780891ea44bfe12704ec87e1a1e7196cbb403
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-26 15:59:50 +0530

    Time: 5970 ms (16.59%), Space: 64.2 MB (46.45%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
@@ -1,20 +1,40 @@
 class Solution:
-    def __init__(self):
-        self.dp = [[[-1] * 3 for _ in range(2)] for _ in range(100001)]
-        self.MOD = 1000000007
-
-    def solve(self, n: int, absent: int, late: int) -> int:
-        if absent >= 2 or late >= 3:
-            return 0
-        if n == 0:
-            return 1
-        if self.dp[n][absent][late] != -1:
-            return self.dp[n][absent][late]
-        A = self.solve(n-1, absent + 1, 0) % self.MOD
-        B = self.solve(n-1, absent, late + 1) % self.MOD
-        C = self.solve(n-1, absent, 0) % self.MOD
-        self.dp[n][absent][late] = ((A + B) % self.MOD + C) % self.MOD
-        return self.dp[n][absent][late]
-                
     def checkRecord(self, n: int) -> int:
-        return self.solve(n, 0, 0)
+        M = 1000000007
+        t = [[[0] * 3 for _ in range(2)] for _ in range(100001)]
+
+        for A in range(2):
+            for L in range(3):
+                t[0][A][L] = 1
+
+        for i in range(1, n + 1):
+            for A in range(2):
+                for L in range(3):
+                    result = t[i - 1][A][0]
+                    result += t[i - 1][A][L + 1] if L < 2 else 0
+                    result += t[i - 1][A + 1][0] if A == 0 else 0
+                    t[i][A][L] = result % M
+
+        return t[n][0][0]
+
+
+# class Solution:
+#     def __init__(self):
+#         self.dp = [[[-1] * 3 for _ in range(2)] for _ in range(100001)]
+#         self.MOD = 1000000007
+
+#     def solve(self, n: int, absent: int, late: int) -> int:
+#         if absent >= 2 or late >= 3:
+#             return 0
+#         if n == 0:
+#             return 1
+#         if self.dp[n][absent][late] != -1:
+#             return self.dp[n][absent][late]
+#         A = self.solve(n-1, absent + 1, 0) % self.MOD
+#         B = self.solve(n-1, absent, late + 1) % self.MOD
+#         C = self.solve(n-1, absent, 0) % self.MOD
+#         self.dp[n][absent][late] = ((A + B) % self.MOD + C) % self.MOD
+#         return self.dp[n][absent][late]
+                
+#     def checkRecord(self, n: int) -> int:
+#         return self.solve(n, 0, 0)
