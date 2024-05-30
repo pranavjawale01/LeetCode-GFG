44237b7c3bb50ae97f1fd640ef5c98d2a5a313ae
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-30 15:51:11 +0530

    Time: 0 ms (100%), Space: 9.5 MB (32.74%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int count = 0;
-        int n = arr.size();
+        vector<int> prefixXor(arr.begin(), arr.end());
+        prefixXor.insert(prefixXor.begin(), 0);
+        int n = prefixXor.size();
+        for (int i = 1; i < n; i++) {
+            prefixXor[i] ^= prefixXor[i-1];
+        }
+        int triplets = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int a = 0;
-                for (int k = i; k < j; k++) {
-                    a ^= arr[k];
-                }
-                int b = 0;
-                for (int k = j; k < n; k++) {
-                    b ^= arr[k];
-                    if (a == b) {
-                        count++;
-                    }
+            for (int k = i + 1; k < n; k++) {
+                if (prefixXor[k] == prefixXor[i]) {
+                    triplets += k - i - 1;
                 }
             }
         }
-        return count;
+        return triplets;
     }
 };
+
+
+// class Solution {
+// public:
+//     int countTriplets(vector<int>& arr) {
+//         int count = 0;
+//         int n = arr.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int a = 0;
+//                 for (int k = i; k < j; k++) {
+//                     a ^= arr[k];
+//                 }
+//                 int b = 0;
+//                 for (int k = j; k < n; k++) {
+//                     b ^= arr[k];
+//                     if (a == b) {
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
