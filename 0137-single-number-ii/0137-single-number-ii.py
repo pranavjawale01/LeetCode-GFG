aa56c004df0aede6fafe33325d2a5183a0cb761f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-31 20:00:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,10 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ones, twos, threes = 0, 0, 0
-        for num in nums:
-            twos |= ones & num
-            ones ^= num
-            threes = ones & twos
-            ones &= ~threes
-            twos &= ~threes
-        return ones
+        ans = 0
+        for i in range(32):
+            ones = 0
+            for num in nums:
+                if num & 1 << i:
+                    ones += 1
+            if ones % 3 != 0:
+                ans |= 1 << i
+        return ans
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         ones, twos, threes = 0, 0, 0
+#         for num in nums:
+#             twos |= ones & num
+#             ones ^= num
+#             threes = ones & twos
+#             ones &= ~threes
+#             twos &= ~threes
+#         return ones
