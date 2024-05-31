1437ba97db8bec394700b0bfd3bd67f6d3d87172
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-31 19:56:21 +0530

    Time: 7 ms (50.89%), Space: 11.8 MB (57.67%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones = 0, twos = 0, threes = 0;
-        for (int num : nums) {
-            twos |= ones & num;
-            ones ^= num;
-            threes = ones & twos;
-            ones &= ~threes;
-            twos &= ~threes;
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int ones = 0, zeros = 0;
+            for (int num : nums) {
+                if (num & (1 << i)) {
+                    ones++;
+                } else {
+                    zeros++;
+                }
+            }
+            if (ones % 3 != 0) {
+                ans |= 1 << i;
+            }
         }
-        return ones;
+        return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int ones = 0, twos = 0, threes = 0;
+//         for (int num : nums) {
+//             twos |= ones & num;
+//             ones ^= num;
+//             threes = ones & twos;
+//             ones &= ~threes;
+//             twos &= ~threes;
+//         }
+//         return ones;
+//     }
+// };
