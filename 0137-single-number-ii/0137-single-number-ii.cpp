1ba0450f0e874bea7e51053db927013b8bc7a494
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-05-31 17:04:42 +0530

    Time: 4 ms (79.68%), Space: 11.7 MB (82.44%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0, threes = 0;
+        for (int num : nums) {
+            twos |= ones & num;
+            ones ^= num;
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+};
