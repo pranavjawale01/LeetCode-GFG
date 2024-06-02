c548262a02a7a7a443673b721687e7db0de28282
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-02 12:35:29 +0530

    Time: 1 ms (27.03%), Space: 45.7 MB (78.1%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,9 +2,21 @@ class Solution {
     public void reverseString(char[] s) {
         int n = s.length;
         for (int i = 0; i < n / 2; i++) {
-            char temp = s[i];
-            s[i] = s[n - i - 1];
-            s[n - i - 1] = temp;
+            s[i] = (char) (s[i] ^ s[n - i - 1]);
+            s[n - i - 1] = (char) (s[i] ^ s[n - i - 1]);
+            s[i] = (char) (s[i] ^ s[n - i - 1]);
         }
     }
 }
+
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int n = s.length;
+//         for (int i = 0; i < n / 2; i++) {
+//             char temp = s[i];
+//             s[i] = s[n - i - 1];
+//             s[n - i - 1] = temp;
+//         }
+//     }
+// }
