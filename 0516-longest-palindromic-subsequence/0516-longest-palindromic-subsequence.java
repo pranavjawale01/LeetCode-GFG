339cbdd31f5b2175809e36063e7a1fa67cc01f30
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-02 16:48:18 +0530

    Time: 41 ms (57.72%), Space: 55 MB (49.13%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,25 +1,48 @@
 class Solution {
-    private int[][] dp = new int[1000][1000];
-    private int solve(String s, int start, int end) {
-        if (start == end) {
-            return 1;
-        }
-        if (start > end) {
-            return 0;
-        }
-        if (dp[start][end] != -1) {
-            return dp[start][end];
-        }
-        if (s.charAt(start) == s.charAt(end)) {
-            return dp[start][end] = 2 + solve(s, start+1, end-1);
-        }
-        return dp[start][end] = Math.max(solve(s, start+1, end), solve(s, start, end-1));
-    }
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[n][n];
+        for (int len = 1; len <= n; len++) {
+            for (int start = 0; start <= n - len; start++) {
+                int end = start + len - 1;
+                if (len == 1) {
+                    dp[start][end] = 1;
+                    continue;
+                }
+                if (s.charAt(start) == s.charAt(end)) {
+                    dp[start][end] = 2 + dp[start+1][end-1];
+                } else {
+                    dp[start][end] = Math.max(dp[start+1][end], dp[start][end-1]);
+                }
+            }
         }
-        return solve(s, 0, s.length()-1);
+        return dp[0][n-1];
     }
 }
+
+
+// class Solution {
+//     private int[][] dp = new int[1000][1000];
+//     private int solve(String s, int start, int end) {
+//         if (start == end) {
+//             return 1;
+//         }
+//         if (start > end) {
+//             return 0;
+//         }
+//         if (dp[start][end] != -1) {
+//             return dp[start][end];
+//         }
+//         if (s.charAt(start) == s.charAt(end)) {
+//             return dp[start][end] = 2 + solve(s, start+1, end-1);
+//         }
+//         return dp[start][end] = Math.max(solve(s, start+1, end), solve(s, start, end-1));
+//     }
+//     public int longestPalindromeSubseq(String s) {
+//         int n = s.length();
+//         for (int i = 0; i < n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return solve(s, 0, s.length()-1);
+//     }
+// }
