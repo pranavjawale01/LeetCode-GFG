c92cf19e380c8ace8b7fd5de834cacaff3a3c84e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-02 13:22:21 +0530

    Time: 305 ms (16.67%), Space: 125.5 MB (16.67%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -3,20 +3,57 @@ public:
     int countDays(int days, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
 
-        int lastDayCovered = 0;
-        int dayWithoutMeeting = 0;
+        vector<vector<int>> merged;
 
-        for (const auto& meeting : meetings) {
-            int start = meeting[0];
-            int end = meeting[1];
-
-            dayWithoutMeeting += max(0, start - lastDayCovered - 1);
-
-            lastDayCovered = max(lastDayCovered, end);
+        for (auto& meeting : meetings) {
+            if (merged.empty() || merged.back()[1] < meeting[0]) {
+                merged.push_back(meeting);
+            } else {
+                merged.back()[1] = max(merged.back()[1], meeting[1]);
+            }
         }
 
-        dayWithoutMeeting += max(0, days - lastDayCovered);
+        int day = 0;
 
-        return dayWithoutMeeting;
+        if (merged[0][0] > 1) {
+            day += merged[0][0] - 1;
+        }
+
+        for (int i = 1; i < merged.size(); i++) {
+            day += merged[i][0] - merged[i-1][1] - 1;
+        }
+
+        if (merged.back()[1] < days) {
+            day += days - merged.back()[1];
+        }
+
+        return day;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int countDays(int days, vector<vector<int>>& meetings) {
+//         sort(meetings.begin(), meetings.end());
+
+//         int lastDayCovered = 0;
+//         int dayWithoutMeeting = 0;
+
+//         for (const auto& meeting : meetings) {
+//             int start = meeting[0];
+//             int end = meeting[1];
+
+//             dayWithoutMeeting += max(0, start - lastDayCovered - 1);
+
+//             lastDayCovered = max(lastDayCovered, end);
+//         }
+
+//         dayWithoutMeeting += max(0, days - lastDayCovered);
+
+//         return dayWithoutMeeting;
+//     }
+// };
