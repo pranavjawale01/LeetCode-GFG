86847658e8fb0bff91181d97b8ed9bed1751be41
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-02 12:38:06 +0530

    Time: 157 ms (96.57%), Space: 20.8 MB (69.97%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,9 +5,22 @@ class Solution:
         """
         n = len(s)
         for i in range(n // 2):
-            s[i] = chr(ord(s[i]) + ord(s[n - i - 1]))
-            s[n - i - 1] = chr(ord(s[i]) - ord(s[n - i - 1]))
-            s[i] = chr(ord(s[i]) - ord(s[n - i - 1]))
+            s[i] = chr(ord(s[i]) ^ ord(s[n - i - 1]))
+            s[n - i - 1] = chr(ord(s[i]) ^ ord(s[n - i - 1]))
+            s[i] = chr(ord(s[i]) ^ ord(s[n - i - 1]))
+
+
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         n = len(s)
+#         for i in range(n // 2):
+#             s[i] = chr(ord(s[i]) + ord(s[n - i - 1]))
+#             s[n - i - 1] = chr(ord(s[i]) - ord(s[n - i - 1]))
+#             s[i] = chr(ord(s[i]) - ord(s[n - i - 1]))
  
 
 # class Solution:
