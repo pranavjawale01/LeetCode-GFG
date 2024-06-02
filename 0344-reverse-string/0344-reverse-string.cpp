d2ec5f3cc6bade70cc0fd6284b6b3fe16f37bbaf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-02 12:35:00 +0530

    Time: 10 ms (95.59%), Space: 27 MB (60.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,12 +3,25 @@ public:
     void reverseString(vector<char>& s) {
         int n = s.size();
         for (int i = 0; i < n / 2; i++) {
-            s[i] = (char) s[i] ^ s[n - i - 1];
-            s[n-i-1] = (char) s[i] ^ s[n - i - 1];
-            s[i] = (char) s[i] ^ s[n - i - 1];
+            s[i] = (char) s[i] + s[n - i - 1];
+            s[n - i - 1] = (char) s[i] - s[n - i - 1];
+            s[i] = (char) s[i] - s[n - i - 1];
         }
     }
 };
+
+
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int n = s.size();
+//         for (int i = 0; i < n / 2; i++) {
+//             s[i] = (char) s[i] ^ s[n - i - 1];
+//             s[n-i-1] = (char) s[i] ^ s[n - i - 1];
+//             s[i] = (char) s[i] ^ s[n - i - 1];
+//         }
+//     }
+// };
 
 
 // class Solution {
