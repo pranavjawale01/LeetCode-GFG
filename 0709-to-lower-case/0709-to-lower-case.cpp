581723524b68bea67fc2bd839438a3b03f1d8875
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 15:44:46 +0530

    Time: 3 ms (43.14%), Space: 7.4 MB (27.02%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            s[i] = tolower(s[i]);
+        }
         return s;
     }
 };
+
+
+// class Solution {
+// public:
+//     string toLowerCase(string s) {
+//         transform(s.begin(), s.end(), s.begin(), ::tolower);
+//         return s;
+//     }
+// };
