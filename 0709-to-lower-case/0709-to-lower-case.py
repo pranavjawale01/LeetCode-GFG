33f48552e5c57d9d45326e27f9a2c13b1d7db1c1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 15:49:23 +0530

    Time: 33 ms (68.63%), Space: 16.4 MB (97.05%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,3 +1,11 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        char = []
+        for c in s:
+            char.append(c.lower())
+        return ''.join(char)
+
+
+# class Solution:
+#     def toLowerCase(self, s: str) -> str:
+#         return s.lower()
