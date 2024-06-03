1841c9eeb674d2f255d01c1c59c39c9322bc87f0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 16:12:40 +0530

    Time: 4 ms (96.66%), Space: 44 MB (90.26%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int v = 0, h = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') {
+                v += 1;
+            } else if (c == 'D') {
+                v -= 1;
+            } else if (c == 'R') {
+                h += 1;
+            } else if (c == 'L') {
+                h -= 1;
+            }
+        }
+        return v == 0 && h == 0;
+    }
+}
