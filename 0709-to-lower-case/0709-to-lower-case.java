ff4c3fcafe32c7020954c95bbd3710903902bce1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 15:47:05 +0530

    Time: 1 ms (24.7%), Space: 41.8 MB (10.81%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,16 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            str.append(Character.toLowerCase(c));
+        }
+        return str.toString();
     }
 }
+
+
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
