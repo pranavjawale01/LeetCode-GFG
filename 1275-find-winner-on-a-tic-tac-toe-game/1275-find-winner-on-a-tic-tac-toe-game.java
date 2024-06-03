b20748f83928eae2f1f66708970a7ee5868d7268
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 16:07:21 +0530

    Time: 0 ms (100%), Space: 41.2 MB (46.87%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String tictactoe(int[][] moves) {
+        char[][] board = new char[3][3];
+        
+        for (int i = 0; i < moves.length; i++) {
+            int row = moves[i][0];
+            int col = moves[i][1];
+            char player = (i % 2 == 0) ? 'X' : 'O';
+            board[row][col] = player;
+            
+            if (board[row][0] == player && board[row][1] == player && board[row][2] == player) {
+                return player == 'X' ? "A" : "B";
+            }
+            
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player) {
+                return player == 'X' ? "A" : "B";
+            }
+            
+            if (row == col && board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+                return player == 'X' ? "A" : "B";
+            }
+            
+            if (row + col == 2 && board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+                return player == 'X' ? "A" : "B";
+            }
+        }
+        
+        if (moves.length == 9) {
+            return "Draw";
+        }
+        
+        return "Pending";
+    }
+}
