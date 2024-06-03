c9a1318071c2605d0405caed2ed2cd87266c3853
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 11:18:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long MOD=1e9+7;
+    long long power(long long a, long long b){
+        long long ans=1;
+        while(b>0){
+            if(b&1){
+                ans*=a;
+                ans%=MOD;
+            }
+            a*=a;
+            a%=MOD;
+            b >>=1;
+        }
+        return ans;
+
+    }
+  
+    int numberOfConsecutiveOnes(int n) {
+        // code here
+        vector<int>a(n);
+        vector<int>b(n);
+        
+        a[0]=1; b[0]=1;
+        for(int i=1;i<n;i++){
+            a[i]=(a[i-1]+b[i-1])%MOD;
+            b[i]=a[i-1];
+        }
+        
+        return (power(2,n)+MOD - (a[n-1]+b[n-1])%MOD)%MOD;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.numberOfConsecutiveOnes(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
