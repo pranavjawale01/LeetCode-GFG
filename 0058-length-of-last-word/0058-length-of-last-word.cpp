df9d7997f3586c6a663b3940a685285d0ba810ad
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-03 15:40:43 +0530

    Time: 0 ms (100%), Space: 7.6 MB (54.78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int idx = s.find_last_not_of(' ') + 1;
-        s.erase(idx);
-        int last_space_idx = s.find_last_of(' ');
-        return last_space_idx == string::npos ? s.length() : s.length() - last_space_idx - 1;
+        int i = s.length() - 1, count = 0;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         int idx = s.find_last_not_of(' ') + 1;
+//         s.erase(idx);
+//         int last_space_idx = s.find_last_of(' ');
+//         return last_space_idx == string::npos ? s.length() : s.length() - last_space_idx - 1;
+//     }
+// };
 
 // class Solution {
 // public:
