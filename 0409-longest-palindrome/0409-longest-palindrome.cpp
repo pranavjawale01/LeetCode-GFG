36cbb1c202c4fba65efc62b036bbed07265d2deb
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-04 13:42:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (26.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,19 +2,46 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         int n = s.length();
-        unordered_set<char> st;
-        int result = 0;
+        unordered_map<char, int> mp;
         for (char c : s) {
-            if (st.count(c)) {
-                result += 2;
-                st.erase(c);
+            mp[c]++;
+        }
+        int result = 0;
+        bool oddFreq = false;
+        for (auto &it : mp) {
+            if (it.second % 2 == 0) {
+                result += it.second;
             } else {
-                st.insert(c);
+                result += it.second - 1;
+                oddFreq = true;
             }
         }
-        if (!st.empty()) {
+        if (oddFreq) {
             result++;
         }
         return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int longestPalindrome(string s) {
+//         int n = s.length();
+//         unordered_set<char> st;
+//         int result = 0;
+//         for (char c : s) {
+//             if (st.count(c)) {
+//                 result += 2;
+//                 st.erase(c);
+//             } else {
+//                 st.insert(c);
+//             }
+//         }
+//         if (!st.empty()) {
+//             result++;
+//         }
+//         return result;
+//     }
+// };
