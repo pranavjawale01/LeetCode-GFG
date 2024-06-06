ec001e7b66734e98af7326c1611f35b7f03b32f5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-07 01:48:23 +0530

    Create 1A-Theatre Square.cpp

---
+++ CodeForces/0001-A-Theatre Square/1A-Theatre Square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+    long long n, m, a;
+    cin >> n >> m >> a;
+    
+    long long len = ceil((double)(n) / (a));
+    long long wid = ceil((double)(m) / (a));
+    
+    cout << len * wid << endl;
+    
+    return 0;
+}
