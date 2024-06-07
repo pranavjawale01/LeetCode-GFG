4fe9a81210ff93993b03ac9cb3c8ae54fd0399dc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-07 14:02:37 +0530

    Time: 32 ms (67.79%), Space: 33.9 MB (72.68%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        if (start < 0 || start >= arr.size()) {
-            return false;
+        queue<int> q;
+        q.push(start);
+        int n = arr.size();
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            if (arr[curr] == 0) {
+                return true;
+            }
+            if (arr[curr] < 0) {
+                continue;
+            }
+            int jump = arr[curr];
+            arr[curr] = -arr[curr];
+            if (curr + jump < n) {
+                q.push(curr + jump);
+            }
+            if (curr - jump >= 0) {
+                q.push(curr - jump);
+            }
         }
-        if (arr[start] < 0) {
-            return false;
-        }
-        if (arr[start] == 0) {
-            return true;
-        }
-        int jump = arr[start];
-        arr[start] = -arr[start];
-        bool can_reach_left = canReach(arr, start - jump);
-        bool can_reach_right = canReach(arr, start + jump);
-        return can_reach_left || can_reach_right;
+        return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool canReach(vector<int>& arr, int start) {
+//         if (start < 0 || start >= arr.size()) {
+//             return false;
+//         }
+//         if (arr[start] < 0) {
+//             return false;
+//         }
+//         if (arr[start] == 0) {
+//             return true;
+//         }
+//         int jump = arr[start];
+//         arr[start] = -arr[start];
+//         bool can_reach_left = canReach(arr, start - jump);
+//         bool can_reach_right = canReach(arr, start + jump);
+//         return can_reach_left || can_reach_right;
+//     }
+// };
