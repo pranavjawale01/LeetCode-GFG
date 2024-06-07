e197fb86dd6cc9023621b5ba74f1cc98438b867a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-07 14:02:30 +0530

    Time: 10 ms (30.08%), Space: 54.7 MB (86.39%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,16 +1,44 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        if (start < 0 || start >= arr.length) {
-            return false;
+        Queue<Integer> q = new LinkedList<>();
+        int n = arr.length;
+        q.offer(start);
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (arr[curr] == 0) {
+                return true;
+            }
+            if (arr[curr] < 0) {
+                continue;
+            }
+            int jump = arr[curr];
+            arr[curr] = -arr[curr];
+            if (curr + jump < n) {
+                q.offer(curr + jump);
+            }
+            if (curr - jump >= 0) {
+                q.offer(curr - jump);
+            }
         }
-        if (arr[start] < 0) {
-            return false;
-        }
-        if (arr[start] == 0) {
-            return true;
-        }
-        int jump = arr[start];
-        arr[start] = -arr[start];
-        return canReach(arr, start - jump) || canReach(arr, start + jump);
+        return false;
     }
 }
+
+
+
+// class Solution {
+//     public boolean canReach(int[] arr, int start) {
+//         if (start < 0 || start >= arr.length) {
+//             return false;
+//         }
+//         if (arr[start] < 0) {
+//             return false;
+//         }
+//         if (arr[start] == 0) {
+//             return true;
+//         }
+//         int jump = arr[start];
+//         arr[start] = -arr[start];
+//         return canReach(arr, start - jump) || canReach(arr, start + jump);
+//     }
+// }
