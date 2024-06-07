047a619510e5f82faeb6b8f029aeca079136eeb6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-07 13:54:59 +0530

    Time: 226 ms (43.87%), Space: 23.1 MB (88.41%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,11 +1,38 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        if start < 0 or start >= len(arr):
-            return False
-        if arr[start] < 0:
-            return False
-        if arr[start] == 0:
-            return True
-        jump = arr[start]
-        arr[start] = -arr[start]
-        return self.canReach(arr, start + jump) or self.canReach(arr, start - jump)
+        q = deque([start])
+        n = len(arr)
+        
+        while q:
+            curr = q.popleft()
+            
+            if arr[curr] == 0:
+                return True
+            
+            if arr[curr] < 0:
+                continue
+            
+            jump = arr[curr]
+            arr[curr] = -arr[curr] 
+            
+            if curr + jump < n:
+                q.append(curr + jump)
+            if curr - jump >= 0:
+                q.append(curr - jump)
+        
+        return False
+
+
+
+
+# class Solution:
+#     def canReach(self, arr: List[int], start: int) -> bool:
+#         if start < 0 or start >= len(arr):
+#             return False
+#         if arr[start] < 0:
+#             return False
+#         if arr[start] == 0:
+#             return True
+#         jump = arr[start]
+#         arr[start] = -arr[start]
+#         return self.canReach(arr, start + jump) or self.canReach(arr, start - jump)
