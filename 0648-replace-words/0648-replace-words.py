a85a13346c993e86ea2f6f6fd8670687e6d7374a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-08 13:51:05 +0530

    Time: 116 ms (56.89%), Space: 36.7 MB (18.96%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,14 +1,56 @@
 class Solution:
-    def findWord(self, word: str, st: set()) -> str:
-        for i in range(1, len(word)):
-            if word[0:i] in st:
-                return word[0:i]
+    class TrieNode:
+        def __init__(self):
+            self.child = [None] * 26
+            self.isEnd = False
+        
+    def __init__(self):
+        self.root = self.TrieNode()
+
+    def insert(self, word):
+        ptr = self.root
+        for ch in word:
+            idx = ord(ch) - ord('a')
+            if not ptr.child[idx]:
+                ptr.child[idx] = self.TrieNode()
+            ptr = ptr.child[idx]
+        ptr.isEnd = True
+    
+    def search(self, word):
+        ptr = self.root
+        for i, ch in enumerate(word):
+            idx = ord(ch) - ord('a')
+            if not ptr.child[idx]:
+                return word
+            ptr = ptr.child[idx]
+            if ptr.isEnd:
+                return word[:i+1]
         return word
 
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        st = set(dictionary)
-        result = []
+        for word in dictionary:
+            self.insert(word)
         words = sentence.split()
+        result = []
         for word in words:
-            result.append(self.findWord(word, st))
-        return " ".join(result)
+            result.append(self.search(word))
+        return ' '.join(result)
+
+        
+
+
+
+# class Solution:
+#     def findWord(self, word: str, st: set()) -> str:
+#         for i in range(1, len(word)):
+#             if word[0:i] in st:
+#                 return word[0:i]
+#         return word
+
+#     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+#         st = set(dictionary)
+#         result = []
+#         words = sentence.split()
+#         for word in words:
+#             result.append(self.findWord(word, st))
+#         return " ".join(result)
