f14c98e9be7cad5a24ae2c0d52bee810eb0c7d88
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-08 13:45:08 +0530

    Time: 66 ms (88.65%), Space: 71.6 MB (74.59%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,23 +1,87 @@
 class Solution {
 public:
-    string findWord(string &word, unordered_set<string> &st) {
-        for (int i = 1; i < word.length(); i++) {
-            string temp = word.substr(0, i);
-            if (st.find(temp) != st.end()) {
-                return temp;
+    struct TrieNode {
+        TrieNode *child[26];
+        bool isEnd;
+        TrieNode() : isEnd(false) {
+            for (int i = 0; i < 26; ++i) {
+                child[i] = nullptr;
+            }
+        }
+    };
+
+    TrieNode* root;
+
+    Solution() {
+        root = new TrieNode();
+    }
+
+    void insert(const string& word) {
+        TrieNode* crawler = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!crawler->child[idx]) {
+                crawler->child[idx] = new TrieNode();
+            }
+            crawler = crawler->child[idx];
+        }
+        crawler->isEnd = true;
+    }
+
+    string search(const string& word) {
+        TrieNode* crawler = root;
+        for (int i = 0; i < word.size(); ++i) {
+            int idx = word[i] - 'a';
+            if (!crawler->child[idx]) {
+                return word;
+            }
+            crawler = crawler->child[idx];
+            if (crawler->isEnd) {
+                return word.substr(0, i + 1);
             }
         }
         return word;
     }
+
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> st(dictionary.begin(), dictionary.end());
+        for (const string& word : dictionary) {
+            insert(word);
+        }
         stringstream ss(sentence);
         string word;
         string result;
-        while (getline(ss, word, ' ')) {
-            result += findWord(word, st) + " ";
+        while (ss >> word) {
+            if (!result.empty()) {
+                result += " ";
+            }
+            result += search(word);
         }
-        result.pop_back();
         return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     string findWord(string &word, unordered_set<string> &st) {
+//         for (int i = 1; i < word.length(); i++) {
+//             string temp = word.substr(0, i);
+//             if (st.find(temp) != st.end()) {
+//                 return temp;
+//             }
+//         }
+//         return word;
+//     }
+//     string replaceWords(vector<string>& dictionary, string sentence) {
+//         unordered_set<string> st(dictionary.begin(), dictionary.end());
+//         stringstream ss(sentence);
+//         string word;
+//         string result;
+//         while (getline(ss, word, ' ')) {
+//             result += findWord(word, st) + " ";
+//         }
+//         result.pop_back();
+//         return result;
+//     }
+// };
