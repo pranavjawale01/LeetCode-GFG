3f4912caa7525fe777f9c856d22a51fcde4be8f1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-09 16:35:43 +0530

    Time: 0 ms (100%), Space: 7.5 MB (83.33%) - LeetHub

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.cpp
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -1,8 +1,36 @@
 class Solution {
 public:
     int numberOfChild(int n, int k) {
-        
+        n--;
+        int rounds  = k / n;
+        int rem = k % n;
+        if (rounds % 2 == 0) {
+            return rem;
+        } else {
+            return n - rem;
+        }
     }
 };
 
 
+// class Solution {
+// public:
+//     int numberOfChild(int n, int k) {
+//         int i = 0;
+//         bool flag= true;
+//         while (k > 0) {
+//             if (i == n - 1) {
+//                 flag = false;
+//             } else if (i == 0) {
+//                 flag = true;
+//             }
+//             if (flag) {
+//                 i++;
+//             } else {
+//                 i--;
+//             }
+//             k--;
+//         }
+//         return i;
+//     }
+// };
