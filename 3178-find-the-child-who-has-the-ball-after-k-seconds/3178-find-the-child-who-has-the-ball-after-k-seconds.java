0c2a4fbdd977d4ce270faaca1704f32aadf3209b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-09 16:27:05 +0530

    Time: 1 ms (100%), Space: 40.5 MB (100%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        int i = 0;
+        boolean flag = true;
+        while (k-- > 0) {
+            if (i == n - 1) {
+                flag = false;
+            } else if (i == 0) {
+                flag = true;
+            }
+            if (flag) {
+                i++;
+            } else {
+                i--;
+            }
+        }
+        return i;
+    }
+}
