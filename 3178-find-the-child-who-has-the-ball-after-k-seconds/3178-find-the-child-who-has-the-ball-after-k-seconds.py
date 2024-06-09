9d4152e9d20da4cc1d23a3e7442d9ebeaca93aef
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-09 16:31:22 +0530

    Time: 44 ms (100%), Space: 16.5 MB (100%) - LeetHub

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.py
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -1,15 +1,25 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
-        i = 0
-        flag = True
-        while k > 0:
-            if i == n - 1:
-                flag = False
-            elif i == 0:
-                flag = True
-            if flag:
-                i += 1
-            else:
-                i -= 1
-            k -= 1
-        return i
+        n -= 1
+        rounds = k // n
+        rem = k % n
+        if rounds % 2 == 0:
+            return rem
+        else:
+            return n - rem
+
+# class Solution:
+#     def numberOfChild(self, n: int, k: int) -> int:
+#         i = 0
+#         flag = True
+#         while k > 0:
+#             if i == n - 1:
+#                 flag = False
+#             elif i == 0:
+#                 flag = True
+#             if flag:
+#                 i += 1
+#             else:
+#                 i -= 1
+#             k -= 1
+#         return i
