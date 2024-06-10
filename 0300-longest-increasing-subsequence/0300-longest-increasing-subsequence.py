9f984759844755b44e3ae948222439b13e291798
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-10 16:45:26 +0530

    Time: 2656 ms (8.09%), Space: 16.8 MB (77.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,24 +1,40 @@
 class Solution:
-    def __init__(self):
-        self.n = 0
-        self.dp = [[-1 for _ in range(2501)] for _ in range(2501)]
-
-    def solve(self, nums: List[int], i: int, p: int) -> int:
-        if i >= self.n:
-            return 0
-        if p != -1 and self.dp[i][p] != -1:
-            return self.dp[i][p]
-        take = 0
-        if p == -1 or nums[i] > nums[p]:
-            take = 1 + self.solve(nums, i+1, i)
-        skip = self.solve(nums, i+1, p)
-        if p != -1:
-            self.dp[i][p] = max(take, skip)
-        return max(take, skip)
-
     def lengthOfLIS(self, nums: List[int]) -> int:
-        self.n = len(nums)
-        return self.solve(nums, 0, -1)
+        n = len(nums)
+        dp = [1] * n
+        maxL = 1
+        for i in range(0, n):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    maxL = max(maxL, dp[i])
+        return maxL
+
+
+
+# TLE
+# class Solution:
+#     def __init__(self):
+#         self.n = 0
+#         self.dp = [[-1 for _ in range(2501)] for _ in range(2501)]
+
+#     def solve(self, nums: List[int], i: int, p: int) -> int:
+#         if i >= self.n:
+#             return 0
+#         if p != -1 and self.dp[i][p] != -1:
+#             return self.dp[i][p]
+#         take = 0
+#         if p == -1 or nums[i] > nums[p]:
+#             take = 1 + self.solve(nums, i+1, i)
+#         skip = self.solve(nums, i+1, p)
+#         if p != -1:
+#             self.dp[i][p] = max(take, skip)
+#         return max(take, skip)
+
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         self.n = len(nums)
+#         return self.solve(nums, 0, -1)
+
 
 
 # class Solution:
