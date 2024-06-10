9b034b2bae3b738218a7bccecdb802ae5e1741f0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-10 16:05:21 +0530

    Time: 525 ms (11.48%), Space: 40.4 MB (32.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails(nums.size(), 0);
-        int size = 0;
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int n = (i + j) / 2;
-                if (nums[n] < x) {
-                    i = n + 1;
-                } else {
-                    j = n;
-                }
-            }
-            nums[i] = x;
-            if (i == size) {
-                size++;
-            }
+    int n;
+    int dp[2501][2501];
+    int solve(vector<int> &nums, int i, int p) {
+        if (i >= n) {
+            return 0;
         }
-        return size;
+        if (p != -1 && dp[i][p] != -1) {
+            return dp[i][p];
+        }
+        int take = 0;
+        if (p == -1 || nums[i] > nums[p]) {
+            take = 1 + solve(nums, i+1, i);
+        }
+        int skip = solve(nums, i+1, p);
+        if (p != -1) {
+            dp[i][p] = max(take, skip);
+        }
+        return max(take, skip);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        n = nums.size();
+        return solve(nums, 0, -1);
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         vector<int> tails(nums.size(), 0);
+//         int size = 0;
+//         for (int x : nums) {
+//             int i = 0, j = size;
+//             while (i != j) {
+//                 int n = (i + j) / 2;
+//                 if (nums[n] < x) {
+//                     i = n + 1;
+//                 } else {
+//                     j = n;
+//                 }
+//             }
+//             nums[i] = x;
+//             if (i == size) {
+//                 size++;
+//             }
+//         }
+//         return size;
+//     }
+// };
