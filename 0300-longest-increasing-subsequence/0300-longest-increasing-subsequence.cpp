7ec20f6be2cd4aa3f4b0fe72f10f761805b30073
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-10 16:43:01 +0530

    Time: 83 ms (50.71%), Space: 12.9 MB (48.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int n;
-    int dp[2501][2501];
-    int solve(vector<int> &nums, int i, int p) {
-        if (i >= n) {
-            return 0;
-        }
-        if (p != -1 && dp[i][p] != -1) {
-            return dp[i][p];
-        }
-        int take = 0;
-        if (p == -1 || nums[i] > nums[p]) {
-            take = 1 + solve(nums, i+1, i);
-        }
-        int skip = solve(nums, i+1, p);
-        if (p != -1) {
-            dp[i][p] = max(take, skip);
-        }
-        return max(take, skip);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        n = nums.size();
-        return solve(nums, 0, -1);
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxL = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    maxL = max(maxL, dp[i]);
+                }
+            }
+        }
+        return maxL;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int n;
+//     int dp[2501][2501];
+//     int solve(vector<int> &nums, int i, int p) {
+//         if (i >= n) {
+//             return 0;
+//         }
+//         if (p != -1 && dp[i][p] != -1) {
+//             return dp[i][p];
+//         }
+//         int take = 0;
+//         if (p == -1 || nums[i] > nums[p]) {
+//             take = 1 + solve(nums, i+1, i);
+//         }
+//         int skip = solve(nums, i+1, p);
+//         if (p != -1) {
+//             dp[i][p] = max(take, skip);
+//         }
+//         return max(take, skip);
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         memset(dp, -1, sizeof(dp));
+//         n = nums.size();
+//         return solve(nums, 0, -1);
+//     }
+// };
 
 
 
