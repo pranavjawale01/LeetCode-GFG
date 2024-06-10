63eedce4b82f8d31e0bdc5815759399aa18b6bda
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-10 11:46:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        char order[] = {'!', '#', '$', '%', '&', '*', '?', '@', '^'};
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[nuts[i]]++;
+        }
+        for (int i = 0, j = 0; i < 9; i++) {
+            if (mp[order[i]]) {
+                nuts[j] = order[i];
+                bolts[j] = order[i];
+                j++;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(n, nuts, bolts);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
