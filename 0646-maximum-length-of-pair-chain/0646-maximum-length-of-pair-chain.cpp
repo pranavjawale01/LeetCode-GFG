ccde7b95b99a1508b0a2c63823fa8a544e418d7a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-12 15:25:15 +0530

    Time: 263 ms (13.06%), Space: 30.6 MB (27.55%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,18 +1,49 @@
+// class Solution {
+// public:
+//     int findLongestChain(vector<vector<int>>& pairs) {
+//         sort(pairs.begin(), pairs.end());
+//         int len = 1;
+//         int n = pairs.size();
+//         vector<int> dp(n, 1);
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 if (pairs[i][0] > pairs[j][1]) {
+//                     dp[i] = max(dp[i], dp[j] + 1);
+//                     len = max(len, dp[i]);
+//                 }
+//             }
+//         }
+//         return len;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end());
-        int len = 1;
-        int n = pairs.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (pairs[i][0] > pairs[j][1]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                    len = max(len, dp[i]);
-                }
-            }
+    int n;
+    int dp[1001][1001];
+    int solve(vector<vector<int>>& pairs, int prev, int i) {
+        if (i == n) {
+            return 0;
         }
-        return len;
+        if (prev != -1 && dp[prev][i] != -1) {
+            return dp[prev][i];
+        }
+        int taken = 0;
+        if (prev == -1 || pairs[i][0] > pairs[prev][1]) {
+            taken = 1 + solve(pairs, i, i+1);
+        }
+        int notTaken = solve(pairs, prev, i+1);
+        if (prev != -1) {
+            dp[prev][i] = max(taken, notTaken);
+        }
+        return max(taken, notTaken);
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        memset(dp, -1, sizeof(dp));
+        n = pairs.size();
+        sort(pairs.begin(), pairs.end());
+        return solve(pairs, -1, 0);
     }
 };
