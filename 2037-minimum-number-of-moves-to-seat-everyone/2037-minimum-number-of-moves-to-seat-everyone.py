821a716a61ad30f02b3b466f79f4a0a285400ebb
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-13 16:05:06 +0530

    Time: 66 ms (16.1%), Space: 16.5 MB (82.54%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -1,9 +1,34 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
         n = len(seats)
+        position_seat = [0] * 101
+        position_stud = [0] * 101
+
+        for pos in seats:
+            position_seat[pos] += 1
+        for pos in students:
+            position_stud[pos] += 1
+        i , j = 0 , 0
         moves = 0
-        for i in range(n):
-            moves += abs(students[i] - seats[i])
+        while i <= 100 and j <= 100:
+            if position_seat[i] == 0:
+                i += 1
+            if position_stud[j] == 0:
+                j += 1
+            if i <= 100 and j <= 100 and position_seat[i] != 0 and position_stud[j] != 0:
+                moves += abs(i - j)
+                position_seat[i] -= 1
+                position_stud[j] -= 1
         return moves
+
+
+
+# class Solution:
+#     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+#         seats.sort()
+#         students.sort()
+#         n = len(seats)
+#         moves = 0
+#         for i in range(n):
+#             moves += abs(students[i] - seats[i])
+#         return moves
