3ee85e430e93936705e16d6065b8b118ae9d0a81
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-13 16:06:26 +0530

    Time: 2 ms (97.35%), Space: 44.1 MB (49.16%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -1,12 +1,43 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
         int n = seats.length;
-        int moves = 0;
-        for (int i = 0; i < n; i++) {
-            moves += Math.abs(seats[i] - students[i]);
+        int[] pos_seat = new int[101];
+        int[] pos_stud = new int[101];
+        for (int seat : seats) {
+            pos_seat[seat]++;
+        }
+        for (int student : students) {
+            pos_stud[student]++;
+        }
+        int i = 0, j = 0, moves = 0;
+        while (i <= 100 && j <= 100) {
+            if (pos_seat[i] == 0) {
+                i++;
+            }
+            if (pos_stud[j] == 0) {
+                j++;
+            }
+            if (i <= 100 && j <= 100 && pos_seat[i] != 0 && pos_stud[j] != 0) {
+                moves += Math.abs(i - j);
+                pos_seat[i]--;
+                pos_stud[j]--;
+            }
         }
         return moves;
     }
 }
+
+
+
+// class Solution {
+//     public int minMovesToSeat(int[] seats, int[] students) {
+//         Arrays.sort(seats);
+//         Arrays.sort(students);
+//         int n = seats.length;
+//         int moves = 0;
+//         for (int i = 0; i < n; i++) {
+//             moves += Math.abs(seats[i] - students[i]);
+//         }
+//         return moves;
+//     }
+// }
