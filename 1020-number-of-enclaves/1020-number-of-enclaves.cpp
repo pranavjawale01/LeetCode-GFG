cc8f6c60452e56959baedcac878ef0ca8fa1211a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-13 16:22:40 +0530

    Time: 54 ms (43.42%), Space: 32.7 MB (58.89%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -5,8 +5,10 @@ public:
             return;
         }
         grid[i][j] = 0;
-        dfs(grid, i, j);
-        dfs(grid, i, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int row = grid.size(), col = grid[0].size();
@@ -19,5 +21,14 @@ public:
                 }
             }
         }
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
