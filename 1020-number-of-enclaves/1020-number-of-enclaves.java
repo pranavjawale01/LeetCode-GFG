3c93b9a0ec812e793ec4103531ee7d1da4e06fb7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-13 16:38:39 +0530

    Time: 7 ms (87.29%), Space: 54.8 MB (73.22%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,21 +1,36 @@
 class Solution {
-    public void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] != 1) {
-            return;
-        }
+    public void bfs(int[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
         grid[i][j] = 0;
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int x = cell[0], y = cell[1];
+            
+            for (int[] dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                
+                if (nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 1) {
+                    grid[nx][ny] = 0;
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
     }
+    
     public int numEnclaves(int[][] grid) {
         int row = grid.length, col = grid[0].length;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (i == 0 || j == 0 || i == row - 1 || j == col - 1) {
                     if (grid[i][j] == 1) {
-                        dfs(grid, i, j);
+                        bfs(grid, i, j);
                     }
                 }
             }
@@ -31,3 +46,39 @@ class Solution {
         return count;
     }
 }
+
+
+
+// class Solution {
+//     public void dfs(int[][] grid, int i, int j) {
+//         if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] != 1) {
+//             return;
+//         }
+//         grid[i][j] = 0;
+//         dfs(grid, i+1, j);
+//         dfs(grid, i-1, j);
+//         dfs(grid, i, j+1);
+//         dfs(grid, i, j-1);
+//     }
+//     public int numEnclaves(int[][] grid) {
+//         int row = grid.length, col = grid[0].length;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (i == 0 || j == 0 || i == row - 1 || j == col - 1) {
+//                     if (grid[i][j] == 1) {
+//                         dfs(grid, i, j);
+//                     }
+//                 }
+//             }
+//         }
+//         int count = 0;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (grid[i][j] == 1) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
