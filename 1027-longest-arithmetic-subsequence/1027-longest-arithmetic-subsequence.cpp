912272faf271f8688a7ea9d5d32849637c59cc03
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-14 13:52:02 +0530

    Time: 103 ms (71.64%), Space: 144 MB (75.13%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,34 +1,56 @@
 class Solution {
 public:
-    int n;
-    int dp[1001][1003];
-    int solve(vector<int> &nums, int i, int diff) {
-        if (i < 0) {
-            return 0;
-        }
-        if (dp[i][diff+501] != -1) {
-            return dp[i][diff + 501];
-        }
-        int ans = 0;
-        for (int k = i - 1; k >= 0; k--) {
-            if (nums[i] - nums[k] == diff) {
-                ans = max(ans, 1 + solve(nums, k, diff));
-            }
-        }
-        return dp[i][diff + 501] = ans;
-    }
     int longestArithSeqLength(vector<int>& nums) {
-        n = nums.size();
+        int n = nums.size();
         if (n <= 2) {
             return n;
         }
-        memset(dp, -1, sizeof(dp));
+        vector<vector<int>> dp(n, vector<int>(1001, 0));
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                ans = max(ans, 2 + solve(nums, i, nums[j]-nums[i]));
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j] + 500;
+                dp[i][diff] = dp[j][diff] > 0 ? dp[j][diff] + 1: 2;
+                ans = max(ans, dp[i][diff]);
             }
         }
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int n;
+//     int dp[1001][1003];
+//     int solve(vector<int> &nums, int i, int diff) {
+//         if (i < 0) {
+//             return 0;
+//         }
+//         if (dp[i][diff+501] != -1) {
+//             return dp[i][diff + 501];
+//         }
+//         int ans = 0;
+//         for (int k = i - 1; k >= 0; k--) {
+//             if (nums[i] - nums[k] == diff) {
+//                 ans = max(ans, 1 + solve(nums, k, diff));
+//             }
+//         }
+//         return dp[i][diff + 501] = ans;
+//     }
+//     int longestArithSeqLength(vector<int>& nums) {
+//         n = nums.size();
+//         if (n <= 2) {
+//             return n;
+//         }
+//         memset(dp, -1, sizeof(dp));
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i+1; j < n; j++) {
+//                 ans = max(ans, 2 + solve(nums, i, nums[j]-nums[i]));
+//             }
+//         }
+//         return ans;
+//     }
+// };
