8c239ef80b1190ccadf853d76f9685c8fda01026
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-15 15:08:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> vec(n);
+        for (int i = 0; i < n; i++) {
+            vec[i] = {capital[i], profits[i]};
+        }
+        sort(vec.begin(), vec.end());
+        int i = 0;
+        priority_queue<int> pq;
+        while (k--) {
+            while (i < n && vec[i].first <= w) {
+                pq.push(vec[i].second);
+                i++;
+            }
+            w += pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
+
+
+
+// heap-buffer-overflow
+// class Solution {
+// public:
+//     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+//         vector<pair<int, int>> vec(n);
+//         int n = profits.size();
+//         for (int i = 0; i < n; i++) {
+//             vec[i[ = {capital[i], profits[i]};
+//         }
+
+//         sort(vec.begin(), vec.end(), [](pair<int, int> &a, pair<int, int> &b) {
+//             if (a.first == b.first) {
+//                 return a.second > b.second;
+//             }
+//             return a.first < b.first;
+//         });
+        
+//         int totalProfit = 0;
+//         for (int i = 0; i < k; i++) {
+//             pair<int, int> temp = vec[i];
+//             if (temp.first <= w) {
+//                 w += temp.second;
+//             } else {
+//                 break;
+//             }
+//         }
+//         return w;
+//     }
+// };
