d510793ce87ea5582650de974406c94adbc36d00
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-15 16:21:46 +0530

    Time: 91 ms (98.11%), Space: 16.6 MB (98.87%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,11 +3,21 @@ class Solution:
         self.row = 0
         self.col = 0
 
-    def dfs(self, grid: List[List[int]], i: int, j: int) -> int:
-        if i < 0 or j < 0 or i >= self.row or j >= self.col or grid[i][j] == 0:
-            return 0
+    def bfs(self, grid: List[List[int]], i: int, j: int) -> int:
+        q = deque()
         grid[i][j] = 0
-        return 1 + self.dfs(grid, i+1, j) + self.dfs(grid, i-1, j) + self.dfs(grid, i, j+1) + self.dfs(grid, i, j-1)
+        q.append([i, j])
+        area = 1
+        while q:
+            x, y = q.popleft()
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.row and 0 <= ny < self.col and grid[nx][ny] == 1:
+                    grid[nx][ny] = 0
+                    area += 1
+                    q.append([nx, ny])
+        return area
+
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         self.row = len(grid)
@@ -16,5 +26,28 @@ class Solution:
         for i in range(self.row):
             for j in range(self.col):
                 if grid[i][j] == 1:
-                    ans = max(ans, self.dfs(grid, i, j))
+                    ans = max(ans, self.bfs(grid, i, j))
         return ans
+
+
+
+# class Solution:
+#     def __init__(self):
+#         self.row = 0
+#         self.col = 0
+
+#     def dfs(self, grid: List[List[int]], i: int, j: int) -> int:
+#         if i < 0 or j < 0 or i >= self.row or j >= self.col or grid[i][j] == 0:
+#             return 0
+#         grid[i][j] = 0
+#         return 1 + self.dfs(grid, i+1, j) + self.dfs(grid, i-1, j) + self.dfs(grid, i, j+1) + self.dfs(grid, i, j-1)
+
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+#         self.row = len(grid)
+#         self.col = len(grid[0])
+#         ans = 0
+#         for i in range(self.row):
+#             for j in range(self.col):
+#                 if grid[i][j] == 1:
+#                     ans = max(ans, self.dfs(grid, i, j))
+#         return ans
