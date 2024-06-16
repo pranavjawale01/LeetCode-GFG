8dd207a10544341b9fac28978bc34b20790f30fa
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-16 10:38:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> getPrimes(int n) {
+        // code here
+        vector<bool> prime(n+1, true);
+        prime[0] = false;
+        prime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i]) {
+                for (int j = i * i ; j <= n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        vector<int> ans(2, -1);
+        for (int i = 2; i <= n; i++) {
+            if (prime[i] && prime[n - i]) {
+                ans[0] = i;
+                ans[1] = n - i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        vector<int> res = obj.getPrimes(n);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
