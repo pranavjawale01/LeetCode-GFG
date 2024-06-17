5c80ae7e081d78583a337be501ba255b4a7634bf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-17 15:51:44 +0530

    Time: 6 ms (48.9%), Space: 46 MB (16.91%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -0,0 +1,47 @@
+class Solution {
+    private int m, n;
+    private final int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    private void dfs(int[][] heights, boolean[][] reach, int i, int j) {
+        reach[i][j] = true;
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (x < 0 || y < 0 || x >= m || y >= n || reach[x][y] || heights[x][y] < heights[i][j]) {
+                continue;
+            }
+            dfs(heights, reach, x, y);
+        }
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        m = heights.length;
+        n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j);
+            dfs(heights, atlantic, m - 1, j);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> cell = new ArrayList<>();
+                    cell.add(i);
+                    cell.add(j);
+                    result.add(cell);
+                }
+            }
+        }
+
+        return result;
+    }
+}
