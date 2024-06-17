e1e4641a4153dfad86a9894f506f806f53522219
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-17 10:31:47 +0530

    Time: 1748 ms (5%), Space: 16.5 MB (79.87%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,9 +1,30 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        a = 0
-        while a * a <= c:
-            b = sqrt(c - a * a)
-            if b == int(b):
+        for a in range(int(c**0.5) + 1):
+            x = c - a * a
+            if self.binarySearch(0, x, x):
                 return True
-            a += 1
         return False
+
+    def binarySearch(self, b_start: int, b_end: int, x: int) -> bool:
+        while b_start <= b_end:
+            mid = b_start + (b_end - b_start) // 2
+            if mid * mid == x:
+                return True
+            elif mid * mid < x:
+                b_start = mid + 1
+            else:
+                b_end = mid - 1
+        return False
+
+
+
+# class Solution:
+#     def judgeSquareSum(self, c: int) -> bool:
+#         a = 0
+#         while a * a <= c:
+#             b = sqrt(c - a * a)
+#             if b == int(b):
+#                 return True
+#             a += 1
+#         return False
