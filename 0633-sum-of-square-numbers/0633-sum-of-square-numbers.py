41d600aa650908a0518dd5cf4437e88ce2907e23
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-17 10:32:07 +0530

    Time: 79 ms (89.03%), Space: 16.4 MB (79.87%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,21 +1,40 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        for a in range(int(c**0.5) + 1):
-            x = c - a * a
-            if self.binarySearch(0, x, x):
+        a = 0
+        b = int(math.sqrt(c))
+
+        while a <= b:
+            k = a * a + b * b
+
+            if k < c:
+                a += 1
+            elif k > c:
+                b -= 1
+            else:
                 return True
+
         return False
 
-    def binarySearch(self, b_start: int, b_end: int, x: int) -> bool:
-        while b_start <= b_end:
-            mid = b_start + (b_end - b_start) // 2
-            if mid * mid == x:
-                return True
-            elif mid * mid < x:
-                b_start = mid + 1
-            else:
-                b_end = mid - 1
-        return False
+
+
+# class Solution:
+#     def judgeSquareSum(self, c: int) -> bool:
+#         for a in range(int(c**0.5) + 1):
+#             x = c - a * a
+#             if self.binarySearch(0, x, x):
+#                 return True
+#         return False
+
+#     def binarySearch(self, b_start: int, b_end: int, x: int) -> bool:
+#         while b_start <= b_end:
+#             mid = b_start + (b_end - b_start) // 2
+#             if mid * mid == x:
+#                 return True
+#             elif mid * mid < x:
+#                 b_start = mid + 1
+#             else:
+#                 b_end = mid - 1
+#         return False
 
 
 
