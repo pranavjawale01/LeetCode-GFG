9ddb3ebdd77b93d83ccf2cd35fa5ace11bc59543
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-18 14:28:18 +0530

    Time: 44 ms (81.96%), Space: 41.1 MB (66.37%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -8,28 +8,55 @@ public:
             vec.push_back({difficulty[i], profit[i]});
         }
         sort(vec.begin(), vec.end());
-        for (int i = 1; i < vec.size(); i++) {
-            vec[i].second = max(vec[i].second, vec[i-1].second);
-        }
+        sort(worker.begin(), worker.end());
         int totalProfit = 0;
+        int j = 0;
+        int maxProfit = 0;
         for (int i = 0; i < m; i++) {
-            int level = worker[i];
-            int l = 0, r = vec.size() - 1;
-            int maxProfit = 0;
-            while (l <= r) {
-                int mid = l + (r - l) / 2;
-                if (vec[mid].first <= level) {
-                    maxProfit = max(maxProfit, vec[mid].second);
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+            while (j < n && worker[i] >= vec[j].first) {
+                maxProfit = max(maxProfit, vec[j].second);
+                j++;
             }
             totalProfit += maxProfit;
         }
         return totalProfit;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+//         int n = profit.size();
+//         int m = worker.size();
+//         vector<pair<int, int>> vec;
+//         for (int i = 0; i < n; i++) {
+//             vec.push_back({difficulty[i], profit[i]});
+//         }
+//         sort(vec.begin(), vec.end());
+//         for (int i = 1; i < vec.size(); i++) {
+//             vec[i].second = max(vec[i].second, vec[i-1].second);
+//         }
+//         int totalProfit = 0;
+//         for (int i = 0; i < m; i++) {
+//             int level = worker[i];
+//             int l = 0, r = vec.size() - 1;
+//             int maxProfit = 0;
+//             while (l <= r) {
+//                 int mid = l + (r - l) / 2;
+//                 if (vec[mid].first <= level) {
+//                     maxProfit = max(maxProfit, vec[mid].second);
+//                     l = mid + 1;
+//                 } else {
+//                     r = mid - 1;
+//                 }
+//             }
+//             totalProfit += maxProfit;
+//         }
+//         return totalProfit;
+//     }
+// };
 
 
 
