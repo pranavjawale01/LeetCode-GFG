a379234df269663f86e54f0174555e42a224461e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-18 14:27:49 +0530

    Time: 425 ms (11.5%), Space: 19.3 MB (93.65%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,17 +1,45 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        pq = []
         n = len(profit)
         m = len(worker)
+        vec = []
         for i in range(n):
-            heapq.heappush(pq, (-profit[i], difficulty[i]))
-        worker.sort()
+            vec.append([difficulty[i], profit[i]])
+        vec.sort()
+        for i in range(1, len(vec)):
+            vec[i][1] = max(vec[i][1], vec[i-1][1])
         totalProfit = 0
-        i = m - 1
-        while pq and i >= 0:
-            if pq[0][1] > worker[i]:
-                heapq.heappop(pq)
-            else:
-                totalProfit += -pq[0][0]
-                i -= 1
+        for i in range(m):
+            level = worker[i]
+            l = 0
+            r = len(vec) - 1
+            maxProfit = 0
+            while l <= r:
+                mid = l + (r - l) // 2
+                if vec[mid][0] <= level:
+                    maxProfit = max(maxProfit, vec[mid][1])
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            totalProfit += maxProfit
         return totalProfit
+
+
+
+# class Solution:
+#     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+#         pq = []
+#         n = len(profit)
+#         m = len(worker)
+#         for i in range(n):
+#             heapq.heappush(pq, (-profit[i], difficulty[i]))
+#         worker.sort()
+#         totalProfit = 0
+#         i = m - 1
+#         while pq and i >= 0:
+#             if pq[0][1] > worker[i]:
+#                 heapq.heappop(pq)
+#             else:
+#                 totalProfit += -pq[0][0]
+#                 i -= 1
+#         return totalProfit
