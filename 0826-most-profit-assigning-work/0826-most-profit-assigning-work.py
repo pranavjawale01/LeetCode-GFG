a49b415ae98e9291d497301418139fcb63902577
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-18 14:29:14 +0530

    Time: 310 ms (38.73%), Space: 19.6 MB (56.95%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,28 +1,48 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        n = len(profit)
-        m = len(worker)
-        vec = []
-        for i in range(n):
-            vec.append([difficulty[i], profit[i]])
-        vec.sort()
-        for i in range(1, len(vec)):
-            vec[i][1] = max(vec[i][1], vec[i-1][1])
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+        
         totalProfit = 0
-        for i in range(m):
-            level = worker[i]
-            l = 0
-            r = len(vec) - 1
-            maxProfit = 0
-            while l <= r:
-                mid = l + (r - l) // 2
-                if vec[mid][0] <= level:
-                    maxProfit = max(maxProfit, vec[mid][1])
-                    l = mid + 1
-                else:
-                    r = mid - 1
+        maxProfit = 0
+        j = 0
+        n = len(jobs)
+        
+        for level in worker:
+            while j < n and level >= jobs[j][0]:
+                maxProfit = max(maxProfit, jobs[j][1])
+                j += 1
             totalProfit += maxProfit
+        
         return totalProfit
+
+
+
+# class Solution:
+#     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+#         n = len(profit)
+#         m = len(worker)
+#         vec = []
+#         for i in range(n):
+#             vec.append([difficulty[i], profit[i]])
+#         vec.sort()
+#         for i in range(1, len(vec)):
+#             vec[i][1] = max(vec[i][1], vec[i-1][1])
+#         totalProfit = 0
+#         for i in range(m):
+#             level = worker[i]
+#             l = 0
+#             r = len(vec) - 1
+#             maxProfit = 0
+#             while l <= r:
+#                 mid = l + (r - l) // 2
+#                 if vec[mid][0] <= level:
+#                     maxProfit = max(maxProfit, vec[mid][1])
+#                     l = mid + 1
+#                 else:
+#                     r = mid - 1
+#             totalProfit += maxProfit
+#         return totalProfit
 
 
 
