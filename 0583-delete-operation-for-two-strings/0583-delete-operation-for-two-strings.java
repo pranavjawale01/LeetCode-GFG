e0357c31aa8de19a00edfe27df4be6d645125d45
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-19 13:27:09 +0530

    Time: 7 ms (80.53%), Space: 44.7 MB (24.79%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,25 +1,52 @@
 class Solution {
-    private int[][] dp = new int[501][501];
-    private int solve(String word1, String word2, int i, int j) {
-        if (i == 0) {
-            return j;
-        }
-        if (j == 0) {
-            return i;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (word1.charAt(i-1) == word2.charAt(j-1)) {
-            return dp[i][j] = solve(word1, word2, i-1, j-1);
-        }
-        return dp[i][j] = 1 + Math.min(solve(word1, word2, i-1, j), solve(word1, word2, i, j-1));
-    }
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
         for (int i = 0; i <= m; i++) {
-            Arrays.fill(dp[i], -1);
+            dp[i][0] = i;
         }
-        return solve(word1, word2, m, n);
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
+
+
+
+// class Solution {
+//     private int[][] dp = new int[501][501];
+//     private int solve(String word1, String word2, int i, int j) {
+//         if (i == 0) {
+//             return j;
+//         }
+//         if (j == 0) {
+//             return i;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         if (word1.charAt(i-1) == word2.charAt(j-1)) {
+//             return dp[i][j] = solve(word1, word2, i-1, j-1);
+//         }
+//         return dp[i][j] = 1 + Math.min(solve(word1, word2, i-1, j), solve(word1, word2, i, j-1));
+//     }
+//     public int minDistance(String word1, String word2) {
+//         int m = word1.length(), n = word2.length();
+//         for (int i = 0; i <= m; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return solve(word1, word2, m, n);
+//     }
+// }
