daf9b3fc4af1555c2c7c55e4908c952dbf2a13c2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-19 12:55:44 +0530

    Time: 30 ms (16.46%), Space: 50.9 MB (25.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,31 +1,28 @@
 class Solution {
-    private int[][] memory;
+    private int[][] t;
+
+    public int LCS(String s1, String s2, int m, int n) {
+        if (m == 0 || n == 0)
+            return t[m][n] = 0;
+
+        if (t[m][n] != -1)
+            return t[m][n];
+
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+            return t[m][n] = 1 + LCS(s1, s2, m - 1, n - 1);
+
+        return t[m][n] = Math.max(LCS(s1, s2, m, n - 1), LCS(s1, s2, m - 1, n));
+    }
 
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
 
-        memory = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            Arrays.fill(memory[i], -1);
+        t = new int[m + 1][n + 1];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
         }
 
-        return solve(text1, text2, 0, 0);
-    }
-
-    private int solve(String s1, String s2, int i, int j) {
-        if (i >= s1.length() || j >= s2.length()) {
-            return 0;
-        }
-
-        if (memory[i][j] != -1) {
-            return memory[i][j];
-        }
-
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return memory[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
-        }
-
-        return memory[i][j] = Math.max(solve(s1, s2, i + 1, j), solve(s1, s2, i, j + 1));
+        return LCS(text1, text2, m, n);
     }
 }
