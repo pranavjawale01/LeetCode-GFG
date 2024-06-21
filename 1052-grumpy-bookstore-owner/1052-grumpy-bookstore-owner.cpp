531fc709a402f0da078a8b86de9147f7db3b2c6d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 12:11:56 +0530

    Time: 22 ms (72.6%), Space: 34.4 MB (47.97%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int maxUnSat = 0;
+        int curUnSat = 0;
         int n = grumpy.size();
-        int ans = 0;
-
+        for (int i = 0; i < minutes; i++) {
+            curUnSat += customers[i] * grumpy[i];
+        }
+        maxUnSat = curUnSat;
+        int i = 0, j = minutes;
+        while (j < n) {
+            curUnSat += customers[j] * grumpy[j];
+            curUnSat -= customers[i] * grumpy[i];
+            maxUnSat = max(maxUnSat, curUnSat);
+            i++;
+            j++;
+        }
+        int totalSat = maxUnSat;
         for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) {
-                ans += customers[i];
-            }
+            totalSat += customers[i] * (1 - grumpy[i]);
         }
-        int temp1 = ans;
-        for (int i = 0; i <= n - minutes; i++) {
-            int temp = temp1;
-            for (int j = i; j < i + minutes; j++) {
-                if (grumpy[j] == 1) {
-                    temp += customers[j];
-                }
-            }
-            ans = max(ans, temp);
-        }
-        return ans;
+        return totalSat;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+//         int n = grumpy.size();
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (grumpy[i] == 0) {
+//                 ans += customers[i];
+//             }
+//         }
+//         int temp1 = ans;
+//         for (int i = 0; i <= n - minutes; i++) {
+//             int temp = temp1;
+//             for (int j = i; j < i + minutes; j++) {
+//                 if (grumpy[j] == 1) {
+//                     temp += customers[j];
+//                 }
+//             }
+//             ans = max(ans, temp);
+//         }
+//         return ans;
+//     }
+// };
