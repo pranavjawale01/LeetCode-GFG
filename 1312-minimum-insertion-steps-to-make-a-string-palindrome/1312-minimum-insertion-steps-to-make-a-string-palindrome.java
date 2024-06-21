d4e061e77f80e776471144045e1caba34af8c01e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 14:41:43 +0530

    Time: 25 ms (65.86%), Space: 45 MB (80.97%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,43 @@
 class Solution {
     public int minInsertions(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
         int n = s.length();
-        int[][] dp = new int[n][n];
-
-        for (int len = 2; len <= n; ++len) {
-            for (int i = 0; i <= n - len; ++i) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1];
+        int[][] dp = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s.charAt(i-1) == reversed.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-
-        return dp[0][n - 1];
+        return n - dp[n][n];
     }
 }
+
+
+
+
+// class Solution {
+//     public int minInsertions(String s) {
+//         int n = s.length();
+//         int[][] dp = new int[n][n];
+
+//         for (int len = 2; len <= n; ++len) {
+//             for (int i = 0; i <= n - len; ++i) {
+//                 int j = i + len - 1;
+//                 if (s.charAt(i) == s.charAt(j)) {
+//                     dp[i][j] = dp[i + 1][j - 1];
+//                 } else {
+//                     dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+//                 }
+//             }
+//         }
+
+//         return dp[0][n - 1];
+//     }
+// }
 
 
 
