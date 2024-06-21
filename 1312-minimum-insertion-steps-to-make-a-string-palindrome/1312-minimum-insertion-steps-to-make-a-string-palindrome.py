9e1d2132d0e9e4d1326f5bda591a3034b434f6c4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 14:42:32 +0530

    Time: 701 ms (32.87%), Space: 18.5 MB (60.43%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,17 +1,33 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        reversed_s = s[::-1]
         n = len(s)
-        dp = [[0] * n for _ in range(n)]
-
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1]
+        dp = [[0 for j in range(n+1)] for i in range(n+1)]
+        
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s[i-1] == reversed_s[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return n - dp[n][n]
 
-        return dp[0][n - 1]
+
+
+# class Solution:
+#     def minInsertions(self, s: str) -> int:
+#         n = len(s)
+#         dp = [[0] * n for _ in range(n)]
+
+#         for length in range(2, n + 1):
+#             for i in range(n - length + 1):
+#                 j = i + length - 1
+#                 if s[i] == s[j]:
+#                     dp[i][j] = dp[i + 1][j - 1]
+#                 else:
+#                     dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1])
+
+#         return dp[0][n - 1]
 
 
 
