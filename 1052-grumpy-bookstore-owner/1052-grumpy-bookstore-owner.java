80ef003067c7c964126e05cabf6fb56ff8c193ff
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 12:14:14 +0530

    Time: 3 ms (91.49%), Space: 47.2 MB (67.88%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,22 +1,50 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int maxUnSat = 0;
+        int curUnSat = 0;
         int n = grumpy.length;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) {
-                ans += customers[i];
-            }
+        for (int i = 0; i < minutes; i++) {
+            curUnSat += customers[i] * grumpy[i];
         }
-        int temp1 = ans;
-        for (int i = 0; i <= n - minutes; i++) {
-            int temp = temp1;
-            for (int j = i; j < i + minutes; j++) {
-                if (grumpy[j] == 1) {
-                    temp += customers[j];
-                }
-            }
-            ans = Math.max(ans, temp);
+        maxUnSat = curUnSat;
+        int i = 0, j = minutes;
+        while (j < n) {
+            curUnSat += customers[j] * grumpy[j];
+            curUnSat -= customers[i] * grumpy[i];
+            maxUnSat = Math.max(maxUnSat, curUnSat);
+            i++;
+            j++;
         }
-        return ans;
+        int totalSat = maxUnSat;
+        for (int k = 0; k < n; k++) {
+            totalSat += customers[k] * (1 - grumpy[k]);
+        }
+        return totalSat;
     }
 }
+
+
+
+
+// class Solution {
+//     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+//         int n = grumpy.length;
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (grumpy[i] == 0) {
+//                 ans += customers[i];
+//             }
+//         }
+//         int temp1 = ans;
+//         for (int i = 0; i <= n - minutes; i++) {
+//             int temp = temp1;
+//             for (int j = i; j < i + minutes; j++) {
+//                 if (grumpy[j] == 1) {
+//                     temp += customers[j];
+//                 }
+//             }
+//             ans = Math.max(ans, temp);
+//         }
+//         return ans;
+//     }
+// }
