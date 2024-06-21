f2c72913982f578934ded3e5cf94e6d36f124095
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 14:40:45 +0530

    Time: 367 ms (82.7%), Space: 18.4 MB (60.43%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,16 +1,33 @@
 class Solution:
-    def solve(self, s: str, i: int, j: int) -> int:
-        if i > j:
-            return 0
-        if self.dp[i][j] != -1:
-            return self.dp[i][j]
-        if s[i] == s[j]:
-            self.dp[i][j] = self.solve(s, i+1, j-1)
-            return self.dp[i][j]
-        self.dp[i][j] = 1 + min(self.solve(s, i+1, j), self.solve(s, i, j-1))
-        return self.dp[i][j]
-
     def minInsertions(self, s: str) -> int:
-        self.n = len(s)
-        self.dp = [[-1 for _ in range(self.n)] for _ in range(self.n)]
-        return self.solve(s, 0, self.n-1)
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1])
+
+        return dp[0][n - 1]
+
+
+
+# class Solution:
+#     def solve(self, s: str, i: int, j: int) -> int:
+#         if i > j:
+#             return 0
+#         if self.dp[i][j] != -1:
+#             return self.dp[i][j]
+#         if s[i] == s[j]:
+#             self.dp[i][j] = self.solve(s, i+1, j-1)
+#             return self.dp[i][j]
+#         self.dp[i][j] = 1 + min(self.solve(s, i+1, j), self.solve(s, i, j-1))
+#         return self.dp[i][j]
+
+#     def minInsertions(self, s: str) -> int:
+#         self.n = len(s)
+#         self.dp = [[-1 for _ in range(self.n)] for _ in range(self.n)]
+#         return self.solve(s, 0, self.n-1)
