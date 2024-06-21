eb9fc8d801ff39dc43e20cc7c08d92e812a109d2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-21 12:14:32 +0530

    Time: 219 ms (45.49%), Space: 18.8 MB (82.99%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,15 +1,38 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        maxUnSat = 0
+        curUnSat = 0
         n = len(grumpy)
-        ans = 0
-        for i in range(n):
-            if grumpy[i] == 0:
-                ans += customers[i]
-        temp1 = ans
-        for i in range(n-minutes+1):
-            temp = temp1
-            for j in range(i, i+minutes):
-                if grumpy[j] == 1:
-                    temp += customers[j]
-            ans = max(ans, temp)
-        return ans
+        for i in range(minutes):
+            curUnSat += customers[i] * grumpy[i]
+        maxUnSat = curUnSat
+        i = 0
+        j = minutes
+        while j < n:
+            curUnSat += customers[j] * grumpy[j]
+            curUnSat -= customers[i] * grumpy[i]
+            maxUnSat = max(maxUnSat, curUnSat)
+            i += 1
+            j += 1
+        totalSat = maxUnSat
+        for k in range(n):
+            totalSat += customers[k] * (1 - grumpy[k])
+        return totalSat
+
+
+
+# class Solution:
+#     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+#         n = len(grumpy)
+#         ans = 0
+#         for i in range(n):
+#             if grumpy[i] == 0:
+#                 ans += customers[i]
+#         temp1 = ans
+#         for i in range(n-minutes+1):
+#             temp = temp1
+#             for j in range(i, i+minutes):
+#                 if grumpy[j] == 1:
+#                     temp += customers[j]
+#             ans = max(ans, temp)
+#         return ans
