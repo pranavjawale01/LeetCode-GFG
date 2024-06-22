75ae649d335ac3a13d6e2c71ff3da6dacbce5185
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-22 12:25:26 +0530

    Time: 100 ms (40.7%), Space: 69.9 MB (65.04%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        int odd = 0;
-        int count = 0;
+    int solve(vector<int> &nums, int k) {
+        int count = 0, ans = 0, start = 0, end = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 1) {
-                odd++;
-            }
-            if (odd == k) {
+        while (end < n) {
+            if (nums[end] % 2 == 1) {
                 count++;
             }
-            if (mp.find(odd - k) != mp.end()) {
-                count += mp[odd - k];
+            while (count > k) {
+                if (nums[start] % 2 == 1) {
+                    count--;
+                }
+                start++;
             }
-            if (mp.find(odd) != mp.end()) {
-                mp[odd]++;
-            } else {
-                mp[odd] = 1;
-            }
+            ans += end - start + 1;
+            end++;
         }
-        return count;
+        return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return solve(nums, k) - solve(nums, k-1);
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+//         int odd = 0;
+//         int count = 0;
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] % 2 == 1) {
+//                 odd++;
+//             }
+//             if (odd == k) {
+//                 count++;
+//             }
+//             if (mp.find(odd - k) != mp.end()) {
+//                 count += mp[odd - k];
+//             }
+//             if (mp.find(odd) != mp.end()) {
+//                 mp[odd]++;
+//             } else {
+//                 mp[odd] = 1;
+//             }
+//         }
+//         return count;
+//     }
+// };
 
 
 
