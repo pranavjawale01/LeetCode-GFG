67ef197880fceb9f80bfbe0183b347873d49f6b5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-22 12:29:32 +0530

    Time: 13 ms (51.25%), Space: 54.9 MB (45.8%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,25 +1,51 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        int odd = 0;
-        int count = 0;
+    private int solve(int[] nums, int k) {
+        int count = 0, ans = 0, start = 0, end = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 1) {
-                odd++;
-            }
-            if (odd == k) {
+        while (end < n) {
+            if (nums[end] % 2 == 1) {
                 count++;
             }
-            if (mp.containsKey(odd - k)) {
-                count += mp.get(odd - k);
+            while (count > k) {
+                if (nums[start] % 2 == 1) {
+                    count--;
+                }
+                start++;
             }
-            if (mp.containsKey(odd)) {
-                mp.put(odd, mp.getOrDefault(odd, 0) + 1);
-            } else {
-                mp.put(odd, 1);
-            }
+            ans += end - start + 1;
+            end++;
         }
-        return count;
+        return ans;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+        return solve(nums, k) - solve(nums, k-1);
     }
 }
+
+
+
+// class Solution {
+//     public int numberOfSubarrays(int[] nums, int k) {
+//         Map<Integer, Integer> mp = new HashMap<>();
+//         int odd = 0;
+//         int count = 0;
+//         int n = nums.length;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] % 2 == 1) {
+//                 odd++;
+//             }
+//             if (odd == k) {
+//                 count++;
+//             }
+//             if (mp.containsKey(odd - k)) {
+//                 count += mp.get(odd - k);
+//             }
+//             if (mp.containsKey(odd)) {
+//                 mp.put(odd, mp.getOrDefault(odd, 0) + 1);
+//             } else {
+//                 mp.put(odd, 1);
+//             }
+//         }
+//         return count;
+//     }
+// }
