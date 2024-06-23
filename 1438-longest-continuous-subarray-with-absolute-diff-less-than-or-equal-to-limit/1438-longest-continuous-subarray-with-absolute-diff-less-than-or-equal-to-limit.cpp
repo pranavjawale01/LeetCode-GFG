1ff4b8b2a1d6722986aa68f89f64d4a3b0d27028
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-23 12:09:50 +0530

    Time: 123 ms (43.71%), Space: 81 MB (30.8%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,28 +2,51 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        priority_queue<pair<int, int>> mpq;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> lpq;
-        
+        multiset<int> st;
         int i = 0, j = 0;
         int maxlen = 0;
         while (j < n) {
-            mpq.push({nums[j], j});
-            lpq.push({nums[j], j});
-
-            while (mpq.top().first - lpq.top().first > limit) {
-                i = min(mpq.top().second, lpq.top().second) + 1;
-                while (mpq.top().second < i) {
-                    mpq.pop();
-                }
-                while (lpq.top().second < i) {
-                    lpq.pop();
-                }
+            st.insert(nums[j]);
+            while (*st.rbegin() - *st.begin() > limit) {
+                st.erase(st.find(nums[i]));
+                i++;
             }
-
             maxlen = max(maxlen, j-i+1);
             j++;
         }
         return maxlen;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums, int limit) {
+//         int n = nums.size();
+//         priority_queue<pair<int, int>> mpq;
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> lpq;
+
+//         int i = 0, j = 0;
+//         int maxlen = 0;
+//         while (j < n) {
+//             mpq.push({nums[j], j});
+//             lpq.push({nums[j], j});
+
+//             while (mpq.top().first - lpq.top().first > limit) {
+//                 i = min(mpq.top().second, lpq.top().second) + 1;
+//                 while (mpq.top().second < i) {
+//                     mpq.pop();
+//                 }
+//                 while (lpq.top().second < i) {
+//                     lpq.pop();
+//                 }
+//             }
+
+//             maxlen = max(maxlen, j-i+1);
+//             j++;
+//         }
+//         return maxlen;
+//     }
+// };
