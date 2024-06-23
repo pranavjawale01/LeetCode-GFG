a66c9de567a8c970f06e21a954e23b109e982209
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-23 12:45:07 +0530

    Time: 74 ms (33.16%), Space: 56.4 MB (69.84%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,21 +1,16 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
         int n = nums.length;
-        PriorityQueue<int[]> mpq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        PriorityQueue<int[]> lpq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        int i = 0, j = 0;
-        int maxlen = 0;
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        int i = 0, j = 0, maxlen = 0;
         while (j < n) {
-            mpq.offer(new int[]{nums[j], j});
-            lpq.offer(new int[]{nums[j], j});
-            while (mpq.peek()[0] - lpq.peek()[0] > limit) {
-                i = Math.min(mpq.peek()[1], lpq.peek()[1]) + 1;
-                while (mpq.peek()[1] < i) {
-                    mpq.poll();
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            while (mp.lastKey() - mp.firstKey() > limit) {
+                mp.put(nums[i], mp.get(nums[i]) - 1);
+                if (mp.get(nums[i]) == 0) {
+                    mp.remove(nums[i]);
                 }
-                while (lpq.peek()[1] < i) {
-                    lpq.poll();
-                }
+                i++;
             }
             maxlen = Math.max(maxlen, j - i + 1);
             j++;
@@ -23,3 +18,32 @@ class Solution {
         return maxlen;
     }
 }
+
+
+
+
+// class Solution {
+//     public int longestSubarray(int[] nums, int limit) {
+//         int n = nums.length;
+//         PriorityQueue<int[]> mpq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+//         PriorityQueue<int[]> lpq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+//         int i = 0, j = 0;
+//         int maxlen = 0;
+//         while (j < n) {
+//             mpq.offer(new int[]{nums[j], j});
+//             lpq.offer(new int[]{nums[j], j});
+//             while (mpq.peek()[0] - lpq.peek()[0] > limit) {
+//                 i = Math.min(mpq.peek()[1], lpq.peek()[1]) + 1;
+//                 while (mpq.peek()[1] < i) {
+//                     mpq.poll();
+//                 }
+//                 while (lpq.peek()[1] < i) {
+//                     lpq.poll();
+//                 }
+//             }
+//             maxlen = Math.max(maxlen, j - i + 1);
+//             j++;
+//         }
+//         return maxlen;
+//     }
+// }
