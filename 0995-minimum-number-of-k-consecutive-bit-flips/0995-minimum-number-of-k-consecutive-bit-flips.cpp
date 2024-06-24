a7ac1dd21df38f311137c7bf82263badb714ca1c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-24 13:16:00 +0530

    Time: 109 ms (13.52%), Space: 112.2 MB (38.93%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,24 +2,53 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
-        int ans = 0, times = 0;
-        vector<int> dp(n, 0);
+        int flips = 0;
+        deque<int> flipq;
+        int flipcount = 0;
         for (int i = 0; i < n; i++) {
             if (i >= k) {
-                times -= dp[i-k];
+                flipcount -= flipq.front();
+                flipq.pop_front();
             }
-            if (nums[i] == 1 && times % 2 == 1 || nums[i] == 0 && times % 2 == 0) {
+            if (flipcount % 2 == nums[i]) {
                 if (i + k > n) {
                     return -1;
                 }
-                ans++;
-                times++;
-                dp[i] = 1;
+                flipcount++;
+                flipq.push_back(1);
+                flips++;
+            } else {
+                flipq.push_back(0);
             }
         }
-        return ans;
+        return flips;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int minKBitFlips(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int ans = 0, times = 0;
+//         vector<int> dp(n, 0);
+//         for (int i = 0; i < n; i++) {
+//             if (i >= k) {
+//                 times -= dp[i-k];
+//             }
+//             if (nums[i] == 1 && times % 2 == 1 || nums[i] == 0 && times % 2 == 0) {
+//                 if (i + k > n) {
+//                     return -1;
+//                 }
+//                 ans++;
+//                 times++;
+//                 dp[i] = 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 
