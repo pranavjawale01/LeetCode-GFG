a33df40b6f1f122aa44b5564f94651e55b7bdd97
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-24 13:28:08 +0530

    Time: 11 ms (44.52%), Space: 55.7 MB (78.71%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -2,21 +2,74 @@ class Solution {
     public int minKBitFlips(int[] nums, int k) {
         int n = nums.length;
         int flips = 0;
-        boolean[] isFlipped = new boolean[n];
+        Deque<Integer> flipq = new LinkedList<>();
         int flipCountFromPastForCurri = 0;
         for (int i = 0; i < n; i++) {
-            if (i >= k && isFlipped[i-k]) {
-                flipCountFromPastForCurri--;
+            if (i >= k) {
+                flipCountFromPastForCurri -= flipq.pollFirst();
             }
             if (flipCountFromPastForCurri % 2 == nums[i]) {
                 if (i + k > n) {
                     return -1;
                 }
                 flipCountFromPastForCurri++;
-                isFlipped[i] = true;
+                flipq.addLast(1);
                 flips++;
+            } else {
+                flipq.addLast(0);
             }
         }
         return flips;
     }
 }
+
+
+
+
+// class Solution {
+//     public int minKBitFlips(int[] nums, int k) {
+//         int n = nums.length;
+//         int flips = 0;
+//         int flipCountFromtPastCurri = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (i >= k && nums[i-k] == 2) {
+//                 flipCountFromtPastCurri--;
+//             }
+//             if (flipCountFromtPastCurri % 2 == nums[i]) {
+//                 if (i + k > n) {
+//                     return -1;
+//                 }
+//                 flipCountFromtPastCurri++;
+//                 nums[i] = 2;
+//                 flips++;
+//             }
+//         }
+//         return flips;
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public int minKBitFlips(int[] nums, int k) {
+//         int n = nums.length;
+//         int flips = 0;
+//         boolean[] isFlipped = new boolean[n];
+//         int flipCountFromPastForCurri = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (i >= k && isFlipped[i-k]) {
+//                 flipCountFromPastForCurri--;
+//             }
+//             if (flipCountFromPastForCurri % 2 == nums[i]) {
+//                 if (i + k > n) {
+//                     return -1;
+//                 }
+//                 flipCountFromPastForCurri++;
+//                 isFlipped[i] = true;
+//                 flips++;
+//             }
+//         }
+//         return flips;
+//     }
+// }
