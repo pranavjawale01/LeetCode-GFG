79ca3cff3530958a5dd28050a7c5cda7a6f21677
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-24 13:04:36 +0530

    Time: 96 ms (39.55%), Space: 113.2 MB (22.34%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = 0, times = 0;
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (i >= k) {
+                times -= dp[i-k];
+            }
+            if (nums[i] == 1 && times % 2 == 1 || nums[i] == 0 && times % 2 == 0) {
+                if (i + k > n) {
+                    return -1;
+                }
+                ans++;
+                times++;
+                dp[i] = 1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+// TLE
+// class Solution {
+// public:
+//     int minKBitFlips(vector<int>& nums, int k) {
+//         int flip = 0;
+//         int n = nums.size();
+        
+//         for (int i = 0; i <= n - k; i++) {
+//             if (nums[i] == 0) {
+//                 for (int j = i; j < i + k; j++) {
+//                     nums[j] ^= 1;
+//                 }
+//                 flip++;
+//             }
+//         }
+        
+//         for (int i = n - k + 1; i < n; i++) {
+//             if (nums[i] == 0) {
+//                 return -1;
+//             }
+//         }
+        
+//         return flip;
+//     }
+// };
