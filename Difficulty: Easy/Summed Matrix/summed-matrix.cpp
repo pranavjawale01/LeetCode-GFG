a2da68b19bfe192ec0d7f4809707c8cbba89cdb9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-24 10:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        if (q > 2 * n || q < 2) {
+            return 0;
+        }
+        if (q + 1 <= n) {
+            return q - 1;
+        }
+        return 2 * n - q + 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
