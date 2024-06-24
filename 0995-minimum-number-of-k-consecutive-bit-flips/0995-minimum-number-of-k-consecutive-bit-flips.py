2ecd39a77224394aaf293632b5efcf14f94543ec
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-24 13:31:38 +0530

    Time: 794 ms (64.08%), Space: 19.6 MB (76.06%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -2,18 +2,40 @@ class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         n = len(nums)
         flips = 0
-        isFlipped = [False] * n
         flipCountFromPastForCurri = 0
 
         for i in range(n):
-            if i >= k and isFlipped[i - k]:
+            if i >= k and nums[i - k] == 2:
                 flipCountFromPastForCurri -= 1
 
             if flipCountFromPastForCurri % 2 == nums[i]:
                 if i + k > n:
                     return -1
                 flipCountFromPastForCurri += 1
-                isFlipped[i] = True
+                nums[i] = 2
                 flips += 1
 
         return flips
+
+
+
+
+# class Solution:
+#     def minKBitFlips(self, nums: List[int], k: int) -> int:
+#         n = len(nums)
+#         flips = 0
+#         isFlipped = [False] * n
+#         flipCountFromPastForCurri = 0
+
+#         for i in range(n):
+#             if i >= k and isFlipped[i - k]:
+#                 flipCountFromPastForCurri -= 1
+
+#             if flipCountFromPastForCurri % 2 == nums[i]:
+#                 if i + k > n:
+#                     return -1
+#                 flipCountFromPastForCurri += 1
+#                 isFlipped[i] = True
+#                 flips += 1
+
+#         return flips
