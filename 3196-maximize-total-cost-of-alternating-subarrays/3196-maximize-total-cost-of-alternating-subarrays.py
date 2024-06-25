c73f2c2fcf6c6d2a7211d5b553b45874266b14ce
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-25 14:14:54 +0530

    Time: 725 ms (50.94%), Space: 54.1 MB (54.57%) - LeetHub

---
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumTotalCost(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [[-1, -1] for _ in range(n)]
+
+        def solve(idx: int, pos: bool) -> int:
+            if idx >= n:
+                return 0
+
+            if dp[idx][pos] != -1:
+                return dp[idx][pos]
+
+            if pos:
+                dp[idx][pos] = nums[idx] + solve(idx + 1, False)
+            else:
+                take = solve(idx + 1, True) - nums[idx]
+                not_take = solve(idx + 1, False) + nums[idx]
+                dp[idx][pos] = max(take, not_take)
+
+            return dp[idx][pos]
+
+        return solve(0, True)
