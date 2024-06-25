5e8ccb87ad6d9c95202afb70661eb7f936ce57c7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-25 14:17:57 +0530

    Time: 503 ms (81.56%), Space: 30.9 MB (81.21%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.py
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.py
@@ -1,22 +1,35 @@
 class Solution:
     def maximumTotalCost(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[-1, -1] for _ in range(n)]
+        dp = [-float('inf')] * (n+1)
+        dp[0] = 0
+        dp[1] = nums[0]
+        for i in range(1, n):
+            dp[i+1] = max(dp[i]+nums[i], dp[i-1] + nums[i-1] - nums[i])
+        return dp[n]
 
-        def solve(idx: int, pos: bool) -> int:
-            if idx >= n:
-                return 0
 
-            if dp[idx][pos] != -1:
-                return dp[idx][pos]
 
-            if pos:
-                dp[idx][pos] = nums[idx] + solve(idx + 1, False)
-            else:
-                take = solve(idx + 1, True) - nums[idx]
-                not_take = solve(idx + 1, False) + nums[idx]
-                dp[idx][pos] = max(take, not_take)
 
-            return dp[idx][pos]
+# class Solution:
+#     def maximumTotalCost(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         dp = [[-1, -1] for _ in range(n)]
 
-        return solve(0, True)
+#         def solve(idx: int, pos: bool) -> int:
+#             if idx >= n:
+#                 return 0
+
+#             if dp[idx][pos] != -1:
+#                 return dp[idx][pos]
+
+#             if pos:
+#                 dp[idx][pos] = nums[idx] + solve(idx + 1, False)
+#             else:
+#                 take = solve(idx + 1, True) - nums[idx]
+#                 not_take = solve(idx + 1, False) + nums[idx]
+#                 dp[idx][pos] = max(take, not_take)
+
+#             return dp[idx][pos]
+
+#         return solve(0, True)
