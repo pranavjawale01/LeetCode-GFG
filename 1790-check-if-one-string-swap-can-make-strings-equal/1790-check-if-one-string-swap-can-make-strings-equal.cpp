b211f6dd3ee92676def86ce63707ed1c98bd3dd9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-25 13:59:11 +0530

    Time: 0 ms (100%), Space: 7.4 MB (76.15%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<int> vec1(26, 0);
-        vector<int> vec2(26, 0);
-        for (char c : s1) {
-            vec1[c - 'a']++;
-        }
-        for (char c : s2) {
-            vec2[c - 'a']++;
-        }
-        if (vec1 == vec2) {
+        if (s1 == s2) {
             return true;
         }
-        return false;
+        int left = 0, right = s1.length() - 1;
+        while (s1[left] == s2[left]) {
+            left++;
+        }
+        while (s1[right] == s2[right]) {
+            right--;
+        }
+        swap(s2[right], s2[left]);
+        return s1 == s2;
     }
 };
