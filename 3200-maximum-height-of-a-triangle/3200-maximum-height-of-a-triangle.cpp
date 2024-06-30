dae25b5ebc1f2d09a23367c0b6bad92867881f37
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-06-30 09:43:37 +0530

    Time: 0 ms (100%), Space: 7.4 MB (62.5%) - LeetHub

--- 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.cpp
+++ 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.cpp
@@ -1,30 +1,64 @@
 class Solution {
 public:
-    int solve(int a, int b) {
-        int row = 0;
-        int len = 1;
+    int solve(int red, int blue) {
         int height = 0;
-        while (a > 0 || b > 0) {
-            if (row == 0) {
-                if (a >= len) {
-                    a -= len;
+        int row = 1;
+        
+        while (true) {
+            if (row % 2 == 1) {
+                if (red >= row) {
+                    red -= row;
+                    height++;
                 } else {
                     break;
                 }
             } else {
-                if (b >= len) {
-                    b -= len;
+                if (blue >= row) {
+                    blue -= row;
+                    height++;
                 } else {
                     break;
                 }
             }
-            row ^= 1;
-            len++;
-            height++;
+            row++;
         }
+        
         return height;
     }
     int maxHeightOfTriangle(int red, int blue) {
         return max(solve(red, blue), solve(blue, red));
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int solve(int a, int b) {
+//         int row = 0;
+//         int len = 1;
+//         int height = 0;
+//         while (a > 0 || b > 0) {
+//             if (row == 0) {
+//                 if (a >= len) {
+//                     a -= len;
+//                 } else {
+//                     break;
+//                 }
+//             } else {
+//                 if (b >= len) {
+//                     b -= len;
+//                 } else {
+//                     break;
+//                 }
+//             }
+//             row ^= 1;
+//             len++;
+//             height++;
+//         }
+//         return height;
+//     }
+//     int maxHeightOfTriangle(int red, int blue) {
+//         return max(solve(red, blue), solve(blue, red));
+//     }
+// };
