0f6bf26ff5fd7662850b4425353f1ab98876b95c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-02 10:55:03 +0530

    Time: 3 ms (90.95%), Space: 43.5 MB (21.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,51 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int m = nums1.length, n = nums2.length;
-        int i = 0, j = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int x : nums1) {
+            mp.put(x, mp.getOrDefault(x, 0) + 1);
+        }
+        int n = nums2.length;
         List<Integer> ans = new ArrayList<>();
-        while (i < m && j < n) {
-            if (nums1[i] == nums2[j]) {
-                ans.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                i++;
+        for (int i = 0; i < n; i++) {
+            if (mp.getOrDefault(nums2[i], 0) > 0) {
+                mp.put(nums2[i], mp.getOrDefault(nums2[i], 0) - 1);
+                ans.add(nums2[i]);
             }
         }
         int[] arr = new int[ans.size()];
-        for (int k = 0; k < ans.size(); k++) {
-            arr[k] = ans.get(k);
+        for (int i = 0; i < ans.size(); i++) {
+            arr[i] = ans.get(i);
         }
         return arr;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         int m = nums1.length, n = nums2.length;
+//         int i = 0, j = 0;
+//         List<Integer> ans = new ArrayList<>();
+//         while (i < m && j < n) {
+//             if (nums1[i] == nums2[j]) {
+//                 ans.add(nums1[i]);
+//                 i++;
+//                 j++;
+//             } else if (nums1[i] > nums2[j]) {
+//                 j++;
+//             } else {
+//                 i++;
+//             }
+//         }
+//         int[] arr = new int[ans.size()];
+//         for (int k = 0; k < ans.size(); k++) {
+//             arr[k] = ans.get(k);
+//         }
+//         return arr;
+//     }
+// }
