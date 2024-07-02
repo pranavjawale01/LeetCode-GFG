94867ae7a440b91e5ef6f6ad7e453bce6cc01257
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-02 11:01:43 +0530

    Time: 13 ms (22.92%), Space: 15.1 MB (76.77%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
+        vector<int> arr(1001, 0);
         int n = nums.size();
-        unordered_map<int, int> mp;
         for (auto num : nums) {
             for (int x : num) {
-                mp[x]++;
+                arr[x]++;
             }
         }
         vector<int> ans;
-        for (auto x : mp) {
-            if (x.second == n) {
-                ans.push_back(x.first);
+        for (int i = 1; i <= 1000; i++) {
+            if (arr[i] == n) {
+                ans.push_back(i);
             }
         }
-        sort(ans.begin(), ans.end());
         return ans;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> intersection(vector<vector<int>>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, int> mp;
+//         for (auto num : nums) {
+//             for (int x : num) {
+//                 mp[x]++;
+//             }
+//         }
+//         vector<int> ans;
+//         for (auto x : mp) {
+//             if (x.second == n) {
+//                 ans.push_back(x.first);
+//             }
+//         }
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
