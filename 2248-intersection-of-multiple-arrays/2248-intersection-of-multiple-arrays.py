b052fef43b00035a06c54d83b6441aeb7d760808
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-02 11:11:57 +0530

    Time: 71 ms (23.7%), Space: 16.8 MB (83.18%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -1,16 +1,33 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        mp = {}
+        arr = [0] * 1001
         n = len(nums)
         for num in nums:
             for x in num:
-                if x not in mp:
-                    mp[x] = 1
-                else:
-                    mp[x] += 1
+                arr[x] += 1
         ans = []
-        for key, val in mp.items():
-            if val == n:
-                ans.append(key)
-        ans.sort()
+        for i in range(0, 1001):
+            if arr[i] == n:
+                ans.append(i)
         return ans
+
+
+
+
+
+# class Solution:
+#     def intersection(self, nums: List[List[int]]) -> List[int]:
+#         mp = {}
+#         n = len(nums)
+#         for num in nums:
+#             for x in num:
+#                 if x not in mp:
+#                     mp[x] = 1
+#                 else:
+#                     mp[x] += 1
+#         ans = []
+#         for key, val in mp.items():
+#             if val == n:
+#                 ans.append(key)
+#         ans.sort()
+#         return ans
