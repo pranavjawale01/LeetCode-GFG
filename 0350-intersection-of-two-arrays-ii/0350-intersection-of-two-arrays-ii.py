8fd088560ee7ad74726f49468c95ec4c0c9e2ca4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-02 10:55:10 +0530

    Time: 39 ms (95.24%), Space: 16.6 MB (71.02%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,16 +1,36 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
-        m, n, i, j = len(nums1), len(nums2), 0, 0
-        ans = []
-        while i < m and j < n:
-            if nums1[i] == nums2[j]:
-                ans.append(nums1[i])
-                i += 1
-                j += 1
-            elif nums1[i] > nums2[j]:
-                j += 1
+        mp = {}
+        for x in nums1:
+            if x not in mp:
+                mp[x] = 1
             else:
-                i += 1
+                mp[x] += 1
+        ans = []
+        for x in nums2:
+            if x in mp and mp[x] > 0:
+                mp[x] -= 1
+                ans.append(x)
         return ans
+
+
+
+
+
+
+# class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         nums1.sort()
+#         nums2.sort()
+#         m, n, i, j = len(nums1), len(nums2), 0, 0
+#         ans = []
+#         while i < m and j < n:
+#             if nums1[i] == nums2[j]:
+#                 ans.append(nums1[i])
+#                 i += 1
+#                 j += 1
+#             elif nums1[i] > nums2[j]:
+#                 j += 1
+#             else:
+#                 i += 1
+#         return ans
