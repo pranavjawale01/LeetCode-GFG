c4f2eb248d7032dbb34e36c95ef8b36535cfc98f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-02 11:09:19 +0530

    Time: 2 ms (100%), Space: 44.6 MB (66.6%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,19 +1,41 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
         int n = nums.length;
-        Map<Integer, Integer> mp = new HashMap<>();
+        int[] arr = new int[1001];
         for (int[] num : nums) {
             for (int x : num) {
-                mp.put(x, mp.getOrDefault(x, 0) + 1);
+                arr[x]++;
             }
         }
         List<Integer> ans = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            if (entry.getValue() == n) {
-                ans.add(entry.getKey());
+        for (int i = 0; i < 1001; i++) {
+            if (arr[i] == n) {
+                ans.add(i);
             }
         }
-        Collections.sort(ans);
         return ans;
     }
 }
+
+
+
+
+// class Solution {
+//     public List<Integer> intersection(int[][] nums) {
+//         int n = nums.length;
+//         Map<Integer, Integer> mp = new HashMap<>();
+//         for (int[] num : nums) {
+//             for (int x : num) {
+//                 mp.put(x, mp.getOrDefault(x, 0) + 1);
+//             }
+//         }
+//         List<Integer> ans = new ArrayList<>();
+//         for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+//             if (entry.getValue() == n) {
+//                 ans.add(entry.getKey());
+//             }
+//         }
+//         Collections.sort(ans);
+//         return ans;
+//     }
+// }
