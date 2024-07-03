4a77eb8d75f214bed6fe887522d10cc8f5028cfc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-03 12:05:03 +0530

    Time: 299 ms (10.38%), Space: 27.5 MB (39.86%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -4,12 +4,44 @@ class Solution:
         if n <= 4:
             return 0
         
-        nums.sort()
+        pq_max = []
+        pq_min = []
         
-        min_diff = float('inf')
-        min_diff = min(min_diff, nums[n-1] - nums[3])
-        min_diff = min(min_diff, nums[n-2] - nums[2])
-        min_diff = min(min_diff, nums[n-3] - nums[1])
-        min_diff = min(min_diff, nums[n-4] - nums[0])
+        for num in nums:
+            heapq.heappush(pq_min, num)
+            heapq.heappush(pq_max, -num)
+            if len(pq_max) > 4:
+                heapq.heappop(pq_max)
+            if len(pq_min) > 4:
+                heapq.heappop(pq_min)
         
-        return min_diff
+        l, r = 3, n - 4
+        
+        while pq_max:
+            nums[l] = -heapq.heappop(pq_max)
+            nums[r] = heapq.heappop(pq_min)
+            l -= 1
+            r += 1
+        
+        return min(nums[n-1] - nums[3], nums[n-2] - nums[2], nums[n-3] - nums[1], nums[n-4] - nums[0])
+
+
+
+
+
+
+# class Solution:
+#     def minDifference(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n <= 4:
+#             return 0
+        
+#         nums.sort()
+        
+#         min_diff = float('inf')
+#         min_diff = min(min_diff, nums[n-1] - nums[3])
+#         min_diff = min(min_diff, nums[n-2] - nums[2])
+#         min_diff = min(min_diff, nums[n-3] - nums[1])
+#         min_diff = min(min_diff, nums[n-4] - nums[0])
+        
+#         return min_diff
