c0ba280c5dd533ac63ade731edbb90dd01c6ff1a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-04 09:53:19 +0530

    Time: 420 ms (75.91%), Space: 259.6 MB (68.34%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -11,22 +11,60 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* ans = new ListNode(0);
-        ListNode* temp = ans;
-        int sum = 0;
         head = head->next;
-        
-        while (head) {
-            if (head->val == 0) {
-                temp->next = new ListNode(sum);
-                temp = temp->next;
-                sum = 0;
-            } else {
-                sum += head->val;
-            }
-            head = head->next;
+        if (!head) {
+            return head;
         }
-        
-        return ans->next;
+        ListNode* temp = head;
+        int sum = 0;
+        while (temp && temp->val != 0) {
+            sum += temp->val;
+            temp = temp->next;
+        }
+        head->val = sum;
+        head->next = mergeNodes(temp);
+        return head;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* mergeNodes(ListNode* head) {
+//         ListNode* ans = new ListNode(0);
+//         ListNode* temp = ans;
+//         int sum = 0;
+//         head = head->next;
+        
+//         while (head) {
+//             if (head->val == 0) {
+//                 temp->next = new ListNode(sum);
+//                 temp = temp->next;
+//                 sum = 0;
+//             } else {
+//                 sum += head->val;
+//             }
+//             head = head->next;
+//         }
+        
+//         return ans->next;
+//     }
+// };
