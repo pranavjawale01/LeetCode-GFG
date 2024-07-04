7fab2c69437d8b9840d3ca13f7645b733b24a9bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-04 09:55:38 +0530

    Time: 13 ms (5.73%), Space: 87.4 MB (8.7%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,20 +10,55 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode ans = new ListNode(0);
-        ListNode temp = ans;
-        int sum = 0;
         head = head.next;
-        while (head != null) {
-            if (head.val == 0) {
-                temp.next = new ListNode(sum);
-                temp = temp.next;
-                sum = 0;
-            } else {
-                sum += head.val;
-            }
-            head = head.next;
+        if (head == null) {
+            return head;
         }
-        return ans.next;
+        ListNode temp = head;
+        int sum = 0;
+        while (temp != null && temp.val != 0) {
+            sum += temp.val;
+            temp = temp.next;
+        }
+        head.val = sum;
+        head.next = mergeNodes(temp);
+        return head;
     }
 }
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode mergeNodes(ListNode head) {
+//         ListNode ans = new ListNode(0);
+//         ListNode temp = ans;
+//         int sum = 0;
+//         head = head.next;
+//         while (head != null) {
+//             if (head.val == 0) {
+//                 temp.next = new ListNode(sum);
+//                 temp = temp.next;
+//                 sum = 0;
+//             } else {
+//                 sum += head.val;
+//             }
+//             head = head.next;
+//         }
+//         return ans.next;
+//     }
+// }
