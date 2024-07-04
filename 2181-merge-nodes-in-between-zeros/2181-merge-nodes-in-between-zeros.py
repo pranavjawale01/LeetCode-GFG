769d6c15ea3e038862fc1a6ef7d69fd1e3d4bd06
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-04 09:55:44 +0530

    Time: 833 ms (83.92%), Space: 68.9 MB (59.1%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -5,16 +5,39 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ans = ListNode(0)
-        temp = ans
-        sum = 0
         head = head.next
-        while head:
-            if head.val == 0:
-                temp.next = ListNode(sum)
-                temp = temp.next
-                sum = 0
-            else:
-                sum += head.val
-            head = head.next
-        return ans.next
+        if not head:
+            return head
+        temp = head
+        sum = 0
+        while temp and temp.val != 0:
+            sum += temp.val
+            temp = temp.next
+        head.val = sum
+        head.next = self.mergeNodes(temp)
+        return head
+
+
+
+
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         ans = ListNode(0)
+#         temp = ans
+#         sum = 0
+#         head = head.next
+#         while head:
+#             if head.val == 0:
+#                 temp.next = ListNode(sum)
+#                 temp = temp.next
+#                 sum = 0
+#             else:
+#                 sum += head.val
+#             head = head.next
+#         return ans.next
