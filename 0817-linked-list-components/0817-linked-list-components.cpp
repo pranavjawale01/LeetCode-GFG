a531e8af84b44c3100de5291f8bb53d2f875e830
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-04 10:14:11 +0530

    Time: 31 ms (47.65%), Space: 25 MB (57.86%) - LeetHub

--- 0817-linked-list-components/0817-linked-list-components.cpp
+++ 0817-linked-list-components/0817-linked-list-components.cpp
@@ -13,19 +13,59 @@ public:
     int numComponents(ListNode* head, vector<int>& nums) {
         int count = 0;
         unordered_set<int> st(nums.begin(), nums.end());
-        bool inComponent = false;
 
         while (head) {
-            if (st.find(head->val) != st.end()) {
-                if (!inComponent) {
-                    count++;
-                    inComponent = true;
-                }
-            } else {
-                inComponent = false;
+            bool flag = false;
+            while (head && st.find(head->val) != st.end()) {
+                flag = true;
+                head = head->next;
             }
-            head = head->next;
+            if (flag) {
+                count++;
+            }
+            if (head) {
+                head = head->next;
+            }
         }
         return count;
     }
 };
+
+
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int numComponents(ListNode* head, vector<int>& nums) {
+//         int count = 0;
+//         unordered_set<int> st(nums.begin(), nums.end());
+//         bool inComponent = false;
+
+//         while (head) {
+//             if (st.find(head->val) != st.end()) {
+//                 if (!inComponent) {
+//                     count++;
+//                     inComponent = true;
+//                 }
+//             } else {
+//                 inComponent = false;
+//             }
+//             head = head->next;
+//         }
+//         return count;
+//     }
+// };
