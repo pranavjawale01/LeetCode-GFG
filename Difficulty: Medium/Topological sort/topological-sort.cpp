362463d61a6eee3ddbf15fa9db2480fad94b04ff
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-04 17:24:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -6,36 +6,85 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to return list containing vertices in Topological order.
-	
-	void dfs(vector<int> adj[], int u, vector<bool> &vis, stack<int> &st) {
-	    vis[u] = true;
-	    for (int &v : adj[u]) {
-	        if (!vis[v]) {
-	            dfs(adj, v, vis, st);
-	        }
-	    }
-	    st.push(u);
-	}
-	
+	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    vector<bool> vis(V, false);
-	    stack<int> st;
-	    for (int i = 0; i < V; i++) {
-	        if (!vis[i]) {
-	            dfs(adj, i, vis, st);
-	        }
-	    }
-	    vector<int> ans;
-	    while (!st.empty()) {
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
+	    vector<int> indegree(V, 0);
+	    for (int u = 0; u < V; ++u) {
+            for (int v : adj[u]) {
+                indegree[v]++;
+            }
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < V; ++i) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> ans;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            ans.push_back(u);
+            
+            for (int v : adj[u]) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        
+        if (ans.size() != V) {
+            return {};
+        }
+        
+        return ans;
 	}
 };
+
+
+
+
+
+
+// class Solution
+// {
+// 	public:
+// 	//Function to return list containing vertices in Topological order.
+	
+// 	void dfs(vector<int> adj[], int u, vector<bool> &vis, stack<int> &st) {
+// 	    vis[u] = true;
+// 	    for (int &v : adj[u]) {
+// 	        if (!vis[v]) {
+// 	            dfs(adj, v, vis, st);
+// 	        }
+// 	    }
+// 	    st.push(u);
+// 	}
+	
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<bool> vis(V, false);
+// 	    stack<int> st;
+// 	    for (int i = 0; i < V; i++) {
+// 	        if (!vis[i]) {
+// 	            dfs(adj, i, vis, st);
+// 	        }
+// 	    }
+// 	    vector<int> ans;
+// 	    while (!st.empty()) {
+// 	        ans.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	    return ans;
+// 	}
+// };
+
 
 //{ Driver Code Starts.
 
