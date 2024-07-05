4e979030cd1eda9bd6f863b71190a49a73b7b6ac
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-05 21:39:13 +0530

    Time: 5 ms (67.71%), Space: 45.6 MB (24.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,84 @@
 class Solution {
+    private boolean isCyclic(Map<Integer, List<Integer>> mp, int v, boolean[] vis, boolean[] rec) {
+        if (rec[v]) {
+            return true;
+        }
+        if (vis[v]) {
+            return false;
+        }
+        vis[v] = true;
+        rec[v] = true;
+        for (int x : mp.getOrDefault(v, new ArrayList<>())) {
+            if (isCyclic(mp, x, vis, rec)) {
+                return true;
+            }
+        }
+        rec[v] = false;
+        return false;
+    }
+    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
         Map<Integer, List<Integer>> mp = new HashMap<>();
+        boolean[] vis = new boolean[numCourses];
+        boolean[] rec = new boolean[numCourses];
         
         for (int[] x : prerequisites) {
-            if (!mp.containsKey(x[1])) {
-                mp.put(x[1], new ArrayList<>());
+            int u = x[1];
+            int v = x[0];
+            if (!mp.containsKey(u)) {
+                mp.put(u, new ArrayList<>());
             }
-            mp.get(x[1]).add(x[0]);
-            indegree[x[0]]++;
+            mp.get(u).add(v);
         }
         
-        Queue<Integer> q = new LinkedList<>();
-        int count = 0;
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-                count++;
+            if (!vis[i] && isCyclic(mp, i, vis, rec)) {
+                return false;
             }
         }
         
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            if (mp.containsKey(curr)) {
-                for (int x : mp.get(curr)) {
-                    indegree[x]--;
-                    if (indegree[x] == 0) {
-                        q.add(x);
-                        count++;
-                    }
-                }
-            }
-        }
-        
-        return count == numCourses;
+        return true;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         int[] indegree = new int[numCourses];
+//         Map<Integer, List<Integer>> mp = new HashMap<>();
+        
+//         for (int[] x : prerequisites) {
+//             if (!mp.containsKey(x[1])) {
+//                 mp.put(x[1], new ArrayList<>());
+//             }
+//             mp.get(x[1]).add(x[0]);
+//             indegree[x[0]]++;
+//         }
+        
+//         Queue<Integer> q = new LinkedList<>();
+//         int count = 0;
+//         for (int i = 0; i < numCourses; i++) {
+//             if (indegree[i] == 0) {
+//                 q.add(i);
+//                 count++;
+//             }
+//         }
+        
+//         while (!q.isEmpty()) {
+//             int curr = q.poll();
+//             if (mp.containsKey(curr)) {
+//                 for (int x : mp.get(curr)) {
+//                     indegree[x]--;
+//                     if (indegree[x] == 0) {
+//                         q.add(x);
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return count == numCourses;
+//     }
+// }
