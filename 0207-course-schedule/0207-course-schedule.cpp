c0bb9a7573c59baba742429ea7b708306b1dfba1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-05 21:38:06 +0530

    Time: 22 ms (19.12%), Space: 19 MB (13.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,80 @@
 class Solution {
 public:
+    bool isCyclic(unordered_map<int, vector<int>> &mp, int v, vector<bool> &vis, vector<bool> &rec) {
+        if (rec[v]) {
+            return true;
+        }
+        if (vis[v]) {
+            return false;
+        }
+        vis[v] = true;
+        rec[v] = true;
+        for (int x : mp[v]) {
+            if (isCyclic(mp, x, vis, rec)) {
+                return true;
+            }
+        }
+        rec[v] = false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses, 0);
-        map<int, vector<int>> mp;
-        
-        for (auto& pre : prerequisites) {
-            int course = pre[0];
-            int prerequisite = pre[1];
-            mp[prerequisite].push_back(course);
-            indegree[course]++;
+        unordered_map<int, vector<int>> mp;
+        vector<bool> vis(numCourses, false);
+        vector<bool> rec(numCourses, false);
+        for (auto &x: prerequisites) {
+            mp[x[1]].push_back(x[0]);
         }
-        
-        queue<int> q;
-        int count = 0;
-        
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-                count++;
+            if (!vis[i] && isCyclic(mp, i, vis, rec)) {
+                return false;
             }
         }
-        
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            for (int next : mp[curr]) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.push(next);
-                    count++;
-                }
-            }
-        }
-        
-        return count == numCourses;
+        return true;
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<int> indegree(numCourses, 0);
+//         map<int, vector<int>> mp;
+        
+//         for (auto& pre : prerequisites) {
+//             int course = pre[0];
+//             int prerequisite = pre[1];
+//             mp[prerequisite].push_back(course);
+//             indegree[course]++;
+//         }
+        
+//         queue<int> q;
+//         int count = 0;
+        
+//         for (int i = 0; i < numCourses; i++) {
+//             if (indegree[i] == 0) {
+//                 q.push(i);
+//                 count++;
+//             }
+//         }
+        
+//         while (!q.empty()) {
+//             int curr = q.front();
+//             q.pop();
+//             for (int next : mp[curr]) {
+//                 indegree[next]--;
+//                 if (indegree[next] == 0) {
+//                     q.push(next);
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         return count == numCourses;
+//     }
+// };
