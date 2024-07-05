894c73a349cb834eb8f130637b4a128399bdeb2f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-05 21:39:01 +0530

    Time: 87 ms (71.45%), Space: 18.6 MB (14.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,59 @@
 class Solution:
+    def isCyclic(self, mp, v, vis, rec):
+        if rec[v]:
+            return True
+        if vis[v]:
+            return False
+        vis[v] = True
+        rec[v] = True
+        for x in mp[v]:
+            if self.isCyclic(mp, x, vis, rec):
+                return True
+        rec[v] = False
+        return False
+        
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
         mp = defaultdict(list)
-
-        for a, b in prerequisites:
-            mp[b].append(a)
-            indegree[a] += 1
+        vis = [False] * numCourses
+        rec = [False] * numCourses
         
-        q = deque()
-        count = 0
-
+        for x in prerequisites:
+            mp[x[1]].append(x[0])
+        
         for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-                count += 1
+            if not vis[i] and self.isCyclic(mp, i, vis, rec):
+                return False
         
-        while q:
-            curr = q.popleft()
-            for x in mp[curr]:
-                indegree[x] -= 1
-                if indegree[x] == 0:
-                    q.append(x)
-                    count += 1
+        return True
+
+
+
+
+
+
+# class Solution:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         indegree = [0] * numCourses
+#         mp = defaultdict(list)
+
+#         for a, b in prerequisites:
+#             mp[b].append(a)
+#             indegree[a] += 1
         
-        return count == numCourses
+#         q = deque()
+#         count = 0
+
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 q.append(i)
+#                 count += 1
+        
+#         while q:
+#             curr = q.popleft()
+#             for x in mp[curr]:
+#                 indegree[x] -= 1
+#                 if indegree[x] == 0:
+#                     q.append(x)
+#                     count += 1
+        
+#         return count == numCourses
