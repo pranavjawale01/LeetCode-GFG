9047ed3097f4efd82b4638712bfef6f12deec511
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-06 17:12:22 +0530

    Time: 1 ms (28.53%), Space: 40.7 MB (10.17%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -1,10 +1,30 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int cycleLength = (n - 1) * 2;
-        int positionInCycle = time % cycleLength;
-        if (positionInCycle < n) {
-            return positionInCycle + 1;
+        int idx = 1;
+        int dir = 1;
+        while (time > 0) {
+            if (idx + dir >= 1 && idx + dir <= n) {
+                idx += dir;
+                time--;
+            } else {
+                dir *= -1;
+            }
         }
-        return 2 * n - positionInCycle - 1;
+        return idx;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int passThePillow(int n, int time) {
+//         int cycleLength = (n - 1) * 2;
+//         int positionInCycle = time % cycleLength;
+//         if (positionInCycle < n) {
+//             return positionInCycle + 1;
+//         }
+//         return 2 * n - positionInCycle - 1;
+//     }
+// }
