a5f5a154dcd45be03dca9bf178b44da33c2f45e0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-06 17:14:17 +0530

    Time: 40 ms (23.04%), Space: 16.4 MB (77.72%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,7 +1,20 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        cycleLength = (n - 1) * 2
-        positionInCycle = time % cycleLength
-        if positionInCycle < n:
-            return positionInCycle + 1
-        return 2 * n - positionInCycle - 1
+        idx, dir = 1, 1
+        while time > 0:
+            if idx + dir >= 1 and idx + dir <= n:
+                idx += dir
+                time -= 1
+            else:
+                dir *= -1
+        return idx
+
+
+
+# class Solution:
+#     def passThePillow(self, n: int, time: int) -> int:
+#         cycleLength = (n - 1) * 2
+#         positionInCycle = time % cycleLength
+#         if positionInCycle < n:
+#             return positionInCycle + 1
+#         return 2 * n - positionInCycle - 1
