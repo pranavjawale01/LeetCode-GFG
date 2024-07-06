475baeaac75c7eb718660192a6c2e93d66479265
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-06 17:15:58 +0530

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int idx = 1;
-        int dir = 1;
-        while (time > 0) {
-            if (idx + dir >= 1 && idx + dir <= n) {
-                idx += dir;
-                time--;
-            } else {
-                dir *= -1;
-            }
+        int totalFullRounds = time / (n-1);
+        int timeLeft = time % (n-1);
+        if (totalFullRounds % 2 == 0) {
+            return timeLeft + 1;
+        } else {
+            return n - timeLeft;
         }
-        return idx;
+        return -1;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int passThePillow(int n, int time) {
+//         int idx = 1;
+//         int dir = 1;
+//         while (time > 0) {
+//             if (idx + dir >= 1 && idx + dir <= n) {
+//                 idx += dir;
+//                 time--;
+//             } else {
+//                 dir *= -1;
+//             }
+//         }
+//         return idx;
+//     }
+// };
 
 
 
