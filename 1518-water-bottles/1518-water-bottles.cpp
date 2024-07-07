242df2611560382e49b5944e4b958f1d8e5daac5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-07 14:36:29 +0530

    Time: 0 ms (100%), Space: 7.1 MB (72.3%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int consumed = 0;
-        while (numBottles >= numExchange) {
-            consumed += numExchange;
-            numBottles -= numExchange;
-            numBottles += 1;
-        }
-        return consumed + numBottles;
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int numWaterBottles(int numBottles, int numExchange) {
+//         int consumed = 0;
+//         while (numBottles >= numExchange) {
+//             consumed += numExchange;
+//             numBottles -= numExchange;
+//             numBottles += 1;
+//         }
+//         return consumed + numBottles;
+//     }
+// };
