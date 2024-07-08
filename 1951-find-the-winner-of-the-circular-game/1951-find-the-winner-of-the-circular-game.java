11ddfa3986fd2e536d1d86b20ef119ed57b361e3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-08 18:59:27 +0530

    Time: 43 ms (19.81%), Space: 44.3 MB (10.99%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,33 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        List<Integer> arr = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            arr.add(i+1);
+            q.offer(i+1);
         }
-        int i = 0;
-        while (arr.size() > 1) {
-            i = (i + k - 1) % arr.size();
-            arr.remove(i);
+        while (q.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                q.add(q.poll());
+            }
+            q.poll();
         }
-        return arr.get(0);
-    } 
+        return q.poll();
+    }
 }
+
+
+
+
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         List<Integer> arr = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             arr.add(i+1);
+//         }
+//         int i = 0;
+//         while (arr.size() > 1) {
+//             i = (i + k - 1) % arr.size();
+//             arr.remove(i);
+//         }
+//         return arr.get(0);
+//     } 
+// }
