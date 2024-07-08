0e60dff4add79c5e946448e53cd3b539172c6944
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-08 19:06:53 +0530

    Time: 29 ms (94.72%), Space: 16.6 MB (18.56%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,11 +1,25 @@
 class Solution:
+    def solve(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        i = self.solve(n-1, k)
+        i = (i + k) % n
+        return i
+
     def findTheWinner(self, n: int, k: int) -> int:
-        q = deque(range(1, n+1))
-        while len(q) > 1:
-            for i in range(k-1):
-                q.append(q.popleft())
-            q.popleft()
-        return q.popleft()
+        return 1 + self.solve(n, k)
+
+
+
+
+# class Solution:
+#     def findTheWinner(self, n: int, k: int) -> int:
+#         q = deque(range(1, n+1))
+#         while len(q) > 1:
+#             for i in range(k-1):
+#                 q.append(q.popleft())
+#             q.popleft()
+#         return q.popleft()
 
 
 
