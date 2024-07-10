578eb5fe75fe854fdbe1ee8130e420032d768537
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-10 23:32:22 +0530

    Time: 2 ms (44.79%), Space: 41.9 MB (67.59%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,5 +1,24 @@
 class Solution {
     public int minOperations(String[] logs) {
+        Stack<String> st = new Stack<>();
+        for (String x : logs) {
+            if (x.equals("../")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } else if (!x.equals("./")) {
+                st.push(x);
+            }
+        }
+        return st.size();
+    }
+}
+
+
+
+/*
+class Solution {
+    public int minOperations(String[] logs) {
         int depth = 0;
         for (String x : logs) {
             if (x.equals("../")) {
@@ -13,3 +32,4 @@ class Solution {
         return depth;
     }
 }
+*/
