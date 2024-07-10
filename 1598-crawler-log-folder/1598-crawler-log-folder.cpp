b10cd029d216e72f1330497074328a1e9a3c5288
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-10 23:28:27 +0530

    Time: 9 ms (22.38%), Space: 14.4 MB (17.13%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int depth = 0;
+        stack<string> st;
         for (auto x : logs) {
             if (x == "../") {
-                depth = max(0, depth - 1);
-            } else if (x == "./") {
-                continue;
-            } else {
-                depth++;
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else if (x != "./") {
+                st.push(x);
             }
         }
-        return depth;
+        return st.size();
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<string>& logs) {
+//         int depth = 0;
+//         for (auto x : logs) {
+//             if (x == "../") {
+//                 depth = max(0, depth - 1);
+//             } else if (x == "./") {
+//                 continue;
+//             } else {
+//                 depth++;
+//             }
+//         }
+//         return depth;
+//     }
+// };
