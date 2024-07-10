af9ef5c1bf8a03239e89fe237fd0c6b4ca5c3f98
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-10 23:34:33 +0530

    Time: 53 ms (31.31%), Space: 16.8 MB (36.19%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,5 +1,19 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
+        st = []
+        for x in logs:
+            if x == "../":
+                if st:
+                    st.pop()
+            elif x != "./":
+                st.append(x)
+        return len(st)
+
+
+
+###
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
         depth = 0
         for x in logs:
             if x == "../":
@@ -7,3 +21,4 @@ class Solution:
             elif x != "./":
                 depth += 1
         return depth
+###
