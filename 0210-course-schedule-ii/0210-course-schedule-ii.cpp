6ac4fff6fd1ed515085d723dceb096e635615105
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-11 18:25:39 +0530

    Time: 11 ms (91.12%), Space: 19.5 MB (8.41%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,43 +1,93 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        map<int, vector<int>> mp;
-        vector<int> indegree(numCourses, 0);
+    bool cycleDetected = false;
 
-        for (auto x : prerequisites) {
-            mp[x[1]].push_back(x[0]);
-            indegree[x[0]]++;
+    void dfs(unordered_map<int, vector<int>>& mp, int u, vector<bool>& vis, vector<bool>& rec, stack<int>& st) {
+        vis[u] = true;
+        rec[u] = true;
+        for (int v : mp[u]) {
+            if (rec[v]) {
+                cycleDetected = true;
+                return;
+            }
+            if (!vis[v]) {
+                dfs(mp, v, vis, rec, st);
+                if (cycleDetected) return;
+            }
         }
+        st.push(u);
+        rec[u] = false;
+    }
 
-        queue<int> q;
-        vector<int> ans;
-        int count = 0;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> mp;
+        for (auto& x : prerequisites) {
+            mp[x[1]].push_back(x[0]);
+        }
+        
+        vector<bool> rec(numCourses, false);
+        vector<bool> vis(numCourses, false);
+        stack<int> st;
 
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-                count++;
-                ans.push_back(i);
+            if (!vis[i]) {
+                dfs(mp, i, vis, rec, st);
+                if (cycleDetected) return {};
             }
         }
 
-        while (!q.empty()) {
-            int x = q.front();
-            q.pop();
-            for (int y : mp[x]) {
-                indegree[y]--;
-                if (indegree[y] == 0) {
-                    q.push(y);
-                    count++;
-                    ans.push_back(y);
-                }
-            }
+        vector<int> result;
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
         }
 
-        if (count != numCourses) {
-            return {};
-        }
-
-        return ans;
+        return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         map<int, vector<int>> mp;
+//         vector<int> indegree(numCourses, 0);
+
+//         for (auto x : prerequisites) {
+//             mp[x[1]].push_back(x[0]);
+//             indegree[x[0]]++;
+//         }
+
+//         queue<int> q;
+//         vector<int> ans;
+//         int count = 0;
+
+//         for (int i = 0; i < numCourses; i++) {
+//             if (indegree[i] == 0) {
+//                 q.push(i);
+//                 count++;
+//                 ans.push_back(i);
+//             }
+//         }
+
+//         while (!q.empty()) {
+//             int x = q.front();
+//             q.pop();
+//             for (int y : mp[x]) {
+//                 indegree[y]--;
+//                 if (indegree[y] == 0) {
+//                     q.push(y);
+//                     count++;
+//                     ans.push_back(y);
+//                 }
+//             }
+//         }
+
+//         if (count != numCourses) {
+//             return {};
+//         }
+
+//         return ans;
+//     }
+// };
