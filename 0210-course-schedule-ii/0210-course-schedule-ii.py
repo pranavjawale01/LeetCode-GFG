8d09210a3c4fc76fc6a2c27f8de6beeac994d1dd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-11 18:26:30 +0530

    Time: 88 ms (71.03%), Space: 18.5 MB (17.56%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,70 @@
 class Solution:
+    def __init__(self):
+        self.cycleDetected = False
+
+    def dfs(self, mp, u, vis, rec, st):
+        vis[u] = True
+        rec[u] = True
+        for v in mp[u]:
+            if rec[v]:
+                self.cycleDetected = True
+                return
+            if not vis[v]:
+                self.dfs(mp, v, vis, rec, st)
+                if self.cycleDetected:
+                    return
+        st.append(u)
+        rec[u] = False
+
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        mp = {i: [] for i in range(numCourses)}
-        indegree = [0] * numCourses
-        
+        mp = defaultdict(list)
         for x in prerequisites:
             mp[x[1]].append(x[0])
-            indegree[x[0]] += 1
         
-        q = deque()
-        ans = []
-        count = 0
-        
+        rec = [False] * numCourses
+        vis = [False] * numCourses
+        st = []
+
         for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-                ans.append(i)
-                count += 1
+            if not vis[i]:
+                self.dfs(mp, i, vis, rec, st)
+                if self.cycleDetected:
+                    return []
         
-        while q:
-            x = q.popleft()
-            for v in mp[x]:
-                indegree[v] -= 1
-                if indegree[v] == 0:
-                    q.append(v)
-                    ans.append(v)
-                    count += 1
+        return st[::-1]
+
+
+
+
+
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+#         mp = {i: [] for i in range(numCourses)}
+#         indegree = [0] * numCourses
         
-        if count == numCourses:
-            return ans
-        return []
+#         for x in prerequisites:
+#             mp[x[1]].append(x[0])
+#             indegree[x[0]] += 1
+        
+#         q = deque()
+#         ans = []
+#         count = 0
+        
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 q.append(i)
+#                 ans.append(i)
+#                 count += 1
+        
+#         while q:
+#             x = q.popleft()
+#             for v in mp[x]:
+#                 indegree[v] -= 1
+#                 if indegree[v] == 0:
+#                     q.append(v)
+#                     ans.append(v)
+#                     count += 1
+        
+#         if count == numCourses:
+#             return ans
+#         return []
