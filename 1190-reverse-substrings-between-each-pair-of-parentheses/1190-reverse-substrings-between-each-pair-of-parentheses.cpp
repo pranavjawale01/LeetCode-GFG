4b27e7ec88a7cb512da31a01c0b641ecef587621
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-11 13:54:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.36%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,20 +1,53 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
+        int n = s.length();
         stack<int> open;
-        string result = "";
-        for (char c : s) {
-            if (c == '(') {
-                int n = result.length();
-                open.push(n);
-            } else if (c == ')') {
-                int start = open.top();
+        vector<int> door(n);
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                open.push(i);
+            } else if (s[i] == ')') {
+                int j = open.top();
                 open.pop();
-                reverse(result.begin() + start, result.end());
-            } else {
-                result += c;
+                door[i] = j;
+                door[j] = i;
             }
         }
-        return result;
+        string ans;
+        int dir = 1;
+        for (int i = 0; i < n; i += dir) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = door[i];
+                dir = -dir;
+            } else {
+                ans += s[i];
+            }
+        }
+        return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     string reverseParentheses(string s) {
+//         stack<int> open;
+//         string result = "";
+//         for (char c : s) {
+//             if (c == '(') {
+//                 int n = result.length();
+//                 open.push(n);
+//             } else if (c == ')') {
+//                 int start = open.top();
+//                 open.pop();
+//                 reverse(result.begin() + start, result.end());
+//             } else {
+//                 result += c;
+//             }
+//         }
+//         return result;
+//     }
+// };
