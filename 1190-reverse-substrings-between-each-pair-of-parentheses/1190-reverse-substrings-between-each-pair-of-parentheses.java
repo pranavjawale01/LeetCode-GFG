d9bd9a73f6097caaf6ddcfd1a394737da759c0d2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-11 13:59:09 +0530

    Time: 1 ms (99.49%), Space: 41.4 MB (77.4%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,26 +1,60 @@
 class Solution {
-    public void reverse(StringBuilder s, int i, int j) {
-        while (i < j) {
-            char temp = s.charAt(i);
-            s.setCharAt(i, s.charAt(j));
-            s.setCharAt(j, temp);
-            i++;
-            j--;
-        }
-    }
     public String reverseParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        StringBuilder str = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                st.push(str.length());
-            } else if (c == ')') {
-                int start = st.pop();
-                reverse(str, start, str.length() - 1);
-            } else {
-                str.append(c);
+        int n = s.length();
+        Stack<Integer> openBracket = new Stack<>();
+        int[] door = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            if (s.charAt(i) == '(') {
+                openBracket.push(i);
+            } else if (s.charAt(i) == ')') {
+                int j = openBracket.pop();
+                door[i] = j;
+                door[j] = i;
             }
         }
-        return str.toString();
+
+        StringBuilder result = new StringBuilder();
+        int direction = 1;
+        for (int i = 0; i < n; i += direction) {
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                i = door[i];
+                direction = -direction;
+            } else {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
     }
 }
+
+
+
+
+
+// class Solution {
+//     public void reverse(StringBuilder s, int i, int j) {
+//         while (i < j) {
+//             char temp = s.charAt(i);
+//             s.setCharAt(i, s.charAt(j));
+//             s.setCharAt(j, temp);
+//             i++;
+//             j--;
+//         }
+//     }
+//     public String reverseParentheses(String s) {
+//         Stack<Integer> st = new Stack<>();
+//         StringBuilder str = new StringBuilder();
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') {
+//                 st.push(str.length());
+//             } else if (c == ')') {
+//                 int start = st.pop();
+//                 reverse(str, start, str.length() - 1);
+//             } else {
+//                 str.append(c);
+//             }
+//         }
+//         return str.toString();
+//     }
+// }
