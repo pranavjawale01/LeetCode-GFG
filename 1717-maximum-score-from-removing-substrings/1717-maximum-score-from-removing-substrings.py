efbb949a8d38480f40b1714278cc42da6681fb9d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-12 17:28:42 +0530

    Time: 387 ms (35.71%), Space: 18.9 MB (9.74%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,24 +1,55 @@
 class Solution:
-    def solve(self, s: str, targetStr: str, points: int) -> int:
+    def solve(self, s: str, targetStr: str, points: int) -> tuple[int, str]:
         totalPoints = 0
-        st = []
-        for char in s:
-            if st and char == targetStr[1] and st[-1] == targetStr[0]:
-                st.pop()
+        n = len(s)
+        write_idx = 0
+        s = list(s)
+        for read_idx in range(n):
+            s[write_idx] = s[read_idx]
+            write_idx += 1
+            if write_idx > 1 and s[write_idx - 1] == targetStr[1] and s[write_idx - 2] == targetStr[0]:
+                write_idx -= 2
                 totalPoints += points
-            else:
-                st.append(char)
         
-        s = ''.join(st)
+        s = ''.join(s[:write_idx])
         return totalPoints, s
 
     def maximumGain(self, s: str, x: int, y: int) -> int:
         s1, s2 = "ab", "ba"
         if y > x:
-            x, y = y, x
             s1, s2 = s2, s1
-        
+            x, y = y, x
+
         points1, s = self.solve(s, s1, x)
         points2, _ = self.solve(s, s2, y)
-        
+
         return points1 + points2
+
+
+
+
+
+# class Solution:
+#     def solve(self, s: str, targetStr: str, points: int) -> int:
+#         totalPoints = 0
+#         st = []
+#         for char in s:
+#             if st and char == targetStr[1] and st[-1] == targetStr[0]:
+#                 st.pop()
+#                 totalPoints += points
+#             else:
+#                 st.append(char)
+        
+#         s = ''.join(st)
+#         return totalPoints, s
+
+#     def maximumGain(self, s: str, x: int, y: int) -> int:
+#         s1, s2 = "ab", "ba"
+#         if y > x:
+#             x, y = y, x
+#             s1, s2 = s2, s1
+        
+#         points1, s = self.solve(s, s1, x)
+#         points2, _ = self.solve(s, s2, y)
+        
+#         return points1 + points2
