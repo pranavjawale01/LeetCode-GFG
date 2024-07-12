76294bedc9132c6d4f1c97ac91436a7d9276ad9f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-12 17:02:41 +0530

    Time: 232 ms (29.29%), Space: 48.6 MB (13.13%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -0,0 +1,45 @@
+class Solution {
+    private int solve(StringBuilder s, String targetStr, int point) {
+        int totalPoints = 0;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (st.isEmpty()) {
+                st.push(s.charAt(i));
+            } else {
+                if (s.charAt(i) == targetStr.charAt(1) && st.peek() == targetStr.charAt(0)) {
+                    st.pop();
+                    totalPoints += point;
+                } else {
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+
+        s.setLength(0);
+        while (!st.isEmpty()) {
+            s.append(st.pop());
+        }
+        s.reverse();
+
+        return totalPoints;
+    }
+
+    public int maximumGain(String s, int x, int y) {
+        String s1 = "ab", s2 = "ba";
+        if (y > x) {
+            String tempStr = s1;
+            s1 = s2;
+            s2 = tempStr;
+
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        int points1 = solve(sb, s1, x);
+        int points2 = solve(sb, s2, y);
+
+        return points1 + points2;
+    }
+}
