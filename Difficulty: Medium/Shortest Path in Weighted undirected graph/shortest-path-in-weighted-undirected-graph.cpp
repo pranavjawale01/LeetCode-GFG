b136aab70babc73b17e7a081587ed63d7ad8f6cf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-13 11:57:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Path in Weighted undirected graph/shortest-path-in-weighted-undirected-graph.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        // Code here
+        vector<vector<pair<int, int>>> adj(n+1);
+        for (auto it : edges) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>> , greater<pair<int, int>>> pq;
+        vector<int> distance(n+1, INT_MAX);
+        vector<int> parent(n+1);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i; 
+        }
+        distance[1] = 0;
+        pq.push({0, 1});
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for (auto it : adj[node]) {
+                int edgeWeight = it.second;
+                int adjNode = it.first;
+                if (dist + edgeWeight < distance[adjNode]) {
+                    distance[adjNode] = dist + edgeWeight;
+                    pq.push({distance[adjNode], adjNode});
+                    parent[adjNode] = node;
+                }
+            }
+        }
+        if (distance[n] == INT_MAX) {
+            return {-1};
+        }
+        vector<int> path;
+        int i = n;
+        while (i != 1) {
+            path.push_back(i);
+            i = parent[i];
+        }
+        path.push_back(1);
+        reverse(path.begin(), path.end());
+        path.insert(path.begin(), distance[n]);
+        return path;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        map<pair<int, int>, int> mp;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+            mp[{edges.back()[0], edges.back()[1]}] = edges.back()[2];
+            mp[{edges.back()[1], edges.back()[0]}] = edges.back()[2];
+        }
+
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        int ans = 0;
+        for (int i = 2; i < res.size(); i++) {
+            ans += mp[{res[i], res[i - 1]}];
+        }
+        if (ans == res[0]) {
+
+        } else if (res.size() == 1 && res[0] == -1)
+            ans = res[0];
+        else
+            ans = -2;
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
