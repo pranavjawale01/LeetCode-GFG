f934ee55eb9516ead355486cc0b68c2a7c681fa8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-16 12:36:30 +0530

    Time: 152 ms (66.21%), Space: 119.5 MB (37.31%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -11,35 +11,20 @@
  */
 class Solution {
 public:
-    TreeNode *lowestCommonAncestor(TreeNode* root, int src, int dest) {
+    bool findPath(TreeNode *root, int target, string &path) {
         if (!root) {
-            return nullptr;
-        }
-        if (root->val == src || root->val == dest) {
-            return root;
-        }
-        TreeNode *l = lowestCommonAncestor(root->left,  src, dest);
-        TreeNode *r = lowestCommonAncestor(root->right, src, dest);
-        if (l && r) {
-            return root;
-        }
-        return l ? l : r;
-    }
-
-    bool findPath(TreeNode *LCA, int target, string &path) {
-        if (!LCA) {
             return false;
         }
-        if (LCA->val == target) {
+        if (root->val == target) {
             return true;
         }
         path.push_back('L');
-        if (findPath(LCA->left, target, path)) {
+        if (findPath(root->left, target, path)) {
             return true;
         }
         path.pop_back();
         path.push_back('R');
-        if (findPath(LCA->right, target, path)) {
+        if (findPath(root->right, target, path)) {
             return true;
         }
         path.pop_back();
@@ -47,13 +32,73 @@ public:
     }
 
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        TreeNode *LCA = lowestCommonAncestor(root, startValue, destValue);
-        string lcaToSrc = "";
-        string lcaToDes = "";
-        findPath(LCA, startValue, lcaToSrc);
-        findPath(LCA, destValue, lcaToDes);
-        string ans(lcaToSrc.size(), 'U');
-        ans += lcaToDes;
+        string rootToSrc = "", rootToDes = "";
+        findPath(root, startValue, rootToSrc);
+        findPath(root, destValue, rootToDes);
+
+        int i = 0;
+        while (i < rootToSrc.size() && i < rootToDes.size() && rootToSrc[i] == rootToDes[i]) {
+            i++;
+        }
+        string ans(rootToSrc.size() - i, 'U');
+        ans += rootToDes.substr(i);
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     TreeNode *lowestCommonAncestor(TreeNode* root, int src, int dest) {
+//         if (!root) {
+//             return nullptr;
+//         }
+//         if (root->val == src || root->val == dest) {
+//             return root;
+//         }
+//         TreeNode *l = lowestCommonAncestor(root->left,  src, dest);
+//         TreeNode *r = lowestCommonAncestor(root->right, src, dest);
+//         if (l && r) {
+//             return root;
+//         }
+//         return l ? l : r;
+//     }
+
+//     bool findPath(TreeNode *LCA, int target, string &path) {
+//         if (!LCA) {
+//             return false;
+//         }
+//         if (LCA->val == target) {
+//             return true;
+//         }
+//         path.push_back('L');
+//         if (findPath(LCA->left, target, path)) {
+//             return true;
+//         }
+//         path.pop_back();
+//         path.push_back('R');
+//         if (findPath(LCA->right, target, path)) {
+//             return true;
+//         }
+//         path.pop_back();
+//         return false;
+//     }
+
+//     string getDirections(TreeNode* root, int startValue, int destValue) {
+//         TreeNode *LCA = lowestCommonAncestor(root, startValue, destValue);
+//         string lcaToSrc = "";
+//         string lcaToDes = "";
+//         findPath(LCA, startValue, lcaToSrc);
+//         findPath(LCA, destValue, lcaToDes);
+//         string ans(lcaToSrc.size(), 'U');
+//         ans += lcaToDes;
+//         return ans;
+//     }
+// };
