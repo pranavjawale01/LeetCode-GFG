f41ed9a27ac89a2bc5881f4420fee03237974c8c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-16 12:37:41 +0530

    Time: 244 ms (88.42%), Space: 52.4 MB (37.25%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,37 +6,71 @@
 #         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: Optional[TreeNode], src: int, dest: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        if root.val == src or root.val == dest:
-            return root
-        l = self.lowestCommonAncestor(root.left, src, dest)
-        r = self.lowestCommonAncestor(root.right, src, dest)
-        if l and r:
-            return root
-        return l if l else r
-
     def findPath(self, root: Optional[TreeNode], target: int, path: List[str]) -> bool:
         if not root:
             return False
         if root.val == target:
             return True
-        path.append("L")
+        path.append('L')
         if self.findPath(root.left, target, path):
             return True
         path.pop()
-        path.append("R")
+        path.append('R')
         if self.findPath(root.right, target, path):
             return True
         path.pop()
         return False
 
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        LCA = self.lowestCommonAncestor(root, startValue, destValue)
-        lcaToSrc, lcaToDes = [], []
-        self.findPath(LCA, startValue, lcaToSrc)
-        self.findPath(LCA, destValue, lcaToDes)
-        result = "U" * len(lcaToSrc)
-        result += "".join(lcaToDes)
+        rootToSrc = []
+        rootToDes = []
+        self.findPath(root, startValue, rootToSrc)
+        self.findPath(root, destValue, rootToDes)
+
+        i = 0
+        while i < len(rootToSrc) and i < len(rootToDes) and rootToSrc[i] == rootToDes[i]:
+            i += 1
+        
+        result = 'U' * (len(rootToSrc) - i)
+        result += ''.join(rootToDes[i:])
         return result
+
+
+
+
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: Optional[TreeNode], src: int, dest: int) -> Optional[TreeNode]:
+#         if not root:
+#             return None
+#         if root.val == src or root.val == dest:
+#             return root
+#         l = self.lowestCommonAncestor(root.left, src, dest)
+#         r = self.lowestCommonAncestor(root.right, src, dest)
+#         if l and r:
+#             return root
+#         return l if l else r
+
+#     def findPath(self, root: Optional[TreeNode], target: int, path: List[str]) -> bool:
+#         if not root:
+#             return False
+#         if root.val == target:
+#             return True
+#         path.append("L")
+#         if self.findPath(root.left, target, path):
+#             return True
+#         path.pop()
+#         path.append("R")
+#         if self.findPath(root.right, target, path):
+#             return True
+#         path.pop()
+#         return False
+
+#     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+#         LCA = self.lowestCommonAncestor(root, startValue, destValue)
+#         lcaToSrc, lcaToDes = [], []
+#         self.findPath(LCA, startValue, lcaToSrc)
+#         self.findPath(LCA, destValue, lcaToDes)
+#         result = "U" * len(lcaToSrc)
+#         result += "".join(lcaToDes)
+#         return result
