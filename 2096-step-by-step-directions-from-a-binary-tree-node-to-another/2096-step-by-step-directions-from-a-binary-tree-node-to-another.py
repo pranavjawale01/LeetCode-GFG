99e354a5b7c4013fbeb6b74277c5c27e9b40dea0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-16 12:30:31 +0530

    Time: 258 ms (74.5%), Space: 51.4 MB (71.14%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: Optional[TreeNode], src: int, dest: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val == src or root.val == dest:
+            return root
+        l = self.lowestCommonAncestor(root.left, src, dest)
+        r = self.lowestCommonAncestor(root.right, src, dest)
+        if l and r:
+            return root
+        return l if l else r
+
+    def findPath(self, root: Optional[TreeNode], target: int, path: List[str]) -> bool:
+        if not root:
+            return False
+        if root.val == target:
+            return True
+        path.append("L")
+        if self.findPath(root.left, target, path):
+            return True
+        path.pop()
+        path.append("R")
+        if self.findPath(root.right, target, path):
+            return True
+        path.pop()
+        return False
+
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        LCA = self.lowestCommonAncestor(root, startValue, destValue)
+        lcaToSrc, lcaToDes = [], []
+        self.findPath(LCA, startValue, lcaToSrc)
+        self.findPath(LCA, destValue, lcaToDes)
+        result = "U" * len(lcaToSrc)
+        result += "".join(lcaToDes)
+        return result
