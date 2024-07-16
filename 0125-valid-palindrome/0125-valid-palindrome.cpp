19d0493474c4f2c263716db7745e08a81ff0bc31
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-16 19:33:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans = "";
+        
+        for (char c : s) {
+            if (isalphadigit(c)) {
+                ans += tolower(c);
+            }
+        }
+        
+        int n = ans.length();
+        int i = 0, j = n - 1;
+        
+        while (i < j) {
+            if (ans[i] != ans[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
