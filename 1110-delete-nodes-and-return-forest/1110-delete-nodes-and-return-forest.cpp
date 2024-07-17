aad47400e45cd5cff3c709c50e650eddcae3a89d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-17 15:28:32 +0530

    Time: 12 ms (81.71%), Space: 27.2 MB (23.69%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *deleteHelper(TreeNode *root, unordered_set<int> &st, vector<TreeNode*> &result) {
+        if (!root) {
+            return nullptr;
+        }
+        root->left = deleteHelper(root->left, st, result);
+        root->right = deleteHelper(root->right, st, result);
+        if (st.find(root->val) != st.end()) {
+            if (root->left) {
+                result.push_back(root->left);
+            }
+            if (root->right) {
+                result.push_back(root->right);
+            }
+            return nullptr;
+        } else {
+            return root;
+        }
+    }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> result;
+        unordered_set<int> st(to_delete.begin(), to_delete.end());
+        deleteHelper(root, st, result);
+        if (st.find(root->val) == st.end()) {
+            result.push_back(root);
+        }
+        return result;
+    }
+};
