c0b39f1abef948239219dbea08cb59fa1e29d45d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-18 19:33:54 +0530

    Time: 348 ms (41.54%), Space: 61.8 MB (55.66%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,56 +1,127 @@
 class Solution {
-    static class Pair {
-        int weight, node;
-        Pair(int weight, int node) {
-            this.weight = weight;
-            this.node = node;
+    private int[] parent;
+    private int[] rank;
+
+    private int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private void union(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+
+        if (xParent == yParent)
+            return;
+
+        if (rank[xParent] > rank[yParent]) {
+            parent[yParent] = xParent;
+        } else if (rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        } else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
         }
     }
-    
-    public int prims(List<List<Pair>> adj, int V) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(p -> p.weight));
-        pq.offer(new Pair(0, 0));
-        boolean[] vis = new boolean[V];
-        int sum = 0;
-        
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            int w = p.weight;
-            int u = p.node;
-            if (vis[u]) {
-                continue;
-            }
-            vis[u] = true;
-            sum += w;
-            for (Pair v : adj.get(u)) {
-                int nei = v.node;
-                int nwt = v.weight;
-                if (!vis[nei]) {
-                    pq.offer(new Pair(nwt, nei));
-                }
-            }
-        }
-        return sum;
-    }
-    
+
     public int minCostConnectPoints(int[][] points) {
         int V = points.length;
-        List<List<Pair>> adj = new ArrayList<>();
+        parent = new int[V];
+        rank = new int[V];
         for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
+            parent[i] = i;
+            rank[i] = 0;
         }
+
+        List<int[]> edges = new ArrayList<>();
         for (int i = 0; i < V; i++) {
             for (int j = i + 1; j < V; j++) {
                 int x1 = points[i][0];
                 int y1 = points[i][1];
                 int x2 = points[j][0];
                 int y2 = points[j][1];
-                
                 int d = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-                adj.get(i).add(new Pair(d, j));
-                adj.get(j).add(new Pair(d, i));
+                edges.add(new int[]{i, j, d});
             }
         }
-        return prims(adj, V);
+
+        edges.sort((a, b) -> a[2] - b[2]);
+
+        int sum = 0;
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            if (find(u) != find(v)) {
+                union(u, v);
+                sum += wt;
+            }
+        }
+
+        return sum;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     static class Pair {
+//         int weight, node;
+//         Pair(int weight, int node) {
+//             this.weight = weight;
+//             this.node = node;
+//         }
+//     }
+    
+//     public int prims(List<List<Pair>> adj, int V) {
+//         PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(p -> p.weight));
+//         pq.offer(new Pair(0, 0));
+//         boolean[] vis = new boolean[V];
+//         int sum = 0;
+        
+//         while (!pq.isEmpty()) {
+//             Pair p = pq.poll();
+//             int w = p.weight;
+//             int u = p.node;
+//             if (vis[u]) {
+//                 continue;
+//             }
+//             vis[u] = true;
+//             sum += w;
+//             for (Pair v : adj.get(u)) {
+//                 int nei = v.node;
+//                 int nwt = v.weight;
+//                 if (!vis[nei]) {
+//                     pq.offer(new Pair(nwt, nei));
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+    
+//     public int minCostConnectPoints(int[][] points) {
+//         int V = points.length;
+//         List<List<Pair>> adj = new ArrayList<>();
+//         for (int i = 0; i < V; i++) {
+//             adj.add(new ArrayList<>());
+//         }
+//         for (int i = 0; i < V; i++) {
+//             for (int j = i + 1; j < V; j++) {
+//                 int x1 = points[i][0];
+//                 int y1 = points[i][1];
+//                 int x2 = points[j][0];
+//                 int y2 = points[j][1];
+                
+//                 int d = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+//                 adj.get(i).add(new Pair(d, j));
+//                 adj.get(j).add(new Pair(d, i));
+//             }
+//         }
+//         return prims(adj, V);
+//     }
+// }
