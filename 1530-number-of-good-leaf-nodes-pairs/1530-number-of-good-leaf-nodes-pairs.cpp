4731279771a2844c990f2a9c9d46b08b8db52a0d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-18 17:01:56 +0530

    Time: 274 ms (6.98%), Space: 118.6 MB (5.24%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void makeGraph(TreeNode *root, TreeNode *prev, unordered_map<TreeNode*, vector<TreeNode*>> &mp, unordered_set<TreeNode*> &st) {
+        if (root == nullptr) {
+            return;
+        }
+        if (!root->left && !root->right) {
+            st.insert(root);
+        }
+        if (prev) {
+            mp[root].push_back(prev);
+            mp[prev].push_back(root);
+        }
+        makeGraph(root->left, root, mp, st);
+        makeGraph(root->right,root, mp, st);
+    }
+
+    int countPairs(TreeNode* root, int distance) {
+        unordered_map<TreeNode*, vector<TreeNode*>> mp;
+        unordered_set<TreeNode*> st;
+        makeGraph(root, nullptr, mp, st);
+        int count = 0;
+        for (auto &leaf : st) {
+            queue<TreeNode*> q;
+            unordered_set<TreeNode*> vis;
+            q.push(leaf);
+            vis.insert(leaf);
+            for (int level = 0; level <= distance; level++) {
+                int size = q.size();
+                while (size--) {
+                    TreeNode *curr = q.front();
+                    q.pop();
+                    if (curr != leaf && st.count(curr)) {
+                        count++;
+                    }
+                    for (auto &x : mp[curr]) {
+                        if (!vis.count(x)) {
+                            q.push(x);
+                            vis.insert(x);
+                        }
+                    }
+                }
+            }
+        }
+        return count / 2;
+    }
+};
