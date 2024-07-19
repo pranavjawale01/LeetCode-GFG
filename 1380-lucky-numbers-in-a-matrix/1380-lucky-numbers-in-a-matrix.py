167b711358da38c63097eade64d615f34fc03d86
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-19 14:36:46 +0530

    Time: 113 ms (47.26%), Space: 17 MB (6.71%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,21 +1,45 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
         row, col = len(matrix), len(matrix[0])
-        minRow = [float('inf')] * row
-        maxCol = [float('-inf')] * col
+        rMinMax = float('-inf')
         for i in range(row):
+            rMin = float('inf')
             for j in range(col):
-                minRow[i] = min(minRow[i], matrix[i][j])
-        
-        for i in range(col):
-            for j in range(row):
-                maxCol[i] = max(maxCol[i], matrix[j][i])
+                rMin = min(rMin, matrix[i][j])
+            rMinMax = max(rMinMax, rMin)
 
+        cMaxMin = float('inf')
+        for j in range(col):
+            cMax = float('-inf')
+            for i in range(row):
+                cMax = max(cMax, matrix[i][j])
+            cMaxMin = min(cMaxMin, cMax)
+        
         ans = []
+        if rMinMax == cMaxMin:
+            ans.append(rMinMax)
 
-        for i in range(row):
-            for j in range(col):
-                if maxCol[j] == minRow[i]:
-                    ans.append(minRow[i])
-        
         return ans
+
+
+# class Solution:
+#     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+#         row, col = len(matrix), len(matrix[0])
+#         minRow = [float('inf')] * row
+#         maxCol = [float('-inf')] * col
+#         for i in range(row):
+#             for j in range(col):
+#                 minRow[i] = min(minRow[i], matrix[i][j])
+        
+#         for i in range(col):
+#             for j in range(row):
+#                 maxCol[i] = max(maxCol[i], matrix[j][i])
+
+#         ans = []
+
+#         for i in range(row):
+#             for j in range(col):
+#                 if maxCol[j] == minRow[i]:
+#                     ans.append(minRow[i])
+        
+#         return ans
