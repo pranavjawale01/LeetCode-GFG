87c4b956531c765a93cb78e05d88934affb1fc0e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-19 14:33:00 +0530

    Time: 13 ms (75.13%), Space: 14.4 MB (18.75%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -2,24 +2,59 @@ class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int row = matrix.size(), col = matrix[0].size();
-        vector<int> minRow(row, INT_MAX);
-        vector<int> maxCol(col, INT_MIN);
+        int rMinMax = INT_MIN;
         for (int i = 0; i < row; i++) {
-            minRow[i] = *min_element(matrix[i].begin(), matrix[i].end());
-        }
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
-                maxCol[i] = max(matrix[j][i], maxCol[i]);
-            }
-        }
-        vector<int> ans;
-        for (int i = 0; i < row; i++) {
+            int rMin = INT_MAX;
             for (int j = 0; j < col; j++) {
-                if (minRow[i] == maxCol[j]) {
-                    ans.push_back(minRow[i]);
-                }
+                rMin = min(rMin, matrix[i][j]);
             }
+            rMinMax = max(rMinMax, rMin);
         }
-        return ans;
+        int cMaxMin = INT_MAX;
+        for (int j = 0; j < col; j++) {
+            int cMax = INT_MIN;
+            for (int i = 0; i < row; i++) {
+                cMax = max(cMax, matrix[i][j]);
+            }
+            cMaxMin = min(cMaxMin, cMax);
+        }
+        if (cMaxMin == rMinMax) {
+            return {rMinMax};
+        }
+        return {};
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+//         int row = matrix.size(), col = matrix[0].size();
+//         vector<int> minRow(row, INT_MAX);
+//         vector<int> maxCol(col, INT_MIN);
+//         for (int i = 0; i < row; i++) {
+//             minRow[i] = *min_element(matrix[i].begin(), matrix[i].end());
+//         }
+//         for (int i = 0; i < col; i++) {
+//             for (int j = 0; j < row; j++) {
+//                 maxCol[i] = max(matrix[j][i], maxCol[i]);
+//             }
+//         }
+//         vector<int> ans;
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (minRow[i] == maxCol[j]) {
+//                     ans.push_back(minRow[i]);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
