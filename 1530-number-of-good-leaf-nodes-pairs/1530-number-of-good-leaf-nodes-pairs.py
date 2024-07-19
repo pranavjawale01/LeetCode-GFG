88bd02c2cde3556aa0688e79e451106157d85fc3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-19 19:30:36 +0530

    Time: 86 ms (68.1%), Space: 17 MB (39.2%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -5,37 +5,72 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def makeGraph(self, root: TreeNode, prev: TreeNode, mp, st):
+    def solve(self, root: Optional[TreeNode], distance: int) -> List[int]:
         if not root:
-            return
+            return []
         if not root.left and not root.right:
-            st.add(root)
-        if prev:
-            mp[root].append(prev)
-            mp[prev].append(root)
-        self.makeGraph(root.left, root, mp, st)
-        self.makeGraph(root.right, root, mp, st)
+            return [1]
+        
+        left = self.solve(root.left, distance)
+        right = self.solve(root.right, distance)
 
-    def countPairs(self, root: TreeNode, distance: int) -> int:
-        mp = defaultdict(list)
-        st = set()
-        self.makeGraph(root, None, mp, st)
-        count = 0
-        for leaf in st:
-            q = deque()
-            vis = set()
-            q.append(leaf)
-            vis.add(leaf)
-            for i in range(distance + 1):
-                size = len(q)
-                while size > 0:
-                    size -= 1
-                    curr = q.popleft()
-                    if curr != leaf and curr in st:
-                        count += 1
-                    for x in mp[curr]:
-                        if x not in vis:
-                            q.append(x)
-                            vis.add(x)
+        for l in left:
+            for r in right:
+                if l != 0 and r != 0 and l + r <= distance:
+                    self.goodLeaf += 1
 
-        return count // 2
+        curr = []
+        for l in left:
+            if l != 0 and l + 1 <= distance:
+                curr.append(l + 1)
+        for r in right:
+            if r != 0 and r + 1 <= distance:
+                curr.append(r + 1)
+                
+        return curr
+
+    def countPairs(self, root: Optional[TreeNode], distance: int) -> int:
+        self.goodLeaf = 0
+        self.solve(root, distance)
+        return self.goodLeaf
+
+
+
+
+
+
+# class Solution:
+#     def makeGraph(self, root: TreeNode, prev: TreeNode, mp, st):
+#         if not root:
+#             return
+#         if not root.left and not root.right:
+#             st.add(root)
+#         if prev:
+#             mp[root].append(prev)
+#             mp[prev].append(root)
+#         self.makeGraph(root.left, root, mp, st)
+#         self.makeGraph(root.right, root, mp, st)
+
+#     def countPairs(self, root: TreeNode, distance: int) -> int:
+#         mp = defaultdict(list)
+#         st = set()
+#         self.makeGraph(root, None, mp, st)
+#         count = 0
+#         for leaf in st:
+#             q = deque()
+#             vis = set()
+#             q.append(leaf)
+#             vis.add(leaf)
+#             for i in range(distance + 1):
+#                 size = len(q)
+#                 while size > 0:
+#                     size -= 1
+#                     curr = q.popleft()
+#                     if curr != leaf and curr in st:
+#                         count += 1
+#                     for x in mp[curr]:
+#                         if x not in vis:
+#                             q.append(x)
+#                             vis.add(x)
+
+#         return count // 2
