29a60e4aa6d6a3fb95fb1564d9c5de2bd30edf11
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-20 23:48:45 +0530

    Time: 105 ms (50%), Space: 30.6 MB (50%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        sort(s.begin(), s.end());
-        int ans = 0;
-        int n = s.length();
-        for (int i = 0; i < n;) {
-            char c = s[i];
-            int count = 0;
-            while (c == s[i]) {
-                i++;
-                count++;
-            }
-            if (count <= 2) {
-                ans += count;
-            } else {
-                if (count % 2 == 1) {
-                    ans += 1;
-                } else {
-                    ans += 2;
-                }
+        int count[26];
+        int remove = 0;
+        for (char c : s) {
+            count[c - 'a']++;
+            if (count[c - 'a'] == 3) {
+                count[c - 'a'] -= 2;
+                remove += 2;
             }
         }
-        return ans;
+        return s.size() - remove;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int minimumLength(string s) {
+//         sort(s.begin(), s.end());
+//         int ans = 0;
+//         int n = s.length();
+//         for (int i = 0; i < n;) {
+//             char c = s[i];
+//             int count = 0;
+//             while (c == s[i]) {
+//                 i++;
+//                 count++;
+//             }
+//             if (count <= 2) {
+//                 ans += count;
+//             } else {
+//                 if (count % 2 == 1) {
+//                     ans += 1;
+//                 } else {
+//                     ans += 2;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
