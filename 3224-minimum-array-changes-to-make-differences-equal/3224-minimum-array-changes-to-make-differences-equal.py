49a3af14348cbae841a0d5339e26074aad6ee2a0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-21 00:21:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minChanges(self, nums: List[int], k: int) -> int:
+        freq = [0] * 26
+        vec = []
+        n = len(nums)
+
+        for i in range(n // 2):
+            diff = abs(nums[i] - nums[n-i-1])
+            freq[diff] += 1
+            a = nums[i]
+            b = nums[n-i-1]
+            threshold = max(max(a, b), k - min(a, b))
+            vec.append(threshold)
+
+        vec.sort()
+        ans = n // 2
+        n //= 2
+
+        for i in range(k+1):
+            rest = n - freq[i]
+            greater = self.lower_bound(vec, i)
+            ans = min(ans, rest+greater)
+        
+        return ans
+
+    def lower_bound(self, vec, target):
+        left = 0
+        right = len(vec)
+        while left < right:
+            mid = (left + right) // 2
+            if vec[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
