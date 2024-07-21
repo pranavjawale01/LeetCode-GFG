3ff2308f51993bee9ecf730f75264bb218c5a55c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-21 10:19:17 +0530

    Time: 47 ms (16.67%), Space: 16.9 MB (16.67%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int n = s.length(), count = 0, ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '0') {
-                ans += count;
-                while (i < n && s[i] != '1') {
-                    i++;
+        string t = "";
+        int count = 0, one = 0;
+        t += s[0];
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == '1') {
+                t += s[i];
+            } else {
+                if (s[i] != s[i-1]) {
+                    t += s[i];
                 }
             }
-            count++;
         }
-        return ans;
+        for (char c : t) {
+            if (c == '1') {
+                one++;
+            } else {
+                count += one;
+            }
+        }
+        return count;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int maxOperations(string s) {
+//         int n = s.length(), count = 0, ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (s[i] == '0') {
+//                 ans += count;
+//                 while (i < n && s[i] != '1') {
+//                     i++;
+//                 }
+//             }
+//             count++;
+//         }
+//         return ans;
+//     }
+// };
