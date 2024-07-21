8c99086885014d7d535dfaebe1883733cca41013
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-21 09:58:37 +0530

    Time: 43 ms (100%), Space: 17.5 MB (20%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count = 0;
+        int n = s.length();
+        for (char c : s) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return false;
+        }
+        return true;
+    }
+};
