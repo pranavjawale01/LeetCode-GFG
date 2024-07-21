ef38acf82d7c9e7d44f7b4caab8d273d1a47ba51
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-21 10:03:37 +0530

    Time: 31 ms (100%), Space: 17.4 MB (20%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        map<int, int> mp;
         for (char c : s) {
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                mp[c]++;
-            }
-        }
-        int sum = 0;
-        for (auto x : mp) {
-            sum += x.second;
-            if (sum > 0) {
                 return true;
             }
         }
         return false;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     bool doesAliceWin(string s) {
+//         map<int, int> mp;
+//         for (char c : s) {
+//             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+//                 mp[c]++;
+//             }
+//         }
+//         int sum = 0;
+//         for (auto x : mp) {
+//             sum += x.second;
+//             if (sum > 0) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
 
 
 
