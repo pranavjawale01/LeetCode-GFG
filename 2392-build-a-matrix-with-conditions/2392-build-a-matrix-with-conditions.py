f12c83fe8006b7053e46427f039117c4ab0764ea
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-21 11:42:40 +0530

    Time: 571 ms (86.26%), Space: 25.4 MB (45.8%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,48 @@
+class Solution:
+    def dfs(self, node: int, adj: Dict[int, List[int]], vis: List[int], stack: List[int], hasCycle: List[bool]) -> None:
+        vis[node] = 1
+        for x in adj[node]:
+            if vis[x] == 0:
+                self.dfs(x, adj, vis, stack, hasCycle)
+                if hasCycle[0]:
+                    return
+            elif vis[x] == 1:
+                hasCycle[0] = True
+                return
+        vis[node] = 2
+        stack.append(node)
+    
+    def topoSort(self, edges: List[List[int]], n: int) -> List[int]:
+        adj = defaultdict(list)
+        stack = []
+        vis = [0] * (n + 1)
+        hasCycle = [False]
+        
+        for u, v in edges:
+            adj[u].append(v)
+        
+        for i in range(1, n + 1):
+            if vis[i] == 0:
+                self.dfs(i, adj, vis, stack, hasCycle)
+                if hasCycle[0]:
+                    return []
+        
+        return stack[::-1]
+    
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        orderRows = self.topoSort(rowConditions, k)
+        orderCols = self.topoSort(colConditions, k)
+        
+        if not orderRows or not orderCols:
+            return []
+        
+        matrix = [[0] * k for _ in range(k)]
+        rowPos = {num: i for i, num in enumerate(orderRows)}
+        colPos = {num: i for i, num in enumerate(orderCols)}
+        
+        for i in range(1, k + 1):
+            r = rowPos[i]
+            c = colPos[i]
+            matrix[r][c] = i
+        
+        return matrix
