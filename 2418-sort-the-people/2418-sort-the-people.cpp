83edc48f0985216ab5170ae7c778e7bf72345a0b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-22 11:32:49 +0530

    Time: 27 ms (70.61%), Space: 24.3 MB (76.5%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+        vector<pair<string, int>> vec(n);
+        for (int i = 0; i < n; i++) {
+            vec[i] = make_pair(names[i], heights[i]);
+        }
+        auto cmp = [](auto &a, auto &b) {
+            return a.second > b.second;
+        };
+        sort(vec.begin(), vec.end(), cmp);
+        for (int i = 0; i < n; i++) {
+            names[i] = vec[i].first;
+        }
+        return names;
+    }
+};
