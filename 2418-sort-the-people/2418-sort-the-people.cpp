2651616708225be2ab64e536fa9c9bf52c10131d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-22 11:36:43 +0530

    Time: 26 ms (75.15%), Space: 27.9 MB (24.15%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -2,17 +2,37 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n = names.size();
-        vector<pair<string, int>> vec(n);
+        vector<pair<int, string>> vec(n);
         for (int i = 0; i < n; i++) {
-            vec[i] = make_pair(names[i], heights[i]);
+            vec[i] = make_pair(heights[i], names[i]);
         }
-        auto cmp = [](auto &a, auto &b) {
-            return a.second > b.second;
-        };
-        sort(vec.begin(), vec.end(), cmp);
+        sort(rbegin(vec), rend(vec));
         for (int i = 0; i < n; i++) {
-            names[i] = vec[i].first;
+            names[i] = vec[i].second;
         }
         return names;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+//         int n = names.size();
+//         vector<pair<string, int>> vec(n);
+//         for (int i = 0; i < n; i++) {
+//             vec[i] = make_pair(names[i], heights[i]);
+//         }
+//         auto cmp = [](auto &a, auto &b) {
+//             return a.second > b.second;
+//         };
+//         sort(vec.begin(), vec.end(), cmp);
+//         for (int i = 0; i < n; i++) {
+//             names[i] = vec[i].first;
+//         }
+//         return names;
+//     }
+// };
