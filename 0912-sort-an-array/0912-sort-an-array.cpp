5f25fa0bc7b7f8398f8051f3f40c036e712bb518
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-25 12:34:18 +0530

    Time: 133 ms (60.73%), Space: 69.3 MB (84.29%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,59 +1,96 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        vector<int> leftVec(n1);
-        vector<int> rightVec(n2);
-
-        for (int i = 0; i < n1; i++) {
-            leftVec[i] = nums[left + i];
+    void heapify(vector<int> &nums, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && nums[left] > nums[largest]) {
+            largest = left;
         }
-        for (int j = 0; j < n2; j++) {
-            rightVec[j] = nums[mid + 1 + j];
+        if (right < n && nums[right] > nums[largest]) {
+            largest = right;
         }
-
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (leftVec[i] <= rightVec[j]) {
-                nums[k] = leftVec[i];
-                i++;
-            } else {
-                nums[k] = rightVec[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1) {
-            nums[k] = leftVec[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            nums[k] = rightVec[j];
-            j++;
-            k++;
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            heapify(nums, n, largest);
         }
     }
-
-    void mergeSort(vector<int>& nums, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-
-            mergeSort(nums, left, mid);
-            mergeSort(nums, mid + 1, right);
-
-            merge(nums, left, mid, right);
+    void heapsort(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
         }
     }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        heapsort(nums);
         return nums;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums, int left, int mid, int right) {
+//         int n1 = mid - left + 1;
+//         int n2 = right - mid;
+//         vector<int> leftVec(n1);
+//         vector<int> rightVec(n2);
+
+//         for (int i = 0; i < n1; i++) {
+//             leftVec[i] = nums[left + i];
+//         }
+//         for (int j = 0; j < n2; j++) {
+//             rightVec[j] = nums[mid + 1 + j];
+//         }
+
+//         int i = 0, j = 0, k = left;
+//         while (i < n1 && j < n2) {
+//             if (leftVec[i] <= rightVec[j]) {
+//                 nums[k] = leftVec[i];
+//                 i++;
+//             } else {
+//                 nums[k] = rightVec[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+
+//         while (i < n1) {
+//             nums[k] = leftVec[i];
+//             i++;
+//             k++;
+//         }
+
+//         while (j < n2) {
+//             nums[k] = rightVec[j];
+//             j++;
+//             k++;
+//         }
+//     }
+
+//     void mergeSort(vector<int>& nums, int left, int right) {
+//         if (left < right) {
+//             int mid = left + (right - left) / 2;
+
+//             mergeSort(nums, left, mid);
+//             mergeSort(nums, mid + 1, right);
+
+//             merge(nums, left, mid, right);
+//         }
+//     }
+
+//     vector<int> sortArray(vector<int>& nums) {
+//         mergeSort(nums, 0, nums.size() - 1);
+//         return nums;
+//     }
+// };
 
 
 
