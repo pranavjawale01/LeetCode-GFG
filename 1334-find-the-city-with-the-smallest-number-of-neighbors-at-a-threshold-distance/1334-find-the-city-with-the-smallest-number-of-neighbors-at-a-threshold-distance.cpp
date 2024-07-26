bea3ee18c6be7947820d5b34bf4bb899366998b7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-26 12:43:46 +0530

    Time: 249 ms (5.01%), Space: 15.4 MB (79.53%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     #define P pair<int, int>
-    void dijkstra(int n, unordered_map<int, vector<P>> &mp, vector<int> &result, int s) {
-        priority_queue<P, vector<P>, greater<P>> pq;
-        pq.push({0, s});
-        //fill(result.begin(), result.end(), INT_MAX);
+    void bellmanFord(int n, vector<vector<int>>& edges, vector<int> &result, int s) {
         result[s] = 0;
-        while (!pq.empty()) {
-            int d = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            for (auto &nei : mp[node]) {
-                int adjNode = nei.first;
-                int dist = nei.second;
-                if (d + dist < result[adjNode]) {
-                    result[adjNode] = d + dist;
-                    pq.push({d+dist, adjNode});
+        for (int i = 1; i < n; i++) {
+            for (auto &x : edges) {
+                int u = x[0];
+                int v = x[1];
+                int w = x[2];
+                if (result[u] != INT_MAX && result[u] + w < result[v]) {
+                    result[v] = result[u] + w;
+                }
+                if (result[v] != INT_MAX && result[v] + w < result[u]) {
+                    result[u] = result[v] + w;
                 }
             }
         }
@@ -42,17 +39,87 @@ public:
         for (int i = 0; i < n; i++) {
             spm[i][i] = 0;
         }
-        unordered_map<int, vector<P>> mp;
         for (auto &x : edges) {
             int u = x[0];
             int v = x[1];
             int w = x[2];
-            mp[u].push_back({v, w});
-            mp[v].push_back({u, w});
+            spm[u][v] = w;
+            spm[v][u] = w;
         }
         for (int i = 0; i < n; i++) {
-            dijkstra(n, mp, spm[i], i);
+            bellmanFord(n, edges, spm[i], i);
         }
         return findResultCity(n, spm, distanceThreshold);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     #define P pair<int, int>
+//     void dijkstra(int n, unordered_map<int, vector<P>> &mp, vector<int> &result, int s) {
+//         priority_queue<P, vector<P>, greater<P>> pq;
+//         pq.push({0, s});
+//         //fill(result.begin(), result.end(), INT_MAX);
+//         result[s] = 0;
+//         while (!pq.empty()) {
+//             int d = pq.top().first;
+//             int node = pq.top().second;
+//             pq.pop();
+//             for (auto &nei : mp[node]) {
+//                 int adjNode = nei.first;
+//                 int dist = nei.second;
+//                 if (d + dist < result[adjNode]) {
+//                     result[adjNode] = d + dist;
+//                     pq.push({d+dist, adjNode});
+//                 }
+//             }
+//         }
+//     }
+//     int findResultCity(int n, vector<vector<int>> &spm, int d) {
+//         int resultCity = -1;
+//         int leastReachCount = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             int countReach = 0;
+//             for (int j = 0; j < n; j++) {
+//                 if (i != j && spm[i][j] <= d) {
+//                     countReach++;
+//                 }
+//             }
+//             if (countReach <= leastReachCount) {
+//                 leastReachCount = countReach;
+//                 resultCity = i;
+//             }
+//         }
+//         return resultCity;
+//     }
+//     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+//         vector<vector<int>> spm(n, vector<int>(n, INT_MAX));
+//         for (int i = 0; i < n; i++) {
+//             spm[i][i] = 0;
+//         }
+//         unordered_map<int, vector<P>> mp;
+//         for (auto &x : edges) {
+//             int u = x[0];
+//             int v = x[1];
+//             int w = x[2];
+//             mp[u].push_back({v, w});
+//             mp[v].push_back({u, w});
+//         }
+//         for (int i = 0; i < n; i++) {
+//             dijkstra(n, mp, spm[i], i);
+//         }
+//         return findResultCity(n, spm, distanceThreshold);
+//     }
+// };
