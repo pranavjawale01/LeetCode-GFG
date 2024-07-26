e318663873148c4ffa0735568e154bfe62fb1717
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-26 12:24:56 +0530

    Time: 249 ms (5.01%), Space: 15.4 MB (79.53%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     #define P pair<int, int>
-    void floydWarshall(int n, vector<vector<int>>& spm) {
-        for (int via = 0; via < n; via++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (spm[i][via] < INT_MAX && spm[via][j] < INT_MAX) {
-                        spm[i][j] = min(spm[i][j], spm[i][via] + spm[via][j]);
-                    }
+    void bellmanFord(int n, vector<vector<int>>& edges, vector<int> &result, int s) {
+        result[s] = 0;
+        for (int i = 1; i < n; i++) {
+            for (auto &x : edges) {
+                int u = x[0];
+                int v = x[1];
+                int w = x[2];
+                if (result[u] != INT_MAX && result[u] + w < result[v]) {
+                    result[v] = result[u] + w;
+                }
+                if (result[v] != INT_MAX && result[v] + w < result[u]) {
+                    result[u] = result[v] + w;
                 }
             }
         }
@@ -41,75 +46,12 @@ public:
             spm[u][v] = w;
             spm[v][u] = w;
         }
-        floydWarshall(n, spm);
+        for (int i = 0; i < n; i++) {
+            bellmanFord(n, edges, spm[i], i);
+        }
         return findResultCity(n, spm, distanceThreshold);
     }
 };
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     #define P pair<int, int>
-//     void bellmanFord(int n, vector<vector<int>>& edges, vector<int> &result, int s) {
-//         result[s] = 0;
-//         for (int i = 1; i < n; i++) {
-//             for (auto &x : edges) {
-//                 int u = x[0];
-//                 int v = x[1];
-//                 int w = x[2];
-//                 if (result[u] != INT_MAX && result[u] + w < result[v]) {
-//                     result[v] = result[u] + w;
-//                 }
-//                 if (result[v] != INT_MAX && result[v] + w < result[u]) {
-//                     result[u] = result[v] + w;
-//                 }
-//             }
-//         }
-//     }
-//     int findResultCity(int n, vector<vector<int>> &spm, int d) {
-//         int resultCity = -1;
-//         int leastReachCount = INT_MAX;
-//         for (int i = 0; i < n; i++) {
-//             int countReach = 0;
-//             for (int j = 0; j < n; j++) {
-//                 if (i != j && spm[i][j] <= d) {
-//                     countReach++;
-//                 }
-//             }
-//             if (countReach <= leastReachCount) {
-//                 leastReachCount = countReach;
-//                 resultCity = i;
-//             }
-//         }
-//         return resultCity;
-//     }
-//     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-//         vector<vector<int>> spm(n, vector<int>(n, INT_MAX));
-//         for (int i = 0; i < n; i++) {
-//             spm[i][i] = 0;
-//         }
-//         for (auto &x : edges) {
-//             int u = x[0];
-//             int v = x[1];
-//             int w = x[2];
-//             spm[u][v] = w;
-//             spm[v][u] = w;
-//         }
-//         for (int i = 0; i < n; i++) {
-//             bellmanFord(n, edges, spm[i], i);
-//         }
-//         return findResultCity(n, spm, distanceThreshold);
-//     }
-// };
-
 
 
 
