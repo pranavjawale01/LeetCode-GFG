cf6390d2f20f3107aee56ef1ab79643f254f6ee4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-27 10:49:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void floydWarshall(vector<vector<int>> &arr) {
+        for (int via = 0; via < 26; via++) {
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < 26; j++) {
+                    if (arr[i][via] < INT_MAX && arr[via][j] < INT_MAX) {
+                        arr[i][j] = min(arr[i][j], arr[i][via] + arr[via][j]);
+                    }
+                }
+            }
+        }
+    }
+
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<int>> arr(26, vector<int>(26, INT_MAX));
+        int n = original.size();
+        
+        for (int i = 0; i < n; i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            arr[u][v] = cost[i];
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            arr[i][i] = 0;
+        }
+        
+        floydWarshall(arr);
+        
+        long long ans = 0;
+        n = source.size();
+        
+        for (int i = 0; i < n; i++) {
+            int u = source[i] - 'a';
+            int v = target[i] - 'a';
+            if (arr[u][v] == INT_MAX) {
+                return -1;
+            }
+            ans += arr[u][v];
+        }
+        return ans;
+    }
+};
