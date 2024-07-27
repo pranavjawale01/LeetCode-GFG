0b9bc138086bee40f3a5a83f2a0bcf8803037213
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-27 11:24:50 +0530

    Time: 401 ms (15.48%), Space: 104.7 MB (40.61%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    void floydWarshall(vector<vector<long long>> &arr) {
-        for (int via = 0; via < 26; via++) {
-            for (int i = 0; i < 26; i++) {
-                for (int j = 0; j < 26; j++) {
-                    if (arr[i][via] < INT_MAX && arr[via][j] < INT_MAX) {
-                        arr[i][j] = min(arr[i][j], arr[i][via] + arr[via][j]);
+    void bellmanFord(vector<vector<long long>> &arr, int s) {
+        int V = arr.size();
+        vector<long long> dist(V, INT_MAX);
+        dist[s] = 0;
+
+        for (int i = 0; i < V - 1; i++) {
+            for (int u = 0; u < V; u++) {
+                for (int v = 0; v < V; v++) {
+                    if (dist[u] != INT_MAX && dist[u] + arr[u][v] < dist[v]) {
+                        dist[v] = dist[u] + arr[u][v];
                     }
                 }
             }
+        }
+
+        for (int i = 0; i < V; i++) {
+            arr[s][i] = dist[i];
         }
     }
 
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         vector<vector<long long>> arr(26, vector<long long>(26, INT_MAX));
         int n = original.size();
-        
+
         for (int i = 0; i < n; i++) {
             int u = original[i] - 'a';
             int v = changed[i] - 'a';
             arr[u][v] = min((long long)cost[i], arr[u][v]);
         }
-        
+
         for (int i = 0; i < 26; i++) {
             arr[i][i] = 0;
         }
-        
-        floydWarshall(arr);
-        
+
+        for (int i = 0; i < 26; i++) {
+            bellmanFord(arr, i);
+        }
+
         long long ans = 0;
         n = source.size();
-        
+
         for (int i = 0; i < n; i++) {
             int u = source[i] - 'a';
             int v = target[i] - 'a';
@@ -42,3 +52,57 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void floydWarshall(vector<vector<long long>> &arr) {
+//         for (int via = 0; via < 26; via++) {
+//             for (int i = 0; i < 26; i++) {
+//                 for (int j = 0; j < 26; j++) {
+//                     if (arr[i][via] < INT_MAX && arr[via][j] < INT_MAX) {
+//                         arr[i][j] = min(arr[i][j], arr[i][via] + arr[via][j]);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+//         vector<vector<long long>> arr(26, vector<long long>(26, INT_MAX));
+//         int n = original.size();
+        
+//         for (int i = 0; i < n; i++) {
+//             int u = original[i] - 'a';
+//             int v = changed[i] - 'a';
+//             arr[u][v] = min((long long)cost[i], arr[u][v]);
+//         }
+        
+//         for (int i = 0; i < 26; i++) {
+//             arr[i][i] = 0;
+//         }
+        
+//         floydWarshall(arr);
+        
+//         long long ans = 0;
+//         n = source.size();
+        
+//         for (int i = 0; i < n; i++) {
+//             int u = source[i] - 'a';
+//             int v = target[i] - 'a';
+//             if (arr[u][v] == INT_MAX) {
+//                 return -1;
+//             }
+//             ans += arr[u][v];
+//         }
+//         return ans;
+//     }
+// };
