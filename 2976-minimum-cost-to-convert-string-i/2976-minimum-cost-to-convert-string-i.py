32a2eceb45582383614b40402d493cde6b23da3a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-27 12:02:37 +0530

    Time: 773 ms (93.91%), Space: 18.4 MB (27.92%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -1,34 +1,38 @@
 class Solution:
-    def bellmanFord(self, arr, s):
-        V = len(arr)
-        dist = [sys.maxsize] * V
-        dist[s] = 0
-        for i in range(V-1):
-            for u in range(V):
-                for v in range(V):
-                    if dist[u] != sys.maxsize and dist[u] + arr[u][v] < dist[v]:
-                        dist[v] = dist[u] + arr[u][v]
-        for i in range(V):
-            arr[s][i] = dist[i]
-
-    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-        arr = [[sys.maxsize for _ in range(26)] for _ in range(26)]
-        n = len(original)
-        for i in range(n):
-            u = ord(original[i]) - ord('a')
-            v = ord(changed[i])  - ord('a')
-            arr[u][v] = min(cost[i], arr[u][v])
-
-        for i in range(26):
-            arr[i][i] = 0
+    def dijkstra(self, s, adj, arr):
+        pq = []
+        dist = [sys.maxsize] * 26
+        dist[ord(s) - ord('a')] = 0
+        heapq.heappush(pq, (0, s))
+        
+        while pq:
+            d, n = heapq.heappop(pq)
+            
+            if d > dist[ord(n) - ord('a')]:
+                continue
+            
+            for node, cost in adj[n]:
+                if dist[ord(node) - ord('a')] > d + cost:
+                    dist[ord(node) - ord('a')] = d + cost
+                    heapq.heappush(pq, (d + cost, node))
         
         for i in range(26):
-            self.bellmanFord(arr, i)
+            arr[ord(s) - ord('a')][i] = dist[i]
 
-        n = len(target)
 
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        adj = defaultdict(list)
+        for i in range(len(original)):
+            adj[original[i]].append((changed[i], cost[i]))
+        
+        arr = [[sys.maxsize] * 26 for _ in range(26)]
+        
+        for c in range(26):
+            self.dijkstra(chr(ord('a') + c), adj, arr)
+        
         ans = 0
-
+        n = len(source)
+        
         for i in range(n):
             u = ord(source[i]) - ord('a')
             v = ord(target[i]) - ord('a')
@@ -37,6 +41,56 @@ class Solution:
             ans += arr[u][v]
         
         return ans
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def bellmanFord(self, arr, s):
+#         V = len(arr)
+#         dist = [sys.maxsize] * V
+#         dist[s] = 0
+#         for i in range(V-1):
+#             for u in range(V):
+#                 for v in range(V):
+#                     if dist[u] != sys.maxsize and dist[u] + arr[u][v] < dist[v]:
+#                         dist[v] = dist[u] + arr[u][v]
+#         for i in range(V):
+#             arr[s][i] = dist[i]
+
+#     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+#         arr = [[sys.maxsize for _ in range(26)] for _ in range(26)]
+#         n = len(original)
+#         for i in range(n):
+#             u = ord(original[i]) - ord('a')
+#             v = ord(changed[i])  - ord('a')
+#             arr[u][v] = min(cost[i], arr[u][v])
+
+#         for i in range(26):
+#             arr[i][i] = 0
+        
+#         for i in range(26):
+#             self.bellmanFord(arr, i)
+
+#         n = len(target)
+
+#         ans = 0
+
+#         for i in range(n):
+#             u = ord(source[i]) - ord('a')
+#             v = ord(target[i]) - ord('a')
+#             if arr[u][v] == sys.maxsize:
+#                 return -1
+#             ans += arr[u][v]
+        
+#         return ans
 
 
 
