b8a601a6102dd99a62ed83173a3075ffae52249a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-28 15:24:54 +0530

    Time: 473 ms (62.28%), Space: 199.7 MB (56.33%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -9,12 +9,15 @@ public:
         }
         vector<int> min1(n+1, INT_MAX), min2(n+1, INT_MAX);
 
-        priority_queue<p, vector<p>, greater<p>> pq;
-        pq.push({0, 1});
+        queue<p> q;
+        q.push({1, 1});
         min1[1] = 0;
-        while (!pq.empty()) {
-            auto [timePassed, node] = pq.top();
-            pq.pop();
+        while (!q.empty()) {
+            auto [node, freq] = q.front();
+            q.pop();
+
+            int timePassed = (freq == 1) ? min1[node] : min2[node];
+
             if (node == n && min2[n] != INT_MAX) {
                 return min2[n];
             }
@@ -23,16 +26,61 @@ public:
                 timePassed = change * (div + 1);
             }
             for (auto &nbr : mp[node]) {
-                if (min1[nbr] > timePassed + time) {
-                    min2[nbr] = min1[nbr];
+                if (min1[nbr] == INT_MAX) {
                     min1[nbr] = timePassed + time;
-                    pq.push({timePassed + time, nbr});
-                } else if (min2[nbr] > timePassed + time && min1[nbr] != timePassed + time) {
+                    q.push({nbr, 1});
+                } else if (min2[nbr] == INT_MAX && min1[nbr] != timePassed + time) {
                     min2[nbr] = timePassed + time;
-                    pq.push({timePassed + time, nbr});
+                    q.push({nbr, 2});
                 }
             }
         }
         return -1;
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     #define p pair<int, int>
+//     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+//         unordered_map<int, vector<int>> mp;
+//         for (auto &x : edges) {
+//             mp[x[0]].push_back(x[1]);
+//             mp[x[1]].push_back(x[0]);
+//         }
+//         vector<int> min1(n+1, INT_MAX), min2(n+1, INT_MAX);
+
+//         priority_queue<p, vector<p>, greater<p>> pq;
+//         pq.push({0, 1});
+//         min1[1] = 0;
+//         while (!pq.empty()) {
+//             auto [timePassed, node] = pq.top();
+//             pq.pop();
+//             if (node == n && min2[n] != INT_MAX) {
+//                 return min2[n];
+//             }
+//             int div = timePassed / change;
+//             if (div % 2 == 1) {
+//                 timePassed = change * (div + 1);
+//             }
+//             for (auto &nbr : mp[node]) {
+//                 if (min1[nbr] > timePassed + time) {
+//                     min2[nbr] = min1[nbr];
+//                     min1[nbr] = timePassed + time;
+//                     pq.push({timePassed + time, nbr});
+//                 } else if (min2[nbr] > timePassed + time && min1[nbr] != timePassed + time) {
+//                     min2[nbr] = timePassed + time;
+//                     pq.push({timePassed + time, nbr});
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
