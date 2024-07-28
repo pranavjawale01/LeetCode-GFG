a362e842deaac6a2b29246c7e2cf0fd1f1550d16
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-28 15:26:27 +0530

    Time: 1813 ms (84.77%), Space: 26.6 MB (40.4%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.py
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.py
@@ -8,11 +8,13 @@ class Solution:
         min1 = [float('inf')] * (n + 1)
         min2 = [float('inf')] * (n + 1)
 
-        pq = [(0, 1)]
+        queue = deque([(1, 1)])
         min1[1] = 0
 
-        while pq:
-            timePassed, node = heapq.heappop(pq)
+        while queue:
+            node, freq = queue.popleft()
+            
+            timePassed = min1[node] if freq == 1 else min2[node]
             
             if node == n and min2[n] != float('inf'):
                 return min2[n]
@@ -20,14 +22,54 @@ class Solution:
             div = timePassed // change
             if div % 2 == 1:
                 timePassed = change * (div + 1)
-
+            
             for neighbor in graph[node]:
-                if min1[neighbor] > timePassed + time:
-                    min2[neighbor] = min1[neighbor]
+                if min1[neighbor] == float('inf'):
                     min1[neighbor] = timePassed + time
-                    heapq.heappush(pq, (timePassed + time, neighbor))
-                elif min2[neighbor] > timePassed + time and min1[neighbor] != timePassed + time:
+                    queue.append((neighbor, 1))
+                elif min2[neighbor] == float('inf') and min1[neighbor] != timePassed + time:
                     min2[neighbor] = timePassed + time
-                    heapq.heappush(pq, (timePassed + time, neighbor))
-
+                    queue.append((neighbor, 2))
+        
         return -1
+
+
+
+
+
+
+
+
+# class Solution:
+#     def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+#         graph = defaultdict(list)
+#         for u, v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
+
+#         min1 = [float('inf')] * (n + 1)
+#         min2 = [float('inf')] * (n + 1)
+
+#         pq = [(0, 1)]
+#         min1[1] = 0
+
+#         while pq:
+#             timePassed, node = heapq.heappop(pq)
+            
+#             if node == n and min2[n] != float('inf'):
+#                 return min2[n]
+            
+#             div = timePassed // change
+#             if div % 2 == 1:
+#                 timePassed = change * (div + 1)
+
+#             for neighbor in graph[node]:
+#                 if min1[neighbor] > timePassed + time:
+#                     min2[neighbor] = min1[neighbor]
+#                     min1[neighbor] = timePassed + time
+#                     heapq.heappush(pq, (timePassed + time, neighbor))
+#                 elif min2[neighbor] > timePassed + time and min1[neighbor] != timePassed + time:
+#                     min2[neighbor] = timePassed + time
+#                     heapq.heappush(pq, (timePassed + time, neighbor))
+
+#         return -1
