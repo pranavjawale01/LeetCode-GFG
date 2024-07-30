534d428a64a8fefe920521db6045baf6ea221677
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-30 12:21:00 +0530

    Time: 40 ms (36.77%), Space: 45.6 MB (13.09%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,18 +1,54 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int count = 0;
-        Stack<Character> stack = new Stack<>();
+        int[] leftB = new int[n];
+        int[] rightA = new int[n];
         
+        int countB = 0;
         for (int i = 0; i < n; i++) {
-            if (!stack.isEmpty() && s.charAt(i) == 'a' && stack.peek() == 'b') {
-                stack.pop();
-                count++;
-            } else {
-                stack.push(s.charAt(i));
+            leftB[i] = countB;
+            if (s.charAt(i) == 'b') {
+                countB++;
             }
+        }
+        
+        int countA = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            rightA[i] = countA;
+            if (s.charAt(i) == 'a') {
+                countA++;
+            }
+        }
+        
+        int count = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            count = Math.min(count, leftB[i] + rightA[i]);
         }
         
         return count;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int n = s.length();
+//         int count = 0;
+//         Stack<Character> stack = new Stack<>();
+        
+//         for (int i = 0; i < n; i++) {
+//             if (!stack.isEmpty() && s.charAt(i) == 'a' && stack.peek() == 'b') {
+//                 stack.pop();
+//                 count++;
+//             } else {
+//                 stack.push(s.charAt(i));
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
