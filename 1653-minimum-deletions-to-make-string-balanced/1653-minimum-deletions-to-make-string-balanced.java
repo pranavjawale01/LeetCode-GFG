5141b29a3e9e59e41daf79556462fcb06a46213f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-30 12:31:25 +0530

    Time: 44 ms (25.07%), Space: 45.5 MB (39.55%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,29 +1,58 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int[] rightA = new int[n];
-        int countA = 0;
-        
+        int counta = 0;
         for (int i = n - 1; i >= 0; i--) {
-            rightA[i] = countA;
             if (s.charAt(i) == 'a') {
-                countA++;
+                counta++;
             }
         }
-        
         int count = Integer.MAX_VALUE;
-        int countB = 0;
-        
+        int countb = 0;
         for (int i = 0; i < n; i++) {
-            count = Math.min(count, countB + rightA[i]);
+            if (s.charAt(i) == 'a') {
+                counta--;
+            }
+            count = Math.min(count, countb + counta);
             if (s.charAt(i) == 'b') {
-                countB++;
+                countb++;
             }
         }
-        
         return count;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int n = s.length();
+//         int[] rightA = new int[n];
+//         int countA = 0;
+        
+//         for (int i = n - 1; i >= 0; i--) {
+//             rightA[i] = countA;
+//             if (s.charAt(i) == 'a') {
+//                 countA++;
+//             }
+//         }
+        
+//         int count = Integer.MAX_VALUE;
+//         int countB = 0;
+        
+//         for (int i = 0; i < n; i++) {
+//             count = Math.min(count, countB + rightA[i]);
+//             if (s.charAt(i) == 'b') {
+//                 countB++;
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
 
 
 
