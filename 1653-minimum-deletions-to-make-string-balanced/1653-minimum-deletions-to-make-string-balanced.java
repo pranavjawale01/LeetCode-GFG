43123df5e714567f14895a1532a5584b8449cf37
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-30 12:24:37 +0530

    Time: 38 ms (43.18%), Space: 45.2 MB (77.16%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,18 +1,9 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int[] leftB = new int[n];
         int[] rightA = new int[n];
-        
-        int countB = 0;
-        for (int i = 0; i < n; i++) {
-            leftB[i] = countB;
-            if (s.charAt(i) == 'b') {
-                countB++;
-            }
-        }
-        
         int countA = 0;
+        
         for (int i = n - 1; i >= 0; i--) {
             rightA[i] = countA;
             if (s.charAt(i) == 'a') {
@@ -21,13 +12,53 @@ class Solution {
         }
         
         int count = Integer.MAX_VALUE;
+        int countB = 0;
+        
         for (int i = 0; i < n; i++) {
-            count = Math.min(count, leftB[i] + rightA[i]);
+            count = Math.min(count, countB + rightA[i]);
+            if (s.charAt(i) == 'b') {
+                countB++;
+            }
         }
         
         return count;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int n = s.length();
+//         int[] leftB = new int[n];
+//         int[] rightA = new int[n];
+        
+//         int countB = 0;
+//         for (int i = 0; i < n; i++) {
+//             leftB[i] = countB;
+//             if (s.charAt(i) == 'b') {
+//                 countB++;
+//             }
+//         }
+        
+//         int countA = 0;
+//         for (int i = n - 1; i >= 0; i--) {
+//             rightA[i] = countA;
+//             if (s.charAt(i) == 'a') {
+//                 countA++;
+//             }
+//         }
+        
+//         int count = Integer.MAX_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             count = Math.min(count, leftB[i] + rightA[i]);
+//         }
+        
+//         return count;
+//     }
+// }
 
 
 
