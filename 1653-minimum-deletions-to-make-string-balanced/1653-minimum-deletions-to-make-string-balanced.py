c696e07c1e54014a7a3259c27eadace0a23c48a3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-30 12:24:13 +0530

    Time: 657 ms (17.67%), Space: 22.4 MB (11%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,14 +1,42 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         n = len(s)
-        count = 0
-        stack = []
+        left_b = [0] * n
+        right_a = [0] * n
         
+        countb = 0
         for i in range(n):
-            if stack and s[i] == 'a' and stack[-1] == 'b':
-                stack.pop()
-                count += 1
-            else:
-                stack.append(s[i])
+            left_b[i] = countb
+            if s[i] == 'b':
+                countb += 1
+        
+        counta = 0
+        for i in range(n - 1, -1, -1):
+            right_a[i] = counta
+            if s[i] == 'a':
+                counta += 1
+        
+        count = float('inf')
+        for i in range(n):
+            count = min(count, left_b[i] + right_a[i])
         
         return count
+
+
+
+
+
+# class Solution:
+#     def minimumDeletions(self, s: str) -> int:
+#         n = len(s)
+#         count = 0
+#         stack = []
+        
+#         for i in range(n):
+#             if stack and s[i] == 'a' and stack[-1] == 'b':
+#                 stack.pop()
+#                 count += 1
+#             else:
+#                 stack.append(s[i])
+        
+#         return count
