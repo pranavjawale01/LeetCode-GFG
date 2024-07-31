1044d9423bd5396f901a22e37f20734d3a98307d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-07-31 13:06:26 +0530

    Time: 3 ms (87.31%), Space: 15.5 MB (32.98%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     int n;
     int WIDTH;
+    int dp[1001][1001];
     int solve(vector<vector<int>> &books, int i, int remain, int maxHt) {
         if (i >= n) {
             return maxHt;
+        }
+        if (dp[i][remain] != -1) {
+            return dp[i][remain];
         }
         int bookW = books[i][0];
         int bookH = books[i][1];
@@ -14,12 +18,13 @@ public:
             keep = solve(books, i+1, remain - bookW, max(maxHt, bookH));
         }
         skip = maxHt + solve(books, i+1, WIDTH - bookW, bookH);
-        return min(keep, skip);
+        return dp[i][remain] = min(keep, skip);
 
     }
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         n = books.size();
         WIDTH = shelfWidth;
+        memset(dp, -1, sizeof(dp));
         int reaminingWidth = shelfWidth;
         return solve(books, 0, reaminingWidth, 0);
     }
