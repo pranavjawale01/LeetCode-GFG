004d47c4dc96676594e8164c6ea6459ae90bb160
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-02 12:50:38 +0530

    Time: 831 ms (6.98%), Space: 20.6 MB (37.84%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,18 +1,41 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
         n = len(nums)
-        temp = nums * 2
         totalOnes = sum(nums)
         i = 0
         j = 0
         currOnes = 0
         maxCount = 0
         while j < 2 * n:
-            if temp[j] == 1:
+            if nums[j%n] == 1:
                 currOnes += 1
             if j - i + 1 > totalOnes:
-                currOnes -= temp[i]
+                currOnes -= nums[i%n]
                 i += 1
             maxCount = max(maxCount, currOnes)
             j += 1
         return totalOnes - maxCount
+
+
+
+
+
+
+# class Solution:
+#     def minSwaps(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         temp = nums * 2
+#         totalOnes = sum(nums)
+#         i = 0
+#         j = 0
+#         currOnes = 0
+#         maxCount = 0
+#         while j < 2 * n:
+#             if temp[j] == 1:
+#                 currOnes += 1
+#             if j - i + 1 > totalOnes:
+#                 currOnes -= temp[i]
+#                 i += 1
+#             maxCount = max(maxCount, currOnes)
+#             j += 1
+#         return totalOnes - maxCount
