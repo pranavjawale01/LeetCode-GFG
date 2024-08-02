998cf510ea6b713e4e15b85bdc0e812607879eb7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-02 12:50:28 +0530

    Time: 16 ms (7.39%), Space: 58.9 MB (83.11%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,19 +1,15 @@
 class Solution {
     public int minSwaps(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[2 * n];
-        for (int i = 0; i < 2 * n; i++) {
-            temp[i] = nums[i % n];
-        }
         int totalOnes = Arrays.stream(nums).sum();
         int i = 0, j = 0;
         int currOnes = 0, maxCount = 0;
         while (j < 2 * n) {
-            if (temp[j] == 1) {
+            if (nums[j%n] == 1) {
                 currOnes++;
             }
             if (j - i + 1 > totalOnes) {
-                currOnes -= temp[i];
+                currOnes -= nums[i%n];
                 i++;
             }
             maxCount = Math.max(maxCount, currOnes);
@@ -22,3 +18,31 @@ class Solution {
         return totalOnes - maxCount;
     }
 }
+
+
+
+
+// class Solution {
+//     public int minSwaps(int[] nums) {
+//         int n = nums.length;
+//         int[] temp = new int[2 * n];
+//         for (int i = 0; i < 2 * n; i++) {
+//             temp[i] = nums[i % n];
+//         }
+//         int totalOnes = Arrays.stream(nums).sum();
+//         int i = 0, j = 0;
+//         int currOnes = 0, maxCount = 0;
+//         while (j < 2 * n) {
+//             if (temp[j] == 1) {
+//                 currOnes++;
+//             }
+//             if (j - i + 1 > totalOnes) {
+//                 currOnes -= temp[i];
+//                 i++;
+//             }
+//             maxCount = Math.max(maxCount, currOnes);
+//             j++;
+//         }
+//         return totalOnes - maxCount;
+//     }
+// }
