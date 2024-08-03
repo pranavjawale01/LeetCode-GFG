6ca7de63f180a3cbb87dd95465885223bc50b41f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-03 12:40:15 +0530

    Time: 74 ms (58.36%), Space: 16.9 MB (22.18%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,5 +1,24 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        arr.sort()
-        target.sort()
-        return True if arr == target else False
+        mp = {}
+        for x in target:
+            if x not in mp:
+                mp[x] = 1
+            else:
+                mp[x] += 1
+        for x in arr:
+            if x not in mp:
+                return False
+            mp[x] -= 1
+            if mp[x] == 0:
+                del mp[x]
+        return len(mp) == 0
+
+
+
+
+# class Solution:
+#     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+#         arr.sort()
+#         target.sort()
+#         return True if arr == target else False
