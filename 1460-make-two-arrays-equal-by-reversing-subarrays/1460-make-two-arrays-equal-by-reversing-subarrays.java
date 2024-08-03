aaa2929d1f1cad22f2c4cff8b904049d49435070
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-03 12:40:09 +0530

    Time: 8 ms (23.09%), Space: 44.3 MB (42.83%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,7 +1,29 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Arrays.sort(target);
-        Arrays.sort(arr);
-        return Arrays.equals(target, arr);
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int x : target) {
+            mp.put(x, mp.getOrDefault(x, 0) + 1);
+        }
+        for (int x : arr) {
+            if (!mp.containsKey(x)) {
+                return false;
+            }
+            mp.put(x, mp.getOrDefault(x, 0) - 1);
+            if (mp.get(x) == 0) {
+                mp.remove(x);
+            }
+        }
+        return mp.size() == 0;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean canBeEqual(int[] target, int[] arr) {
+//         Arrays.sort(target);
+//         Arrays.sort(arr);
+//         return Arrays.equals(target, arr);
+//     }
+// }
