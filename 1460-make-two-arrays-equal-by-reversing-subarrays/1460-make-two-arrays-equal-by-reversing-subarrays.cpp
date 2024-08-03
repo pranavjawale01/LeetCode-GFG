3cee8bd57bbc400c7f3f3456edca98a605f836d3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-03 12:38:13 +0530

    Time: 10 ms (52.84%), Space: 19.8 MB (16.35%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
-        return arr == target ? true : false;
+        map<int, int> mp;
+        for (int x : target) {
+            mp[x]++;
+        }
+        for (int x : arr) {
+            if (mp.find(x) == mp.end()) {
+                return false;
+            }
+            mp[x]--;
+            if (mp[x] == 0) {
+                mp.erase(x);
+            }
+        }
+        return mp.size() == 0;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+//         sort(target.begin(), target.end());
+//         sort(arr.begin(), arr.end());
+//         return arr == target ? true : false;
+//     }
+// };
