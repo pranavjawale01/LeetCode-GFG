3354b074d5a161a8b76469775c88537fb5afb300
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-06 10:59:34 +0530

    Time: 3 ms (39.41%), Space: 9.5 MB (9.12%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char, int> mp;
+        unordered_map<int, int> mp;
+        int key = 2;
+        int ans = 0;
         for (char c : word) {
-            mp[c]++;
+            if (key > 9) {
+                key = 2;
+            }
+            mp[key]++;
+            ans += mp[key];
+            key++;
         }
-        if (mp.size() <= 8) {
-            return word.size();
-        }
-        if (mp.size() > 8 && mp.size() < 17) {
-            return 8 + (mp.size() - 8) * 2;
-        }
-        if (mp.size() > 16 && mp.size() < 25) {
-            return 8 + 16 + (mp.size() - 16) * 3;
-        }
-        return 8 + 16 + 24 + (mp.size() - 24) * 4;
+        return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         unordered_map<char, int> mp;
+//         for (char c : word) {
+//             mp[c]++;
+//         }
+//         if (mp.size() <= 8) {
+//             return word.size();
+//         }
+//         if (mp.size() > 8 && mp.size() < 17) {
+//             return 8 + (mp.size() - 8) * 2;
+//         }
+//         if (mp.size() > 16 && mp.size() < 25) {
+//             return 8 + 16 + (mp.size() - 16) * 3;
+//         }
+//         return 8 + 16 + 24 + (mp.size() - 24) * 4;
+//     }
+// };
