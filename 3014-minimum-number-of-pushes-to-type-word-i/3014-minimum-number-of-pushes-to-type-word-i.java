cdce5db32edc8a08a594a3c134540a83e4ad5a6a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-06 11:00:09 +0530

    Time: 2 ms (16.38%), Space: 41.8 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,19 +1,41 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character, Integer> mp = new HashMap<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int key = 2;
+        int ans = 0;
         for (char c : word.toCharArray()) {
-            mp.put(c, mp.getOrDefault(c, 0) + 1);
+            if (key > 9) {
+                key = 2;
+            }
+            mp.put(key, mp.getOrDefault(key, 0) + 1);
+            ans += mp.get(key);
+            key++;
         }
-        int size = mp.size();
-        if (size <= 8) {
-            return word.length();
-        }
-        if (size > 8 && size < 17) {
-            return 8 + (size - 8) * 2;
-        }
-        if (size > 16 && size < 25) {
-            return 8 + 16 + (size - 16) * 3;
-        }
-        return 8 + 16 + 24 + (size - 24) * 4;
+        return ans;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int minimumPushes(String word) {
+//         HashMap<Character, Integer> mp = new HashMap<>();
+//         for (char c : word.toCharArray()) {
+//             mp.put(c, mp.getOrDefault(c, 0) + 1);
+//         }
+//         int size = mp.size();
+//         if (size <= 8) {
+//             return word.length();
+//         }
+//         if (size > 8 && size < 17) {
+//             return 8 + (size - 8) * 2;
+//         }
+//         if (size > 16 && size < 25) {
+//             return 8 + 16 + (size - 16) * 3;
+//         }
+//         return 8 + 16 + 24 + (size - 24) * 4;
+//     }
+// }
