a25de83799db4b3c1e588f1b51cfb2fa9e098a8c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-07 11:29:45 +0530

    Time: 3 ms (66.96%), Space: 12.2 MB (35.39%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    unordered_map<int, string> ten = {{0, ""}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+
+    unordered_map<int, string> twenty = {{10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"}};
+
+    unordered_map<int, string> hundred = {{2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"}, {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"}};
+
+    string solve(int num) {
+        if (num < 10) {
+            return ten[num];
+        }
+        if (num < 20) {
+            return twenty[num];
+        }
+        if (num < 100) {
+            return hundred[num / 10] + ((num % 10 != 0) ? " " + ten[num % 10] : "");
+        }
+        if (num < 1000) {
+            return ten[num / 100] + " Hundred" + ((num % 100 != 0) ? " " + solve(num % 100) : "");
+        }
+        if (num < 1000000) {
+            return solve(num / 1000) + " Thousand" + ((num % 1000 != 0) ? " " + solve(num % 1000) : "");
+        }
+        if (num < 1000000000) {
+            return solve(num / 1000000) + " Million" + ((num % 1000000 != 0) ? " " + solve(num % 1000000) : "");
+        }
+        return solve(num / 1000000000) + " Billion" + ((num % 1000000000 != 0) ? " " + solve(num % 1000000000) : "");
+    }
+
+    string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        return solve(num);
+    }
+};
