57d1f0ceb3f58a1a8fa1d6fca5376eaeafcb0f66
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-14 12:44:46 +0530

    Time: 7 ms (96.26%), Space: 43.9 MB (96.26%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,22 +1,63 @@
 class Solution {
+    public int slidingWindowCount(int[] nums, int d) {
+        int i = 0, j = 0;
+        int n = nums.length;
+        int pairCount = 0;
+        
+        while (j < n) {
+            while (j < n && nums[j] - nums[i] > d) {
+                i++;
+            }
+            pairCount += (j - i);
+            j++;
+        }
+        
+        return pairCount;
+    }
     public int smallestDistancePair(int[] nums, int k) {
         int n = nums.length;
-        int maxEl = 0;
-        for (int i = 0; i < n; i++) {
-            maxEl = Math.max(maxEl, nums[i]);
-        }
-        int[] arr = new int[maxEl+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                arr[Math.abs(nums[i] - nums[j])]++;
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums[n - 1] - nums[0];
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int countPair = slidingWindowCount(nums, mid);
+            
+            if (countPair < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-        for (int d = 0; d < maxEl + 1; d++) {
-            k -= arr[d];
-            if (k <= 0) {
-                return d;
-            }
-        }
-        return -1;
+        
+        return l;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int smallestDistancePair(int[] nums, int k) {
+//         int n = nums.length;
+//         int maxEl = 0;
+//         for (int i = 0; i < n; i++) {
+//             maxEl = Math.max(maxEl, nums[i]);
+//         }
+//         int[] arr = new int[maxEl+1];
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 arr[Math.abs(nums[i] - nums[j])]++;
+//             }
+//         }
+//         for (int d = 0; d < maxEl + 1; d++) {
+//             k -= arr[d];
+//             if (k <= 0) {
+//                 return d;
+//             }
+//         }
+//         return -1;
+//     }
+// }
