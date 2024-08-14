01ecb88408e10a4271d1a9dd419ca872b812361d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-14 12:43:23 +0530

    Time: 3 ms (98.95%), Space: 12.8 MB (53.94%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,22 +1,66 @@
 class Solution {
 public:
+    int slidingWindowCount(vector<int>& nums, int d) {
+        int i = 0, j = 0;
+        int n = nums.size();
+        int pairCount = 0;
+        
+        while (j < n) {
+            while (j < n && nums[j] - nums[i] > d) {
+                i++;
+            }
+            pairCount += (j - i);
+            j++;
+        }
+        
+        return pairCount;
+    }
+
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> vec(n * (n - 1) / 2);
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int d = abs(nums[i] - nums[j]);
-                vec[idx] = d;
-                idx++;
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int r = nums[n-1] - nums[0];
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int countPair = slidingWindowCount(nums, mid);
+            
+            if (countPair < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-
-        nth_element(vec.begin(), vec.begin() + (k - 1), vec.end());
-
-        return vec[k - 1];
+        
+        return l;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int smallestDistancePair(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> vec(n * (n - 1) / 2);
+//         int idx = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int d = abs(nums[i] - nums[j]);
+//                 vec[idx] = d;
+//                 idx++;
+//             }
+//         }
+
+//         nth_element(vec.begin(), vec.begin() + (k - 1), vec.end());
+
+//         return vec[k - 1];
+//     }
+// };
 
 
 
