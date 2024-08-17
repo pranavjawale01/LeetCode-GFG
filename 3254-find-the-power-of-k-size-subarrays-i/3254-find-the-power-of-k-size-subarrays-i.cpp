c515c38adc523788c315501644a6fcbe31b90b4e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-18 00:03:25 +0530

    Time: 12 ms (100%), Space: 33.4 MB (21.43%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -2,19 +2,43 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> ans(n - k + 1, -1);
-        for (int i = 0; i < n - k  + 1; i++) {
-            bool flag = true;
-            for (int j = i; j < i + k - 1; j++) {
-                if (nums[j] != nums[j+1] - 1) {
-                    flag = false;
-                    break;
-                }
+        vector<int> dp(n, 1);
+        vector<int> ans(n-k+1, -1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i-1] + 1) {
+                dp[i] = dp[i-1] + 1;
             }
-            if (flag) {
-                ans[i] = nums[i+k-1];
+        }
+        for (int i = k - 1, j = 0; i < n; i++, j++) {
+            if (dp[i] >= k) {
+                ans[j] = nums[i];
             }
         }
         return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> resultsArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> ans(n - k + 1, -1);
+//         for (int i = 0; i < n - k  + 1; i++) {
+//             bool flag = true;
+//             for (int j = i; j < i + k - 1; j++) {
+//                 if (nums[j] != nums[j+1] - 1) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) {
+//                 ans[i] = nums[i+k-1];
+//             }
+//         }
+//         return ans;
+//     }
+// };
