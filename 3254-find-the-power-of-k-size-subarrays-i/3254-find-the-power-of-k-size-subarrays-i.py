c4f89deac466dc25ce9bfa91a5d705b61316f6bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-18 00:06:08 +0530

    Time: 85 ms (100%), Space: 16.6 MB (100%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,13 +1,33 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        ans = [-1] * (n-k+1)
-        for i in range(0, n-k+1):
-            flag = True
-            for j in range(i, i+k-1):
-                if nums[j] != nums[j+1] - 1:
-                    flag = False
-                    break
-            if flag:
-                ans[i] = nums[i+k-1]
+        dp = [1] * n
+        ans = [-1] * (n - k + 1)
+        for i in range(1, n):
+            if nums[i] == nums[i-1] + 1:
+                dp[i] = dp[i-1] + 1
+        i , j = k - 1, 0
+        while i < n:
+            if dp[i] >= k:
+                ans[j] = nums[i]
+            i += 1
+            j += 1
         return ans
+
+
+
+
+
+# class Solution:
+#     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+#         n = len(nums)
+#         ans = [-1] * (n-k+1)
+#         for i in range(0, n-k+1):
+#             flag = True
+#             for j in range(i, i+k-1):
+#                 if nums[j] != nums[j+1] - 1:
+#                     flag = False
+#                     break
+#             if flag:
+#                 ans[i] = nums[i+k-1]
+#         return ans
