f7d6034f97efd428ee62b4f749fd406aa0edb760
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-18 00:06:04 +0530

    Time: 1 ms (100%), Space: 45.5 MB (100%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,21 +1,46 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        for (int i = 0; i < n - k  + 1; i++) {
-            boolean flag = true;
-            for (int j = i; j < i + k - 1; j++) {
-                if (nums[j] != nums[j+1] - 1) {
-                    flag = false;
-                    break;
-                }
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int[] ans = new int[n-k+1];
+        Arrays.fill(ans, -1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i-1] + 1) {
+                dp[i] = dp[i-1] + 1;
             }
-            if (flag) {
-                ans[i] = nums[i+k-1];
-            } else {
-                ans[i] = -1;
+        }
+        for (int i = k - 1, j = 0; i < n; i++, j++) {
+            if (dp[i] >= k) {
+                ans[j] = nums[i];
             }
         }
         return ans;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int[] resultsArray(int[] nums, int k) {
+//         int n = nums.length;
+//         int[] ans = new int[n - k + 1];
+//         for (int i = 0; i < n - k  + 1; i++) {
+//             boolean flag = true;
+//             for (int j = i; j < i + k - 1; j++) {
+//                 if (nums[j] != nums[j+1] - 1) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) {
+//                 ans[i] = nums[i+k-1];
+//             } else {
+//                 ans[i] = -1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
