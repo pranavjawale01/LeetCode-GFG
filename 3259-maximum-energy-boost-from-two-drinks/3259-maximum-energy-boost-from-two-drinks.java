ff45721ef54cc9325c7b70869cd07c98165f417b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-18 12:10:52 +0530

    Time: 3 ms (100%), Space: 57.4 MB (100%) - LeetHub

--- 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.java
+++ 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.java
@@ -1,25 +1,42 @@
 class Solution {
-    int n;
-    long[][] dp;
-
-    public long solve(int i, int j, int[] a, int[] b) {
-        if (i >= n) {
-            return 0;
+    public long maxEnergyBoost(int[] A, int[] B) {
+        long A1 = 0 , B1 = 0 , A2 = 0 , B2 = 0;
+        for (int i = 0 ; i < A.length ; i++){
+            long currA = A[i] + Math.max(A1 , B2);
+            long currB = B[i] + Math.max(B1 , A2);
+            A2 = A1 ; A1 = currA;
+            B2 = B1 ; B1 = currB;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        long ans = (j == 0) ? a[i] : b[i];
-        ans += Math.max(solve(i + 1, j, a, b), solve(i + 2, 1 - j, a, b));
-        return dp[i][j] = ans;
-    }
-
-    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
-        n = energyDrinkA.length;
-        dp = new long[n][2];
-        for (long[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        return Math.max(solve(0, 0, energyDrinkA, energyDrinkB), solve(0, 1, energyDrinkA, energyDrinkB));
+        return Math.max(A1 , B1);
     }
 }
+
+
+
+
+
+// class Solution {
+//     int n;
+//     long[][] dp;
+
+//     public long solve(int i, int j, int[] a, int[] b) {
+//         if (i >= n) {
+//             return 0;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         long ans = (j == 0) ? a[i] : b[i];
+//         ans += Math.max(solve(i + 1, j, a, b), solve(i + 2, 1 - j, a, b));
+//         return dp[i][j] = ans;
+//     }
+
+//     public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
+//         n = energyDrinkA.length;
+//         dp = new long[n][2];
+//         for (long[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+//         return Math.max(solve(0, 0, energyDrinkA, energyDrinkB), solve(0, 1, energyDrinkA, energyDrinkB));
+//     }
+// }
