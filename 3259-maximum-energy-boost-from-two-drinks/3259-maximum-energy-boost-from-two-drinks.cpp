6424533c9fe5aebcb1fabefec32753be2ab15e46
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-18 12:09:58 +0530

    Time: 178 ms (100%), Space: 195.4 MB (100%) - LeetHub

--- 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.cpp
+++ 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-    int n;
-    long long dp[100000][2];
-    long long solve(int i, int j, vector<int> &a, vector<int> &b) {
-        if (i >= n) {
-            return 0;
+    using ll = long long;
+
+    long long maxEnergyBoost(vector<int> &A, vector<int> &B) {
+        ll a1 = 0, b1 = 0;
+        ll a2 = 0, b2 = 0;
+        int n = A.size();
+        for (int i = 0; i < n; i++) {
+            ll curra = A[i] + max(a1, b2);
+            ll currb = B[i] + max(b1, a2);
+
+            a2 = a1;
+            a1 = curra;
+
+            b2 = b1;
+            b1 = currb;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        long long ans = (j == 0) ? a[i] : b[i];
-        ans += max(solve(i+1, j, a, b), solve(i+2, 1-j, a, b));
-        return dp[i][j] = ans;
-    }
-    long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
-        n = energyDrinkA.size();
-        memset(dp, -1, sizeof(dp));
-        return max(solve(0, 0, energyDrinkA, energyDrinkB), solve(0, 1, energyDrinkA, energyDrinkB));
+        return max(a1, b1);
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int n;
+//     long long dp[100000][2];
+//     long long solve(int i, int j, vector<int> &a, vector<int> &b) {
+//         if (i >= n) {
+//             return 0;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         long long ans = (j == 0) ? a[i] : b[i];
+//         ans += max(solve(i+1, j, a, b), solve(i+2, 1-j, a, b));
+//         return dp[i][j] = ans;
+//     }
+//     long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
+//         n = energyDrinkA.size();
+//         memset(dp, -1, sizeof(dp));
+//         return max(solve(0, 0, energyDrinkA, energyDrinkB), solve(0, 1, energyDrinkA, energyDrinkB));
+//     }
+// };
