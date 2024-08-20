fc0d8d255a07e21d720fcf5084ceee22abd2fecf
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-20 12:09:24 +0530

    Time: 142 ms (46.74%), Space: 16.5 MB (66.61%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,13 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+        dp = piles[:]
+        for i in range(1, n):
+            for j in range(0, n - i):
+                dp[j] = max(piles[j] - dp[j+1], piles[j+i] - dp[j])
+        return dp[0] > 0
+
+
+# class Solution:
+#     def stoneGame(self, piles: List[int]) -> bool:
+#         return True
