3576998ab08bbaa4305a1a5afb2b5dba6147b336
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-20 12:09:19 +0530

    Time: 4 ms (36.39%), Space: 41.5 MB (48.04%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,24 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = piles[i];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dp[j] = Math.max(piles[j] - dp[j+1], piles[j+i] - dp[j]);
+            }
+        }
+        return dp[0] > 0;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         return true;
+//     }
+// }
