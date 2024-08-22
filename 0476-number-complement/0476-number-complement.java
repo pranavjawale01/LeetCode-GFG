c3b13496b13a581f9fe629ae1330f44252508e4a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-22 10:32:56 +0530

    Time: 0 ms (100%), Space: 40.2 MB (67.03%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+        int i = 31;
+        while (i >= 0 && (num & (1 << i)) == 0) {
+            i--;
+        }
+        for (; i >= 0; i--) {
+            num = num ^ (1 << i);
+        }
+        return num;
+    }
+}
