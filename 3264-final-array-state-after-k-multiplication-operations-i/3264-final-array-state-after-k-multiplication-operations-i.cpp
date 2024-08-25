62783008e27be09bf20efc8bbef61cb17ef981a4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-25 14:02:37 +0530

    Time: 6 ms (100%), Space: 28.7 MB (20%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,11 +1,41 @@
+typedef pair<long, int> pi;
 class Solution {
 public:
-    vector<int> getFinalState(vector<int>& nums, int k, int mult) {
-        for (int i = 0; i < k; ++i) 
-        {
-            auto min_elem = min_element(nums.begin(), nums.end());
-            *min_elem *= mult;
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            pq.push({nums[i], i});
+        }
+        while (k--) {
+            auto top = pq.top();
+            pq.pop();
+            int val = top.first * multiplier;
+            int idx = top.second;
+            pq.push({val, idx});
+        }
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            int val = top.first;
+            int idx = top.second;
+            nums[idx] = val;
         }
         return nums;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> getFinalState(vector<int>& nums, int k, int mult) {
+//         for (int i = 0; i < k; ++i) 
+//         {
+//             auto min_elem = min_element(nums.begin(), nums.end());
+//             *min_elem *= mult;
+//         }
+//         return nums;
+//     }
+// };
