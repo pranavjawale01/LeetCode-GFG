1136f5278bb64e88b76b54cee4b34e6a4e5999dd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-25 12:23:39 +0530

    Time: 70 ms (100%), Space: 16.5 MB (100%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for _ in range(k):
+            min_index = nums.index(min(nums))
+            nums[min_index] *= multiplier
+        return nums
