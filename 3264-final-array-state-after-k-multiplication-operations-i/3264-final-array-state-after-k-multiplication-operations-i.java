c7d8a8831cb1ee6ef3868f4f39caedf6bd59a728
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-25 14:06:59 +0530

    Time: 3 ms (100%), Space: 45 MB (100%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,14 +1,46 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        for (int i = 0; i < k; ++i) {
-            int minIndex = 0;
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[j] < nums[minIndex]) {
-                    minIndex = j;
-                }
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->{
+            if(a[0] != b[0]){
+                return a[0] - b[0];
+            }else {
+                return a[1] - b[1];
             }
-            nums[minIndex] *= multiplier;
+        });
+
+        for(int i = 0; i < nums.length; i++){
+            pq.add(new int[]{nums[i], i});
         }
+
+        while(k > 0){
+            int[] minEleObj = pq.poll();
+            int ind = minEleObj[1];
+            int val = minEleObj[0];
+
+            nums[ind] = nums[ind] * multiplier;
+            pq.add(new int[]{nums[ind], ind});
+            k--;
+        }
+
         return nums;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int[] getFinalState(int[] nums, int k, int multiplier) {
+//         for (int i = 0; i < k; ++i) {
+//             int minIndex = 0;
+//             for (int j = 1; j < nums.length; j++) {
+//                 if (nums[j] < nums[minIndex]) {
+//                     minIndex = j;
+//                 }
+//             }
+//             nums[minIndex] *= multiplier;
+//         }
+//         return nums;
+//     }
+// }
