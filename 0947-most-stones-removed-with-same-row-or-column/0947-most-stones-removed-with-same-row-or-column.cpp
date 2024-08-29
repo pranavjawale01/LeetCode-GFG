8518aeb4ee165f3802bf40a390600661f6fbf52b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-08-29 14:48:49 +0530

    Time: 71 ms (28.96%), Space: 18.2 MB (79.11%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,83 +1,54 @@
-class DSU {
-    vector<int> rank, parent, size;
-public:
-    DSU(int n) {
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        size.resize(n+1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        }
-        return parent[node] = find(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int u_parent = find(u);
-        int v_parent = find(v);
-        if (u_parent == v_parent) {
-            return;
-        }
-        if (rank[u_parent] < rank[v_parent]) {
-            parent[u_parent] = v_parent;
-        } else if (rank[v_parent] < rank[u_parent]) {
-            parent[v_parent] = u_parent;
-        } else {
-            parent[v_parent] = u_parent;
-            rank[u_parent]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int u_parent = find(u);
-        int v_parent = find(v);
-        if (u_parent == v_parent) {
-            return;
-        }
-        if (size[u_parent] < size[v_parent]) {
-            parent[u_parent] = v_parent;
-            size[v_parent] += size[u_parent];
-        } else {
-            parent[v_parent] = u_parent;
-            size[u_parent] += size[v_parent];
-        }
-    }
-};
-
 class Solution {
 public:
-    int removeStones(vector<vector<int>>& stones) {
-        int maxRow = 0;
-        int maxCol = 0;
-        for (auto x : stones) {
-            maxRow = max(maxRow, x[0]);
-            maxCol = max(maxCol, x[1]);
+    vector<int> parent;
+    vector<int> rank;
+    int n;
+
+    int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
         }
-        DSU ds(maxRow + maxCol + 1);
-        unordered_map<int, int> mp;
-        for (auto x : stones) {
-            int nodeRow = x[0];
-            int nodeCol = x[1];
-            ds.unionBySize(nodeRow, nodeCol);
-            mp[nodeRow] = 1;
-            mp[nodeCol] = 1;
-        }
-        int count = 0;
-        for (auto x : mp) {
-            if (ds.find(x.first) == x.first) {
-                count++;
+        return parent[i];
+    }
+
+    void Union(int u, int v) {
+        int root_u = find(u);
+        int root_v = find(v);
+        if (root_u != root_v) {
+            if (rank[root_u] > rank[root_v]) {
+                parent[root_v] = root_u;
+            } else if (rank[root_u] < rank[root_v]) {
+                parent[root_u] = root_v;
+            } else {
+                parent[root_v] = root_u;
             }
         }
-        return stones.size() - count;
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        parent.resize(n);
+        rank.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    Union(i, j);
+                }
+            }
+        }
+        int groups = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                groups++;
+            }
+        }
+        return n - groups;
     }
 };
-
 
 
 
