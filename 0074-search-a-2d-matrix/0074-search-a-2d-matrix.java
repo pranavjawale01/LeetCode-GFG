0b4b073b47e5d60dbc267718ddfd1c4ac8c81eb4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-10 11:43:06 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -5,25 +5,54 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        int row = 0;
-        while (row < n && target > matrix[row][m - 1]) {
-            row++;
-        }
-        
-        if (row == n) return false;
-        
-        int low = 0, high = m - 1;
+        int low = 0, high = n * m - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (matrix[row][mid] == target) {
-                return true;
-            } else if (matrix[row][mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            int row = mid / m;
+            int col = mid % m;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
         
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         if (matrix.length == 0 || matrix[0].length == 0) return false;
+        
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+        
+//         int row = 0;
+//         while (row < n && target > matrix[row][m - 1]) {
+//             row++;
+//         }
+        
+//         if (row == n) return false;
+        
+//         int low = 0, high = m - 1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (matrix[row][mid] == target) {
+//                 return true;
+//             } else if (matrix[row][mid] < target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+        
+//         return false;
+//     }
+// }
