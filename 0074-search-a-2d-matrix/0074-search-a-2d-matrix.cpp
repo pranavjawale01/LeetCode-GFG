fea5e986d61cd58b06a91878efce1a480732142a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-10 11:42:39 +0530

    Time: 0 ms (100%), Space: 12.1 MB (93.14%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        int row = 0;
-        while (row < n && target > matrix[row][m - 1]) {
-            row++;
-        }
-        
-        if (row == n) return false;
 
-        int low = 0, high = m - 1;
+        int low = 0, high = n * m - 1;
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (matrix[row][mid] == target) {
-                return true;
-            } else if (matrix[row][mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            int mid = (low + high) / 2;
+            int row = mid / m, col = mid % m;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        
-        return false;
+    return false;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         if (matrix.empty() || matrix[0].empty()) return false;
+
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+        
+//         int row = 0;
+//         while (row < n && target > matrix[row][m - 1]) {
+//             row++;
+//         }
+        
+//         if (row == n) return false;
+
+//         int low = 0, high = m - 1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (matrix[row][mid] == target) {
+//                 return true;
+//             } else if (matrix[row][mid] < target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+        
+//         return false;
+//     }
+// };
