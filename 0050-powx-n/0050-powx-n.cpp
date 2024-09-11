b0cb3adfc188267012ca6b77827a8b15a3744f4a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-11 09:52:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (74.69%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res=1.0;
-        for(int i=n;i;i/=2){
-            if(i&1)res*=x;
-            x*=x;
-            
+        double ans = 1.0;
+        long long temp = n;
+        if (temp < 0) {
+            temp = temp * -1;
         }
-        return n>=0?res:1/res; 
+        while (temp) {
+            if (temp % 2) {
+                ans = ans * x;
+                temp = temp - 1;
+            } else {
+                x = x * x;
+                temp = temp / 2;
+            }
+        }
+        if (n < 0) {
+            ans = 1.0 / ans;
+        }
+        return ans;
     }
 };
