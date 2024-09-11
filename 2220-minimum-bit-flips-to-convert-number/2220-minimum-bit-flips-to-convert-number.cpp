1dc7203e5215aa6a671769437ee824aca19c95ef
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-11 09:21:56 +0530

    Time: 0 ms (100%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count = 0;
-        for (int i = 0; i < 32; i++) {
-            int bit1 = start & 1;
-            int bit2 = goal  & 1;
-            start >>= 1;
-            goal  >>= 1;
-            if (bit1 != bit2) {
-                count++;
-            }
-        }
-        return count;
+        int ans = start ^ goal;
+        return __builtin_popcount(ans);
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int minBitFlips(int start, int goal) {
+//         int count = 0;
+//         for (int i = 0; i < 32; i++) {
+//             int bit1 = start & 1;
+//             int bit2 = goal  & 1;
+//             start >>= 1;
+//             goal  >>= 1;
+//             if (bit1 != bit2) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
