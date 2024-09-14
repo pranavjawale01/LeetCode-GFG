58b87c41298996f7d85cd5ce5c6aaf0fa93b544b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-14 11:15:29 +0530

    Time: 4 ms (12.7%), Space: 8.1 MB (22.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int dp[m+1][n+1];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
+    vector<vector<int>> dp;
+    int solve(int i, int j, int m, int n) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
         }
-        return dp[m-1][n-1];
+        if (i >= m || j >= n) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        return dp[i][j] = solve(i+1, j, m, n) + solve(i, j+1, m, n);
+    }
+    int uniquePaths(int m, int n) {
+        dp.resize(m+1, vector<int>(n+1, -1));
+        return solve(0, 0, m, n);
     }
 };
-
-
-// class Solution {
-// public:
-//     int dp[101][101]{};
-//     int uniquePaths(int m, int n, int i = 0, int j = 0) {
-//         if(i >= m || j >= n) return 0;
-//         if(i == m-1 && j == n-1) return 1;
-//         if(dp[i][j]) return dp[i][j];
-//         return dp[i][j] = uniquePaths(m, n, i+1, j) + uniquePaths(m, n, i, j+1);
-//     }
-// };
