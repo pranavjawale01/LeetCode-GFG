7059a293e62e166b48171ce45efeca397f9868f5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-15 13:37:26 +0530

    Time: 54 ms (48.04%), Space: 45.1 MB (83.99%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,30 +1,57 @@
 class Solution {
     public int findTheLongestSubstring(String s) {
-        Map<String, Integer> mp = new HashMap<>();
-        int[] state = new int[5];
-        String currState = "00000";
-        mp.put(currState, -1);
+        Map<Integer, Integer> mp = new HashMap<>();
+        int mask = 0;
+        mp.put(mask, -1);
         int ans = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') state[0] = (state[0] + 1) % 2;
-            else if (s.charAt(i) == 'e') state[1] = (state[1] + 1) % 2;
-            else if (s.charAt(i) == 'i') state[2] = (state[2] + 1) % 2;
-            else if (s.charAt(i) == 'o') state[3] = (state[3] + 1) % 2;
-            else if (s.charAt(i) == 'u') state[4] = (state[4] + 1) % 2;
+            if (s.charAt(i) == 'a') mask = mask ^ (1 << 0);
+            else if (s.charAt(i) == 'e') mask = mask ^ (1 << 1);
+            else if (s.charAt(i) == 'i') mask = mask ^ (1 << 2);
+            else if (s.charAt(i) == 'o') mask = mask ^ (1 << 3);
+            else if (s.charAt(i) == 'u') mask = mask ^ (1 << 4);
 
-            currState = "";
-            for (int j = 0; j < 5; j++) {
-                currState += state[j];
-            }
-
-            if (mp.containsKey(currState)) {
-                ans = Math.max(ans, i - mp.get(currState));
+            if (mp.containsKey(mask)) {
+                ans = Math.max(ans, i - mp.get(mask));
             } else {
-                mp.put(currState, i);
+                mp.put(mask, i);
             }
         }
 
         return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int findTheLongestSubstring(String s) {
+//         Map<String, Integer> mp = new HashMap<>();
+//         int[] state = new int[5];
+//         String currState = "00000";
+//         mp.put(currState, -1);
+//         int ans = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == 'a') state[0] = (state[0] + 1) % 2;
+//             else if (s.charAt(i) == 'e') state[1] = (state[1] + 1) % 2;
+//             else if (s.charAt(i) == 'i') state[2] = (state[2] + 1) % 2;
+//             else if (s.charAt(i) == 'o') state[3] = (state[3] + 1) % 2;
+//             else if (s.charAt(i) == 'u') state[4] = (state[4] + 1) % 2;
+
+//             currState = "";
+//             for (int j = 0; j < 5; j++) {
+//                 currState += state[j];
+//             }
+
+//             if (mp.containsKey(currState)) {
+//                 ans = Math.max(ans, i - mp.get(currState));
+//             } else {
+//                 mp.put(currState, i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
