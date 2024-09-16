79d8df2b8ddd02c23f51dad6f15fd9a114420362
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-16 12:47:39 +0530

    Time: 63 ms (86.31%), Space: 19.7 MB (36.73%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -0,0 +1,24 @@
+class Solution:
+    def toMinutes(self, time: str) -> int:
+        # Convert "HH:MM" format to total minutes since midnight
+        hours = int(time[:2])
+        minutes = int(time[3:])
+        return hours * 60 + minutes
+
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        if len(timePoints) > 1440:
+            return 0
+
+        minutesList = [self.toMinutes(time) for time in timePoints]
+
+        minutesList.sort()
+
+        ans = float('inf')
+
+        for i in range(1, len(minutesList)):
+            ans = min(ans, minutesList[i] - minutesList[i - 1])
+
+        circularDiff = 1440 + minutesList[0] - minutesList[-1]
+        ans = min(ans, circularDiff)
+
+        return ans
