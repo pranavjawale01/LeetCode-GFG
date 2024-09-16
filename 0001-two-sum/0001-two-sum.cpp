afca36c14f57ff2992be42cd0619b835eb98d9c0
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-16 12:22:04 +0530

    Time: 8 ms (77.46%), Space: 14.3 MB (37.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,14 +4,35 @@ public:
         int n = nums.size();
         unordered_map<int, int> mp;
         for (int i = 0; i < n; i++) {
-            mp[nums[i]] = i;
-        }
-        for (int i = 0; i < n; i++) {
             int req = target - nums[i];
-            if (mp.find(req) != mp.end() && i != mp[req]) {
+            if (mp.find(req) != mp.end()) {
                 return {i, mp[req]};
             }
+            mp[nums[i]] = i;
         }
         return {};
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         unordered_map<int, int> mp;
+//         for (int i = 0; i < n; i++) {
+//             mp[nums[i]] = i;
+//         }
+//         for (int i = 0; i < n; i++) {
+//             int req = target - nums[i];
+//             if (mp.find(req) != mp.end() && i != mp[req]) {
+//                 return {i, mp[req]};
+//             }
+//         }
+//         return {};
+//     }
+// };
