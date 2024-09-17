44261c78675c7a684dd5c7c9173160c9a623da0a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-17 15:34:57 +0530

    Time: 21 ms (85.3%), Space: 16.3 MB (40.53%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
-        if (nums.size() < 4) {
-            return result;
-        }
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 3; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            for(int j = i + 1; j < nums.size() - 2; j++) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                int front = j + 1;
-                int back = nums.size() - 1;
-                long long newTarget = static_cast<long long>(target) - nums[i] - nums[j];
-                while(front < back) {
-                    long long sum = static_cast<long long>(nums[front]) + static_cast<long long>(nums[back]);
-                    if(sum > newTarget) {
-                        back--;
-                    } 
-                    else if(sum < newTarget) {
-                        front++;
-                    } 
-                    else {
-                        vector<int> quad = {nums[i], nums[j], nums[front], nums[back]};
-                        result.push_back(quad);
-                        while(front < back && nums[front] == quad[2]) {
-                            front++;
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if (n < 4) {
+            return ans;
+        }
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                int low = j + 1, high = n - 1;
+                long long curr = (long long)target - (long long)nums[i] - (long long)nums[j];
+                while (low < high) {
+                    long long sum = (long long) (nums[low] + nums[high]);
+                    if (sum > curr) {
+                        high--;
+                    } else if (sum < curr) {
+                        low++;
+                    } else {
+                        vector<int> temp = {nums[i], nums[j], nums[low], nums[high]};
+                        ans.push_back(temp);
+                        while (low < high && nums[low] == temp[2]) {
+                            low++;
                         }
-                        while(front < back && nums[back] == quad[3]) {
-                            back--;
+                        while (low < high && nums[high] == temp[3]) {
+                            high--;
                         }
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
