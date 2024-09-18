e54822216d055dd883eda15409d128856de873b3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-18 10:48:37 +0530

    Time: 0 ms (100%), Space: 15.4 MB (94.62%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        auto myComparator = [](int &a, int &b) {
-            string c = to_string(a);
-            string d = to_string(b);
-            return c+d > d+c;
+        auto cmp = [](int a, int b) {
+            string s1 = to_string(a), s2 = to_string(b);
+            return (s1 + s2) > (s2 + s1);
         };
-        sort(nums.begin(), nums.end(), myComparator);
+
+        sort(nums.begin(), nums.end(), cmp);
+
         if (nums[0] == 0) {
             return "0";
-        } 
-        string ans = "";
-        for (int x : nums) {
-            ans += to_string(x);
         }
+
+        string ans = "";
+        for (int num : nums) {
+            ans += to_string(num);
+        }
+
         return ans;
     }
 };
-// class Solution {
-// public:
-//     static bool myComparator(int &s1, int &s2) {
-//         string a = to_string(s1);
-//         string b = to_string(s2);
-//         return a+b > b+a;
-//     }
-//     string largestNumber(vector<int>& nums) {
-//         sort(nums.begin(), nums.end(), myComparator);
-//         if (nums[0] == 0) {
-//             return "0";
-//         } 
-//         string ans = "";
-//         for (int x : nums) {
-//             ans += to_string(x);
-//         }
-//         return ans;
-//     }
-// };
