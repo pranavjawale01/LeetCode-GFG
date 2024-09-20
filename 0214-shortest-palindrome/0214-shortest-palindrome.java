2e8152a2f3a3bd4319dbe3f544423ea83b031a05
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-20 17:27:57 +0530

    Time: 7 ms (76.2%), Space: 44.8 MB (46.05%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,12 +1,52 @@
 class Solution {
-    public String shortestPalindrome(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            if (s.substring(0, n - i).equals(rev.substring(i))) {
-                return rev.substring(0, i) + s;
+    private void computeLPS(String pattern, int[] lps) {
+        int M = pattern.length();
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+
+        while (i < M) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return rev + s;
+    }
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String temp = s + "#" + rev;
+
+        int[] lps = new int[temp.length()];
+        computeLPS(temp, lps);
+
+        int longestPalindromePrefixLen = lps[lps.length - 1];
+
+        String ans = rev.substring(0, s.length() - longestPalindromePrefixLen) + s;
+        return ans;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public String shortestPalindrome(String s) {
+//         String rev = new StringBuilder(s).reverse().toString();
+//         int n = s.length();
+//         for (int i = 0; i < n; i++) {
+//             if (s.substring(0, n - i).equals(rev.substring(i))) {
+//                 return rev.substring(0, i) + s;
+//             }
+//         }
+//         return rev + s;
+//     }
+// }
