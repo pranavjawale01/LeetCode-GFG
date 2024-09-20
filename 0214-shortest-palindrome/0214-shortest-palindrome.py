4f61adfec41e16c8228476c0e744c27cec5c9412
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-20 17:28:04 +0530

    Time: 45 ms (96.26%), Space: 20.8 MB (8.96%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,8 +1,43 @@
 class Solution:
+    def computeLPS(self, pattern: str) -> list[int]:
+        M = len(pattern)
+        lps = [0] * M
+        length = 0
+        i = 1
+
+        while i < M:
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+
     def shortestPalindrome(self, s: str) -> str:
         rev = s[::-1]
-        n = len(s)
-        for i in range(n):
-            if s[:n - i] == rev[i:]:
-                return rev[:i] + s
-        return rev + s
+        temp = s + "#" + rev
+
+        lps = self.computeLPS(temp)
+
+        longest_palindrome_prefix_len = lps[-1]
+
+        ans = rev[:len(s) - longest_palindrome_prefix_len] + s
+        return ans
+
+
+
+
+
+# class Solution:
+#     def shortestPalindrome(self, s: str) -> str:
+#         rev = s[::-1]
+#         n = len(s)
+#         for i in range(n):
+#             if s[:n - i] == rev[i:]:
+#                 return rev[:i] + s
+#         return rev + s
