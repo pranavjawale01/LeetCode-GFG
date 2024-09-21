89008e30d5a05a87e230b507af7702cb240d080c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-21 14:28:48 +0530

    Time: 1 ms (100%), Space: 48.4 MB (63.55%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,10 +1,40 @@
 class Solution {
+    public void solve(int curr, int n, List<Integer> ans) {
+        if (curr > n) {
+            return;
+        }
+        ans.add(curr);
+        for (int digit = 0; digit < 10; digit++) {
+            int next = curr * 10 + digit;
+            if (next > n) {
+                return;
+            }
+            solve(next, n, ans);
+        }
+    }
     public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            ans.add(i);
+        for (int num = 1; num < 10; num++) {
+            solve(num, n, ans);
         }
-        Collections.sort(ans, (a, b) -> String.valueOf(a).compareTo(String.valueOf(b)));
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<Integer> lexicalOrder(int n) {
+//         List<Integer> ans = new ArrayList<>();
+//         for (int i = 1; i <= n; i++) {
+//             ans.add(i);
+//         }
+//         Collections.sort(ans, (a, b) -> String.valueOf(a).compareTo(String.valueOf(b)));
+//         return ans;
+//     }
+// }
