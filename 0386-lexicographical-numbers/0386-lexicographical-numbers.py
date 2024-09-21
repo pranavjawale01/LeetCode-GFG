e50938178c65156ad937c1effdb78009b4d0b809
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-21 14:28:56 +0530

    Time: 73 ms (68.42%), Space: 22.2 MB (91.75%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,7 +1,27 @@
 class Solution:
+    def solve(self, curr: int, n: int, ans: List[int]):
+        if curr > n:
+            return
+        ans.append(curr)
+        for i in range(0, 10):
+            next = curr * 10 + i
+            if next > n:
+                return
+            self.solve(next, n, ans)
+
     def lexicalOrder(self, n: int) -> List[int]:
         ans = []
-        for i in range(1, n+1):
-            ans.append(i)
-        ans.sort(key=str)
+        for i in range(1, 10):
+            self.solve(i, n, ans)
         return ans
+
+
+
+
+# class Solution:
+#     def lexicalOrder(self, n: int) -> List[int]:
+#         ans = []
+#         for i in range(1, n+1):
+#             ans.append(i)
+#         ans.sort(key=str)
+#         return ans
