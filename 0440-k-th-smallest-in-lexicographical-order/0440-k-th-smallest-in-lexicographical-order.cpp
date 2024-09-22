99ead6ed595407c1b39e23f6f17fb4d0756d8758
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-22 17:13:06 +0530

    Time: 2 ms (53.78%), Space: 7.4 MB (54.58%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,38 +1,73 @@
 class Solution {
 public:
-    int count = 0;
-    int result = 0;
-
-    bool solve(long long curr, int n, int k) {
-        if (curr > n) {
-            return false;
+    int Count(long curr, long next, int n) {
+        int countNum = 0;
+        while (curr <= n) {
+            countNum += (next - curr);
+            curr *= 10;
+            next *= 10;
+            next = min(next, long(n+1));
         }
-        count++;
-        if (count == k) {
-            result = curr;
-            return true;
-        }
-        for (int digit = 0; digit <= 9; digit++) {
-            long long nextnum = curr * 10 + digit;
-            if (nextnum > n) {
-                break;
-            }
-            if (solve(nextnum, n, k)) {
-                return true;
-            }
-        }
-        return false;
+        return countNum;
     }
-
     int findKthNumber(int n, int k) {
-        for (long long num = 1; num <= 9; num++) {
-            if (solve(num, n, k)) {
-                break;
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int count = Count(curr, curr + 1, n);
+            if (count <= k) {
+                curr++;
+                k -= count;
+            } else {
+                curr *= 10;
+                k--;
             }
         }
-        return result;
+        return curr;
     }
 };
+
+
+
+
+
+
+// TLE
+// class Solution {
+// public:
+//     int count = 0;
+//     int result = 0;
+
+//     bool solve(long long curr, int n, int k) {
+//         if (curr > n) {
+//             return false;
+//         }
+//         count++;
+//         if (count == k) {
+//             result = curr;
+//             return true;
+//         }
+//         for (int digit = 0; digit <= 9; digit++) {
+//             long long nextnum = curr * 10 + digit;
+//             if (nextnum > n) {
+//                 break;
+//             }
+//             if (solve(nextnum, n, k)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     int findKthNumber(int n, int k) {
+//         for (long long num = 1; num <= 9; num++) {
+//             if (solve(num, n, k)) {
+//                 break;
+//             }
+//         }
+//         return result;
+//     }
+// };
 
 
 
