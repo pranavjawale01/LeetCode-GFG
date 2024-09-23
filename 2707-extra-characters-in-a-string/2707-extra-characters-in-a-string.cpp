ad47e2cbb9cce6d9f1438c02631a36b4cff97517
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-23 16:05:59 +0530

    Time: 129 ms (55.41%), Space: 86.8 MB (52.7%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
-    vector<int> dp;
-    
-    int solve(int i, string &s, unordered_set<string> &st, int &n) {
-        if (i >= n) {
-            return 0;
-        }
-        
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int result = 1 + solve(i + 1, s, st, n);
-
-        for (int j = i; j < n; j++) {
-            string curr = s.substr(i, j - i + 1);
-            
-            if (st.find(curr) != st.end()) {
-                result = min(result, solve(j + 1, s, st, n));
-            }
-        }
-
-        return dp[i] = result;
-    }
-
     int minExtraChar(string s, vector<string>& dictionary) {
         int n = s.length();
-        dp.resize(n+1, -1);
         unordered_set<string> st(dictionary.begin(), dictionary.end());
-        return solve(0, s, st, n);
+        vector<int> dp(n+1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i+1];
+            for (int j = i; j < n; j++) {
+                string curr = s.substr(i, j - i + 1);
+                if (st.find(curr) != st.end()) {
+                    dp[i] = min(dp[i], dp[j+1]);
+                }
+            }
+        }
+        return dp[0];
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> dp;
+    
+//     int solve(int i, string &s, unordered_set<string> &st, int &n) {
+//         if (i >= n) {
+//             return 0;
+//         }
+        
+//         if (dp[i] != -1) {
+//             return dp[i];
+//         }
+
+//         int result = 1 + solve(i + 1, s, st, n);
+
+//         for (int j = i; j < n; j++) {
+//             string curr = s.substr(i, j - i + 1);
+            
+//             if (st.find(curr) != st.end()) {
+//                 result = min(result, solve(j + 1, s, st, n));
+//             }
+//         }
+
+//         return dp[i] = result;
+//     }
+
+//     int minExtraChar(string s, vector<string>& dictionary) {
+//         int n = s.length();
+//         dp.resize(n+1, -1);
+//         unordered_set<string> st(dictionary.begin(), dictionary.end());
+//         return solve(0, s, st, n);
+//     }
+// };
