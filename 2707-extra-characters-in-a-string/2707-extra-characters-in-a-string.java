e93ad17331815225088afe52b45f8fefe02c99ed
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-23 16:07:47 +0530

    Time: 45 ms (55.1%), Space: 45.5 MB (13.27%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,36 +1,60 @@
 class Solution {
-    int[] dp;
-    
     public int minExtraChar(String s, String[] dictionary) {
         int n = s.length();
-        dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        
         Set<String> st = new HashSet<>(Arrays.asList(dictionary));
-        
-        return solve(0, s, st, n);
-    }
-    
-    private int solve(int i, String s, Set<String> st, int n) {
-        if (i >= n) {
-            return 0;
-        }
-        
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int result = 1 + solve(i + 1, s, st, n);
-
-        for (int j = i; j < n; j++) {
-            String curr = s.substring(i, j + 1);
-            
-            if (st.contains(curr)) {
-                result = Math.min(result, solve(j + 1, s, st, n));
+        int[] dp = new int[n+1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i+1];
+            for (int j = i; j < n; j++) {
+                String curr = s.substring(i, j + 1);
+                if (st.contains(curr)) {
+                    dp[i] = Math.min(dp[i], dp[j+1]);
+                }
             }
         }
-
-        dp[i] = result;
-        return result;
+        return dp[0];
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     int[] dp;
+    
+//     public int minExtraChar(String s, String[] dictionary) {
+//         int n = s.length();
+//         dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+        
+//         Set<String> st = new HashSet<>(Arrays.asList(dictionary));
+        
+//         return solve(0, s, st, n);
+//     }
+    
+//     private int solve(int i, String s, Set<String> st, int n) {
+//         if (i >= n) {
+//             return 0;
+//         }
+        
+//         if (dp[i] != -1) {
+//             return dp[i];
+//         }
+
+//         int result = 1 + solve(i + 1, s, st, n);
+
+//         for (int j = i; j < n; j++) {
+//             String curr = s.substring(i, j + 1);
+            
+//             if (st.contains(curr)) {
+//                 result = Math.min(result, solve(j + 1, s, st, n));
+//             }
+//         }
+
+//         dp[i] = result;
+//         return result;
+//     }
+// }
