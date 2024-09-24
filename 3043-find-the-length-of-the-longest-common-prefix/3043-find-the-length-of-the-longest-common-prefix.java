86df2ebdf93b4e30fee64de17a18885e7f8b7a7d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-24 18:29:53 +0530

    Time: 34 ms (91.99%), Space: 55 MB (96.8%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,27 +1,87 @@
+class TrieNode {
+    TrieNode[] children;
+    public TrieNode() {
+        children = new TrieNode[10];
+    }
+}
+
 class Solution {
+    public TrieNode getTrieNode() {
+        return new TrieNode();
+    }
+
+    public void insert(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numstr = Integer.toString(num);
+        for (char c : numstr.toCharArray()) {
+            int idx = c - '0';
+            if (crawl.children[idx] == null) {
+                crawl.children[idx] = getTrieNode();
+            }
+            crawl = crawl.children[idx];
+        }
+    }
+
+    public int search(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numstr = Integer.toString(num);
+        int len = 0;
+        for (char c : numstr.toCharArray()) {
+            int idx = c - '0';
+            if (crawl.children[idx] != null) {
+                len++;
+                crawl = crawl.children[idx];
+            } else {
+                break;
+            }
+        }
+        return len;
+    }
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Set<Integer> st = new HashSet<>();
-        
-        for (int x : arr1) {
-            int val = x;
-            while (!st.contains(val) && val > 0) {
-                st.add(val);
-                val /= 10;
-            }
+        TrieNode root = getTrieNode();
+        for (int num : arr1) {
+            insert(num, root);
         }
-
         int ans = 0;
-
-        for (int x : arr2) {
-            int num = x;
-            while (!st.contains(num) && num > 0) {
-                num /= 10;
-            }
-            if (num > 0) {
-                ans = Math.max(ans, (int) Math.log10(num) + 1);
-            }
+        for (int num : arr2) {
+            ans = Math.max(ans, search(num, root));
         }
-        
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+//         Set<Integer> st = new HashSet<>();
+        
+//         for (int x : arr1) {
+//             int val = x;
+//             while (!st.contains(val) && val > 0) {
+//                 st.add(val);
+//                 val /= 10;
+//             }
+//         }
+
+//         int ans = 0;
+
+//         for (int x : arr2) {
+//             int num = x;
+//             while (!st.contains(num) && num > 0) {
+//                 num /= 10;
+//             }
+//             if (num > 0) {
+//                 ans = Math.max(ans, (int) Math.log10(num) + 1);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// }
