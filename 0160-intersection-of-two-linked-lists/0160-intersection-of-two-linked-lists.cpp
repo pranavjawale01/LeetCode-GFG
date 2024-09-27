a091a75cf9826adfbc74cb642015d4418017a859
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-28 01:06:13 +0530

    Time: 33 ms (82.91%), Space: 17.4 MB (24.76%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,18 +8,69 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> st;
-        while (headA) {
-            st.insert(headA);
-            headA = headA->next;
+    int getDifference(ListNode* head1, ListNode* head2) {
+        int len1 = 0, len2 = 0;
+        
+        while (head1 != NULL) {
+            ++len1;
+            head1 = head1->next;
         }
-        while (headB) {
-            if (st.find(headB) != st.end()) {
-                return headB;
+        
+        while (head2 != NULL) {
+            ++len2;
+            head2 = head2->next;
+        }
+        
+        return len1 - len2;
+    }
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+        int diff = getDifference(head1, head2);
+    
+        if (diff < 0) {
+            while (diff++ != 0) {
+                head2 = head2->next;
             }
-            headB = headB->next;
+        } else {
+            while (diff-- != 0) {
+                head1 = head1->next;
+            }
         }
-        return nullptr;
+        
+        while (head1 != NULL && head2 != NULL) {
+            if (head1 == head2) {
+                return head1;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        
+        return NULL;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         unordered_set<ListNode*> st;
+//         while (headA) {
+//             st.insert(headA);
+//             headA = headA->next;
+//         }
+//         while (headB) {
+//             if (st.find(headB) != st.end()) {
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+//         return nullptr;
+//     }
+// };
