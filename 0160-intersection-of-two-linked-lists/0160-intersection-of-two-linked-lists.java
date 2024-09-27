093f55ef406aaf695baf3b53eefdce64f359b22b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-09-28 01:07:20 +0530

    Time: 1 ms (99.89%), Space: 48.5 MB (46.69%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,19 +10,62 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) {
-            return null;
+    public int getDifference(ListNode head1, ListNode head2) {
+        int len1 = 0, len2 = 0;
+        while (head1 != null) {
+            len1++;
+            head1 = head1.next;
         }
-
-        ListNode aPointer = headA;
-        ListNode bPointer = headB;
-
-        while (aPointer != bPointer) {
-            aPointer = (aPointer == null) ? headB : aPointer.next;
-            bPointer = (bPointer == null) ? headA : bPointer.next;
+        while (head2 != null) {
+            len2++;
+            head2 = head2.next;
         }
+        return len1 - len2;
+    }
 
-        return aPointer;
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        int diff = getDifference(head1, head2);
+        if (diff < 0) {
+            while (diff++ != 0) {
+                head2 = head2.next;
+            }
+        } else {
+            while (diff-- != 0) {
+                head1 = head1.next;
+            }
+        }
+        while (head1 != null && head2 != null) {
+            if (head1 == head2) {
+                return head1;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         if (headA == null || headB == null) {
+//             return null;
+//         }
+
+//         ListNode aPointer = headA;
+//         ListNode bPointer = headB;
+
+//         while (aPointer != bPointer) {
+//             aPointer = (aPointer == null) ? headB : aPointer.next;
+//             bPointer = (bPointer == null) ? headA : bPointer.next;
+//         }
+
+//         return aPointer;
+//     }
+// }
