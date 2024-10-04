e3c3ffb4d84bb54db46b37f376da5a8fe490ce58
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-04 16:53:29 +0530

    Time: 417 ms (27.08%), Space: 29.5 MB (31.15%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,15 +1,44 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         n = len(skill)
-        skill.sort()
-        i, j = 0, n - 1
-        s = skill[i] + skill[j]
+        vec = [0] * 1001
+        sum_ = 0
+        for x in skill:
+            sum_ += x
+            vec[x] += 1
+        
+        teams = n // 2
+        if sum_ % teams != 0:
+            return -1
+        
+        target = sum_ // teams
         chem = 0
-        while i < j:
-            currSkill = skill[i] + skill[j]
-            if currSkill != s:
+        for i in range(n):
+            currSkill = skill[i]
+            remainSkill = target - currSkill
+            if remainSkill < 0 or remainSkill >= len(vec) or vec[remainSkill] <= 0:
                 return -1
-            chem += skill[i] * skill[j]
-            i += 1
-            j -= 1
-        return chem
+            chem += currSkill * remainSkill
+            vec[remainSkill] -= 1
+        
+        return chem // 2
+
+
+
+
+
+# class Solution:
+#     def dividePlayers(self, skill: List[int]) -> int:
+#         n = len(skill)
+#         skill.sort()
+#         i, j = 0, n - 1
+#         s = skill[i] + skill[j]
+#         chem = 0
+#         while i < j:
+#             currSkill = skill[i] + skill[j]
+#             if currSkill != s:
+#                 return -1
+#             chem += skill[i] * skill[j]
+#             i += 1
+#             j -= 1
+#         return chem
