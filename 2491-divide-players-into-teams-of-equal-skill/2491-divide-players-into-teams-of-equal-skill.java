4845a59b04b479dea02a3860618002e194c256ed
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-04 16:53:22 +0530

    Time: 3 ms (96.47%), Space: 56.2 MB (92.7%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -1,17 +1,51 @@
 class Solution {
     public long dividePlayers(int[] skill) {
         int n = skill.length;
-        Arrays.sort(skill);
-        int i = 0, j = n - 1;
-        int s = skill[i] + skill[j];
-        long chem = 0;
-        while (i < j) {
-            int currSkill = skill[i] + skill[j];
-            if (currSkill != s) return -1;
-            chem += (long)(skill[i]) * (long)(skill[j]);
-            i++;
-            j--;
+        int[] vec = new int[1001];
+        int sum = 0;
+        for (int x : skill) {
+            sum += x;
+            vec[x]++;
         }
-        return chem;
+        int teams = n / 2;
+        if (sum % teams != 0) return -1;
+        int target = sum / teams;
+        long chem = 0;
+        for (int i = 0; i < n; i++) {
+            int currSkill = skill[i];
+            int remainSkill = target - currSkill;
+            if (vec[remainSkill] <= 0) {
+                return -1;
+            }
+            chem += (long)(currSkill) * (long)(remainSkill);
+            vec[remainSkill] -= 1;
+        }
+        return chem / 2;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public long dividePlayers(int[] skill) {
+//         int n = skill.length;
+//         Arrays.sort(skill);
+//         int i = 0, j = n - 1;
+//         int s = skill[i] + skill[j];
+//         long chem = 0;
+//         while (i < j) {
+//             int currSkill = skill[i] + skill[j];
+//             if (currSkill != s) return -1;
+//             chem += (long)(skill[i]) * (long)(skill[j]);
+//             i++;
+//             j--;
+//         }
+//         return chem;
+//     }
+// }
