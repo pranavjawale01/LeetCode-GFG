274d64bce65db0f10dd33d6a67055b5c383f388f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-04 16:47:44 +0530

    Time: 47 ms (98.8%), Space: 55.8 MB (98.4%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -2,17 +2,54 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int n = skill.size();
-        sort(skill.begin(), skill.end());
-        int i = 0, j = n - 1;
-        int s = skill[i] + skill[j];
-        long long chem = 0;
-        while (i < j) {
-            int currSkill = skill[i] + skill[j];
-            if (currSkill != s) return -1;
-            chem += (long long) (skill[i]) * (long long) (skill[j]);
-            i++;
-            j--;
+
+        vector<int> vec(1001, 0);
+        int SUM = 0;
+        for (int& s : skill) {
+            SUM += s;
+            vec[s]++;
         }
-        return chem;
+
+        int teams = n / 2;
+
+        if (SUM % teams != 0) {
+            return -1;
+        }
+
+        int target = SUM / teams;
+        long long chem = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currSkill = skill[i];
+            int remainSkill = target - currSkill;
+
+            if (vec[remainSkill] <= 0) {
+                return -1;
+            }
+
+            chem += (long long)currSkill * (long long)(remainSkill);
+            vec[remainSkill] -= 1;
+        }
+
+        return chem / 2;
     }
 };
+
+// class Solution {
+// public:
+//     long long dividePlayers(vector<int>& skill) {
+//         int n = skill.size();
+//         sort(skill.begin(), skill.end());
+//         int i = 0, j = n - 1;
+//         int s = skill[i] + skill[j];
+//         long long chem = 0;
+//         while (i < j) {
+//             int currSkill = skill[i] + skill[j];
+//             if (currSkill != s) return -1;
+//             chem += (long long) (skill[i]) * (long long) (skill[j]);
+//             i++;
+//             j--;
+//         }
+//         return chem;
+//     }
+// };
