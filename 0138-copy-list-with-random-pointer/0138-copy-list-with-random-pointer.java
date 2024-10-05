50bfdefe62438e3456a34d627c58d040f98ccc5b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-05 11:55:20 +0530

    Time: 0 ms (100%), Space: 44.9 MB (7.04%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,20 +15,66 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node temp = head;
-        Map<Node, Node> mp = new HashMap<>();
-        while (temp != null) {
-            Node newNode = new Node(temp.val);
-            mp.put(temp, newNode);
-            temp = temp.next;
+        if (head == null) {
+            return null;
         }
-        temp = head;
-        while (temp != null) {
-            Node copyNode = mp.get(temp);
-            copyNode.next = mp.get(temp.next);
-            copyNode.random = mp.get(temp.random);
-            temp = temp.next;
+
+        Node curr = head;
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
         }
-        return mp.get(head);
+
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node newHead = head.next;
+        Node copy = null;
+        while (curr != null) {
+            copy = curr.next;
+            curr.next = copy.next;
+            if (copy.next != null) {
+                copy.next = copy.next.next;
+            }
+            curr = curr.next;
+        }
+
+        return newHead;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         Node temp = head;
+//         Map<Node, Node> mp = new HashMap<>();
+//         while (temp != null) {
+//             Node newNode = new Node(temp.val);
+//             mp.put(temp, newNode);
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         while (temp != null) {
+//             Node copyNode = mp.get(temp);
+//             copyNode.next = mp.get(temp.next);
+//             copyNode.random = mp.get(temp.random);
+//             temp = temp.next;
+//         }
+//         return mp.get(head);
+//     }
+// }
