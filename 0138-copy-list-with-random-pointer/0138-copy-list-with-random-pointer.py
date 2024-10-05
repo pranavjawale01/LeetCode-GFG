37161a8fa46d07e8d2a41473f7c47ba1829d0d5c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-05 11:55:25 +0530

    Time: 32 ms (89.24%), Space: 17.4 MB (82.54%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -12,19 +12,51 @@ class Solution:
         if not head:
             return None
 
-        temp = head
-        mp = {}
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            copy.next = curr.next
+            curr.next = copy
+            curr = copy.next
 
-        while temp:
-            newNode = Node(temp.val)
-            mp[temp] = newNode
-            temp = temp.next
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
 
-        temp = head
-        while temp:
-            copyNode = mp[temp]
-            copyNode.next = mp.get(temp.next)
-            copyNode.random = mp.get(temp.random)
-            temp = temp.next
+        curr = head
+        new_head = head.next
+        copy = None
+        while curr:
+            copy = curr.next
+            curr.next = copy.next
+            if copy.next:
+                copy.next = copy.next.next
+            curr = curr.next
 
-        return mp[head]
+        return new_head
+
+
+
+# class Solution:
+#     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+#         if not head:
+#             return None
+
+#         temp = head
+#         mp = {}
+
+#         while temp:
+#             newNode = Node(temp.val)
+#             mp[temp] = newNode
+#             temp = temp.next
+
+#         temp = head
+#         while temp:
+#             copyNode = mp[temp]
+#             copyNode.next = mp.get(temp.next)
+#             copyNode.random = mp.get(temp.random)
+#             temp = temp.next
+
+#         return mp[head]
