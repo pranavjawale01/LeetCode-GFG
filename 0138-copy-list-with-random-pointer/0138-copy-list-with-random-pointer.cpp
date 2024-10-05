360a453483bcab70bfed2b0e7b03b94f310aa050
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-05 11:54:40 +0530

    Time: 11 ms (14.53%), Space: 14.6 MB (65.49%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,20 +17,62 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node *temp = head;
-        unordered_map<Node*, Node*> mp;
-        while (temp) {
-            Node *newNode = new Node(temp->val);
-            mp[temp] = newNode;
-            temp = temp->next;
+        if (!head) {
+            return head;
         }
-        temp = head;
-        while (temp) {
-            Node *copyNode = mp[temp];
-            copyNode->next = mp[temp->next];
-            copyNode->random = mp[temp->random];
-            temp = temp->next;
+        Node *curr = head;
+        while (curr) {
+            Node *copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = copy->next;
         }
-        return mp[head];
+        curr = head;
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        curr = head;
+        Node *newHead = head->next;
+        Node *copy = nullptr;
+        while (curr) {
+            copy = curr->next;
+            curr->next = copy->next;
+            if (copy->next) {
+                copy->next = copy->next->next;
+            }
+            curr = curr->next;
+        }
+        return newHead;
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         Node *temp = head;
+//         unordered_map<Node*, Node*> mp;
+//         while (temp) {
+//             Node *newNode = new Node(temp->val);
+//             mp[temp] = newNode;
+//             temp = temp->next;
+//         }
+//         temp = head;
+//         while (temp) {
+//             Node *copyNode = mp[temp];
+//             copyNode->next = mp[temp->next];
+//             copyNode->random = mp[temp->random];
+//             temp = temp->next;
+//         }
+//         return mp[head];
+//     }
+// };
