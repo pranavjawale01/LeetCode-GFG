3f545fcc0d7519ff25f1086f1172037f19e0233a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-07 18:17:34 +0530

    Time: 4 ms (85%), Space: 43.4 MB (42.63%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,12 +1,38 @@
 class Solution {
     public int minLength(String s) {
-        while (s.contains("AB") || s.contains("CD")) {
-            if (s.contains("AB")) {
-                s = s.replaceFirst("AB", "");
-            } else if (s.contains("CD")) {
-                s = s.replaceFirst("CD", "");
+        Stack<Character> st = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (st.isEmpty()) {
+                st.push(c);
+            } else {
+                if (c == 'B' && st.peek() == 'A') {
+                    st.pop();
+                } else if (c == 'D' && st.peek() == 'C') {
+                    st.pop();
+                } else {
+                    st.push(c);
+                }
             }
         }
-        return s.length();
+        
+        return st.size();
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int minLength(String s) {
+//         while (s.contains("AB") || s.contains("CD")) {
+//             if (s.contains("AB")) {
+//                 s = s.replaceFirst("AB", "");
+//             } else if (s.contains("CD")) {
+//                 s = s.replaceFirst("CD", "");
+//             }
+//         }
+//         return s.length();
+//     }
+// }
