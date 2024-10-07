0055f9d4d935ace443439ddfc0a935eeaca49867
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-07 18:13:53 +0530

    Time: 8 ms (42.58%), Space: 11.6 MB (31.24%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     int minLength(string s) {
-        while (s.find("AB") != -1 || s.find("CD") != -1) {
-            if (s.find("AB") != -1) {
-                s.erase(s.find("AB"), 2);
-            } else if (s.find("CD") != -1) {
-                s.erase(s.find("CD"), 2);
+        stack<char> st;
+        for (char &c : s) {
+            if (st.empty()) {
+                st.push(c);
+            } else {
+                if (c == 'B' && st.top() == 'A') {
+                    st.pop();
+                } else if (c == 'D' && st.top() == 'C') {
+                    st.pop();
+                } else {
+                    st.push(c);
+                }
             }
         }
-        return s.length();
+        return st.size();
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int minLength(string s) {
+//         while (s.find("AB") != -1 || s.find("CD") != -1) {
+//             if (s.find("AB") != -1) {
+//                 s.erase(s.find("AB"), 2);
+//             } else if (s.find("CD") != -1) {
+//                 s.erase(s.find("CD"), 2);
+//             }
+//         }
+//         return s.length();
+//     }
+// };
