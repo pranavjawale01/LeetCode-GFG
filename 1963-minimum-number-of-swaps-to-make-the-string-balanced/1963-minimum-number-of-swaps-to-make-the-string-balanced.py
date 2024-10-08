992eadf341840bfbb84e0507f22b486faaf0322a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-08 22:02:39 +0530

    Time: 168 ms (91.05%), Space: 27.9 MB (45.06%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,9 +1,27 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        st = []
+        size = 0
         for c in s:
             if c == '[':
-                st.append(c)
-            elif st:
-                st.pop()
-        return (len(st) + 1) // 2
+                size += 1
+            elif size:
+                size -= 1
+        return (size + 1) // 2
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def minSwaps(self, s: str) -> int:
+#         st = []
+#         for c in s:
+#             if c == '[':
+#                 st.append(c)
+#             elif st:
+#                 st.pop()
+#         return (len(st) + 1) // 2
