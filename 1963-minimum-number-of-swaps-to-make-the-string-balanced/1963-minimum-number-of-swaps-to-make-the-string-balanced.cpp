94b5e50ccde027db69e7ac8c0770acc0bb6607a7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-08 22:00:01 +0530

    Time: 96 ms (85.08%), Space: 32.7 MB (79.33%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        stack<char> st;
-        for (char &ch : s) {
-            if (ch == '[') {
-                st.push(ch);
-            } else if (!st.empty()) {
-                st.pop();
+        int size = 0;
+        for (char &c : s) {
+            if (c == '[') {
+                size++;
+            } else if (size != 0) {
+                size--;
             }
         }
-        return (st.size() + 1) / 2;
+        return (size + 1) / 2;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int minSwaps(string s) {
+//         stack<char> st;
+//         for (char &ch : s) {
+//             if (ch == '[') {
+//                 st.push(ch);
+//             } else if (!st.empty()) {
+//                 st.pop();
+//             }
+//         }
+//         return (st.size() + 1) / 2;
+//     }
+// };
