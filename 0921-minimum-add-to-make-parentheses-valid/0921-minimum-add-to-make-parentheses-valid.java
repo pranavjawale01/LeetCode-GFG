fd43840c361936b5b7ee25c33e9730eb043003be
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-09 08:28:03 +0530

    Time: 0 ms (100%), Space: 41.7 MB (8.99%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,47 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        int count = 0;
+        int open = 0, close = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                st.push(c);
+                open++;
             } else {
-                if (st.isEmpty()) {
-                    count++;
+                if (open == 0) {
+                    close++;
                 } else {
-                    st.pop();
+                    open--;
                 }
             }
         }
-        return st.size() + count;
+        return open + close;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         int count = 0;
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') {
+//                 st.push(c);
+//             } else {
+//                 if (st.isEmpty()) {
+//                     count++;
+//                 } else {
+//                     st.pop();
+//                 }
+//             }
+//         }
+//         return st.size() + count;
+//     }
+// }
