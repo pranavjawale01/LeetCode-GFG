a1b4264855f47e333ef6fbae7b26ac3eb7975963
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-10 09:54:12 +0530

    Time: 58 ms (95.69%), Space: 167.6 MB (29.08%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,33 +1,99 @@
 class Solution {
+    public void dfs(int node, List<Integer>[] adj, boolean[] vis) {
+        vis[node] = true;
+        for (int x : adj[node]) {
+            if (!vis[x]) {
+                dfs(x, adj, vis);
+            }
+        }
+    }
+    
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-        Set<Integer> bug = new HashSet<>();
-        bug.add(k);
-        boolean flag;
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
+        for (int[] x : invocations) {
+            int u = x[0];
+            int v = x[1];
+            adj[u].add(v);
+        }
 
-        do {
-            flag = false;
-            for (int[] x : invocations) {
-                int a = x[0], b = x[1];
-                if (bug.contains(a) && !bug.contains(b)) {
-                    bug.add(b);
-                    flag = true;
+        boolean[] vis = new boolean[n];
+        dfs(k, adj, vis);
+
+        boolean suspicious = true;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                for (int x : adj[i]) {
+                    if (vis[x]) {
+                        suspicious = false;
+                    }
                 }
             }
-        } while (flag);
+        }
 
-        for (int[] x : invocations) {
-            int a = x[0], b = x[1];
-            if (!bug.contains(a) && bug.contains(b)) {
-                List<Integer> ans = new ArrayList<>();
-                for (int i = 0; i < n; i++) ans.add(i);
-                return ans;
+        // Prepare result
+        List<Integer> ans = new ArrayList<>();
+        if (suspicious) {
+            for (int i = 0; i < n; i++) {
+                if (!vis[i]) {
+                    ans.add(i);
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                ans.add(i);
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (!bug.contains(i)) ans.add(i);
-        }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// TLE
+// class Solution {
+//     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+//         Set<Integer> bug = new HashSet<>();
+//         bug.add(k);
+//         boolean flag;
+
+//         do {
+//             flag = false;
+//             for (int[] x : invocations) {
+//                 int a = x[0], b = x[1];
+//                 if (bug.contains(a) && !bug.contains(b)) {
+//                     bug.add(b);
+//                     flag = true;
+//                 }
+//             }
+//         } while (flag);
+
+//         for (int[] x : invocations) {
+//             int a = x[0], b = x[1];
+//             if (!bug.contains(a) && bug.contains(b)) {
+//                 List<Integer> ans = new ArrayList<>();
+//                 for (int i = 0; i < n; i++) ans.add(i);
+//                 return ans;
+//             }
+//         }
+
+//         List<Integer> ans = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             if (!bug.contains(i)) ans.add(i);
+//         }
+//         return ans;
+//     }
+// }
