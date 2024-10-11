aedab035617ac884153f7fb52561799efc987b22
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-11 11:14:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -6,30 +6,64 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to find the minimum number of platforms required at the
     // railway station such that no train waits.
     int findPlatform(vector<int>& arr, vector<int>& dep) {
         // Your code here
+        vector<pair<int, char>> order;
         int n = arr.size();
-        sort(arr.begin(), arr.end());
-        sort(dep.begin(), dep.end());
-        int ans = 1, count = 1;
-        int i = 1, j = 0;
-        while (i < n && j < n) {
-            if (arr[i] <= dep[j]) {
-                count++;
-                i++;
-            } else {
-                count--;
-                j++;
-            }
-            ans = max(ans, count);
+        
+        for (int i = 0; i < n; i++) {
+            order.push_back({arr[i], 'a'});
+            order.push_back({dep[i], 'd'});
         }
+        
+        sort(order.begin(), order.end());
+        
+        int ans = 1;
+        int plat = 0;
+
+        for (auto x : order) {
+            if (x.second == 'a') {
+                plat++;
+            } else {
+                plat--;
+            }
+            ans = max(ans, plat);
+        }
+        
         return ans;
     }
 };
+
+
+// class Solution {
+//   public:
+//     // Function to find the minimum number of platforms required at the
+//     // railway station such that no train waits.
+//     int findPlatform(vector<int>& arr, vector<int>& dep) {
+//         // Your code here
+//         int n = arr.size();
+//         sort(arr.begin(), arr.end());
+//         sort(dep.begin(), dep.end());
+//         int ans = 1, count = 1;
+//         int i = 1, j = 0;
+//         while (i < n && j < n) {
+//             if (arr[i] <= dep[j]) {
+//                 count++;
+//                 i++;
+//             } else {
+//                 count--;
+//                 j++;
+//             }
+//             ans = max(ans, count);
+//         }
+//         return ans;
+//     }
+// };
 
 
 //{ Driver Code Starts.
