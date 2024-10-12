16662b92a62ff3409169abbb1051118641f4cddc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-12 21:37:58 +0530

    Time: 9 ms (45.45%), Space: 27.3 MB (63.64%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    // int pranavjawale01(int low, int high, int val) {
-    //     int best = -1;
-    //     while (low <= high) {
-    //         int mid = low + (high - low) / 2;
-    //         if ((mid | (mid + 1)) == val) {
-    //             best = mid;
-    //             high = mid - 1;
-    //         } else if ((mid | (mid + 1)) > val) {
-    //             high = mid - 1;
-    //         } else {
-    //             low = mid + 1;
-    //         }
-    //     }
-    //     return best;
-    // }
     int pranavjawale01(int val) {
         int ans = INT_MAX;
         for (int i = 0; i < 32; i++) {
