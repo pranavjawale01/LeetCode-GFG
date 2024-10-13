0374b7f07b8aa41cad50e5e9bcc93ba6d94c69b1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-13 09:54:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Smallest Positive Integer that can not be represented as Sum/smallest-positive-integer-that-can-not-be-represented-as-sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+public:
+    long long smallestpositive(vector<long long> array, int n)
+    { 
+        // code here 
+        sort(array.begin(), array.end());
+        
+        long long smallest_positive = 1;
+        
+        for (int i = 0; i < n; i++) {
+            if (array[i] > smallest_positive) {
+                break;
+            }
+            smallest_positive += array[i];
+        }
+        
+        return smallest_positive;
+    } 
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<long long> array(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin>>array[i];
+        }
+        Solution ob;
+        cout<<ob.smallestpositive(array,n)<<"\n";
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
