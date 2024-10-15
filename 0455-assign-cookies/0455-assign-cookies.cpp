f07f4da487a637e17cfea1fa1de177a8c8bb5dac
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-15 08:54:33 +0530

    Time: 41 ms (58.16%), Space: 17.9 MB (100%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        // if (g.empty() || s.empty()) {
+        //     return 0;
+        // }
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         int count = 0;
