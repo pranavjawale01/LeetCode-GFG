c3a392b3738a9eea2475e997ea1c8028695d69a7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-17 13:08:34 +0530

    Time: 3 ms (38.18%), Space: 7.7 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,23 +1,52 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        int len = log10(num) + 1;
+        vector<int> maxRight(10, -1);
         string s = to_string(num);
-        vector<int> maxRight(len);
-        maxRight[len - 1] = len - 1;
-        for (int i = len - 2; i >= 0; i--) {
-            int rightMaxIdx = maxRight[i+1];
-            int rightMaxElement = s[rightMaxIdx];
-            maxRight[i] = (s[i] > rightMaxElement) ? i : rightMaxIdx;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - '0';
+            maxRight[idx] = i;
         }
-        for (int i = 0; i < len; i++) {
-            int maxRightIdx = maxRight[i];
-            int maxRightElement = s[maxRightIdx];
-            if (s[i] < maxRightElement) {
-                swap(s[i], s[maxRightIdx]);
-                return stoi(s);
+        for (int i = 0; i < n; i++) {
+            char curr = s[i];
+            int currDigit = curr - '0';
+            for (int digit = 9; digit > currDigit; digit--) {
+                if (maxRight[digit] > i) {
+                    swap(s[i], s[maxRight[digit]]);
+                    return stoi(s);
+                }
             }
         }
         return num;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maximumSwap(int num) {
+//         int len = log10(num) + 1;
+//         string s = to_string(num);
+//         vector<int> maxRight(len);
+//         maxRight[len - 1] = len - 1;
+//         for (int i = len - 2; i >= 0; i--) {
+//             int rightMaxIdx = maxRight[i+1];
+//             int rightMaxElement = s[rightMaxIdx];
+//             maxRight[i] = (s[i] > rightMaxElement) ? i : rightMaxIdx;
+//         }
+//         for (int i = 0; i < len; i++) {
+//             int maxRightIdx = maxRight[i];
+//             int maxRightElement = s[maxRightIdx];
+//             if (s[i] < maxRightElement) {
+//                 swap(s[i], s[maxRightIdx]);
+//                 return stoi(s);
+//             }
+//         }
+//         return num;
+//     }
+// };
