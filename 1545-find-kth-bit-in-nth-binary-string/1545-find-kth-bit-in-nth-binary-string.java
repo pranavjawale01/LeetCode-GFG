80220cd4f9e5eef71bdd76835170fdde8b54c97a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-19 23:32:34 +0530

    Time: 0 ms (100%), Space: 41.1 MB (60.31%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -1,16 +1,39 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        String s = "0";
-        
-        for (int i = 2; i <= n; i++) {
-            StringBuilder inverted = new StringBuilder(s);
-            for (int j = 0; j < inverted.length(); j++) {
-                inverted.setCharAt(j, inverted.charAt(j) == '0' ? '1' : '0');
-            }
-            inverted.reverse();
-            s = s + "1" + inverted.toString();
+        if (n == 1) {
+            return '0';
         }
+
+        int len = (1 << n) - 1;
         
-        return s.charAt(k - 1);
+        if (k < (len + 1) / 2) {
+            return findKthBit(n - 1, k);
+        } else if (k == (len + 1) / 2) {
+            return '1';
+        } else {
+            char ch = findKthBit(n - 1, len - k + 1);
+            return (ch == '0') ? '1' : '0';
+        }
     }
 }
+
+
+
+
+
+// class Solution {
+//     public char findKthBit(int n, int k) {
+//         String s = "0";
+        
+//         for (int i = 2; i <= n; i++) {
+//             StringBuilder inverted = new StringBuilder(s);
+//             for (int j = 0; j < inverted.length(); j++) {
+//                 inverted.setCharAt(j, inverted.charAt(j) == '0' ? '1' : '0');
+//             }
+//             inverted.reverse();
+//             s = s + "1" + inverted.toString();
+//         }
+        
+//         return s.charAt(k - 1);
+//     }
+// }
