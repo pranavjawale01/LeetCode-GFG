6110520ae4ade8fc9ea4ce5da48419726e1f0af7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-19 23:23:45 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.46%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        string s = "0";
-        
-        for (int i = 2; i <= n; ++i) {
-            string inverted = s;
-            transform(inverted.begin(), inverted.end(), inverted.begin(), [](char c) { return c == '0' ? '1' : '0'; });
-            reverse(inverted.begin(), inverted.end());
-            s = s + "1" + inverted;
+        if (n == 1) {
+            return '0';
         }
-        
-        return s[k - 1];
+        int len = pow(2, n) - 1; // (1 << n) - 1
+        if (k < ceil(len / 2.0)) {
+            return findKthBit(n-1, k);
+        } else if (k == ceil(len / 2.0)) {
+            return '1';
+        } else {
+            char ch = findKthBit(n-1, len - (k-1));
+            return (ch == '0') ? '1' : '0';
+        }
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     char findKthBit(int n, int k) {
+//         string s = "0";
+        
+//         for (int i = 2; i <= n; ++i) {
+//             string inverted = s;
+//             transform(inverted.begin(), inverted.end(), inverted.begin(), [](char c) { return c == '0' ? '1' : '0'; });
+//             reverse(inverted.begin(), inverted.end());
+//             s = s + "1" + inverted;
+//         }
+        
+//         return s[k - 1];
+//     }
+// };
