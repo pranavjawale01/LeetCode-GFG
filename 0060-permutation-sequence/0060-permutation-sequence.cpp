95ed6742ba9d2dacb521f96205e24e6ea1943596
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-21 14:58:13 +0530

    Time: 0 ms (100%), Space: 8.2 MB (64.79%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string result;
+        int fact = 1;
         vector<int> nums;
-
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i < n; i++) {
+            fact *= i;
             nums.push_back(i);
         }
-
-        while (n > 0) {
-            int fact = 1;
-            for (int i = 1; i < n; ++i) {
-                fact *= i;
-            }
-
-            int index = (k - 1) / fact;
-            k = (k - 1) % fact + 1;
-
-            result.push_back(nums[index] + '0');
-            nums.erase(nums.begin() + index);
-
-            --n;
+        nums.push_back(n);
+        string ans = "";
+        k--;
+        while (true) {
+            ans += to_string(nums[k / fact]);
+            nums.erase(nums.begin() + k / fact);
+            if (nums.empty()) break;
+            k = k % fact;
+            fact /= nums.size();
         }
-
-        return result;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// TLE
+// class Solution {
+// public:
+//     void solve(string &s, int idx, vector<string> &res) {
+//         if (idx == s.size()) {
+//             res.push_back(s);
+//             return;
+//         }
+//         for (int i = idx; i < s.size(); i++) {
+//             swap(s[i], s[idx]);
+//             solve(s, idx + 1, res);
+//             swap(s[i], s[idx]);
+//         }
+//     }
+//     string getPermutation(int n, int k) {
+//         string ans;
+//         vector<string> res;
+//         for (int i = 1; i <= n; i++) {
+//             ans.push_back(i + '0');
+//         }
+//         solve(ans, 0, res);
+//         sort(res.begin(), res.end());
+//         auto it = res.begin() + (k - 1);
+//         return *it;
+//     }
+// };
