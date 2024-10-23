d1f12413202f834fb898ddfcd391ea707a5141b1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-23 14:46:07 +0530

    Time: 14 ms (96.61%), Space: 69.9 MB (94.92%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -19,46 +19,85 @@ class Solution {
 
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        List<Integer> levelSum = new ArrayList<>();
-
-        while (!q.isEmpty()) {
-            int currLvlSum = 0;
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode curr = q.poll();
-                currLvlSum += curr.val;
-                if (curr.left != null) {
-                    q.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    q.offer(curr.right);
-                }
-            }
-            levelSum.add(currLvlSum);
-        }
-
-        q.offer(root);
-        root.val = 0;
-        int level = 1;
+        int levelSum = root.val;
 
         while (!q.isEmpty()) {
             int n = q.size();
+            int nextLevelSum = 0;
+
             for (int i = 0; i < n; i++) {
                 TreeNode curr = q.poll();
+                curr.val = levelSum - curr.val;
                 int siblingSum = (curr.left != null ? curr.left.val : 0) + (curr.right != null ? curr.right.val : 0);
 
                 if (curr.left != null) {
-                    curr.left.val = levelSum.get(level) - siblingSum;
+                    nextLevelSum += curr.left.val;
+                    curr.left.val = siblingSum;
                     q.offer(curr.left);
                 }
                 if (curr.right != null) {
-                    curr.right.val = levelSum.get(level) - siblingSum;
+                    nextLevelSum += curr.right.val;
+                    curr.right.val = siblingSum;
                     q.offer(curr.right);
                 }
             }
-            level++;
+            levelSum = nextLevelSum;
         }
 
         return root;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public TreeNode replaceValueInTree(TreeNode root) {
+//         if (root == null) return root;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+//         List<Integer> levelSum = new ArrayList<>();
+
+//         while (!q.isEmpty()) {
+//             int currLvlSum = 0;
+//             int n = q.size();
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode curr = q.poll();
+//                 currLvlSum += curr.val;
+//                 if (curr.left != null) {
+//                     q.offer(curr.left);
+//                 }
+//                 if (curr.right != null) {
+//                     q.offer(curr.right);
+//                 }
+//             }
+//             levelSum.add(currLvlSum);
+//         }
+
+//         q.offer(root);
+//         root.val = 0;
+//         int level = 1;
+
+//         while (!q.isEmpty()) {
+//             int n = q.size();
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode curr = q.poll();
+//                 int siblingSum = (curr.left != null ? curr.left.val : 0) + (curr.right != null ? curr.right.val : 0);
+
+//                 if (curr.left != null) {
+//                     curr.left.val = levelSum.get(level) - siblingSum;
+//                     q.offer(curr.left);
+//                 }
+//                 if (curr.right != null) {
+//                     curr.right.val = levelSum.get(level) - siblingSum;
+//                     q.offer(curr.right);
+//                 }
+//             }
+//             level++;
+//         }
+
+//         return root;
+//     }
+// }
