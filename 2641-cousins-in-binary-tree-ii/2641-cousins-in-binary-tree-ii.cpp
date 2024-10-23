d7893fbe6299eb04bbf1e23610886eb14e14bae6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-23 14:45:20 +0530

    Time: 3 ms (100%), Space: 317.1 MB (26.5%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -15,44 +15,83 @@ public:
         if (!root) return root;
         queue<TreeNode*> q;
         q.push(root);
-        vector<int> levelSum;
+        int levelSum = root->val;
         while (!q.empty()) {
-            int currLvlSum = 0;
             int n = q.size();
+            int nextLevelSum = 0;
             while (n--) {
                 TreeNode *curr = q.front();
                 q.pop();
-                currLvlSum += curr->val;
-                if (curr->left) {
-                    q.push(curr->left);
-                }
-                if (curr->right) {
-                    q.push(curr->right);
-                }
-            }
-            levelSum.push_back(currLvlSum);
-        }
-        q.push(root);
-        root->val = 0;
-        int i = 1;
-        while (!q.empty()) {
-            int n = q.size();
-            while (n--) {
-                TreeNode *curr = q.front();
-                q.pop();
+                curr->val = levelSum - curr->val;
                 int siblingSum = curr->left ? curr->left->val : 0;
                 siblingSum += curr->right ? curr->right->val : 0;
                 if (curr->left) {
-                    curr->left->val = levelSum[i] - siblingSum;
+                    nextLevelSum += curr->left->val;
+                    curr->left->val = siblingSum;
                     q.push(curr->left);
-                } 
+                }
                 if (curr->right) {
-                    curr->right->val = levelSum[i] - siblingSum;
+                    nextLevelSum += curr->right->val;
+                    curr->right->val = siblingSum;
                     q.push(curr->right);
                 }
             }
-            i++;
+            levelSum = nextLevelSum;
         }
         return root;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     TreeNode* replaceValueInTree(TreeNode* root) {
+//         if (!root) return root;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         vector<int> levelSum;
+//         while (!q.empty()) {
+//             int currLvlSum = 0;
+//             int n = q.size();
+//             while (n--) {
+//                 TreeNode *curr = q.front();
+//                 q.pop();
+//                 currLvlSum += curr->val;
+//                 if (curr->left) {
+//                     q.push(curr->left);
+//                 }
+//                 if (curr->right) {
+//                     q.push(curr->right);
+//                 }
+//             }
+//             levelSum.push_back(currLvlSum);
+//         }
+//         q.push(root);
+//         root->val = 0;
+//         int i = 1;
+//         while (!q.empty()) {
+//             int n = q.size();
+//             while (n--) {
+//                 TreeNode *curr = q.front();
+//                 q.pop();
+//                 int siblingSum = curr->left ? curr->left->val : 0;
+//                 siblingSum += curr->right ? curr->right->val : 0;
+//                 if (curr->left) {
+//                     curr->left->val = levelSum[i] - siblingSum;
+//                     q.push(curr->left);
+//                 } 
+//                 if (curr->right) {
+//                     curr->right->val = levelSum[i] - siblingSum;
+//                     q.push(curr->right);
+//                 }
+//             }
+//             i++;
+//         }
+//         return root;
+//     }
+// };
