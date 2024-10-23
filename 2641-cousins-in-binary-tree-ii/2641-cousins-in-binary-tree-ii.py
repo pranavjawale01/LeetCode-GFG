adae9ceca3c95e58c922ad9488c42add889637c7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-23 14:46:13 +0530

    Time: 100 ms (98.95%), Space: 57.3 MB (82.17%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
@@ -4,45 +4,82 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
-
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return root
         
         q = deque([root])
-        level_sum = []
-
-        while q:
-            curr_lvl_sum = 0
-            n = len(q)
-            for _ in range(n):
-                curr = q.popleft()
-                curr_lvl_sum += curr.val
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
-            level_sum.append(curr_lvl_sum)
-
-        q.append(root)
-        root.val = 0
-        level = 1
-
+        level_sum = root.val
+        
         while q:
             n = len(q)
+            next_level_sum = 0
+            
             for _ in range(n):
                 curr = q.popleft()
+                curr.val = level_sum - curr.val
                 sibling_sum = (curr.left.val if curr.left else 0) + (curr.right.val if curr.right else 0)
-
+                
                 if curr.left:
-                    curr.left.val = level_sum[level] - sibling_sum
+                    next_level_sum += curr.left.val
+                    curr.left.val = sibling_sum
                     q.append(curr.left)
                 if curr.right:
-                    curr.right.val = level_sum[level] - sibling_sum
+                    next_level_sum += curr.right.val
+                    curr.right.val = sibling_sum
                     q.append(curr.right)
-            level += 1
-
+            
+            level_sum = next_level_sum
+        
         return root
+
+
+
+
+
+
+
+
+
+# from collections import deque
+
+# class Solution:
+#     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root:
+#             return root
+        
+#         q = deque([root])
+#         level_sum = []
+
+#         while q:
+#             curr_lvl_sum = 0
+#             n = len(q)
+#             for _ in range(n):
+#                 curr = q.popleft()
+#                 curr_lvl_sum += curr.val
+#                 if curr.left:
+#                     q.append(curr.left)
+#                 if curr.right:
+#                     q.append(curr.right)
+#             level_sum.append(curr_lvl_sum)
+
+#         q.append(root)
+#         root.val = 0
+#         level = 1
+
+#         while q:
+#             n = len(q)
+#             for _ in range(n):
+#                 curr = q.popleft()
+#                 sibling_sum = (curr.left.val if curr.left else 0) + (curr.right.val if curr.right else 0)
+
+#                 if curr.left:
+#                     curr.left.val = level_sum[level] - sibling_sum
+#                     q.append(curr.left)
+#                 if curr.right:
+#                     curr.right.val = level_sum[level] - sibling_sum
+#                     q.append(curr.right)
+#             level += 1
+
+#         return root
