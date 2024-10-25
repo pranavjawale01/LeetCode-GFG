e46980ae48fc3e0c22d64d76faad2aff28fbb04c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-25 22:13:33 +0530

    Time: 42 ms (75.31%), Space: 55.1 MB (80.2%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,27 +1,56 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Set<String> set = new HashSet<>(Arrays.asList(folder));
+        Arrays.sort(folder);
         List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
 
-        for (String path : folder) {
-            boolean isSubFolder = false;
-            String currentPath = path;
+        for (int i = 1; i < folder.length; i++) {
+            String currFolder = folder[i];
+            String lastFolder = ans.get(ans.size() - 1) + "/";
 
-            while (!currentPath.isEmpty()) {
-                int position = currentPath.lastIndexOf('/');
-                currentPath = currentPath.substring(0, position);
-
-                if (set.contains(currentPath)) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-
-            if (!isSubFolder) {
-                ans.add(path);
+            if (!currFolder.startsWith(lastFolder)) {
+                ans.add(currFolder);
             }
         }
 
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<String> removeSubfolders(String[] folder) {
+//         Set<String> set = new HashSet<>(Arrays.asList(folder));
+//         List<String> ans = new ArrayList<>();
+
+//         for (String path : folder) {
+//             boolean isSubFolder = false;
+//             String currentPath = path;
+
+//             while (!currentPath.isEmpty()) {
+//                 int position = currentPath.lastIndexOf('/');
+//                 currentPath = currentPath.substring(0, position);
+
+//                 if (set.contains(currentPath)) {
+//                     isSubFolder = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!isSubFolder) {
+//                 ans.add(path);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
