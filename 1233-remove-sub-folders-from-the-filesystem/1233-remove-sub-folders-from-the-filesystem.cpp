67fae951d1d9fffe868643a0a06c5b873bcb5364
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-25 22:12:07 +0530

    Time: 70 ms (91.48%), Space: 53.5 MB (64.56%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        unordered_set<string> st(folder.begin(), folder.end());
+        sort(folder.begin(), folder.end());
         vector<string> ans;
-        for (auto &x : folder) {
-            bool isSubFolder = false;
-            string res = x;
-            while (!x.empty()) {
-                size_t position = x.find_last_of('/');
-                x = x.substr(0, position);
-                if (st.find(x) != st.end()) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-            if (!isSubFolder) {
-                ans.push_back(res);
+        ans.push_back(folder[0]);
+        for (int i = 1; i < folder.size(); i++) {
+            string currFolder = folder[i];
+            string lastFolder = ans.back();
+            lastFolder += "/";
+            if (currFolder.find(lastFolder) != 0) {
+                ans.push_back(currFolder);
             }
         }
         return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> removeSubfolders(vector<string>& folder) {
+//         unordered_set<string> st(folder.begin(), folder.end());
+//         vector<string> ans;
+//         for (auto &x : folder) {
+//             bool isSubFolder = false;
+//             string res = x;
+//             while (!x.empty()) {
+//                 size_t position = x.find_last_of('/');
+//                 x = x.substr(0, position);
+//                 if (st.find(x) != st.end()) {
+//                     isSubFolder = true;
+//                     break;
+//                 }
+//             }
+//             if (!isSubFolder) {
+//                 ans.push_back(res);
+//             }
+//         }
+//         return ans;
+//     }
+// };
