0593901596fab9ef6bb272896cba5e56a1dd0e7f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-25 22:13:42 +0530

    Time: 15 ms (99.66%), Space: 29.7 MB (82.93%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,21 +1,40 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder_set = set(folder)
-        ans = []
+        folder.sort()
+        ans = [folder[0]]
 
-        for path in folder:
-            is_subfolder = False
-            current_path = path
+        for i in range(1, len(folder)):
+            curr_folder = folder[i]
+            last_folder = ans[-1] + "/"
 
-            while current_path:
-                position = current_path.rfind('/')
-                current_path = current_path[:position]
-
-                if current_path in folder_set:
-                    is_subfolder = True
-                    break
-
-            if not is_subfolder:
-                ans.append(path)
+            if not curr_folder.startswith(last_folder):
+                ans.append(curr_folder)
 
         return ans
+
+
+
+
+
+
+# class Solution:
+#     def removeSubfolders(self, folder: List[str]) -> List[str]:
+#         folder_set = set(folder)
+#         ans = []
+
+#         for path in folder:
+#             is_subfolder = False
+#             current_path = path
+
+#             while current_path:
+#                 position = current_path.rfind('/')
+#                 current_path = current_path[:position]
+
+#                 if current_path in folder_set:
+#                     is_subfolder = True
+#                     break
+
+#             if not is_subfolder:
+#                 ans.append(path)
+
+#         return ans
