9279b5e5b3248a8554db9c68530023dc897a2b19
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-10-26 18:06:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -10,25 +10,37 @@ using namespace std;
 
 class Solution {
   public:
-    vector<string> ans;
-    void solve(int n, vector<vector<int>> &mat, int i, int j, string s) {
-        if (i < 0 || j < 0 || i >= n || j >= n || mat[i][j] == 0) {
-            return;
-        }
+    void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string move, vector<vector<int>> &vis, int di[], int dj[]) {
         if (i == n - 1 && j == n - 1) {
-            ans.push_back(s);
+            ans.push_back(move);
             return;
         }
-        mat[i][j] = 1 - mat[i][j];
-        solve(n, mat, i-1, j, s + "U");
-        solve(n, mat, i+1, j, s + "D");
-        solve(n, mat, i, j-1, s + "L");
-        solve(n, mat, i, j+1, s + "R");
-        mat[i][j] = 1 - mat[i][j];
+        
+        string dir = "DLRU";
+        for (int ind = 0; ind < 4; ind++) {
+            int nexti = i + di[ind];
+            int nextj = j + dj[ind];
+            
+            if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
+                vis[i][j] = 1;
+                solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
+                vis[i][j] = 0;
+            }
+        }
     }
-    vector<string> findPath(vector<vector<int>> &mat) {
+    
+    vector<string> findPath(vector<vector<int>> &m) {
         // Your code goes here
-        solve(mat.size(), mat, 0, 0, "");
+        vector<string> ans;
+        int n = m.size();
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        int di[] = {+1, 0, 0, -1};
+        int dj[] = {0, -1, 1, 0};
+        
+        if (m[0][0] == 1) {
+            solve(0, 0, m, n, ans, "", vis, di, dj);
+        }
+        
         return ans;
     }
 };
@@ -57,6 +69,9 @@ int main() {
             for (int i = 0; i < result.size(); i++)
                 cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
