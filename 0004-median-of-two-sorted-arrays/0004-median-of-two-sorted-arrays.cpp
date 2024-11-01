53677c716125f6c646aa3a8ff6176fab9212d3a7
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-01 11:57:25 +0530

    Time: 0 ms (100%), Space: 94.4 MB (99.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<int> merged;
-        int i = 0, j = 0;
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            
 
-        while (i < m && j < n) 
-        {
-            if (nums1[i] <= nums2[j]) 
-            {
-                merged.push_back(nums1[i]);
-                i++;
-            } else 
-            {
-                merged.push_back(nums2[j]);
-                j++;
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) {
+                    return max(l1, l2);
+                } else {
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
             }
         }
-        while (i < m) 
-        {
-            merged.push_back(nums1[i]);
-            i++;
-        }
-        while (j < n) 
-        {
-            merged.push_back(nums2[j]);
-            j++;
-        }
-        int len = merged.size();
-        if (len % 2 == 0) 
-        {
-            return (static_cast<double>(merged[len / 2 - 1]) + static_cast<double>(merged[len / 2])) / 2.0;
-        }
-        else 
-        {
-            return static_cast<double>(merged[len / 2]);
-        }
+        return 0;
     }
 };
