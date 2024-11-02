906e0e51782510d07c60fbe8119cc2806c6039fb
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-02 08:09:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -10,33 +10,30 @@ class Solution {
     int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
         // code here
         int m = arr1.size(), n = arr2.size();
-        int ans = 0;
-        int i = 0, j = 0;
-
-        while (i < m && j < n && k > 0) {
-            if (arr1[i] <= arr2[j]) {
-                ans = arr1[i];
-                i++;
+        
+        if (m > n) return kthElement(k, arr2, arr1);
+        
+        int low = max(0, k - n), high = min(k, m);
+        
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = k - mid1;
+            
+            int l1 = (mid1 > 0) ? arr1[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? arr2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < m) ? arr1[mid1] : INT_MAX;
+            int r2 = (mid2 < n) ? arr2[mid2] : INT_MAX;
+            
+            if (l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            } else if (l1 > r2) {
+                high = mid1 - 1;
             } else {
-                ans = arr2[j];
-                j++;
+                low = mid1 + 1;
             }
-            k--;
         }
-
-        while (i < m && k > 0) {
-            ans = arr1[i];
-            i++;
-            k--;
-        }
-
-        while (j < n && k > 0) {
-            ans = arr2[j];
-            j++;
-            k--;
-        }
-
-        return ans;
+        
+        return 0; 
     }
 };
 
@@ -69,7 +66,7 @@ int main() {
         }
 
         Solution ob;
-        cout << ob.kthElement(k, arr1, arr2) << endl;
+        cout << ob.kthElement(k, arr1, arr2) << endl << "~\n";
     }
     return 0;
 }
