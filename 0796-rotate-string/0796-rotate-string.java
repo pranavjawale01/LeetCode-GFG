3e3b110739786eaed22db72c9762735725f2736e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-03 09:58:27 +0530

    Time: 0 ms (100%), Space: 41.5 MB (27.67%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         int m = s.length(), n = goal.length();
-        if (m != n) {
-            return false;
-        }
-        for (int i = 1; i <= m; i++) {
-            s = s.substring(1) + s.charAt(0);
-            if (s.equals(goal)) {
-                return true;
-            }
+        if (m == n && (s + s).contains(goal)) {
+            return true;
         }
         return false;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         int m = s.length(), n = goal.length();
+//         if (m != n) {
+//             return false;
+//         }
+//         for (int i = 1; i <= m; i++) {
+//             s = s.substring(1) + s.charAt(0);
+//             if (s.equals(goal)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
