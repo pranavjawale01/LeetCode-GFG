88d7325c52d616c9b4259fa556c2a849199c8a53
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-06 18:15:45 +0530

    Time: 1 ms (100%), Space: 44.7 MB (21.38%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -1,27 +1,57 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        int n = nums.length;
-        boolean swapped = true;
-        for (int i = 0; i < n; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] <= nums[j + 1]) {
-                    continue;
-                } else {
-                    if (Integer.bitCount(nums[j]) == Integer.bitCount(nums[j + 1])) {
-                        int temp = nums[j];
-                        nums[j] = nums[j + 1];
-                        nums[j + 1] = temp;
-                        swapped = true;
-                    } else {
-                        return false;
-                    }
+        int numOfSetBits = Integer.bitCount(nums[0]);
+        int maxOfSegment = nums[0];
+        int minOfSegment = nums[0];
+        int maxOfPrevSegment = Integer.MIN_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (Integer.bitCount(nums[i]) == numOfSetBits) {
+                maxOfSegment = Math.max(maxOfSegment, nums[i]);
+                minOfSegment = Math.min(minOfSegment, nums[i]);
+            } else {
+                if (minOfSegment < maxOfPrevSegment) {
+                    return false;
                 }
-            }
-            if (!swapped) {
-                break;
+                maxOfPrevSegment = maxOfSegment;
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
+                numOfSetBits = Integer.bitCount(nums[i]);
             }
         }
-        return true;
+        
+        return minOfSegment >= maxOfPrevSegment;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public boolean canSortArray(int[] nums) {
+//         int n = nums.length;
+//         boolean swapped = true;
+//         for (int i = 0; i < n; i++) {
+//             swapped = false;
+//             for (int j = 0; j < n - i - 1; j++) {
+//                 if (nums[j] <= nums[j + 1]) {
+//                     continue;
+//                 } else {
+//                     if (Integer.bitCount(nums[j]) == Integer.bitCount(nums[j + 1])) {
+//                         int temp = nums[j];
+//                         nums[j] = nums[j + 1];
+//                         nums[j + 1] = temp;
+//                         swapped = true;
+//                     } else {
+//                         return false;
+//                     }
+//                 }
+//             }
+//             if (!swapped) {
+//                 break;
+//             }
+//         }
+//         return true;
+//     }
+// }
