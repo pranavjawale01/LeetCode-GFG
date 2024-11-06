7aac05476c9d253e38c3174c86cfb47d7a3c89f4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-06 18:15:51 +0530

    Time: 11 ms (57.27%), Space: 16.5 MB (78.85%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,18 +1,43 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        n = len(nums)
-        swapped = True
-        for i in range(n):
-            swapped = False
-            for j in range(n - i - 1):
-                if nums[j] <= nums[j + 1]:
-                    continue
-                else:
-                    if bin(nums[j]).count('1') == bin(nums[j + 1]).count('1'):
-                        nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                        swapped = True
-                    else:
-                        return False
-            if not swapped:
-                break
-        return True
+        numOfSetBits = bin(nums[0]).count('1')
+        maxOfSegment = nums[0]
+        minOfSegment = nums[0]
+        maxOfPrevSegment = float('-inf')
+
+        for i in range(1, len(nums)):
+            if bin(nums[i]).count('1') == numOfSetBits:
+                maxOfSegment = max(maxOfSegment, nums[i])
+                minOfSegment = min(minOfSegment, nums[i])
+            else:
+                if minOfSegment < maxOfPrevSegment:
+                    return False
+                maxOfPrevSegment = maxOfSegment
+                maxOfSegment = nums[i]
+                minOfSegment = nums[i]
+                numOfSetBits = bin(nums[i]).count('1')
+        
+        return minOfSegment >= maxOfPrevSegment
+
+
+
+
+
+# class Solution:
+#     def canSortArray(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         swapped = True
+#         for i in range(n):
+#             swapped = False
+#             for j in range(n - i - 1):
+#                 if nums[j] <= nums[j + 1]:
+#                     continue
+#                 else:
+#                     if bin(nums[j]).count('1') == bin(nums[j + 1]).count('1'):
+#                         nums[j], nums[j + 1] = nums[j + 1], nums[j]
+#                         swapped = True
+#                     else:
+#                         return False
+#             if not swapped:
+#                 break
+#         return True
