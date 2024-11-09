cb594a3919ee6a4d31ff81e21f5b93da4d29fa77
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-09 11:20:51 +0530

    Time: 0 ms (100%), Space: 9.3 MB (17.13%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,10 +1,36 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long num = x;
-        for (int i = 1; i < n; i++) {
-            num = (num + 1) | x;
+        n--;
+        vector<int> xBit(64, 0), nBit(64, 0);
+        for (int i = 0; i < 32; i++) {
+            xBit[i] = (x >> i) & 1;
+            nBit[i] = (n >> i) & 1;
         }
-        return num;
+        for (int i = 0, j = 0; i < 64 && j < 32; i++) {
+            if (xBit[i] == 0) {
+                xBit[i] = nBit[j++];
+            }
+        }
+        long long ans = 0;
+        for (int i = 0; i < 64; i++) {
+            ans |= (static_cast<long long>(xBit[i]) << i);
+        }
+        return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     long long minEnd(int n, int x) {
+//         long long num = x;
+//         for (int i = 1; i < n; i++) {
+//             num = (num + 1) | x;
+//         }
+//         return num;
+//     }
+// };
