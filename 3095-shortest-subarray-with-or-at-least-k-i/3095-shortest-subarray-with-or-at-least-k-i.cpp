f61d296d5ae3b0e7fd3d2015f7667e1229cd86e3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-10 11:59:10 +0530

    Time: 1 ms (25.71%), Space: 26.9 MB (8.06%) - LeetHub

--- 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    int minimumSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans = INT_MAX;
-        int l = 0, orVal = 0;
-        if (k == 0) return 1;
-        for (int i = 0; i < n; i++) {
-            orVal |= nums[i];
-            while (orVal >= k && l < n) {
-                ans = min(ans, i - l + 1);
-                orVal ^= nums[l];
-                l++;
+    void solve1(int val, vector<int> &bitarr, bool isAdd) {
+        int i = 0;
+        while (val > 0) {
+            bitarr[i] = bitarr[i] + (isAdd ? 1 : -1) * val % 2;
+            val /= 2;
+            i++;
+        }
+    }
+    bool solve2(int k, vector<int> &bitarr) {
+        int val = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bitarr[i]) {
+                val |= 1 << i;
             }
         }
-        return (ans == INT_MAX) ? -1 : ans;
+        return val >= k;
+    }
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size(), i = 0, j = 0;
+        int ans = INT_MAX;
+        int sumor = 0;
+        vector<int> bitarr(32, 0);
+        while (j < n) {
+            solve1(nums[j], bitarr, true);
+            while (i <= j && solve2(k, bitarr)) {
+                ans = min(ans, j - i + 1);
+                solve1(nums[i], bitarr, false);
+                i++;
+            }
+            j++;
+        }
+        return ans == INT_MAX ? -1 : ans;
     }
 };
