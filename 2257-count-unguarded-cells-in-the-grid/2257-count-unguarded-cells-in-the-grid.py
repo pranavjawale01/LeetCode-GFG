bf8e73c0d40a63f6c156db5901ff36f77262010a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-21 14:48:17 +0530

    Time: 357 ms (71%), Space: 38.7 MB (68.42%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,30 +1,67 @@
 class Solution:
-    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[0] * n for _ in range(m)]
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    def dfs(self, grid, row, col, rows, cols, direction):
+        if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == 1 or grid[row][col] == 2:
+            return
+        grid[row][col] = 3
+        if direction == 1:
+            self.dfs(grid, row - 1, col, rows, cols, direction)
+        elif direction == 2:
+            self.dfs(grid, row + 1, col, rows, cols, direction)
+        elif direction == 3:
+            self.dfs(grid, row, col - 1, rows, cols, direction)
+        else:
+            self.dfs(grid, row, col + 1, rows, cols, direction)
 
-        for wall in walls:
-            grid[wall[0]][wall[1]] = 1
-
+    def countUnguarded(self, rows: int, cols: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        grid = [[0] * cols for _ in range(rows)]
         for guard in guards:
             grid[guard[0]][guard[1]] = 2
-
+        for wall in walls:
+            grid[wall[0]][wall[1]] = 1
         for guard in guards:
-            x, y = guard
+            guardRow = guard[0]
+            guardCol = guard[1]
+            self.dfs(grid, guardRow - 1, guardCol, rows, cols, 1)
+            self.dfs(grid, guardRow + 1, guardCol, rows, cols, 2)
+            self.dfs(grid, guardRow, guardCol - 1, rows, cols, 3)
+            self.dfs(grid, guardRow, guardCol + 1, rows, cols, 4)
+        unguardedCount = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 0:
+                    unguardedCount += 1
+        return unguardedCount
 
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
 
-                while 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != 1 and grid[nx][ny] != 2:
-                    if grid[nx][ny] == 0:
-                        grid[nx][ny] = 3
-                    nx += dx
-                    ny += dy
 
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    count += 1
 
-        return count
+# class Solution:
+#     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+#         grid = [[0] * n for _ in range(m)]
+#         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+#         for wall in walls:
+#             grid[wall[0]][wall[1]] = 1
+
+#         for guard in guards:
+#             grid[guard[0]][guard[1]] = 2
+
+#         for guard in guards:
+#             x, y = guard
+
+#             for dx, dy in directions:
+#                 nx, ny = x + dx, y + dy
+
+#                 while 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != 1 and grid[nx][ny] != 2:
+#                     if grid[nx][ny] == 0:
+#                         grid[nx][ny] = 3
+#                     nx += dx
+#                     ny += dy
+
+#         count = 0
+#         for i in range(m):
+#             for j in range(n):
+#                 if grid[i][j] == 0:
+#                     count += 1
+
+#         return count
