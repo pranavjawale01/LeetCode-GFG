26ad161a6009e8c1026c2a189fc3e2b97e420aac
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-22 12:20:10 +0530

    Time: 15 ms (85.51%), Space: 74.5 MB (73.51%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -2,18 +2,47 @@ class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        int ans = 0;
-        for (auto &curr : matrix) {
-            vector<int> inv(n);
+        unordered_map<string, int> mp;
+        for (auto &row : matrix) {
+            string temp = "";
+            int val = row[0];
             for (int i = 0; i < n; i++) {
-                inv[i] = curr[i] == 0 ? 1 : 0;
+                temp += (row[i] == val) ? "p" : "j";
             }
-            int count = 0;
-            for (auto &row : matrix) {
-                if (row == curr || row == inv) count++;
-            }
-            ans = max(ans, count);
+            mp[temp]++;
+        }
+        int ans = 0;
+        for (auto &x : mp) {
+            ans = max(ans, x.second);
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+//         int m = matrix.size(), n = matrix[0].size();
+//         int ans = 0;
+//         for (auto &curr : matrix) {
+//             vector<int> inv(n);
+//             for (int i = 0; i < n; i++) {
+//                 inv[i] = curr[i] == 0 ? 1 : 0;
+//             }
+//             int count = 0;
+//             for (auto &row : matrix) {
+//                 if (row == curr || row == inv) count++;
+//             }
+//             ans = max(ans, count);
+//         }
+//         return ans;
+//     }
+// };
