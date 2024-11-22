9b13db5072dd3a4dd5ca207df438e3215a75479c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-22 12:21:17 +0530

    Time: 40 ms (87.04%), Space: 19.3 MB (49.75%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,16 +1,43 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        ans = 0
+        pattern_count = defaultdict(int)
 
-        for curr in matrix:
-            inv = [1 - x for x in curr]
-            count = 0
+        for row in matrix:
+            pattern = []
+            first_value = row[0]
 
-            for row in matrix:
-                if row == curr or row == inv:
-                    count += 1
+            for value in row:
+                pattern.append('p' if value == first_value else 'j')
 
-            ans = max(ans, count)
+            pattern_str = ''.join(pattern)
+            pattern_count[pattern_str] += 1
 
-        return ans
+        return max(pattern_count.values())
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+#         m, n = len(matrix), len(matrix[0])
+#         ans = 0
+
+#         for curr in matrix:
+#             inv = [1 - x for x in curr]
+#             count = 0
+
+#             for row in matrix:
+#                 if row == curr or row == inv:
+#                     count += 1
+
+#             ans = max(ans, count)
+
+#         return ans
