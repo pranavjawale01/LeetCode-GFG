98db5362302ed93d8f114c4d2ca1cd0ac53c398c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-23 21:42:55 +0530

    Time: 0 ms (100%), Space: 7.3 MB (36.36%) - LeetHub

---
+++ 3360-stone-removal-game/3360-stone-removal-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canAliceWin(int n) {
+        int stone = 10;
+        while (n >= 0) {
+            if (n >= stone) {
+                n -= stone;
+                stone--;
+            } else {
+                return false;
+            }
+            if (n >= stone) {
+                n -= stone;
+                stone--;
+            } else {
+                return true;
+            }
+        }
+        return true;
+    }
+};
