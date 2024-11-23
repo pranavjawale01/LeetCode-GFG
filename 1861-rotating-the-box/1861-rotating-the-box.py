178d60f0ec7ce01f0694ba35533143b57d03c86d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-23 12:18:28 +0530

    Time: 1849 ms (92.4%), Space: 29 MB (43.28%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,24 +1,55 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         m, n = len(box), len(box[0])
-        ans = [[''] * m for _ in range(n)]
-
-        for i in range(m):
-            for j in range(n):
-                ans[j][m - 1 - i] = box[i][j]
-
+        result = [[''] * m for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                result[i][j] = box[j][i]
+        
+        for row in result:
+            row.reverse()
+        
         for j in range(m):
+            spaceBottomRow = n - 1
             for i in range(n - 1, -1, -1):
-                if ans[i][j] == '.':
-                    stone_row = -1
-                    for k in range(i - 1, -1, -1):
-                        if ans[k][j] == '*':
-                            break
-                        if ans[k][j] == '#':
-                            stone_row = k
-                            break
-                    if stone_row != -1:
-                        ans[i][j] = '#'
-                        ans[stone_row][j] = '.'
+                if result[i][j] == '*':
+                    spaceBottomRow = i - 1
+                elif result[i][j] == '#':
+                    result[i][j] = '.'
+                    result[spaceBottomRow][j] = '#'
+                    spaceBottomRow -= 1
+        
+        return result
 
-        return ans
+
+
+
+
+
+
+
+# class Solution:
+#     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+#         m, n = len(box), len(box[0])
+#         ans = [[''] * m for _ in range(n)]
+
+#         for i in range(m):
+#             for j in range(n):
+#                 ans[j][m - 1 - i] = box[i][j]
+
+#         for j in range(m):
+#             for i in range(n - 1, -1, -1):
+#                 if ans[i][j] == '.':
+#                     stone_row = -1
+#                     for k in range(i - 1, -1, -1):
+#                         if ans[k][j] == '*':
+#                             break
+#                         if ans[k][j] == '#':
+#                             stone_row = k
+#                             break
+#                     if stone_row != -1:
+#                         ans[i][j] = '#'
+#                         ans[stone_row][j] = '.'
+
+#         return ans
