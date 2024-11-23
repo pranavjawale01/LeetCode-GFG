82dea6778140060376afea875bf07d81943c99bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-11-23 10:52:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
@@ -1,45 +1,54 @@
 //{ Driver Code Starts
-//Initial template for C++
-
+// Initial template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function Template for C++
 
-class Solution{   
-public:
-    int getMinDiff(int arr[], int n, int k) {
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int k, vector<int> &arr) {
         // code here
-        sort(arr,arr+n);
-        int maxi = arr[n-1];
-        int mini = arr[0];
-        int res = maxi - mini;
-        
-        for(int i = 1; i < n; i++){
-            mini = min(arr[0] + k, arr[i] - k);
-            maxi = max(arr[n-1] - k,arr[i-1] + k);
-            res = min(res, maxi - mini);
+        sort(arr.begin(), arr.end());
+        int ans = arr.back() - arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            int maxEl = max(arr.back() - k, arr[i-1] + k);
+            int minEl = min(arr[0] + k, arr[i] - k);
+            ans = min(ans, maxEl - minEl);
         }
-        return res;
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
 int main() {
     int t;
+
     cin >> t;
+    cin.ignore();
+
     while (t--) {
-        int n, k;
+        int k;
         cin >> k;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
+        int n = arr.size();
         Solution ob;
-        auto ans = ob.getMinDiff(arr, n, k);
-        cout << ans << "\n";
+        int res = ob.getMinDiff(k, arr);
+        cout << res;
+
+        cout << "\n";
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
