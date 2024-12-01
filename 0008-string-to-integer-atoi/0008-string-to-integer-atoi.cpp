28b4b5e98ead413ca4cda37301d6310551507581
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-01 09:34:52 +0530

    Time: 0 ms (100%), Space: 9 MB (67.52%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int len = s.size();
-        if(len==0)
-        {
-            return 0;
-        }
-        int i = 0, sign = 1, result = 0;
-        while (i < len && s[i] == ' ') 
-        {
+    int myAtoi(string s) {
+        int n = s.size(), i = 0;
+        while (i < n && s[i] == ' ') i++;
+        bool neg = false;
+        if (i < n && s[i] == '-') {
+            neg = true;
+            i++;
+        } else if (i < n && s[i] == '+') {
             i++;
         }
-        if (i < len && (s[i] == '+' || s[i] == '-')) 
-        {
-            sign = (s[i] == '-') ? -1 : 1;
-            i++;
-        }
-        while (i < len && isdigit(s[i])) {
+        int ans = 0;
+        while (i < n && s[i] >= '0' && s[i] <='9') {
             int digit = s[i] - '0';
-            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) 
-            {
-                return (sign == 1) ? INT_MAX : INT_MIN;
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                return neg ? INT_MIN : INT_MAX;
             }
-            result = result * 10 + digit;
+            ans = ans * 10 + digit;
             i++;
         }
-        return result * sign;
+        return neg ? -ans : ans;
     }
 };
