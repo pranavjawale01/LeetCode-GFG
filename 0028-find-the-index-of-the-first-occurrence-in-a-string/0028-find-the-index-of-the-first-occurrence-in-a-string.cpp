31b35b8c954845b5e19e6682d578a1b22c4960e2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-03 16:02:38 +0530

    Time: 4 ms (6.06%), Space: 7.4 MB (100%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n = haystack.size(), m = needle.size();
-        for (int i = 0; i <= n - m; i++) {
-            bool flag = true;
-            for (int j = 0; j < m; j++) {
-                if (haystack[j+i] != needle[j]) {
-                    flag = false;
-                    break;
-                }
+        int i = 0, j = 0, count = 0;
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i < m; i++) {
+            if (haystack[i] != needle[j]) {
+                j = 0;
+                i = count;
+                count++;
+            } else {
+                j++;
             }
-            if (flag) {
-                return i;
+            if (j == n) {
+                return count;
             }
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n = haystack.size(), m = needle.size();
+//         for (int i = 0; i <= n - m; i++) {
+//             bool flag = true;
+//             for (int j = 0; j < m; j++) {
+//                 if (haystack[j+i] != needle[j]) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
