2648007bb3b47a26086de72c7808a155a799a8a4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-03 16:04:32 +0530

    Time: 2 ms (11.65%), Space: 17.5 MB (6.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,8 +1,34 @@
 class Solution:
+    MOD = int(1e9 + 7)
+
+    def hashValue(self, s: str, RADIX: int, m: int) -> int:
+        ans = 0
+        factor = 1
+        for i in range(m - 1, -1, -1):
+            ans = (ans + ((ord(s[i]) - ord('a')) * factor) % self.MOD) % self.MOD
+            factor = (factor * RADIX) % self.MOD
+        return ans % self.MOD
+
     def strStr(self, haystack: str, needle: str) -> int:
-        h, n = len(haystack), len(needle)
-        hash_n = hash(needle)
-        for i in range(h-n+1):
-            if hash(haystack[i:i+n]) == hash_n:
-                return i
+        n, m = len(haystack), len(needle)
+        if n < m:
+            return -1
+
+        RADIX = 26
+        MAX_WEIGHT = 1
+        for _ in range(m):
+            MAX_WEIGHT = (MAX_WEIGHT * RADIX) % self.MOD
+
+        hashNeedle = self.hashValue(needle, RADIX, m)
+        hashHay = 0
+
+        for i in range(n - m + 1):
+            if i == 0:
+                hashHay = self.hashValue(haystack, RADIX, m)
+            else:
+                hashHay = ((hashHay * RADIX) % self.MOD - ((ord(haystack[i - 1]) - ord('a')) * MAX_WEIGHT) % self.MOD + (ord(haystack[i + m - 1]) - ord('a')) + self.MOD) % self.MOD
+            
+            if hashNeedle == hashHay:
+                if haystack[i:i + m] == needle:
+                    return i
         return -1
