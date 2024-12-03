06cd7fb50395c366c496bb40640f2bf62afd698b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-03 16:03:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (8.23%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
+    #define ll long long
+    const ll MOD = 1e9 + 7;
+    // Rabin Karp Algorithm
+    ll hashValue(string &s, ll RADIX, ll m) {
+        ll ans = 0, factor = 1;
+        for (ll i = m - 1; i >= 0; i--) {
+            ans = (ans + ((s[i] - 'a') * factor) % MOD) % MOD;
+            factor = (factor * RADIX) % MOD;
+        }
+        return ans % MOD;
+    }
+
     int strStr(string haystack, string needle) {
-        int i = 0, j = 0, count = 0;
-        int m = haystack.size(), n = needle.size();
-        for (int i = 0; i < m; i++) {
-            if (haystack[i] != needle[j]) {
-                j = 0;
-                i = count;
-                count++;
+        ll n = haystack.length(), m = needle.length();
+        if (n < m) return -1;
+        ll RADIX = 26, MAX_WEIGHT = 1;
+        for (ll i = 1; i <= m; i++) {
+            MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD;
+        }
+
+        ll hashNeedle = hashValue(needle, RADIX, m), hashHay = 0;
+        for (ll i = 0; i <= n - m; i++) {
+            if (i == 0) {
+                hashHay = hashValue(haystack, RADIX, m);
             } else {
-                j++;
+                hashHay = ((hashHay * RADIX) % MOD - ((haystack[i - 1] - 'a') * MAX_WEIGHT) % MOD + (haystack[i + m - 1] - 'a') + MOD) % MOD;
             }
-            if (j == n) {
-                return count;
+            if (hashNeedle == hashHay) {
+                for (ll j = 0; j < m; j++) {
+                    if (needle[j] != haystack[j + i]) break;
+                    if (j == m - 1) return i;
+                }
             }
         }
         return -1;
     }
 };
-
-// class Solution {
-// public:
-//     int strStr(string haystack, string needle) {
-//         int n = haystack.size(), m = needle.size();
-//         for (int i = 0; i <= n - m; i++) {
-//             bool flag = true;
-//             for (int j = 0; j < m; j++) {
-//                 if (haystack[j+i] != needle[j]) {
-//                     flag = false;
-//                     break;
-//                 }
-//             }
-//             if (flag) {
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// };
