7b5049a9400a8a51e026c9922ff137118ae1df01
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-07 09:42:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int mergeAndCount(vector<int>& arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        vector<int> leftArr(n1), rightArr(n2);
+        
+        for (int i = 0; i < n1; ++i) leftArr[i] = arr[left + i];
+        for (int i = 0; i < n2; ++i) rightArr[i] = arr[mid + 1 + i];
+        
+        int i = 0, j = 0, k = left, invCount = 0;
+        
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                invCount += (n1 - i);
+            }
+        }
+        
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+        
+        return invCount;
+    }
+    
+    int mergeSortAndCount(vector<int>& arr, int left, int right) {
+        int invCount = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            invCount += mergeSortAndCount(arr, left, mid);
+            invCount += mergeSortAndCount(arr, mid + 1, right);
+            invCount += mergeAndCount(arr, left, mid, right);
+        }
+        return invCount;
+    }
+    
+    int inversionCount(vector<int>& arr) {
+        return mergeSortAndCount(arr, 0, arr.size() - 1);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n;
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+        Solution obj;
+        cout << obj.inversionCount(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
