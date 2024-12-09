f3feaef505ad93d638a772937cb8a65cb43a53fd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-09 16:09:21 +0530

    Time: 3 ms (86.83%), Space: 98.5 MB (47.92%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.java
+++ 3152-special-array-ii/3152-special-array-ii.java
@@ -27,3 +27,36 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+//         int n = nums.length;
+//         int m = queries.length;
+
+//         int[] cumSum = new int[n];
+//         cumSum[0] = 0;
+
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] % 2 == nums[i - 1] % 2) {
+//                 cumSum[i] = cumSum[i - 1] + 1;
+//             } else {
+//                 cumSum[i] = cumSum[i - 1];
+//             }
+//         }
+
+//         boolean[] result = new boolean[m];
+//         int i = 0;
+//         for (int[] query : queries) {
+//             int start = query[0];
+//             int end = query[1];
+
+//             result[i] = cumSum[end] - cumSum[start] == 0;
+//             i++;
+//         }
+
+//         return result;
+//     }
+// }
