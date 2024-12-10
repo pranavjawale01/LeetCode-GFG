0c11258f4d141bd464338e3f398e905fcbb4a709
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-10 11:13:30 +0530

    Time: 11 ms (63.29%), Space: 15.2 MB (28.06%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,21 +2,56 @@ class Solution {
 public:
     int maximumLength(string s) {
         int n = s.size();
-        map<pair<char, int>, int> mp;
-        int maxlen = -1;
-        for (int i = 0; i < n; i++) {
-            int len = 0;
-            char curr = s[i];
-            for (int j = i; j < n && s[j] == curr; j++) {
+        vector<vector<int>> freq(26, vector<int>(n + 1, 0));
+        for (int i = 0, len = 1; i < n; i++) {
+            char c = s[i];
+            if (i > 0 && s[i] == s[i - 1]) {
                 len++;
-                mp[{curr, len}]++;
+            } else {
+                len = 1;
+            }
+            freq[c - 'a'][len]++;
+        }
+        int ans = 0;
+        for (int ch = 0; ch < 26; ch++) {
+            for (int len = n; len > 0; len--) {
+                if (len < n) {
+                    freq[ch][len] += freq[ch][len + 1];
+                }
+                if (freq[ch][len] >= 3) {
+                    ans = max(ans, len);
+                }
             }
         }
-        for (auto &x : mp) {
-            if (x.second >= 3) {
-                maxlen = max(maxlen, x.first.second);
-            }
-        }
-        return maxlen == 0 ? -1 : maxlen;
+        return ans == 0 ? -1 : ans;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maximumLength(string s) {
+//         int n = s.size();
+//         map<pair<char, int>, int> mp;
+//         int maxlen = -1;
+//         for (int i = 0; i < n; i++) {
+//             int len = 0;
+//             char curr = s[i];
+//             for (int j = i; j < n && s[j] == curr; j++) {
+//                 len++;
+//                 mp[{curr, len}]++;
+//             }
+//         }
+//         for (auto &x : mp) {
+//             if (x.second >= 3) {
+//                 maxlen = max(maxlen, x.first.second);
+//             }
+//         }
+//         return maxlen == 0 ? -1 : maxlen;
+//     }
+// };
