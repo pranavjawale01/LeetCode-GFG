eb7e734971cb7bc11d535f247e22235763ad937b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-11 19:25:58 +0530

    Time: 247 ms (39.31%), Space: 28 MB (45.21%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,12 +1,17 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
+        n = len(nums)
+        i, j = 0, 0
         max_beauty = 0
-        for i in range(len(nums)):
-            upper_bound = bisect_right(nums, nums[i] + 2 * k)
-            max_beauty = max(max_beauty, upper_bound - i)
-        return max_beauty
 
+        while i < n:
+            while j < n and nums[j] - nums[i] <= 2 * k:
+                j += 1
+            max_beauty = max(max_beauty, j - i)
+            i += 1
+
+        return max_beauty
 
 
 
@@ -14,18 +19,32 @@ class Solution:
 
 # class Solution:
 #     def maximumBeauty(self, nums: List[int], k: int) -> int:
-#         ranges = []
-#         for x in nums:
-#             ranges.append((x - k, x + k))
-#         ranges.sort()
-
+#         nums.sort()
 #         max_beauty = 0
-#         dq = deque()
-
-#         for start, end in ranges:
-#             while dq and dq[0] < start:
-#                 dq.popleft()
-#             dq.append(end)
-#             max_beauty = max(max_beauty, len(dq))
-
+#         for i in range(len(nums)):
+#             upper_bound = bisect_right(nums, nums[i] + 2 * k)
+#             max_beauty = max(max_beauty, upper_bound - i)
 #         return max_beauty
+
+
+
+
+
+
+# # class Solution:
+# #     def maximumBeauty(self, nums: List[int], k: int) -> int:
+# #         ranges = []
+# #         for x in nums:
+# #             ranges.append((x - k, x + k))
+# #         ranges.sort()
+
+# #         max_beauty = 0
+# #         dq = deque()
+
+# #         for start, end in ranges:
+# #             while dq and dq[0] < start:
+# #                 dq.popleft()
+# #             dq.append(end)
+# #             max_beauty = max(max_beauty, len(dq))
+
+# #         return max_beauty
