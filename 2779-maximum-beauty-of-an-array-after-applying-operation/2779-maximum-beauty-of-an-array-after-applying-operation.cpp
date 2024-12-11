9d71aaac984598bf27105b14e455fe1df02405f5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-11 19:24:55 +0530

    Time: 43 ms (84.29%), Space: 105 MB (67.7%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
+        
+        int i = 0;
+        int j = 0;
         int maxBeauty = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            auto ub = upper_bound(nums.begin(), nums.end(), nums[i] + 2 * k);
-            maxBeauty = max(maxBeauty, int(ub - nums.begin()) - i);
+
+        while (i < n) {
+
+            while (j < n && nums[j] - nums[i] <= 2 * k) {
+                j++;
+            }
+
+            maxBeauty = max(maxBeauty, j - i);
+
+            i++;
         }
+
         return maxBeauty;
     }
 };
@@ -16,23 +28,43 @@ public:
 
 
 
+
+
 // class Solution {
 // public:
 //     int maximumBeauty(vector<int>& nums, int k) {
-//         vector<pair<int, int>> ranges;
-//         for (int x : nums) {
-//             ranges.push_back({x-k, x+k});
-//         }
-//         sort(ranges.begin(), ranges.end());
+//         sort(nums.begin(), nums.end());
 //         int maxBeauty = 0;
-//         deque<int> dq;
-//         for (auto &x : ranges) {
-//             while (!dq.empty() && dq.front() < x.first) {
-//                 dq.pop_front();
-//             }
-//             dq.push_back(x.second);
-//             maxBeauty = max(maxBeauty, (int)dq.size());
+//         for (int i = 0; i < nums.size(); i++) {
+//             auto ub = upper_bound(nums.begin(), nums.end(), nums[i] + 2 * k);
+//             maxBeauty = max(maxBeauty, int(ub - nums.begin()) - i);
 //         }
 //         return maxBeauty;
 //     }
 // };
+
+
+
+
+
+
+// // class Solution {
+// // public:
+// //     int maximumBeauty(vector<int>& nums, int k) {
+// //         vector<pair<int, int>> ranges;
+// //         for (int x : nums) {
+// //             ranges.push_back({x-k, x+k});
+// //         }
+// //         sort(ranges.begin(), ranges.end());
+// //         int maxBeauty = 0;
+// //         deque<int> dq;
+// //         for (auto &x : ranges) {
+// //             while (!dq.empty() && dq.front() < x.first) {
+// //                 dq.pop_front();
+// //             }
+// //             dq.push_back(x.second);
+// //             maxBeauty = max(maxBeauty, (int)dq.size());
+// //         }
+// //         return maxBeauty;
+// //     }
+// // };
