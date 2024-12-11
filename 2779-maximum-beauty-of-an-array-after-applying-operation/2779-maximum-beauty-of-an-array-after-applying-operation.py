a67eccef51dd2ff9744a8c881b6ecf09bf046a95
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-11 19:24:21 +0530

    Time: 281 ms (32.48%), Space: 28 MB (45.21%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,17 +1,31 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        ranges = []
-        for x in nums:
-            ranges.append((x - k, x + k))
-        ranges.sort()
-
+        nums.sort()
         max_beauty = 0
-        dq = deque()
-
-        for start, end in ranges:
-            while dq and dq[0] < start:
-                dq.popleft()
-            dq.append(end)
-            max_beauty = max(max_beauty, len(dq))
-
+        for i in range(len(nums)):
+            upper_bound = bisect_right(nums, nums[i] + 2 * k)
+            max_beauty = max(max_beauty, upper_bound - i)
         return max_beauty
+
+
+
+
+
+
+# class Solution:
+#     def maximumBeauty(self, nums: List[int], k: int) -> int:
+#         ranges = []
+#         for x in nums:
+#             ranges.append((x - k, x + k))
+#         ranges.sort()
+
+#         max_beauty = 0
+#         dq = deque()
+
+#         for start, end in ranges:
+#             while dq and dq[0] < start:
+#                 dq.popleft()
+#             dq.append(end)
+#             max_beauty = max(max_beauty, len(dq))
+
+#         return max_beauty
