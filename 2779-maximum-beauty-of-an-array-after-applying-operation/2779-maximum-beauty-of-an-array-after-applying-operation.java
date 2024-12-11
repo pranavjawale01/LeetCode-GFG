1d32995c2525c38302292aba719e2e9a8512bc58
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-11 19:25:50 +0530

    Time: 39 ms (75.27%), Space: 60.3 MB (50%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,25 +1,18 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
-        int maxBeauty = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int upperBound = findUpperBound(nums, nums[i] + 2 * k);
-            maxBeauty = Math.max(maxBeauty, upperBound - i);
-        }
-        return maxBeauty;
-    }
+        int n = nums.length;
+        int i = 0, j = 0, maxBeauty = 0;
 
-    private int findUpperBound(int[] nums, int target) {
-        int low = 0, high = nums.length;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] <= target) {
-                low = mid + 1;
-            } else {
-                high = mid;
+        while (i < n) {
+            while (j < n && nums[j] - nums[i] <= 2 * k) {
+                j++;
             }
+            maxBeauty = Math.max(maxBeauty, j - i);
+            i++;
         }
-        return low;
+
+        return maxBeauty;
     }
 }
 
@@ -27,28 +20,57 @@ class Solution {
 
 
 
-
-
-
 // class Solution {
 //     public int maximumBeauty(int[] nums, int k) {
-//         List<int[]> ranges = new ArrayList<>();
-//         for (int x : nums) {
-//             ranges.add(new int[]{x - k, x + k});
-//         }
-//         ranges.sort((a, b) -> Integer.compare(a[0], b[0]));
-
+//         Arrays.sort(nums);
 //         int maxBeauty = 0;
-//         Deque<Integer> dq = new LinkedList<>();
-
-//         for (int[] range : ranges) {
-//             while (!dq.isEmpty() && dq.peekFirst() < range[0]) {
-//                 dq.pollFirst();
-//             }
-//             dq.addLast(range[1]);
-//             maxBeauty = Math.max(maxBeauty, dq.size());
+//         for (int i = 0; i < nums.length; i++) {
+//             int upperBound = findUpperBound(nums, nums[i] + 2 * k);
+//             maxBeauty = Math.max(maxBeauty, upperBound - i);
 //         }
-
 //         return maxBeauty;
 //     }
+
+//     private int findUpperBound(int[] nums, int target) {
+//         int low = 0, high = nums.length;
+//         while (low < high) {
+//             int mid = low + (high - low) / 2;
+//             if (nums[mid] <= target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid;
+//             }
+//         }
+//         return low;
+//     }
 // }
+
+
+
+
+
+
+
+
+// // class Solution {
+// //     public int maximumBeauty(int[] nums, int k) {
+// //         List<int[]> ranges = new ArrayList<>();
+// //         for (int x : nums) {
+// //             ranges.add(new int[]{x - k, x + k});
+// //         }
+// //         ranges.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+// //         int maxBeauty = 0;
+// //         Deque<Integer> dq = new LinkedList<>();
+
+// //         for (int[] range : ranges) {
+// //             while (!dq.isEmpty() && dq.peekFirst() < range[0]) {
+// //                 dq.pollFirst();
+// //             }
+// //             dq.addLast(range[1]);
+// //             maxBeauty = Math.max(maxBeauty, dq.size());
+// //         }
+
+// //         return maxBeauty;
+// //     }
+// // }
