a5a6abe10647420813bbd107b41b11b2fb217988
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-11 19:23:15 +0530

    Time: 86 ms (24.18%), Space: 60.4 MB (35.64%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,22 +1,54 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        List<int[]> ranges = new ArrayList<>();
-        for (int x : nums) {
-            ranges.add(new int[]{x - k, x + k});
-        }
-        ranges.sort((a, b) -> Integer.compare(a[0], b[0]));
-
+        Arrays.sort(nums);
         int maxBeauty = 0;
-        Deque<Integer> dq = new LinkedList<>();
-
-        for (int[] range : ranges) {
-            while (!dq.isEmpty() && dq.peekFirst() < range[0]) {
-                dq.pollFirst();
-            }
-            dq.addLast(range[1]);
-            maxBeauty = Math.max(maxBeauty, dq.size());
+        for (int i = 0; i < nums.length; i++) {
+            int upperBound = findUpperBound(nums, nums[i] + 2 * k);
+            maxBeauty = Math.max(maxBeauty, upperBound - i);
         }
-
         return maxBeauty;
     }
+
+    private int findUpperBound(int[] nums, int target) {
+        int low = 0, high = nums.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maximumBeauty(int[] nums, int k) {
+//         List<int[]> ranges = new ArrayList<>();
+//         for (int x : nums) {
+//             ranges.add(new int[]{x - k, x + k});
+//         }
+//         ranges.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+//         int maxBeauty = 0;
+//         Deque<Integer> dq = new LinkedList<>();
+
+//         for (int[] range : ranges) {
+//             while (!dq.isEmpty() && dq.peekFirst() < range[0]) {
+//                 dq.pollFirst();
+//             }
+//             dq.addLast(range[1]);
+//             maxBeauty = Math.max(maxBeauty, dq.size());
+//         }
+
+//         return maxBeauty;
+//     }
+// }
