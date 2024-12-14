71d306d5bb3ea9a95345ae27b6a98e0e908b0e58
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-14 12:15:59 +0530

    Time: 837 ms (21.69%), Space: 44.1 MB (10.89%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.py
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -1,15 +1,39 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        count_map = Counter()
+        minpq = []
+        maxpq = []
         i = 0
         count = 0
         for j in range(n):
-            count_map[nums[j]] += 1
-            while max(count_map) - min(count_map) > 2:
-                count_map[nums[i]] -= 1
-                if count_map[nums[i]] == 0:
-                    del count_map[nums[i]]
+            heapq.heappush(minpq, (nums[j], j))
+            heapq.heappush(maxpq, (-nums[j], j))
+            while abs(-maxpq[0][0] - minpq[0][0]) > 2:
                 i += 1
+                while maxpq and maxpq[0][1] < i:
+                    heapq.heappop(maxpq)
+                while minpq and minpq[0][1] < i:
+                    heapq.heappop(minpq)
             count += j - i + 1
         return count
+
+
+
+
+
+
+# class Solution:
+#     def continuousSubarrays(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         count_map = Counter()
+#         i = 0
+#         count = 0
+#         for j in range(n):
+#             count_map[nums[j]] += 1
+#             while max(count_map) - min(count_map) > 2:
+#                 count_map[nums[i]] -= 1
+#                 if count_map[nums[i]] == 0:
+#                     del count_map[nums[i]]
+#                 i += 1
+#             count += j - i + 1
+#         return count
