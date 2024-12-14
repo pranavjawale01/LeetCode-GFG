cb2a6e26581600b52008cff970af1950dbdc0dde
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-14 12:14:50 +0530

    Time: 153 ms (47.31%), Space: 129.5 MB (39.5%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    #define p pair<int, int>
     long long continuousSubarrays(vector<int>& nums) {
         int n = nums.size();
-        map<int, int> mp;
+        priority_queue<p, vector<p>, greater<p>> minpq;
+        priority_queue<p> maxpq;
         int i = 0, j = 0;
         long long count = 0;
         while (j < n) {
-            mp[nums[j]]++;
-            while (abs(mp.rbegin()->first - mp.begin()->first) > 2) {
-                mp[nums[i]] -= 1;
-                if (mp[nums[i]] == 0) {
-                    mp.erase(nums[i]);
-                }
+            minpq.push({nums[j], j});
+            maxpq.push({nums[j], j});
+            while (abs(maxpq.top().first - minpq.top().first) > 2) {
                 i++;
+                while (!maxpq.empty() && maxpq.top().second < i) {
+                    maxpq.pop();
+                }
+                while (!minpq.empty() && minpq.top().second < i) {
+                    minpq.pop();
+                }
             }
             count += j - i + 1;
             j++;
@@ -20,3 +25,34 @@ public:
         return count;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     long long continuousSubarrays(vector<int>& nums) {
+//         int n = nums.size();
+//         map<int, int> mp;
+//         int i = 0, j = 0;
+//         long long count = 0;
+//         while (j < n) {
+//             mp[nums[j]]++;
+//             while (abs(mp.rbegin()->first - mp.begin()->first) > 2) {
+//                 mp[nums[i]] -= 1;
+//                 if (mp[nums[i]] == 0) {
+//                     mp.erase(nums[i]);
+//                 }
+//                 i++;
+//             }
+//             count += j - i + 1;
+//             j++;
+//         }
+//         return count;
+//     }
+// };
