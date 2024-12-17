f9e817995a7632916d4520bbdb4eba8644c85224
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-17 12:04:24 +0530

    Time: 31 ms (64.41%), Space: 45.9 MB (27.5%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
@@ -5,37 +5,86 @@ class Solution {
             count[ch - 'a']++;
         }
 
-        StringBuilder result = new StringBuilder();
-        int i = 25;
-
-        while (i >= 0) {
-            if (count[i] == 0) {
-                i--;
-                continue;
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                pq.add((char) ('a' + i));
             }
+        }
 
-            char ch = (char) ('a' + i);
-            int freq = Math.min(count[i], repeatLimit);
+        StringBuilder result = new StringBuilder();
+        while (!pq.isEmpty()) {
+            char ch = pq.poll();
 
+            int freq = Math.min(count[ch - 'a'], repeatLimit);
             for (int k = 0; k < freq; k++) {
                 result.append(ch);
             }
-            count[i] -= freq;
+            count[ch - 'a'] -= freq;
 
-            if (count[i] > 0) {
-                int j = i - 1;
-                while (j >= 0 && count[j] == 0) {
-                    j--;
+            if (count[ch - 'a'] > 0 && !pq.isEmpty()) {
+                char nextChar = pq.poll();
+
+                result.append(nextChar);
+                count[nextChar - 'a']--;
+
+                if (count[nextChar - 'a'] > 0) {
+                    pq.add(nextChar);
                 }
-
-                if (j < 0) {
-                    break;
-                }
-
-                result.append((char) ('a' + j));
-                count[j]--;
+                pq.add(ch);
             }
         }
         return result.toString();
     }
 }
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public String repeatLimitedString(String s, int repeatLimit) {
+//         int[] count = new int[26];
+//         for (char ch : s.toCharArray()) {
+//             count[ch - 'a']++;
+//         }
+
+//         StringBuilder result = new StringBuilder();
+//         int i = 25;
+
+//         while (i >= 0) {
+//             if (count[i] == 0) {
+//                 i--;
+//                 continue;
+//             }
+
+//             char ch = (char) ('a' + i);
+//             int freq = Math.min(count[i], repeatLimit);
+
+//             for (int k = 0; k < freq; k++) {
+//                 result.append(ch);
+//             }
+//             count[i] -= freq;
+
+//             if (count[i] > 0) {
+//                 int j = i - 1;
+//                 while (j >= 0 && count[j] == 0) {
+//                     j--;
+//                 }
+
+//                 if (j < 0) {
+//                     break;
+//                 }
+
+//                 result.append((char) ('a' + j));
+//                 count[j]--;
+//             }
+//         }
+//         return result.toString();
+//     }
+// }
