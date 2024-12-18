1f4f2c1fa5d8434487d900f3e216ee26844a636f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-18 22:22:57 +0530

    Time: 0 ms (100%), Space: 14.2 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,14 +2,35 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
+        stack<int> st;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[i] >= prices[j]) {
-                    prices[i] -= prices[j];
-                    break;
-                }
+            while(!st.empty() && prices[i] <= prices[st.top()]) {
+                prices[st.top()] -= prices[i];
+                st.pop();
             }
+            st.push(i);
         }
         return prices;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         int n = prices.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (prices[i] >= prices[j]) {
+//                     prices[i] -= prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return prices;
+//     }
+// };
