5795e1045e9d3afa6bff0c49a5c67e9677e43617
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-18 22:24:51 +0530

    Time: 4 ms (40.47%), Space: 43.9 MB (38.43%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,14 +1,32 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
+        Stack<Integer> st = new Stack<>();
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[i] >= prices[j]) {
-                    prices[i] -= prices[j];
-                    break;
-                }
+            while (!st.isEmpty() && prices[i] <= prices[st.peek()]) {
+                prices[st.peek()] -= prices[i];
+                st.pop();
             }
+            st.push(i);
         }
         return prices;
     }
 }
+
+
+
+
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         int n = prices.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (prices[i] >= prices[j]) {
+//                     prices[i] -= prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return prices;
+//     }
+// }
