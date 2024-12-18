1f0cdb1e78ceedc7060a1de4b3bf26fc08d586b5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-18 22:25:04 +0530

    Time: 3 ms (55.13%), Space: 17.9 MB (10.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,9 +1,23 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         n = len(prices)
+        st = []
         for i in range(n):
-            for j in range(i+1, n):
-                if prices[i] >= prices[j]:
-                    prices[i] -= prices[j]
-                    break
+            while st and prices[i] <= prices[st[-1]]:
+                prices[st[-1]] -= prices[i]
+                st.pop()
+            st.append(i)
         return prices
+
+
+
+
+# class Solution:
+#     def finalPrices(self, prices: List[int]) -> List[int]:
+#         n = len(prices)
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if prices[i] >= prices[j]:
+#                     prices[i] -= prices[j]
+#                     break
+#         return prices
