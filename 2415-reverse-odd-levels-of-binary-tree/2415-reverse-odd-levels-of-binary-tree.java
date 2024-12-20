c93e8238dfbc67b1514c54e5608c60ae10955b62
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-20 18:31:43 +0530

    Time: 10 ms (34.32%), Space: 48.4 MB (61.99%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -14,21 +14,63 @@
  * }
  */
 class Solution {
-    public void solve(TreeNode l, TreeNode r, int lvl) {
-        if (l == null || r == null) {
-            return;
-        }
-        if (lvl % 2 == 1) {
-            int temp = l.val;
-            l.val = r.val;
-            r.val = temp;
-        }
-        solve(l.left, r.right, lvl + 1);
-        solve(l.right, r.left, lvl + 1);
-    }
-
     public TreeNode reverseOddLevels(TreeNode root) {
-        solve(root.left, root.right, 1);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> levelNodes = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelNodes.add(node);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            if (level % 2 == 1) {
+                int left = 0, right = levelNodes.size() - 1;
+                while (left < right) {
+                    int temp = levelNodes.get(left).val;
+                    levelNodes.get(left).val = levelNodes.get(right).val;
+                    levelNodes.get(right).val = temp;
+                    left++;
+                    right--;
+                }
+            }
+            level++;
+        }
         return root;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public void solve(TreeNode l, TreeNode r, int lvl) {
+//         if (l == null || r == null) {
+//             return;
+//         }
+//         if (lvl % 2 == 1) {
+//             int temp = l.val;
+//             l.val = r.val;
+//             r.val = temp;
+//         }
+//         solve(l.left, r.right, lvl + 1);
+//         solve(l.right, r.left, lvl + 1);
+//     }
+
+//     public TreeNode reverseOddLevels(TreeNode root) {
+//         solve(root.left, root.right, 1);
+//         return root;
+//     }
+// }
