c0ab6fa004a99adeca492ad4cd7042ff41e9e0ec
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-20 18:30:52 +0530

    Time: 12 ms (8.97%), Space: 84.2 MB (13.13%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -11,20 +11,62 @@
  */
 class Solution {
 public:
-    void solve(TreeNode *l, TreeNode *r, int lvl) {
-        if (!l || !r) {
-            return;
-        }
-        if (lvl % 2 == 1) {
-            int temp = l->val;
-            l->val = r->val;
-            r->val = temp;
-        }
-        solve(l->left, r->right, lvl + 1);
-        solve(l->right, r->left, lvl + 1);
-    }
     TreeNode* reverseOddLevels(TreeNode* root) {
-        solve(root->left, root->right, 1);
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while (!q.empty()) {
+            int n = q.size();
+            vector<TreeNode*> lvlNodes;
+            while (n--) {
+                TreeNode *temp = q.front();
+                q.pop();
+                lvlNodes.push_back(temp);
+                if (temp->left) {
+                    q.push(temp->left);
+                }
+                if (temp->right) {
+                    q.push(temp->right);
+                }
+            }
+            if (level % 2 == 1) {
+                int l = 0, r = lvlNodes.size() - 1;
+                while (l < r) {
+                    int temp = lvlNodes[l]->val;
+                    lvlNodes[l]->val = lvlNodes[r]->val;
+                    lvlNodes[r]->val = temp;
+                    l++;
+                    r--;
+                }
+            }
+            level++;
+        }
         return root;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void solve(TreeNode *l, TreeNode *r, int lvl) {
+//         if (!l || !r) {
+//             return;
+//         }
+//         if (lvl % 2 == 1) {
+//             int temp = l->val;
+//             l->val = r->val;
+//             r->val = temp;
+//         }
+//         solve(l->left, r->right, lvl + 1);
+//         solve(l->right, r->left, lvl + 1);
+//     }
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+//         solve(root->left, root->right, 1);
+//         return root;
+//     }
+// };
