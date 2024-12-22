8034a26a269eef2b97d35dbee038ee6a2896fd0c
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-22 07:25:24 +0530

    Time: 86 ms (100%), Space: 56.9 MB (100%) - LeetHub

---
+++ 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    private static final int MOD = 1000000007;
+    private int[][][] dp;
+
+    private int solve(int[][] grid, int i, int j, int x, int k) {
+        int n = grid.length, m = grid[0].length;
+        if (i >= n || j >= m) return 0;
+        x ^= grid[i][j];
+        if (dp[i][j][x] != -1) return dp[i][j][x];
+
+        int ans = 0;
+        if (i == n - 1 && j == m - 1 && x == k) {
+            ans = 1;
+        }
+        ans = (ans + solve(grid, i + 1, j, x, k)) % MOD;
+        ans = (ans + solve(grid, i, j + 1, x, k)) % MOD;
+
+        return dp[i][j][x] = ans;
+    }
+
+    public int countPathsWithXorValue(int[][] grid, int k) {
+        int n = grid.length, m = grid[0].length;
+        dp = new int[n][m][16];
+        for (int[][] layer : dp) {
+            for (int[] row : layer) {
+                Arrays.fill(row, -1);
+            }
+        }
+        return solve(grid, 0, 0, 0, k);
+    }
+}
