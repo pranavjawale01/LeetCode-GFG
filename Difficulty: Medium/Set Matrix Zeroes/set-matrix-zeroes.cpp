12f28aa3f9c8165fafbca057b41dfc504328abf9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-25 10:11:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+        bool row = false, col = false;
+        int n = mat.size(), m = mat[0].size();
+        for (int i = 0; i < m; i++) {
+            if (mat[0][i] == 0) {
+                row = true;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] == 0) {
+                col = true;
+                break;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][0] == 0 || mat[0][j] == 0) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        if (row) {
+            for (int i = 0; i < m; i++) mat[0][i] = 0;
+        }
+        if (col) {
+            for (int i = 0; i < n; i++) mat[i][0] = 0;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        ob.setMatrixZeroes(arr);
+        for (auto x : arr) {
+            for (auto y : x)
+                cout << y << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
