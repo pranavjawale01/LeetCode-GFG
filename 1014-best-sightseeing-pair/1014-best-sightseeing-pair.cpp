b1c10e9607a52d0bc730079be672dc4f33d0b262
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-27 12:08:27 +0530

    Time: 0 ms (100%), Space: 47.6 MB (16.09%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -2,14 +2,36 @@ class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
+        vector<int> vec(n, 0);
+        vec[0] = values[0] + 0;
+        for (int i = 1; i < n; i++) {
+            vec[i] = max(values[i] + i, vec[i-1]);
+        }
         int ans = 0;
         for (int j = 1; j < n; j++) {
-            int maxval = 0;
-            for (int i = j - 1; i >= 0; i--) {
-                maxval = max(maxval, values[i] + i);
-            }
-            ans = max(ans, maxval + values[j] - j);
+            int x = values[j] - j;
+            int y = vec[j-1];
+            ans = max(ans, x + y);
         }
         return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int maxScoreSightseeingPair(vector<int>& values) {
+//         int n = values.size();
+//         int ans = 0;
+//         for (int j = 1; j < n; j++) {
+//             int maxval = 0;
+//             for (int i = j - 1; i >= 0; i--) {
+//                 maxval = max(maxval, values[i] + i);
+//             }
+//             ans = max(ans, maxval + values[j] - j);
+//         }
+//         return ans;
+//     }
+// };
