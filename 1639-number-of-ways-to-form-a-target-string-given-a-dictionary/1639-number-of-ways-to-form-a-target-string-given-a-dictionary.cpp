02fa82cf42406e150de2b74f2b555e141848b9bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-29 10:39:31 +0530

    Time: 97 ms (27.98%), Space: 84.5 MB (17.28%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,27 +1,65 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int m, k, MOD = 1e9 + 7;
-
-    int solve(int i, int j, vector<vector<long long>> &freq, string &target) {
-        if (i == m) return 1;
-        if (j == k) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        int take = freq[target[i] - 'a'][j] * solve(i+1, j+1, freq, target) % MOD;
-        int nottake = solve(i, j+1, freq, target) % MOD;
-        return dp[i][j] = (take + nottake) % MOD;
-    }
-
+    int MOD = 1e9 + 7;
     int numWays(vector<string>& words, string target) {
-        k = words[0].size();
-        m = target.size();
+        int k = words[0].size(), m = target.size();
+        
         vector<vector<long long>> freq(26, vector<long long>(k));
-        for (int col = 0; col < k; col++) {
+        
+        for (int i = 0; i < k; i++) {
             for (string &x : words) {
-                freq[x[col] - 'a'][col]++;
+                freq[x[i] - 'a'][i]++;
             }
         }
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, freq, target);
+        
+        vector<vector<long long>> dp(m + 1, vector<long long>(k + 1));
+        dp[0][0] = 1;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j < k) {
+                    dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
+                    if (i < m && j < k) {
+                        dp[i + 1][j + 1] = (dp[i + 1][j + 1] + freq[target[i] - 'a'][j] * dp[i][j]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[m][k];
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int dp[1001][1001];
+//     int m, k, MOD = 1e9 + 7;
+
+//     int solve(int i, int j, vector<vector<long long>> &freq, string &target) {
+//         if (i == m) return 1;
+//         if (j == k) return 0;
+//         if (dp[i][j] != -1) return dp[i][j];
+//         int take = freq[target[i] - 'a'][j] * solve(i+1, j+1, freq, target) % MOD;
+//         int nottake = solve(i, j+1, freq, target) % MOD;
+//         return dp[i][j] = (take + nottake) % MOD;
+//     }
+
+//     int numWays(vector<string>& words, string target) {
+//         k = words[0].size();
+//         m = target.size();
+//         vector<vector<long long>> freq(26, vector<long long>(k));
+//         for (int col = 0; col < k; col++) {
+//             for (string &x : words) {
+//                 freq[x[col] - 'a'][col]++;
+//             }
+//         }
+//         memset(dp, -1, sizeof(dp));
+//         return solve(0, 0, freq, target);
+//     }
+// };
