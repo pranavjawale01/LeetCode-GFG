baec2e7747c53faa74f41df2fdcad94f5be0b1c9
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-30 10:22:10 +0530

    Time: 99 ms (74.05%), Space: 21.3 MB (79.27%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,23 +1,45 @@
 class Solution:
-    def __init__(self):
-        self.m = int(1e9 + 7)
-        self.dp = []
-
-    def solve(self, L):
-        if L > self.h:
-            return 0
-        if self.dp[L] != -1:
-            return self.dp[L]
-        add_one = 1 if self.l <= L <= self.h else 0
-        take_zero = self.solve(L + self.z)
-        take_one = self.solve(L + self.o)
-        self.dp[L] = (add_one + take_zero + take_one) % self.m
-        return self.dp[L]
-
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        self.l = low
-        self.h = high
-        self.z = zero
-        self.o = one
-        self.dp = [-1] * (high + 1)
-        return self.solve(0)
+        dp = [0] * (high + 1)
+        dp[0] = 1
+        mod = 10**9 + 7
+
+        for i in range(1, high + 1):
+            if i - zero >= 0:
+                dp[i] = (dp[i] + dp[i - zero]) % mod
+            if i - one >= 0:
+                dp[i] = (dp[i] + dp[i - one]) % mod
+
+        ans = 0
+        for i in range(low, high + 1):
+            ans = (ans + dp[i]) % mod
+
+        return ans
+
+
+
+
+
+# class Solution:
+#     def __init__(self):
+#         self.m = int(1e9 + 7)
+#         self.dp = []
+
+#     def solve(self, L):
+#         if L > self.h:
+#             return 0
+#         if self.dp[L] != -1:
+#             return self.dp[L]
+#         add_one = 1 if self.l <= L <= self.h else 0
+#         take_zero = self.solve(L + self.z)
+#         take_one = self.solve(L + self.o)
+#         self.dp[L] = (add_one + take_zero + take_one) % self.m
+#         return self.dp[L]
+
+#     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+#         self.l = low
+#         self.h = high
+#         self.z = zero
+#         self.o = one
+#         self.dp = [-1] * (high + 1)
+#         return self.solve(0)
