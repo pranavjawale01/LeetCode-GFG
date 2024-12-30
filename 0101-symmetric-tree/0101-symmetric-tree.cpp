7a5cd2a400af7e82d881a4e212817d393d2dae00
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-31 00:38:39 +0530

    Time: 0 ms (100%), Space: 18.8 MB (5.63%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,11 +13,48 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         if (!root) return true;
-        function<bool(TreeNode*, TreeNode*)> sym = [&](TreeNode *l, TreeNode *r) {
-            if (!l && !r) return true;
-            if (!l || !r || l->val != r->val) return false;
-            return sym(l->left, r->right) && sym(l->right, r->left);
-        };
-        return sym(root->left, root->right);
+        queue<TreeNode*> q;
+        q.push(root); 
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> temp;
+            while (n--) {
+                TreeNode *curr = q.front();
+                q.pop();
+                if (curr) {
+                    temp.push_back(curr->val);
+                    q.push(curr->left);
+                    q.push(curr->right);
+                } else {
+                    temp.push_back(-200);
+                }
+            }
+            int size = temp.size();
+            for (int i = 0; i < size / 2; i++) {
+                if (temp[i] != temp[size - i - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool isSymmetric(TreeNode* root) {
+//         if (!root) return true;
+//         function<bool(TreeNode*, TreeNode*)> sym = [&](TreeNode *l, TreeNode *r) {
+//             if (!l && !r) return true;
+//             if (!l || !r || l->val != r->val) return false;
+//             return sym(l->left, r->right) && sym(l->right, r->left);
+//         };
+//         return sym(root->left, root->right);
+//     }
+// };
