6c0b443342b541d1f07f17ddcac49542f40ec549
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-30 10:14:42 +0530

    Time: 11 ms (24.66%), Space: 19.6 MB (32.29%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -11,7 +11,7 @@ public:
         if (l <= L && L <= h) addOne = true;
         int take_zero = solve(L + z);
         int take_one  = solve(L + o);
-        return dp[L] = (addOne + take_zero + take_one);
+        return dp[L] = (addOne + take_zero + take_one) % m;
     }
 
     int countGoodStrings(int low, int high, int zero, int one) {
