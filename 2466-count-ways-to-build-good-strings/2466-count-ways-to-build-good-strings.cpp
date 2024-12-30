a407ad788f8164593c3dcd99401ccda53f64e0bd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-30 10:12:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ans, l, h, z, o;
+    int m = 1e9 + 7;
+    vector<int> dp;
+
+    int solve(int L) {
+        if (L > h) return 0;
+        if (dp[L] != -1) return dp[L];
+        bool addOne = false;
+        if (l <= L && L <= h) addOne = true;
+        int take_zero = solve(L + z);
+        int take_one  = solve(L + o);
+        return dp[L] = (addOne + take_zero + take_one);
+    }
+
+    int countGoodStrings(int low, int high, int zero, int one) {
+        ans = 0;
+        l = low;
+        h = high;
+        z = zero;
+        o = one;
+        dp.resize(h+1, -1);
+        return solve(0);
+    }
+};
