dd9b7b38fd7ef8cea6fe9127c0d144a61df296c8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2024-12-31 22:51:46 +0530

    Time: 3 ms (21.77%), Space: 41.6 MB (50.02%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,31 +1,65 @@
 class Solution {
-    int[] dp;
-
-    private int solve(int[] days, int[] costs, int n, int i) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        int cost1 = costs[0] + solve(days, costs, n, i + 1);
-
-        int idx = i;
-        while (idx < n && days[idx] < days[i] + 7) {
-            idx++;
-        }
-        int cost7 = costs[1] + solve(days, costs, n, idx);
-
-        int jdx = i;
-        while (jdx < n && days[jdx] < days[i] + 30) {
-            jdx++;
-        }
-        int cost30 = costs[2] + solve(days, costs, n, jdx);
-
-        return dp[i] = Math.min(cost1, Math.min(cost7, cost30));
-    }
-
     public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(days, costs, n, 0);
+        Set<Integer> travelDays = new HashSet<>();
+        for (int day : days) {
+            travelDays.add(day);
+        }
+
+        int lastDay = days[days.length - 1];
+        int[] dp = new int[lastDay + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= lastDay; i++) {
+            if (!travelDays.contains(i)) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+
+            int day1 = dp[Math.max(0, i - 1)] + costs[0];
+            int day7 = dp[Math.max(0, i - 7)] + costs[1];
+            int day30 = dp[Math.max(0, i - 30)] + costs[2];
+
+            dp[i] = Math.min(day1, Math.min(day7, day30));
+        }
+
+        return dp[lastDay];
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     int[] dp;
+
+//     private int solve(int[] days, int[] costs, int n, int i) {
+//         if (i >= n) return 0;
+//         if (dp[i] != -1) return dp[i];
+
+//         int cost1 = costs[0] + solve(days, costs, n, i + 1);
+
+//         int idx = i;
+//         while (idx < n && days[idx] < days[i] + 7) {
+//             idx++;
+//         }
+//         int cost7 = costs[1] + solve(days, costs, n, idx);
+
+//         int jdx = i;
+//         while (jdx < n && days[jdx] < days[i] + 30) {
+//             jdx++;
+//         }
+//         int cost30 = costs[2] + solve(days, costs, n, jdx);
+
+//         return dp[i] = Math.min(cost1, Math.min(cost7, cost30));
+//     }
+
+//     public int mincostTickets(int[] days, int[] costs) {
+//         int n = days.length;
+//         dp = new int[n];
+//         Arrays.fill(dp, -1);
+//         return solve(days, costs, n, 0);
+//     }
+// }
