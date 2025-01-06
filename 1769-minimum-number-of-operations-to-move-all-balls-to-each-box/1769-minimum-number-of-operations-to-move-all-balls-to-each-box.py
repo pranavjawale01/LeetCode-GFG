0278d6247d1ae718eae30a15040d837fd2cca88e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-06 16:27:25 +0530

    Time: 7 ms (89.2%), Space: 17.9 MB (41.05%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -2,8 +2,32 @@ class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
         ans = [0] * n
+        cumVal = cumSum = 0
+
         for i in range(n):
-            if boxes[i] == '1':
-                for j in range(n):
-                    ans[j] += abs(j-i)
+            ans[i] = cumSum
+            cumVal += 0 if boxes[i] == '0' else 1
+            cumSum += cumVal
+
+        cumVal = cumSum = 0
+
+        for i in range(n - 1, -1, -1):
+            ans[i] += cumSum
+            cumVal += 0 if boxes[i] == '0' else 1
+            cumSum += cumVal
+
         return ans
+
+
+
+
+
+# class Solution:
+#     def minOperations(self, boxes: str) -> List[int]:
+#         n = len(boxes)
+#         ans = [0] * n
+#         for i in range(n):
+#             if boxes[i] == '1':
+#                 for j in range(n):
+#                     ans[j] += abs(j-i)
+#         return ans
