39afacb7f0d1031056fb54f799ee716108969afe
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-07 11:25:41 +0530

    Time: 19 ms (6.64%), Space: 19.2 MB (5.35%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,12 +1,52 @@
 class Solution {
 public:
+    vector<int> computeLPS(string &pattern) {
+        int m = pattern.size();
+        vector<int> lps(m, 0);
+        int len = 0, i = 1;
+        while (i < m) {
+            if (pattern[i] == pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len-1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    bool kmpsearch(string &text, string &pattern) {
+        int n = text.size(), m = pattern.size();
+        vector<int> lps = computeLPS(pattern);
+        int i = 0, j = 0;
+        while (i < n) {
+            if (text[i] == pattern[j]) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                return true;
+            } else if (i < n && text[i] != pattern[j]) {
+                if (j != 0) {
+                    j = lps[j-1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
     vector<string> stringMatching(vector<string>& words) {
         vector<string> ans;
-        for (string x : words) {
-            for (string y : words) {
-                if (x == y) continue;
-                if (y.find(x) != string::npos) {
-                    ans.push_back(x);
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (i != j && kmpsearch(words[j], words[i])) {
+                    ans.push_back(words[i]);
                     break;
                 }
             }
@@ -14,3 +54,24 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> stringMatching(vector<string>& words) {
+//         vector<string> ans;
+//         for (string x : words) {
+//             for (string y : words) {
+//                 if (x == y) continue;
+//                 if (y.find(x) != string::npos) {
+//                     ans.push_back(x);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
