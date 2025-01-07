4049e06c7d58a2f7fbd876c413d4daf13bf5b0b4
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-07 11:44:43 +0530

    Time: 115 ms (5.67%), Space: 17.8 MB (19.52%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -1,11 +1,65 @@
 class Solution:
-    def stringMatching(self, words: List[str]) -> List[str]:
+    def computeLPS(self, pattern):
+        m = len(pattern)
+        lps = [0] * m
+        length = 0
+        i = 1
+        
+        while i < m:
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+    
+    def kmpSearch(self, text, pattern):
+        n, m = len(text), len(pattern)
+        lps = self.computeLPS(pattern)
+        i = j = 0
+        
+        while i < n:
+            if text[i] == pattern[j]:
+                i += 1
+                j += 1
+            if j == m:
+                return True
+            elif i < n and text[i] != pattern[j]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+        return False
+    
+    def stringMatching(self, words):
         ans = []
-        for x in words:
-            for y in words:
-                if x == y:
-                    continue
-                if x in y:
-                    ans.append(x)
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i != j and self.kmpSearch(words[j], words[i]):
+                    ans.append(words[i])
                     break
         return ans
+
+        
+
+
+
+
+
+
+# class Solution:
+#     def stringMatching(self, words: List[str]) -> List[str]:
+#         ans = []
+#         for x in words:
+#             for y in words:
+#                 if x == y:
+#                     continue
+#                 if x in y:
+#                     ans.append(x)
+#                     break
+#         return ans
