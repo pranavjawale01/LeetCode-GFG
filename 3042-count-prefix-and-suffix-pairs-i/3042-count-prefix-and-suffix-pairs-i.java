39232a0c4f97332afa0335f37bd3d0cb50d2a306
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-08 13:47:54 +0530

    Time: 8 ms (5.56%), Space: 45.8 MB (5.56%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -1,21 +1,94 @@
-class Solution {
-    public boolean isPrefixAndSuffix(String c, String s) {
-        int n = c.length();
-        String s1 = s.substring(0, n);
-        String s2 = s.substring(s.length() - n);
-        return s1.equals(c) && s2.equals(c);
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord = false;
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
     }
 
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
+
+class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        int count = 0;
         int n = words.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (words[i].length() <= words[j].length() && isPrefixAndSuffix(words[i], words[j])) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            Trie prefixTrie = new Trie();
+            Trie suffixTrie = new Trie();
+
+            prefixTrie.insert(words[j]);
+
+            String reversed = new StringBuilder(words[j]).reverse().toString();
+            suffixTrie.insert(reversed);
+
+            for (int i = 0; i < j; i++) {
+                if (words[i].length() > words[j].length()) continue;
+
+                String rev = new StringBuilder(words[i]).reverse().toString();
+                if (prefixTrie.searchPrefix(words[i]) && suffixTrie.searchPrefix(rev)) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isPrefixAndSuffix(String c, String s) {
+//         int n = c.length();
+//         String s1 = s.substring(0, n);
+//         String s2 = s.substring(s.length() - n);
+//         return s1.equals(c) && s2.equals(c);
+//     }
+
+//     public int countPrefixSuffixPairs(String[] words) {
+//         int count = 0;
+//         int n = words.length;
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (words[i].length() <= words[j].length() && isPrefixAndSuffix(words[i], words[j])) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
