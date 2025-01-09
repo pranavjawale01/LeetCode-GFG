b3d47dbede0408dab722587dc2926605a2ce4507
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-09 12:37:57 +0530

    Time: 4 ms (10.57%), Space: 45.8 MB (5.1%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -1,12 +1,67 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        int n = pref.length();
-        int count = 0;
-        for (String x : words) {
-            if (x.length() >= n && x.substring(0, n).equals(pref)) {
-                count++;
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.addWord(word);
+        }
+        return trie.countPrefix(pref);
+    }
+
+    private class Trie {
+        private class Node {
+            Node[] links;
+            int count;
+
+            Node() {
+                links = new Node[26];
+                count = 0;
             }
         }
-        return count;
+
+        private Node root;
+
+        Trie() {
+            root = new Node();
+        }
+
+        void addWord(String word) {
+            Node curr = root;
+            for (char c : word.toCharArray()) {
+                if (curr.links[c - 'a'] == null) {
+                    curr.links[c - 'a'] = new Node();
+                }
+                curr = curr.links[c - 'a'];
+                curr.count++;
+            }
+        }
+
+        int countPrefix(String pref) {
+            Node curr = root;
+            for (char c : pref.toCharArray()) {
+                if (curr.links[c - 'a'] == null) {
+                    return 0;
+                }
+                curr = curr.links[c - 'a'];
+            }
+            return curr.count;
+        }
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int prefixCount(String[] words, String pref) {
+//         int n = pref.length();
+//         int count = 0;
+//         for (String x : words) {
+//             if (x.length() >= n && x.substring(0, n).equals(pref)) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
