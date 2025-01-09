29558e7b8a73217bcf71a192ba948ec33488b294
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-09 12:37:14 +0530

    Time: 43 ms (5.04%), Space: 71 MB (5.04%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,11 +1,62 @@
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int n = pref.size();
-        int count = 0;
-        for (string x : words) {
-            if (x.substr(0, n) == pref) count++;
+        Trie trie;
+        for (string &x : words) {
+            trie.addWord(x);
         }
-        return count;
+        return trie.countPrefix(pref);
     }
+
+private:
+    class Trie {
+        struct Node {
+            vector<Node*> links;
+            int count;
+            Node() : links(26, nullptr), count(0){}
+        };
+        Node *root;
+
+    public:
+        Trie() {
+            root = new Node();
+        }
+
+        void addWord(string &word) {
+            Node *curr = root;
+            for (char c : word) {
+                if (curr->links[c - 'a'] == nullptr) {
+                    curr->links[c-'a'] = new Node();
+                }
+                curr = curr->links[c-'a'];
+                curr->count++;
+            }
+        }
+
+        int countPrefix(string &pref) {
+            Node *curr = root;
+            for (char c : pref) {
+                if (curr->links[c-'a'] == nullptr) return 0;
+                curr = curr->links[c-'a'];
+            }
+            return curr->count;
+        }
+    };
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int prefixCount(vector<string>& words, string pref) {
+//         int n = pref.size();
+//         int count = 0;
+//         for (string x : words) {
+//             if (x.substr(0, n) == pref) count++;
+//         }
+//         return count;
+//     }
+// };
