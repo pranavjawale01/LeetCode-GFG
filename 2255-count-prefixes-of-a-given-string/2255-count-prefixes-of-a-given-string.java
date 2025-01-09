cb7ee94a8ee0d3bc6e0426fd955e6eb02eb9e820
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-09 13:11:30 +0530

    Time: 1 ms (38.93%), Space: 44.8 MB (5.1%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
@@ -1,11 +1,66 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
+        Trie trie = new Trie();
         int count = 0;
+        trie.addWord(s);
         for (String x : words) {
-            if (x.length() <= s.length() && s.substring(0, x.length()).equals(x)) {
+            if (trie.find(x)) {
                 count++;
             }
         }
         return count;
     }
+
+    private class Trie {
+        private class Node {
+            Node[] links;
+            Node() {
+                links = new Node[26];
+            }
+        }
+
+        private Node root;
+
+        Trie() {
+            root = new Node();
+        }
+
+        void addWord(String word) {
+            Node curr = root;
+            for (char c : word.toCharArray()) {
+                if (curr.links[c-'a'] == null) {
+                    curr.links[c-'a'] = new Node();
+                }
+                curr = curr.links[c-'a'];
+            }
+        }
+
+        boolean find(String x) {
+            Node curr = root;
+            for (char c : x.toCharArray()) {
+                if (curr.links[c-'a'] == null) {
+                    return false;
+                }
+                curr = curr.links[c-'a'];
+            }
+            return true;
+        }
+    }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int countPrefixes(String[] words, String s) {
+//         int count = 0;
+//         for (String x : words) {
+//             if (x.length() <= s.length() && s.substring(0, x.length()).equals(x)) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
