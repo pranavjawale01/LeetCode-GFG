c96572eacd39fcd871d532b82d087d8b8df8fc54
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-11 08:24:35 +0530

    Time: 27 ms (92.07%), Space: 18.1 MB (34.36%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False 
+            
+        freq = Counter(s)
+        odd_count = sum(1 for count in freq.values() if count % 2 != 0)
+
+        return odd_count <= k
