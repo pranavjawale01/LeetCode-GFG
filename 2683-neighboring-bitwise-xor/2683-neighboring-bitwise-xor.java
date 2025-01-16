0becc36ffa5873614504329137ec01a1d1309d72
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-16 18:34:28 +0530

    Time: 4 ms (36.53%), Space: 60.5 MB (80.84%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -1,9 +1,38 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int XOR = 0;
-        for (int x : derived) {
-            XOR ^= x;
+        int n = derived.length;
+        int[] original = new int[n];
+        
+        original[0] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            original[i + 1] = original[i] ^ derived[i];
         }
-        return XOR == 0;
+        if ((original[n - 1] ^ original[0]) == derived[n - 1]) {
+            return true;
+        }
+        
+        original[0] = 1;
+        for (int i = 0; i < n - 1; i++) {
+            original[i + 1] = original[i] ^ derived[i];
+        }
+        if ((original[n - 1] ^ original[0]) == derived[n - 1]) {
+            return true;
+        }
+        
+        return false;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public boolean doesValidArrayExist(int[] derived) {
+//         int XOR = 0;
+//         for (int x : derived) {
+//             XOR ^= x;
+//         }
+//         return XOR == 0;
+//     }
+// }
