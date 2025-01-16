1452de7491e0e801d8318e0944ff067dba347b95
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-16 10:47:27 +0530

    Time: 0 ms (100%), Space: 64.3 MB (36.18%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
+        int n = nums1.size(), m = nums2.size();
         int ans = 0;
-        if (m % 2 != 0) {
-            for (int &x : nums2) {
-                ans ^= x;
+        if (n % 2 == m % 2) {
+            if (n % 2 == 0) {
+                ans = 0;
+            } else {
+                for (int i = 0; i < n; i++) ans ^= nums1[i];
+                for (int i = 0; i < m; i++) ans ^= nums2[i];
             }
-        }
-        if (n % 2 != 0) {
-            for (int &x : nums1) {
-                ans ^= x;
+        } else {
+            if (n % 2 == 1) {
+                for (int i = 0; i < m; i++) ans ^= nums2[i];
+            } else {
+                for (int i = 0; i < n; i++) ans ^= nums1[i];
             }
         }
         return ans;
@@ -22,19 +26,44 @@ public:
 
 
 
+
 // class Solution {
 // public:
 //     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
 //         int m = nums1.size(), n = nums2.size();
-//         unordered_map<int, long> mp;
-//         for (int x : nums1) mp[x] += n;
-//         for (int x : nums2) mp[x] += m;
 //         int ans = 0;
-//         for (auto &x : mp) {
-//             int val = x.first;
-//             int frq = x.second;
-//             if (frq % 2 != 0) ans ^= val;
+//         if (m % 2 != 0) {
+//             for (int &x : nums2) {
+//                 ans ^= x;
+//             }
+//         }
+//         if (n % 2 != 0) {
+//             for (int &x : nums1) {
+//                 ans ^= x;
+//             }
 //         }
 //         return ans;
 //     }
 // };
+
+
+
+
+
+
+// // class Solution {
+// // public:
+// //     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+// //         int m = nums1.size(), n = nums2.size();
+// //         unordered_map<int, long> mp;
+// //         for (int x : nums1) mp[x] += n;
+// //         for (int x : nums2) mp[x] += m;
+// //         int ans = 0;
+// //         for (auto &x : mp) {
+// //             int val = x.first;
+// //             int frq = x.second;
+// //             if (frq % 2 != 0) ans ^= val;
+// //         }
+// //         return ans;
+// //     }
+// // };
