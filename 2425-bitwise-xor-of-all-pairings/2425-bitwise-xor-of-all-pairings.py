b844abfa32bcaffe4a28bb1d2b304fd64e031d51
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-16 10:43:08 +0530

    Time: 3 ms (73.99%), Space: 36.6 MB (48.56%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -1,13 +1,31 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
-        mp = {}
-        for x in nums1:
-            mp[x] = mp.get(x, 0) + n
-        for x in nums2:
-            mp[x] = mp.get(x, 0) + m
         ans = 0
-        for val, frq in mp.items():
-            if frq % 2 != 0:
-                ans ^= val
+        if m % 2 != 0:
+            for x in nums2:
+                ans ^= x
+        if n % 2 != 0:
+            for x in nums1:
+                ans ^= x
         return ans
+
+
+
+
+
+
+
+# class Solution:
+#     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+#         m, n = len(nums1), len(nums2)
+#         mp = {}
+#         for x in nums1:
+#             mp[x] = mp.get(x, 0) + n
+#         for x in nums2:
+#             mp[x] = mp.get(x, 0) + m
+#         ans = 0
+#         for val, frq in mp.items():
+#             if frq % 2 != 0:
+#                 ans ^= val
+#         return ans
