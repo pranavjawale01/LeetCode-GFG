907b572d2520c6715e55484ec5a55643164c5e51
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-16 10:42:57 +0530

    Time: 1 ms (100%), Space: 61.3 MB (58.62%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
@@ -1,13 +1,35 @@
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        Map<Integer, Long> mp = new HashMap<>();
-        for (int x : nums1) mp.put(x, mp.getOrDefault(x, 0L) + n);
-        for (int x : nums2) mp.put(x, mp.getOrDefault(x, 0L) + m);
         int ans = 0;
-        for (Map.Entry<Integer, Long> x : mp.entrySet()) {
-            if (x.getValue() % 2 != 0) ans ^= x.getKey();
+        if (m % 2 != 0) {
+            for (int x : nums2) {
+                ans ^= x;
+            }
+        }
+        if (n % 2 != 0) {
+            for (int x : nums1) {
+                ans ^= x;
+            }
         }
         return ans;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int xorAllNums(int[] nums1, int[] nums2) {
+//         int m = nums1.length, n = nums2.length;
+//         Map<Integer, Long> mp = new HashMap<>();
+//         for (int x : nums1) mp.put(x, mp.getOrDefault(x, 0L) + n);
+//         for (int x : nums2) mp.put(x, mp.getOrDefault(x, 0L) + m);
+//         int ans = 0;
+//         for (Map.Entry<Integer, Long> x : mp.entrySet()) {
+//             if (x.getValue() % 2 != 0) ans ^= x.getKey();
+//         }
+//         return ans;
+//     }
+// }
