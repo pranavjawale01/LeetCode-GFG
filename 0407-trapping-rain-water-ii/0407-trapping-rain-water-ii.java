0db46265dfbb1aca42a22663161778e55e290fd5
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-19 15:26:04 +0530

    Time: 20 ms (47.67%), Space: 46.4 MB (65.55%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -6,14 +6,14 @@ class Solution {
         for (int i = 0; i < m; i++) {
             pq.offer(new int[]{heightMap[i][0], i, 0});
             vis[i][0] = true;
-            pq.offer(new int[]{heightMap[i][n-1], i, n-1});
-            vis[i][0] = true;
+            pq.offer(new int[]{heightMap[i][n - 1], i, n - 1});
+            vis[i][n - 1] = true;
         }
         for (int i = 0; i < n; i++) {
             pq.offer(new int[]{heightMap[0][i], 0, i});
             vis[0][i] = true;
-            pq.offer(new int[]{heightMap[m-1][i], m-1, i});
-            vis[m-1][i] = true;
+            pq.offer(new int[]{heightMap[m - 1][i], m - 1, i});
+            vis[m - 1][i] = true;
         }
         int ans = 0;
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
@@ -21,9 +21,8 @@ class Solution {
             int[] curr = pq.poll();
             int height = curr[0], i = curr[1], j = curr[2];
             for (int[] dir : dirs) {
-                int x = i + dir[0];
-                int y = j + dir[1];
-                if (x >= 0 && x < m && y >= 0 && y < n && vis[x][y] == false) {
+                int x = i + dir[0], y = j + dir[1];
+                if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
                     ans += Math.max(0, height - heightMap[x][y]);
                     pq.offer(new int[]{Math.max(height, heightMap[x][y]), x, y});
                     vis[x][y] = true;
