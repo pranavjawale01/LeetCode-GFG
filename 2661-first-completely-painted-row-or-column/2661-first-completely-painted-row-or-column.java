f5c81e9c0751a6684e2c7ad5a7ba93e674f803d6
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-20 16:05:39 +0530

    Time: 31 ms (36.32%), Space: 68.6 MB (46.77%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -1,24 +1,57 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         int m = mat.length, n = mat[0].length;
-        Map<Integer, int[]> mp = new HashMap<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mp.put(mat[i][j], new int[]{i, j});
-            }
-        }
-        int[] rowCount = new int[m];
-        int[] colCount = new int[n];
+        Map<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
-            int val = arr[i];
-            int[] pos = mp.get(val);
-            int row = pos[0], col = pos[1];
-            rowCount[row]++;
-            colCount[col]++;
-            if (rowCount[row] == n || colCount[col] == m) {
-                return i;
-            }
+            mp.put(arr[i], i);
         }
-        return -1;
+        int minIdx = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            int lastIdx = Integer.MIN_VALUE;
+            for (int j = 0; j < n; j++) {
+                int val = mat[i][j];
+                lastIdx = Math.max(lastIdx, mp.get(val));
+            }
+            minIdx = Math.min(minIdx, lastIdx);
+        }
+        for (int i = 0; i < n; i++) {
+            int lastIdx = Integer.MIN_VALUE;
+            for (int j = 0; j < m; j++) {
+                int val = mat[j][i];
+                lastIdx = Math.max(lastIdx, mp.get(val));
+            }
+            minIdx = Math.min(minIdx, lastIdx);
+        }
+        return minIdx;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int firstCompleteIndex(int[] arr, int[][] mat) {
+//         int m = mat.length, n = mat[0].length;
+//         Map<Integer, int[]> mp = new HashMap<>();
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 mp.put(mat[i][j], new int[]{i, j});
+//             }
+//         }
+//         int[] rowCount = new int[m];
+//         int[] colCount = new int[n];
+//         for (int i = 0; i < arr.length; i++) {
+//             int val = arr[i];
+//             int[] pos = mp.get(val);
+//             int row = pos[0], col = pos[1];
+//             rowCount[row]++;
+//             colCount[col]++;
+//             if (rowCount[row] == n || colCount[col] == m) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
