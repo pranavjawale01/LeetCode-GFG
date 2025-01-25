1007b0efc01b2b9fdd06b059e1579cf11411d437
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-25 15:19:57 +0530

    Time: 347 ms (29.05%), Space: 234.1 MB (25%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,25 +2,62 @@ class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            while (true) {
-                int smallVal = nums[i];
-                int idx = -1;
-                for (int j = i + 1; j < n; j++) {
-                    if (abs(nums[i] - nums[j]) <= limit) {
-                        if (nums[j] < smallVal) {
-                            smallVal = nums[j];
-                            idx = j;
-                        }
-                    }
-                }
-                if (idx != -1) {
-                    swap(nums[i], nums[idx]);
-                } else {
-                    break;
-                }
+        vector<int> vec = nums;
+        sort(vec.begin(), vec.end());
+        int groupNum = 0;
+        unordered_map<int, int> numToGroup;
+        numToGroup[vec[0]] = groupNum;
+        unordered_map<int, list<int>> groupToList;
+        groupToList[groupNum].push_back(vec[0]);
+        for (int i = 1; i < n; i++) {
+            if (abs(vec[i] - vec[i-1]) > limit) {
+                groupNum++;
             }
+            numToGroup[vec[i]] = groupNum;
+            groupToList[groupNum].push_back(vec[i]);
         }
-        return nums;
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int group = numToGroup[num];
+            res[i] = *groupToList[group].begin();
+            groupToList[group].pop_front();
+        }
+        return res;
     }
 };
+
+
+
+
+
+
+
+
+// TLE
+// class Solution {
+// public:
+//     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             while (true) {
+//                 int smallVal = nums[i];
+//                 int idx = -1;
+//                 for (int j = i + 1; j < n; j++) {
+//                     if (abs(nums[i] - nums[j]) <= limit) {
+//                         if (nums[j] < smallVal) {
+//                             smallVal = nums[j];
+//                             idx = j;
+//                         }
+//                     }
+//                 }
+//                 if (idx != -1) {
+//                     swap(nums[i], nums[idx]);
+//                 } else {
+//                     break;
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// };
