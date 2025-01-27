553115f98a843d4de78ee39b102fa42e5ed2fba1
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-27 18:44:14 +0530

    Time: 931 ms (5.14%), Space: 48.4 MB (41.18%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,28 +1,78 @@
 class Solution {
-    public boolean solve(Map<Integer, List<Integer>> mp, int s, int e, boolean[] vis) {
+    Map<Integer, List<Integer>> mp = new HashMap<>();
+
+    public boolean solve(int s, int e, boolean[] vis) {
         vis[s] = true;
         if (s == e) return true;
         boolean reachable = false;
         for (int x : mp.getOrDefault(s, new ArrayList<>())) {
             if (!vis[x]) {
-                reachable |= solve(mp, x, e, vis);
+                reachable |= solve(x, e, vis);
             }
         }
         return reachable;
     }
 
+    public void help(int numCourses, boolean[][] check) {
+        for (int i = 0; i < numCourses; i++) {
+            for (int j = 0; j < numCourses; j++) {
+                if (i != j) {
+                    boolean[] vis = new boolean[numCourses];
+                    if (solve(i, j, vis)) {
+                        check[i][j] = true;
+                    }
+                }
+            }
+        }
+    }
+
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        Map<Integer, List<Integer>> mp = new HashMap<>();
         for (int[] pair : prerequisites) {
             mp.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
         }
+        boolean[][] check = new boolean[numCourses][numCourses];
+        help(numCourses, check);
+
         List<Boolean> ans = new ArrayList<>();
         for (int[] query : queries) {
-            int u = query[0];
-            int v = query[1];
-            boolean[] vis = new boolean[numCourses];
-            ans.add(solve(mp, u, v, vis));
+            ans.add(check[query[0]][query[1]]);
         }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean solve(Map<Integer, List<Integer>> mp, int s, int e, boolean[] vis) {
+//         vis[s] = true;
+//         if (s == e) return true;
+//         boolean reachable = false;
+//         for (int x : mp.getOrDefault(s, new ArrayList<>())) {
+//             if (!vis[x]) {
+//                 reachable |= solve(mp, x, e, vis);
+//             }
+//         }
+//         return reachable;
+//     }
+
+//     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+//         Map<Integer, List<Integer>> mp = new HashMap<>();
+//         for (int[] pair : prerequisites) {
+//             mp.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
+//         }
+//         List<Boolean> ans = new ArrayList<>();
+//         for (int[] query : queries) {
+//             int u = query[0];
+//             int v = query[1];
+//             boolean[] vis = new boolean[numCourses];
+//             ans.add(solve(mp, u, v, vis));
+//         }
+//         return ans;
+//     }
+// }
