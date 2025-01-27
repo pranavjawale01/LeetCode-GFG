0545576f332119e95de36dd81c6656c699dd254f
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-27 18:43:13 +0530

    Time: 584 ms (5.08%), Space: 74.4 MB (35.38%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    bool solve(unordered_map<int, vector<int>> &mp, int s, int e, vector<bool> &vis) {
+    unordered_map<int, vector<int>> mp;
+
+    bool solve(int s, int e, vector<bool> &vis) {
         vis[s] = true;
         if (s == e) return true;
         bool reachable = false;
         for (auto &x : mp[s]) {
             if (!vis[x]) {
-                reachable |= solve(mp, x, e, vis);
+                reachable |= solve(x, e, vis);
             }
         }
         return reachable;
     }
+
+    void help(int numCourses, vector<vector<bool>> &check) {
+        for (int i = 0; i < numCourses; i++) {
+            for (int j = 0; j < numCourses; j++) {
+                if (i != j) {
+                    vector<bool> vis(numCourses, false);
+                    if (solve(i, j, vis)) {
+                        check[i][j] = true;
+                    }
+                }
+            }
+        }
+    }
+
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        unordered_map<int, vector<int>> mp;
         for (auto &x : prerequisites) {
             mp[x[0]].push_back(x[1]);
         }
+        vector<vector<bool>> check(numCourses, vector<bool>(numCourses, false));
+        help(numCourses, check);
         int q = queries.size();
         vector<bool> ans(q);
         for (int i = 0; i < q; i++) {
-            int u = queries[i][0];
-            int v = queries[i][1];
-            vector<bool> vis(numCourses, false);
-            ans[i] = solve(mp, u, v, vis);
+            int u = queries[i][0], v = queries[i][1];
+            ans[i] = check[u][v];
         }
         return ans;
     }
@@ -33,36 +48,75 @@ public:
 
 
 
-// TLE
+
+
+
+
 // class Solution {
 // public:
-//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-//         int n = prerequisites.size();
-//         int m = queries.size();
-//         if (n == 0) {
-//             return vector<bool>(m, false);
-//         }
-//         unordered_map<int, vector<int>> mp;
-//         for (int i = 0; i < n; i++) {
-//             mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
-//         }
-//         vector<bool> ans(m, true);
-//         function<bool(int, int)> solve = [&](int a, int b) {
-//             for (int x : mp[a]) {
-//                 if (x == b) {
-//                     return true;
-//                 }
-//                 if (solve(x, b)) {
-//                     return true;
-//                 }
+//     bool solve(unordered_map<int, vector<int>> &mp, int s, int e, vector<bool> &vis) {
+//         vis[s] = true;
+//         if (s == e) return true;
+//         bool reachable = false;
+//         for (auto &x : mp[s]) {
+//             if (!vis[x]) {
+//                 reachable |= solve(mp, x, e, vis);
 //             }
-//             return false;
-//         };
-//         for (int i = 0; i < m; i++) {
+//         }
+//         return reachable;
+//     }
+//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+//         unordered_map<int, vector<int>> mp;
+//         for (auto &x : prerequisites) {
+//             mp[x[0]].push_back(x[1]);
+//         }
+//         int q = queries.size();
+//         vector<bool> ans(q);
+//         for (int i = 0; i < q; i++) {
 //             int u = queries[i][0];
 //             int v = queries[i][1];
-//             ans[i] = solve(u, v);
+//             vector<bool> vis(numCourses, false);
+//             ans[i] = solve(mp, u, v, vis);
 //         }
 //         return ans;
 //     }
 // };
+
+
+
+
+
+
+// // TLE
+// // class Solution {
+// // public:
+// //     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+// //         int n = prerequisites.size();
+// //         int m = queries.size();
+// //         if (n == 0) {
+// //             return vector<bool>(m, false);
+// //         }
+// //         unordered_map<int, vector<int>> mp;
+// //         for (int i = 0; i < n; i++) {
+// //             mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+// //         }
+// //         vector<bool> ans(m, true);
+// //         function<bool(int, int)> solve = [&](int a, int b) {
+// //             for (int x : mp[a]) {
+// //                 if (x == b) {
+// //                     return true;
+// //                 }
+// //                 if (solve(x, b)) {
+// //                     return true;
+// //                 }
+// //             }
+// //             return false;
+// //         };
+// //         for (int i = 0; i < m; i++) {
+// //             int u = queries[i][0];
+// //             int v = queries[i][1];
+// //             ans[i] = solve(u, v);
+// //         }
+// //         return ans;
+// //     }
+// // };
