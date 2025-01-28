cb956d74db38e946dde264c21d28f9d66fdee554
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-28 19:36:12 +0530

    Time: 2 ms (86.87%), Space: 96 MB (68.65%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -3,24 +3,16 @@ public:
     int m, n;
     vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    int bfs(int i, int j, vector<vector<int>> &grid) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
+    int dfs(int i, int j, vector<vector<int>> &grid) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
         int fishCount = grid[i][j];
         grid[i][j] = 0;
-        while (!q.empty()) {
-            i = q.front().first;
-            j = q.front().second;
-            q.pop();
-            for (auto &d : dirs) {
-                int x = d[0] + i;
-                int y = d[1] + j;
-                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] > 0) {
-                    q.push({x, y});
-                    fishCount += grid[x][y];
-                    grid[x][y] = 0;
-                }
-            }
+        for (auto &d : dirs) {
+            int x = i + d[0];
+            int y = j + d[1];
+            fishCount += dfs(x, y, grid);
         }
         return fishCount;
     }
@@ -31,10 +23,52 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] > 0) {
-                    fish = max(fish, bfs(i, j, grid));
+                    fish = max(fish, dfs(i, j, grid));
                 }
             }
         }
         return fish;
     }
 };
+
+
+// class Solution {
+// public:
+//     int m, n;
+//     vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+//     int bfs(int i, int j, vector<vector<int>> &grid) {
+//         queue<pair<int, int>> q;
+//         q.push({i, j});
+//         int fishCount = grid[i][j];
+//         grid[i][j] = 0;
+//         while (!q.empty()) {
+//             i = q.front().first;
+//             j = q.front().second;
+//             q.pop();
+//             for (auto &d : dirs) {
+//                 int x = d[0] + i;
+//                 int y = d[1] + j;
+//                 if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] > 0) {
+//                     q.push({x, y});
+//                     fishCount += grid[x][y];
+//                     grid[x][y] = 0;
+//                 }
+//             }
+//         }
+//         return fishCount;
+//     }
+
+//     int findMaxFish(vector<vector<int>>& grid) {
+//         m = grid.size(), n = grid[0].size();
+//         int fish = 0;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (grid[i][j] > 0) {
+//                     fish = max(fish, bfs(i, j, grid));
+//                 }
+//             }
+//         }
+//         return fish;
+//     }
+// };
