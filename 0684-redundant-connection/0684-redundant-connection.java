9a6076f1a81dfebcd17231d19c39eb008a17e2c2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-29 11:55:31 +0530

    Time: 6 ms (19.08%), Space: 45.8 MB (5.2%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,10 +1,16 @@
 class Solution {
-    public boolean dfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
-        vis[u] = true;
-        if (u == v) return true;
-        for (int x : mp.get(u)) {
-            if (vis[x]) continue;
-            if (dfs(mp, x, v, vis)) return true;
+    public boolean bfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(u);
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            vis[curr] = true;
+            if (curr == v) return true;
+            for (int x : mp.get(curr)) {
+                if (vis[x] == false) {
+                    q.offer(x);
+                }
+            }
         }
         return false;
     }
@@ -15,7 +21,7 @@ class Solution {
         for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
             boolean[] vis = new boolean[n + 1];
-            if (mp.containsKey(u) && mp.containsKey(v) && dfs(mp, u, v, vis)) {
+            if (mp.containsKey(u) && mp.containsKey(v) && bfs(mp, u, v, vis)) {
                 return edge;
             }
             mp.putIfAbsent(u, new ArrayList<>());
@@ -26,3 +32,35 @@ class Solution {
         return new int[0];
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean dfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
+//         vis[u] = true;
+//         if (u == v) return true;
+//         for (int x : mp.get(u)) {
+//             if (vis[x]) continue;
+//             if (dfs(mp, x, v, vis)) return true;
+//         }
+//         return false;
+//     }
+
+//     public int[] findRedundantConnection(int[][] edges) {
+//         int n = edges.length;
+//         Map<Integer, List<Integer>> mp = new HashMap<>();
+//         for (int[] edge : edges) {
+//             int u = edge[0], v = edge[1];
+//             boolean[] vis = new boolean[n + 1];
+//             if (mp.containsKey(u) && mp.containsKey(v) && dfs(mp, u, v, vis)) {
+//                 return edge;
+//             }
+//             mp.putIfAbsent(u, new ArrayList<>());
+//             mp.putIfAbsent(v, new ArrayList<>());
+//             mp.get(u).add(v);
+//             mp.get(v).add(u);
+//         }
+//         return new int[0];
+//     }
+// }
