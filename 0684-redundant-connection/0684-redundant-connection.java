3f49389f63479c36015c90edc631c00aa520cdd8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-29 12:04:44 +0530

    Time: 1 ms (75.21%), Space: 43.5 MB (43.8%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,33 +1,42 @@
-class Solution {
-    public boolean bfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(u);
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            vis[curr] = true;
-            if (curr == v) return true;
-            for (int x : mp.get(curr)) {
-                if (vis[x] == false) {
-                    q.offer(x);
-                }
-            }
+class DSU {
+    int[] parent, rank;
+
+    DSU(int n) {
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
         }
-        return false;
     }
 
+    int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void union(int x, int y) {
+        int xpar = find(x), ypar = find(y);
+        if (xpar == ypar) return;
+        if (rank[xpar] > rank[ypar]) {
+            parent[ypar] = xpar;
+        } else if (rank[ypar] > rank[xpar]) {
+            parent[xpar] = ypar;
+        } else {
+            parent[ypar] = xpar;
+            rank[xpar]++;
+        }
+    }
+}
+
+class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        Map<Integer, List<Integer>> mp = new HashMap<>();
+        DSU dsu = new DSU(n);
         for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
-            boolean[] vis = new boolean[n + 1];
-            if (mp.containsKey(u) && mp.containsKey(v) && bfs(mp, u, v, vis)) {
-                return edge;
-            }
-            mp.putIfAbsent(u, new ArrayList<>());
-            mp.putIfAbsent(v, new ArrayList<>());
-            mp.get(u).add(v);
-            mp.get(v).add(u);
+            if (dsu.find(u) == dsu.find(v)) return edge;
+            dsu.union(u, v);
         }
         return new int[0];
     }
@@ -36,13 +45,21 @@ class Solution {
 
 
 
+
+
 // class Solution {
-//     public boolean dfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
-//         vis[u] = true;
-//         if (u == v) return true;
-//         for (int x : mp.get(u)) {
-//             if (vis[x]) continue;
-//             if (dfs(mp, x, v, vis)) return true;
+//     public boolean bfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(u);
+//         while (!q.isEmpty()) {
+//             int curr = q.poll();
+//             vis[curr] = true;
+//             if (curr == v) return true;
+//             for (int x : mp.get(curr)) {
+//                 if (vis[x] == false) {
+//                     q.offer(x);
+//                 }
+//             }
 //         }
 //         return false;
 //     }
@@ -53,7 +70,7 @@ class Solution {
 //         for (int[] edge : edges) {
 //             int u = edge[0], v = edge[1];
 //             boolean[] vis = new boolean[n + 1];
-//             if (mp.containsKey(u) && mp.containsKey(v) && dfs(mp, u, v, vis)) {
+//             if (mp.containsKey(u) && mp.containsKey(v) && bfs(mp, u, v, vis)) {
 //                 return edge;
 //             }
 //             mp.putIfAbsent(u, new ArrayList<>());
@@ -64,3 +81,35 @@ class Solution {
 //         return new int[0];
 //     }
 // }
+
+
+
+
+// // class Solution {
+// //     public boolean dfs(Map<Integer, List<Integer>> mp, int u, int v, boolean[] vis) {
+// //         vis[u] = true;
+// //         if (u == v) return true;
+// //         for (int x : mp.get(u)) {
+// //             if (vis[x]) continue;
+// //             if (dfs(mp, x, v, vis)) return true;
+// //         }
+// //         return false;
+// //     }
+
+// //     public int[] findRedundantConnection(int[][] edges) {
+// //         int n = edges.length;
+// //         Map<Integer, List<Integer>> mp = new HashMap<>();
+// //         for (int[] edge : edges) {
+// //             int u = edge[0], v = edge[1];
+// //             boolean[] vis = new boolean[n + 1];
+// //             if (mp.containsKey(u) && mp.containsKey(v) && dfs(mp, u, v, vis)) {
+// //                 return edge;
+// //             }
+// //             mp.putIfAbsent(u, new ArrayList<>());
+// //             mp.putIfAbsent(v, new ArrayList<>());
+// //             mp.get(u).add(v);
+// //             mp.get(v).add(u);
+// //         }
+// //         return new int[0];
+// //     }
+// // }
