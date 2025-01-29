8809a35b7c160b5fb16016d7b952a8c9dd215794
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-29 12:04:53 +0530

    Time: 3 ms (58.46%), Space: 18.2 MB (61.58%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,47 +1,82 @@
-class Solution:
-    def bfs(self, mp, u, v, vis):
-        q = deque([u])
-        vis[u] = True
-        while q:
-            curr = q.popleft()
-            if curr == v:
-                return True
-            for x in mp[curr]:
-                if not vis.get(x, False):
-                    vis[x] = True
-                    q.append(x)
-        return False
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.rank = [0] * (n + 1)
 
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xpar, ypar = self.find(x), self.find(y)
+        if xpar == ypar:
+            return
+        if self.rank[xpar] > self.rank[ypar]:
+            self.parent[ypar] = xpar
+        elif self.rank[ypar] > self.rank[xpar]:
+            self.parent[xpar] = ypar
+        else:
+            self.parent[ypar] = xpar
+            self.rank[xpar] += 1
+            
+            
+class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        mp = defaultdict(list)
+        dsu = DSU(len(edges))
         for u, v in edges:
-            vis = {}
-            if u in mp and v in mp and self.bfs(mp, u, v, vis):
+            if dsu.find(u) == dsu.find(v):
                 return [u, v]
-            mp[u].append(v)
-            mp[v].append(u)
+            dsu.union(u, v)
         return []
 
 
 
+
 # class Solution:
-#     def dfs(self, mp, u, v, vis):
-#         if u == v:
-#             return True
-#         vis.add(u)
-#         for x in mp[u]:
-#             if x in vis:
-#                 continue
-#             if self.dfs(mp, x, v, vis):
+#     def bfs(self, mp, u, v, vis):
+#         q = deque([u])
+#         vis[u] = True
+#         while q:
+#             curr = q.popleft()
+#             if curr == v:
 #                 return True
+#             for x in mp[curr]:
+#                 if not vis.get(x, False):
+#                     vis[x] = True
+#                     q.append(x)
 #         return False
 
 #     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 #         mp = defaultdict(list)
 #         for u, v in edges:
-#             vis = set()
-#             if u in mp and v in mp and self.dfs(mp, u, v, vis):
+#             vis = {}
+#             if u in mp and v in mp and self.bfs(mp, u, v, vis):
 #                 return [u, v]
 #             mp[u].append(v)
 #             mp[v].append(u)
 #         return []
+
+
+
+# # class Solution:
+# #     def dfs(self, mp, u, v, vis):
+# #         if u == v:
+# #             return True
+# #         vis.add(u)
+# #         for x in mp[u]:
+# #             if x in vis:
+# #                 continue
+# #             if self.dfs(mp, x, v, vis):
+# #                 return True
+# #         return False
+
+# #     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+# #         mp = defaultdict(list)
+# #         for u, v in edges:
+# #             vis = set()
+# #             if u in mp and v in mp and self.dfs(mp, u, v, vis):
+# #                 return [u, v]
+# #             mp[u].append(v)
+# #             mp[v].append(u)
+# #         return []
