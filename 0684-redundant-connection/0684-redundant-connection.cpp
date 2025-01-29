469b42194ac3d113810c70d58e7ffcc7cb7b41bc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-01-29 11:49:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int, vector<int>> &mp, int u, int v, vector<bool> &vis) {
-        vis[u] = true;
-        if (u == v) return true;
-        for (auto &x : mp[u]) {
-            if (vis[x]) continue;
-            if (dfs(mp, x, v, vis)) return true;
+    bool bfs(unordered_map<int, vector<int>> &mp, int u, int v, vector<bool> &vis) {
+        queue<int> q;
+        q.push(u);
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            vis[curr] = true;
+            if (curr == v) return true;
+            for (int &x : mp[curr]) {
+                q.push(x);
+            }
         }
         return false;
     }
@@ -16,7 +21,7 @@ public:
         for (auto &x : edges) {
             int u = x[0], v = x[1];
             vector<bool> vis(n + 1, false);
-            if (mp.count(u) && mp.count(v) && dfs(mp, u, v, vis)) {
+            if (mp.count(u) && mp.count(v) && bfs(mp, u, v, vis)) {
                 return x;
             }
             mp[u].push_back(v);
@@ -25,3 +30,38 @@ public:
         return {};
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool dfs(unordered_map<int, vector<int>> &mp, int u, int v, vector<bool> &vis) {
+//         vis[u] = true;
+//         if (u == v) return true;
+//         for (auto &x : mp[u]) {
+//             if (vis[x]) continue;
+//             if (dfs(mp, x, v, vis)) return true;
+//         }
+//         return false;
+//     }
+
+//     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+//         int n = edges.size();
+//         unordered_map<int, vector<int>> mp;
+//         for (auto &x : edges) {
+//             int u = x[0], v = x[1];
+//             vector<bool> vis(n + 1, false);
+//             if (mp.count(u) && mp.count(v) && dfs(mp, u, v, vis)) {
+//                 return x;
+//             }
+//             mp[u].push_back(v);
+//             mp[v].push_back(u);
+//         }
+//         return {};
+//     }
+// };
