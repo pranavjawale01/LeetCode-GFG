08e29f6724d2be667b1e08e1e151becb7351148d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-01 15:48:21 +0530

    Time: 1 ms (35.36%), Space: 17.7 MB (33.33%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] % 2 == nums[i-1] % 2:
+                return False
+        return True
