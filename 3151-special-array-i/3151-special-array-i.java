2f934888ac5bc309322f258075fc495426907c70
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-01 15:48:14 +0530

    Time: 0 ms (100%), Space: 43.8 MB (8.78%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] % 2 == nums[i-1] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
