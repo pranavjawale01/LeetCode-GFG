3945dc3bda4ca817cad7d0720487ec512558686a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-02 11:56:25 +0530

    Time: 2 ms (4.22%), Space: 41.3 MB (64.78%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,25 +1,17 @@
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-        int[] sorted = new int[n];
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
 
         for (int r = 0; r < n; r++) {
-            int idx = 0;
-            for (int i = r; i < n; i++) {
-                sorted[idx++] = nums[i];
-            }
-            for (int i = 0; i < r; i++) {
-                sorted[idx++] = nums[i];
-            }
-
             boolean isSorted = true;
-            for (int i = 0; i < n - 1; i++) {
-                if (sorted[i] > sorted[i + 1]) {
+            for (int i = 0; i < n; i++) {
+                if (sorted[i] != nums[(i + r) % n]) {
                     isSorted = false;
                     break;
                 }
             }
-
             if (isSorted) {
                 return true;
             }
@@ -27,3 +19,41 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int n = nums.length;
+//         int[] sorted = new int[n];
+
+//         for (int r = 0; r < n; r++) {
+//             int idx = 0;
+//             for (int i = r; i < n; i++) {
+//                 sorted[idx++] = nums[i];
+//             }
+//             for (int i = 0; i < r; i++) {
+//                 sorted[idx++] = nums[i];
+//             }
+
+//             boolean isSorted = true;
+//             for (int i = 0; i < n - 1; i++) {
+//                 if (sorted[i] > sorted[i + 1]) {
+//                     isSorted = false;
+//                     break;
+//                 }
+//             }
+
+//             if (isSorted) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
