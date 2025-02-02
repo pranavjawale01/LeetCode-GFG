ae902eb8dde47e1709b2311af68e3e86cc92fb33
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-02 11:59:36 +0530

    Time: 0 ms (100%), Space: 41.9 MB (5.63%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-        int[] sorted = nums.clone();
-        Arrays.sort(sorted);
-
-        for (int r = 0; r < n; r++) {
-            boolean isSorted = true;
-            for (int i = 0; i < n; i++) {
-                if (sorted[i] != nums[(i + r) % n]) {
-                    isSorted = false;
-                    break;
-                }
-            }
-            if (isSorted) {
-                return true;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i+1) % n]) {
+                count++;
             }
         }
-        return false;
+        return count <= 1;
     }
 }
-
-
-
-
-
 
 
 
@@ -31,25 +17,17 @@ class Solution {
 // class Solution {
 //     public boolean check(int[] nums) {
 //         int n = nums.length;
-//         int[] sorted = new int[n];
+//         int[] sorted = nums.clone();
+//         Arrays.sort(sorted);
 
 //         for (int r = 0; r < n; r++) {
-//             int idx = 0;
-//             for (int i = r; i < n; i++) {
-//                 sorted[idx++] = nums[i];
-//             }
-//             for (int i = 0; i < r; i++) {
-//                 sorted[idx++] = nums[i];
-//             }
-
 //             boolean isSorted = true;
-//             for (int i = 0; i < n - 1; i++) {
-//                 if (sorted[i] > sorted[i + 1]) {
+//             for (int i = 0; i < n; i++) {
+//                 if (sorted[i] != nums[(i + r) % n]) {
 //                     isSorted = false;
 //                     break;
 //                 }
 //             }
-
 //             if (isSorted) {
 //                 return true;
 //             }
@@ -57,3 +35,41 @@ class Solution {
 //         return false;
 //     }
 // }
+
+
+
+
+
+
+
+
+
+// // class Solution {
+// //     public boolean check(int[] nums) {
+// //         int n = nums.length;
+// //         int[] sorted = new int[n];
+
+// //         for (int r = 0; r < n; r++) {
+// //             int idx = 0;
+// //             for (int i = r; i < n; i++) {
+// //                 sorted[idx++] = nums[i];
+// //             }
+// //             for (int i = 0; i < r; i++) {
+// //                 sorted[idx++] = nums[i];
+// //             }
+
+// //             boolean isSorted = true;
+// //             for (int i = 0; i < n - 1; i++) {
+// //                 if (sorted[i] > sorted[i + 1]) {
+// //                     isSorted = false;
+// //                     break;
+// //                 }
+// //             }
+
+// //             if (isSorted) {
+// //                 return true;
+// //             }
+// //         }
+// //         return false;
+// //     }
+// // }
