88f72ab2a5d4b590008d37acb29995dfec37a114
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-06 20:20:54 +0530

    Time: 229 ms (48.08%), Space: 68.5 MB (73.08%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -1,23 +1,43 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length, count = 0;
-
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j > i; j--) {
-                int val = nums[i] * nums[j];
-                Set<Integer> st = new HashSet<>();
-                for (int k = i + 1; k < j; k++) {
-                    if (val % nums[k] == 0) {
-                        int data = val / nums[k];
-                        if (st.contains(data)) {
-                            count++;
-                        }
-                        st.add(nums[k]);
-                    }
-                }
+            for (int j = i + 1; j < n; j++) {
+                mp.put(nums[i] * nums[j], mp.getOrDefault(nums[i] * nums[j], 0) + 1);
             }
         }
-        return count * 8;
+        int ans = 0;
+        for (int x : mp.values()) {
+            ans += (x * (x - 1)) / 2;
+        }
+        return ans * 8;
     }
 }
+
+
+
+
+// class Solution {
+//     public int tupleSameProduct(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length, count = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = n - 1; j > i; j--) {
+//                 int val = nums[i] * nums[j];
+//                 Set<Integer> st = new HashSet<>();
+//                 for (int k = i + 1; k < j; k++) {
+//                     if (val % nums[k] == 0) {
+//                         int data = val / nums[k];
+//                         if (st.contains(data)) {
+//                             count++;
+//                         }
+//                         st.add(nums[k]);
+//                     }
+//                 }
+//             }
+//         }
+//         return count * 8;
+//     }
+// }
