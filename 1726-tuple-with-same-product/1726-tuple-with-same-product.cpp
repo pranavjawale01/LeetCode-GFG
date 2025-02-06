704cea4439ce4952facd909b46eadc96666f3611
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-06 20:11:57 +0530

    Time: 360 ms (23.2%), Space: 85.1 MB (45.45%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int count = 0;
+        unordered_map<int, int> mp;
         for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j > i; j--) {
-                int val = nums[i] * nums[j];
-                unordered_set<int> st;
-                for (int k = i + 1; k < j; k++) {
-                    if (val % nums[k] == 0) {
-                        int data = val / nums[k];
-                        if (st.count(data)) {
-                            count++;
-                        }
-                        st.insert(nums[k]);
-                    }
-                }
+            for (int j = i + 1; j < n; j++) {
+                mp[nums[i] * nums[j]]++;
             }
         }
-        return count * 8;
+        int ans = 0;
+        for (auto &x : mp) {
+            ans += (x.second * (x.second - 1)) / 2;
+        }
+        return ans * 8;
     }
 };
 
 
 
-
-
-
-
-
-// TLE
 // class Solution {
 // public:
 //     int tupleSameProduct(vector<int>& nums) {
@@ -39,11 +26,15 @@ public:
 //         int count = 0;
 //         for (int i = 0; i < n; i++) {
 //             for (int j = n - 1; j > i; j--) {
+//                 int val = nums[i] * nums[j];
+//                 unordered_set<int> st;
 //                 for (int k = i + 1; k < j; k++) {
-//                     for (int l = j - 1; l > k; l--) {
-//                         int p1 = nums[i] * nums[j];
-//                         int p2 = nums[k] * nums[l];
-//                         if (p1 == p2) count++;
+//                     if (val % nums[k] == 0) {
+//                         int data = val / nums[k];
+//                         if (st.count(data)) {
+//                             count++;
+//                         }
+//                         st.insert(nums[k]);
 //                     }
 //                 }
 //             }
@@ -63,23 +54,52 @@ public:
 // // class Solution {
 // // public:
 // //     int tupleSameProduct(vector<int>& nums) {
-// //         int count = 0;
+// //         sort(nums.begin(), nums.end());
 // //         int n = nums.size();
+// //         int count = 0;
 // //         for (int i = 0; i < n; i++) {
-// //             for (int j = i + 1; j < n; j++) {
-// //                 for (int k = 0; k < n; k++) {
-// //                     for (int l = k + 1; l < n; l++) {
-// //                         if (i != k && i != l && j != k && j != l) {
-// //                             int p1 = nums[i] * nums[j];
-// //                             int p2 = nums[k] * nums[l];
-// //                             if (p1 == p2) {
-// //                                 count++;
-// //                             }
-// //                         }
+// //             for (int j = n - 1; j > i; j--) {
+// //                 for (int k = i + 1; k < j; k++) {
+// //                     for (int l = j - 1; l > k; l--) {
+// //                         int p1 = nums[i] * nums[j];
+// //                         int p2 = nums[k] * nums[l];
+// //                         if (p1 == p2) count++;
 // //                     }
 // //                 }
 // //             }
 // //         }
-// //         return (count / 2) * 8;
+// //         return count * 8;
 // //     }
 // // };
+
+
+
+
+
+
+
+
+// // // TLE
+// // // class Solution {
+// // // public:
+// // //     int tupleSameProduct(vector<int>& nums) {
+// // //         int count = 0;
+// // //         int n = nums.size();
+// // //         for (int i = 0; i < n; i++) {
+// // //             for (int j = i + 1; j < n; j++) {
+// // //                 for (int k = 0; k < n; k++) {
+// // //                     for (int l = k + 1; l < n; l++) {
+// // //                         if (i != k && i != l && j != k && j != l) {
+// // //                             int p1 = nums[i] * nums[j];
+// // //                             int p2 = nums[k] * nums[l];
+// // //                             if (p1 == p2) {
+// // //                                 count++;
+// // //                             }
+// // //                         }
+// // //                     }
+// // //                 }
+// // //             }
+// // //         }
+// // //         return (count / 2) * 8;
+// // //     }
+// // // };
