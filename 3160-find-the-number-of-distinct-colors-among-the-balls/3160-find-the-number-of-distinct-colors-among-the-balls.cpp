025b347f4db1f992ffac1408feb3123973c4cee3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-07 20:54:06 +0530

    Time: 99 ms (95.82%), Space: 155.7 MB (66.56%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        unordered_map<int, int> colorCount;
+        unordered_map<int, int> prevColor;
         int n = queries.size();
-        unordered_map<int, int> mp;
-        vector<int> arr(limit+1, -1);
-        vector<int> res(n);
+        vector<int> res(n, 0);
         for (int i = 0; i < n; i++) {
             int ball = queries[i][0];
             int color = queries[i][1];
-            if (arr[ball] != -1) {
-                int prevColor = arr[ball];
-                mp[prevColor]--;
-                if (mp[prevColor] == 0) {
-                    mp.erase(prevColor);
-                }
+            if (prevColor.count(ball)) {
+                int prev = prevColor[ball];
+                colorCount[prev]--;
+                if (colorCount[prev] == 0) colorCount.erase(prev);   
             }
-            arr[ball] = color;
-            mp[color]++;
-            res[i] = mp.size();
+            prevColor[ball] = color;
+            colorCount[color]++;
+            res[i] = colorCount.size();
         }
         return res;
     }
 };
+
+
+
+
+
+
+// MLE
+// class Solution {
+// public:
+//     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+//         int n = queries.size();
+//         unordered_map<int, int> mp;
+//         vector<int> arr(limit+1, -1);
+//         vector<int> res(n);
+//         for (int i = 0; i < n; i++) {
+//             int ball = queries[i][0];
+//             int color = queries[i][1];
+//             if (arr[ball] != -1) {
+//                 int prevColor = arr[ball];
+//                 mp[prevColor]--;
+//                 if (mp[prevColor] == 0) {
+//                     mp.erase(prevColor);
+//                 }
+//             }
+//             arr[ball] = color;
+//             mp[color]++;
+//             res[i] = mp.size();
+//         }
+//         return res;
+//     }
+// };
