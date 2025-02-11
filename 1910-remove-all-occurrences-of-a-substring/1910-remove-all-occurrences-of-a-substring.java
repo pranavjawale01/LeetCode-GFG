d3f1e8dfdd74fe1b9af9e13f32c86a51b3fee84e
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-11 18:39:48 +0530

    Time: 4 ms (49.85%), Space: 44.3 MB (42.68%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,29 +1,13 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        Stack<Character> st = new Stack<>();
+        StringBuilder ans = new StringBuilder();
         int n = part.length();
 
-        for (char ch : s.toCharArray()) {
-            st.push(ch);
-            if (st.size() >= n) {
-                boolean match = true;
-                for (int i = 0; i < n; i++) {
-                    if (st.get(st.size() - n + i) != part.charAt(i)) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
-                    for (int i = 0; i < n; i++) {
-                        st.pop();
-                    }
-                }
+        for (char c : s.toCharArray()) {
+            ans.append(c);
+            if (ans.length() >= n && ans.substring(ans.length() - n).equals(part)) {
+                ans.setLength(ans.length() - n);
             }
-        }
-
-        StringBuilder ans = new StringBuilder();
-        for (char ch : st) {
-            ans.append(ch);
         }
         return ans.toString();
     }
@@ -36,9 +20,45 @@ class Solution {
 
 // class Solution {
 //     public String removeOccurrences(String s, String part) {
-//         while (s.contains(part)) {
-//             s = s.replaceFirst(part, "");
+//         Stack<Character> st = new Stack<>();
+//         int n = part.length();
+
+//         for (char ch : s.toCharArray()) {
+//             st.push(ch);
+//             if (st.size() >= n) {
+//                 boolean match = true;
+//                 for (int i = 0; i < n; i++) {
+//                     if (st.get(st.size() - n + i) != part.charAt(i)) {
+//                         match = false;
+//                         break;
+//                     }
+//                 }
+//                 if (match) {
+//                     for (int i = 0; i < n; i++) {
+//                         st.pop();
+//                     }
+//                 }
+//             }
 //         }
-//         return s;
+
+//         StringBuilder ans = new StringBuilder();
+//         for (char ch : st) {
+//             ans.append(ch);
+//         }
+//         return ans.toString();
 //     }
 // }
+
+
+
+
+
+
+// // class Solution {
+// //     public String removeOccurrences(String s, String part) {
+// //         while (s.contains(part)) {
+// //             s = s.replaceFirst(part, "");
+// //         }
+// //         return s;
+// //     }
+// // }
