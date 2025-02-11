c9c0958ccaf15875eeddcf90cad903af06ac0819
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-11 18:35:19 +0530

    Time: 14 ms (25.76%), Space: 17.9 MB (32.79%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -1,5 +1,21 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        while part in s:
-            s = s.replace(part, "", 1)
-        return s
+        st = []
+        n = len(part)
+
+        for ch in s:
+            st.append(ch)
+            if len(st) >= n and "".join(st[-n:]) == part:
+                for _ in range(n):
+                    st.pop()
+
+        return "".join(st)
+
+
+
+
+# class Solution:
+#     def removeOccurrences(self, s: str, part: str) -> str:
+#         while part in s:
+#             s = s.replace(part, "", 1)
+#         return s
