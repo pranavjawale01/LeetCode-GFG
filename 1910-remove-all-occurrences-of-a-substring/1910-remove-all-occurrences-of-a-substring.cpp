e93130b37521ff89578df9a879d4ebf3a1e78d0b
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-11 18:34:40 +0530

    Time: 11 ms (5.85%), Space: 25.4 MB (5.32%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while (true) {
-            int idx = s.find(part);
-            if (idx == string::npos) {
-                break;
-            }
-            s.erase(idx, part.size());
+    bool check(stack<char> &st, string &part, int n) {
+        stack<char> temp = st;
+        for (int i = n - 1; i >= 0; i--) {
+            if (temp.top() != part[i]) return false;
+            temp.pop();
         }
-        return s;
+        return true;
+    }
+    string removeOccurrences(string s, string part) {
+        stack<char> st;
+        int m = s.size(), n = part.size();
+        for (int i = 0; i < m; i++) {
+            st.push(s[i]);
+            if (st.size() >= n && check(st, part, n)) {
+                for (int j = 0; j < n; j++) st.pop();
+            }
+        }
+        string ans = "";
+        while (!st.empty()) ans.push_back(st.top()), st.pop();
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         while (true) {
+//             int idx = s.find(part);
+//             if (idx == string::npos) {
+//                 break;
+//             }
+//             s.erase(idx, part.size());
+//         }
+//         return s;
+//     }
+// };
