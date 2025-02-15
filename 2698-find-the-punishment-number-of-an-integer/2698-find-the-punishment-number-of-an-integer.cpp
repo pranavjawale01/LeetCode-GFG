956093b2fa7cc6ec759c9691cb4b23465ec2fba8
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-15 13:17:44 +0530

    Time: 759 ms (10.33%), Space: 538.8 MB (5.54%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool solve(int i, int currSum, string s, int num, vector<vector<int>> &dp) {
+        if (i == s.size()) {
+            return currSum == num;
+        }
+        if (currSum > num) return false;
+        if (dp[i][currSum] != -1) {
+            return dp[i][currSum];
+        }
+        bool flag = false;
+        for (int j = i; j < s.size(); j++) {
+            string currStr = s.substr(i, j-i+1);
+            int added = stoi(currStr);
+            flag |= solve(j+1, currSum + added, s, num, dp);
+            if (flag) return true;
+        }
+        return dp[i][currSum] = flag;
+    }
+    int punishmentNumber(int n) {
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int sq = i * i;
+            string s = to_string(sq);
+            vector<vector<int>> dp(s.size(), vector<int>(i+1, -1));
+            if (solve(0, 0, s, i, dp)) {
+                ans += sq;
+            }
+        }
+        return ans;
+    }
+};
