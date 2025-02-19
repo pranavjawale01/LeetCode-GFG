473c5a32c1eed06d85c6a505ac3df22965b93b90
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-19 18:48:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (95.26%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,60 @@
 class Solution {
 public:
-    vector<string> ans;
-
-    void solve(int n, string &curr) {
+    string ans = "";
+    void solve(int n, int &k, string &curr) {
         if (curr.size() == n) {
-            ans.push_back(curr);
+            k--;
+            if (k == 0) {
+                ans = curr;
+                return;
+            }
             return;
         }
         for (char ch = 'a'; ch <= 'c'; ch++) {
-            if (!curr.empty() && curr.back() == ch) continue;
+            if (!curr.empty() && curr.back() == ch) {
+                continue;
+            }
             curr.push_back(ch);
-            solve(n, curr);
+            solve(n, k, curr);
+            if (ans.size()) return;
             curr.pop_back();
         }
     }
-
     string getHappyString(int n, int k) {
         string curr = "";
-        solve(n, curr);
-        return ans.size() < k ? "" : ans[k-1];
+        solve(n, k, curr);
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> ans;
+
+//     void solve(int n, string &curr) {
+//         if (curr.size() == n) {
+//             ans.push_back(curr);
+//             return;
+//         }
+//         for (char ch = 'a'; ch <= 'c'; ch++) {
+//             if (!curr.empty() && curr.back() == ch) continue;
+//             curr.push_back(ch);
+//             solve(n, curr);
+//             curr.pop_back();
+//         }
+//     }
+
+//     string getHappyString(int n, int k) {
+//         string curr = "";
+//         solve(n, curr);
+//         return ans.size() < k ? "" : ans[k-1];
+//     }
+// };
