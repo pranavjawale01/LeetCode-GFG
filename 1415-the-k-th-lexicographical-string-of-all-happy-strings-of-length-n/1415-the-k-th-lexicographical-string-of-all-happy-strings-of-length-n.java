a77a4fd093ff6efa44a9dadb047dc9d870546825
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-19 18:49:10 +0530

    Time: 1 ms (99.09%), Space: 41.2 MB (94.08%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,21 +1,53 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
+    String ans = "";
 
-    void solve(int n, StringBuilder curr) {
+    void solve(int n, int[] k, StringBuilder curr) {
         if (curr.length() == n) {
-            ans.add(curr.toString());
+            k[0]--;
+            if (k[0] == 0) {
+                ans = curr.toString();
+                return;
+            }
             return;
         }
         for (char ch = 'a'; ch <= 'c'; ch++) {
             if (curr.length() > 0 && curr.charAt(curr.length() - 1) == ch) continue;
             curr.append(ch);
-            solve(n, curr);
+            solve(n, k, curr);
+            if (!ans.isEmpty()) return;
             curr.deleteCharAt(curr.length() - 1);
         }
     }
 
     public String getHappyString(int n, int k) {
-        solve(n, new StringBuilder());
-        return ans.size() < k ? "" : ans.get(k - 1);
+        solve(n, new int[]{k}, new StringBuilder());
+        return ans;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     List<String> ans = new ArrayList<>();
+
+//     void solve(int n, StringBuilder curr) {
+//         if (curr.length() == n) {
+//             ans.add(curr.toString());
+//             return;
+//         }
+//         for (char ch = 'a'; ch <= 'c'; ch++) {
+//             if (curr.length() > 0 && curr.charAt(curr.length() - 1) == ch) continue;
+//             curr.append(ch);
+//             solve(n, curr);
+//             curr.deleteCharAt(curr.length() - 1);
+//         }
+//     }
+
+//     public String getHappyString(int n, int k) {
+//         solve(n, new StringBuilder());
+//         return ans.size() < k ? "" : ans.get(k - 1);
+//     }
+// }
