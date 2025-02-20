1d8716311846704fd34065e5fc31a2c9b33b8825
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-20 18:38:34 +0530

    Time: 6 ms (21.28%), Space: 42.3 MB (21.41%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,14 +1,18 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
-        StringBuilder ans = new StringBuilder();
-        
-        for (int i = 0; i < n; i++) {
-            char ch = nums[i].charAt(i);
-            ans.append(ch == '0' ? '1' : '0');
+        Set<Integer> st = new HashSet<>();
+        for (String x : nums) {
+            st.add(Integer.parseInt(x, 2));
         }
-        
-        return ans.toString();
+        int n = nums.length;
+        String ans = "";
+        for (int num = 0; num <= n; num++) {
+            if (!st.contains(num)) {
+                ans = String.format("%16s", Integer.toBinaryString(num)).replace(' ', '0');
+                break;
+            }
+        }
+        return ans.substring(16-n);
     }
 }
 
@@ -18,32 +22,53 @@ class Solution {
 
 
 
+
 // class Solution {
-//     Set<String> st = new HashSet<>();
-//     String ans = "";
-
-//     void solve(StringBuilder curr, int n) {
-//         if (curr.length() == n) {
-//             if (!st.contains(curr.toString())) {
-//                 ans = curr.toString();
-//             }
-//             return;
-//         }
-//         if (!ans.isEmpty()) return;
-
-//         curr.append('0');
-//         solve(curr, n);
-//         curr.deleteCharAt(curr.length() - 1);
-
-//         curr.append('1');
-//         solve(curr, n);
-//         curr.deleteCharAt(curr.length() - 1);
-//     }
-
 //     public String findDifferentBinaryString(String[] nums) {
-//         int n = nums[0].length();
-//         for (String num : nums) st.add(num);
-//         solve(new StringBuilder(), n);
-//         return ans;
+//         int n = nums.length;
+//         StringBuilder ans = new StringBuilder();
+        
+//         for (int i = 0; i < n; i++) {
+//             char ch = nums[i].charAt(i);
+//             ans.append(ch == '0' ? '1' : '0');
+//         }
+        
+//         return ans.toString();
 //     }
 // }
+
+
+
+
+
+
+
+// // class Solution {
+// //     Set<String> st = new HashSet<>();
+// //     String ans = "";
+
+// //     void solve(StringBuilder curr, int n) {
+// //         if (curr.length() == n) {
+// //             if (!st.contains(curr.toString())) {
+// //                 ans = curr.toString();
+// //             }
+// //             return;
+// //         }
+// //         if (!ans.isEmpty()) return;
+
+// //         curr.append('0');
+// //         solve(curr, n);
+// //         curr.deleteCharAt(curr.length() - 1);
+
+// //         curr.append('1');
+// //         solve(curr, n);
+// //         curr.deleteCharAt(curr.length() - 1);
+// //     }
+
+// //     public String findDifferentBinaryString(String[] nums) {
+// //         int n = nums[0].length();
+// //         for (String num : nums) st.add(num);
+// //         solve(new StringBuilder(), n);
+// //         return ans;
+// //     }
+// // }
