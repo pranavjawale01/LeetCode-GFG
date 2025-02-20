437dba76401b22c797292d91a44611825d61f994
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-20 18:30:00 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,31 +1,52 @@
 class Solution {
 public:
-    unordered_set<string> st;
-    string ans = "";
-
-    void solve(string &curr, int n) {
-        if (curr.size() == n) {
-            if (st.find(curr) == st.end()) {
-                ans = curr;
-            }
-            return;
-        }
-        if (!ans.empty()) return;
-
-        curr.push_back('0');
-        solve(curr, n);
-        curr.pop_back();
-
-        curr.push_back('1');
-        solve(curr, n);
-        curr.pop_back();
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums[0].size();
-        for (string x : nums) st.insert(x);
-        string curr = "";
-        solve(curr, n);
+        int n = nums.size();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            char ch = nums[i][i];
+            ans += (ch == '0') ? '1' : '0';
+        }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     unordered_set<string> st;
+//     string ans = "";
+
+//     void solve(string &curr, int n) {
+//         if (curr.size() == n) {
+//             if (st.find(curr) == st.end()) {
+//                 ans = curr;
+//             }
+//             return;
+//         }
+//         if (!ans.empty()) return;
+
+//         curr.push_back('0');
+//         solve(curr, n);
+//         curr.pop_back();
+
+//         curr.push_back('1');
+//         solve(curr, n);
+//         curr.pop_back();
+//     }
+
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         int n = nums[0].size();
+//         for (string x : nums) st.insert(x);
+//         string curr = "";
+//         solve(curr, n);
+//         return ans;
+//     }
+// };
