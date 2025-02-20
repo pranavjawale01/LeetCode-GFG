b76f26651aecb83fc0bb526718005bd713f09c1d
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-20 18:34:18 +0530

    Time: 0 ms (100%), Space: 17.6 MB (88.03%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,20 +1,35 @@
 class Solution:
-    def __init__(self):
-        self.st = set()
-        self.ans = ""
-
-    def solve(self, curr, n):
-        if len(curr) == n:
-            if curr not in self.st:
-                self.ans = curr
-            return
-        if self.ans:
-            return
-
-        self.solve(curr + '0', n)
-        self.solve(curr + '1', n)
-
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        self.st = set(nums)
-        self.solve("", len(nums[0]))
-        return self.ans
+        n = len(nums)
+        ans = ""
+        
+        for i in range(n):
+            ch = nums[i][i]
+            ans += '1' if ch == '0' else '0'
+        
+        return ans
+
+
+
+
+
+# class Solution:
+#     def __init__(self):
+#         self.st = set()
+#         self.ans = ""
+
+#     def solve(self, curr, n):
+#         if len(curr) == n:
+#             if curr not in self.st:
+#                 self.ans = curr
+#             return
+#         if self.ans:
+#             return
+
+#         self.solve(curr + '0', n)
+#         self.solve(curr + '1', n)
+
+#     def findDifferentBinaryString(self, nums: List[str]) -> str:
+#         self.st = set(nums)
+#         self.solve("", len(nums[0]))
+#         return self.ans
