e38c81d8afa41c75edff63fde07c6338739ed678
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-02-21 18:46:32 +0530

    Time: 5 ms (74.33%), Space: 22.2 MB (66.31%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -5,19 +5,26 @@
 #         self.left = left
 #         self.right = right
 class FindElements:
-      
-    def dfs(self, root, x):
-        if not root:
-            return
-        root.val = x
-        self.st.add(x)
-        self.dfs(root.left, 2 * x + 1)
-        self.dfs(root.right, 2 * x + 2)
 
     def __init__(self, root: Optional[TreeNode]):
         self.st = set()
         if root:
-            self.dfs(root, 0)
+            self.bfs(root, 0)
+
+    def bfs(self, root, x):
+        queue = deque([root])
+        root.val = x
+
+        while queue:
+            temp = queue.popleft()
+            self.st.add(temp.val)
+
+            if temp.left:
+                temp.left.val = 2 * temp.val + 1
+                queue.append(temp.left)
+            if temp.right:
+                temp.right.val = 2 * temp.val + 2
+                queue.append(temp.right)
 
     def find(self, target: int) -> bool:
         return target in self.st
@@ -26,3 +33,35 @@ class FindElements:
 # Your FindElements object will be instantiated and called as such:
 # obj = FindElements(root)
 # param_1 = obj.find(target)
+
+
+
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class FindElements:
+      
+#     def dfs(self, root, x):
+#         if not root:
+#             return
+#         root.val = x
+#         self.st.add(x)
+#         self.dfs(root.left, 2 * x + 1)
+#         self.dfs(root.right, 2 * x + 2)
+
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.st = set()
+#         if root:
+#             self.dfs(root, 0)
+
+#     def find(self, target: int) -> bool:
+#         return target in self.st
+
+
+# # Your FindElements object will be instantiated and called as such:
+# # obj = FindElements(root)
+# # param_1 = obj.find(target)
