1e6ad47718a47bce3cebc1f6716bd38584d394b3
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-07-19 21:12:14 +0530

    Update 1901-A-Line Trip.cpp

--- CodeForces/1901-A-Line Trip/1901-A-Line Trip.cpp
+++ CodeForces/1901-A-Line Trip/1901-A-Line Trip.cpp
@@ -1,37 +1,95 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_exp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+template<typename T>
+void input1d(vector<T> &arr) {
+    for (auto &x : arr) cin >> x;
+}
+
+template<typename T>
+void input2d(vector<vector<T>> &arr) {
+    for (auto &row : arr) {
+        for (auto &x : row) {
+            cin >> x;
+        }
+    }
+}
+
+template<typename T>
+void output1d(const vector<T> &arr) {
+    for (const auto &x : arr) cout << x << " ";
+    cout << endl;
+}
+
+template<typename T>
+void output2d(const vector<vector<T>> &arr) {
+    for (const auto &row : arr) {
+        for (const auto &x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+//----------------------------------Solution code here-----------------------------//
 void pranavjawale01() {
-    int n, x;
-    cin >> n >> x;
-    vector<int> fuel(n, 0);
-    
-    for (int i = 0; i < n; i++) {
-        cin >> fuel[i];
+    ll n, d;
+    cin >> n >> d;
+    vll arr(n);
+    input1d(arr);
+    ll ans = abs(arr[0] - 0);
+    for (ll i = 1; i < n; i++) {
+        ans = max(ans, abs(arr[i] - arr[i-1]));
     }
-    
-    int ans = abs(fuel[0] - 0);
-    
-    for (int i = 1; i < n; i++) {
-        ans = max(ans, abs(fuel[i] - fuel[i-1]));
-    }
-    
-    ans = max(ans, 2 * abs(fuel[n-1] - x));
-    
+    ans = max(ans, 2 * abs(arr[n-1] - d));
     cout << ans << endl;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    int t;
+    fast_io();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
     cin >> t;
-    
     while (t--) {
         pranavjawale01();
     }
-    
     return 0;
 }
