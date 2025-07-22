71fd56b22ececc6c7a1e81140df83cc26dc727bd
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-07-22 14:07:56 +0530

    Update 1862-B-Sequence Game.cpp

--- CodeForces/1862-B-Sequence Game/1862-B-Sequence Game.cpp
+++ CodeForces/1862-B-Sequence Game/1862-B-Sequence Game.cpp
@@ -1,35 +1,95 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void pranavjawale01() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    vector<int> brr;
-    brr.push_back(arr[0]);
-    for (int i = 1; i < n; i++) {
-        if (arr[i-1] > arr[i]) {
-            brr.push_back(arr[i]);
-            brr.push_back(arr[i]);
-        } else {
-            brr.push_back(arr[i]);
-        }
-    }
-    cout << brr.size() << endl;
-    for (int i = 0; i < brr.size(); i++) {
-        cout << brr[i] << " ";
-    }
-    cout << endl;
-}
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
-int main() {
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t;
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_exp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+template<typename T>
+void input1d(vector<T> &arr) {
+    for (auto &x : arr) cin >> x;
+}
+
+template<typename T>
+void input2d(vector<vector<T>> &arr) {
+    for (auto &row : arr) {
+        for (auto &x : row) {
+            cin >> x;
+        }
+    }
+}
+
+template<typename T>
+void output1d(const vector<T> &arr) {
+    for (const auto &x : arr) cout << x << " ";
+    cout << endl;
+}
+
+template<typename T>
+void output2d(const vector<vector<T>> &arr) {
+    for (const auto &row : arr) {
+        for (const auto &x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+//----------------------------------Solution code here-----------------------------//
+void pranavjawale01() {
+    ll n;
+    cin >> n;
+    vll arr(n), ans;
+    input1d(arr);
+    ans.pb(arr[0]);
+    for (ll i = 1; i < n; i++) {
+        if (arr[i] < arr[i-1]) {
+            ans.pb(arr[i]);
+        }
+        ans.pb(arr[i]);
+    }
+    cout << ans.size() << nl;
+    output1d(ans);
+}
+
+int main() {
+    fast_io();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
     cin >> t;
     while (t--) {
         pranavjawale01();
