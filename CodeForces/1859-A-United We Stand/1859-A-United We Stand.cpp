d3a25ebfbc3fee3be4ed82fb56ceab1cbdd21ffc
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-07-22 14:47:57 +0530

    Update 1859-A-United We Stand.cpp

--- CodeForces/1859-A-United We Stand/1859-A-United We Stand.cpp
+++ CodeForces/1859-A-United We Stand/1859-A-United We Stand.cpp
@@ -1,40 +1,102 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
-void pranavjawale01() {
-    ll n;
-    cin >> n;
-    vector<ll> a(n);
-    for (ll i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    sort(a.begin(), a.end());
-    if (a.back() == a[0]) {
-        cout << -1 << endl;
-        return;
-    }
-    ll idx = 0;
-    while (a[idx] == a[0]) {
-        idx++;
-    }
-    cout << idx << " " << n - idx << endl;
-    for (int i = 0; i < idx; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    for (int i = idx; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
 
-int main() {
+void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    ll t;
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_exp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+template<typename T>
+void input1d(vector<T> &arr) {
+    for (auto &x : arr) cin >> x;
+}
+
+template<typename T>
+void input2d(vector<vector<T>> &arr) {
+    for (auto &row : arr) {
+        for (auto &x : row) {
+            cin >> x;
+        }
+    }
+}
+
+template<typename T>
+void output1d(const vector<T> &arr) {
+    for (const auto &x : arr) cout << x << " ";
+    cout << endl;
+}
+
+template<typename T>
+void output2d(const vector<vector<T>> &arr) {
+    for (const auto &row : arr) {
+        for (const auto &x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+//----------------------------------Solution code here-----------------------------//
+void pranavjawale01() {
+    ll n;
+    cin >> n;
+    vll arr(n);
+    input1d(arr);
+    map<ll, ll> mp;
+    for (auto x : arr) mp[x]++;
+    if (mp.size() > 1) {
+        auto it = mp.begin();
+        vll a, b;
+        while (it->ss--) a.pb(it->ff);
+        it++;
+        for (; it != mp.end(); it++) {
+            while (it->ss--) b.pb(it->ff);
+        }
+        cout << a.size() << " " << b.size() << nl;
+        output1d(a);
+        output1d(b);
+    } else {
+        cout << -1 << nl;
+    }
+}
+
+int main() {
+    fast_io();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
     cin >> t;
     while (t--) {
         pranavjawale01();
