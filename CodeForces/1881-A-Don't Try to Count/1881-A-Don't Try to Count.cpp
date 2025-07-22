849e9729d5ce163ec5712d998ffd55d3ce9f4070
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-07-22 13:02:13 +0530

    Update 1881-A-Don't Try to Count.cpp

--- CodeForces/1881-A-Don't Try to Count/1881-A-Don't Try to Count.cpp
+++ CodeForces/1881-A-Don't Try to Count/1881-A-Don't Try to Count.cpp
@@ -1,33 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pranavjawale01() {
-    int n, m;
-    cin >> n >> m;
-    string x, s;
-    cin >> x >> s;
-    int count = 6, result = 0;
-    bool flag = false;
-    while (count--) {
-        if (x.find(s) != string::npos) {
-            flag = true;
-            break;
-        }
-        result++;
-        x += x;
-    }
-    if (flag) {
-        cout << result << endl;
-    } else {
-        cout << -1 << endl;
-    }
-}
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
-int main() {
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t;
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_exp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+template<typename T>
+void input1d(vector<T> &arr) {
+    for (auto &x : arr) cin >> x;
+}
+
+template<typename T>
+void input2d(vector<vector<T>> &arr) {
+    for (auto &row : arr) {
+        for (auto &x : row) {
+            cin >> x;
+        }
+    }
+}
+
+template<typename T>
+void output1d(const vector<T> &arr) {
+    for (const auto &x : arr) cout << x << nl;
+    cout << endl;
+}
+
+template<typename T>
+void output2d(const vector<vector<T>> &arr) {
+    for (const auto &row : arr) {
+        for (const auto &x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+//----------------------------------Solution code here-----------------------------//
+void pranavjawale01() {
+    ll n, m;
+    cin >> n >> m;
+    string x, s;
+    cin >> x >> s;
+    for (ll i = 0; i < 6; i++) {
+        if (x.find(s) != string::npos) {
+            cout << i << nl;
+            return;
+        }
+        x += x;
+    }
+    cout << -1 << nl;
+}
+
+int main() {
+    fast_io();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
     cin >> t;
     while (t--) {
         pranavjawale01();
