a4d2a84b8b5d3906ed63ed111eecbbac1e1fc8e2
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2025-07-23 13:12:48 +0530

    Update 0785-A-Anton and Polyhedrons.cpp

--- CodeForces/0785-A-Anton and Polyhedrons/0785-A-Anton and Polyhedrons.cpp
+++ CodeForces/0785-A-Anton and Polyhedrons/0785-A-Anton and Polyhedrons.cpp
@@ -1,36 +1,100 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pranavjawale01() {
-    int n;
-    cin >> n;
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        if (s[0] == 'T') {
-            count += 4;
-        }
-        if (s[0] == 'C') {
-            count += 6;
-        }
-        if (s[0] == 'O') {
-            count += 8;
-        }
-        if (s[0] == 'D') {
-            count += 12;
-        }
-        if (s[0] == 'I') {
-            count += 20;
-        }
-    }
-    cout << count << endl;
-}
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
-int main() {
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    pranavjawale01();
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_exp(ll base, ll exp, ll mod = MOD) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+template<typename T>
+void input1d(vector<T> &arr) {
+    for (auto &x : arr) cin >> x;
+}
+
+template<typename T>
+void input2d(vector<vector<T>> &arr) {
+    for (auto &row : arr) {
+        for (auto &x : row) {
+            cin >> x;
+        }
+    }
+}
+
+template<typename T>
+void output1d(const vector<T> &arr) {
+    for (const auto &x : arr) cout << x << " ";
+    cout << endl;
+}
+
+template<typename T>
+void output2d(const vector<vector<T>> &arr) {
+    for (const auto &row : arr) {
+        for (const auto &x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+//----------------------------------Solution code here-----------------------------//
+void pranavjawale01() {
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    unordered_map<string, int> mp;
+    mp["Tetrahedron"] = 4;
+    mp["Cube"] = 6;
+    mp["Octahedron"] = 8;
+    mp["Dodecahedron"] = 12;
+    mp["Icosahedron"] = 20;
+    while (n--) {
+        string s;
+        cin >> s;
+        ans += mp[s];
+    }
+    cout << ans << nl;
+}
+
+int main() {
+    fast_io();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        pranavjawale01();
+    }
     return 0;
 }
